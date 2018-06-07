af1d47b1b09846772644584bbd89a7e14ddec57f
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-07 15:51:35 +0200

    added wizard json

---
+++ app/meldingen/incident/definitions/wizard.js
@@ -0,0 +1,76 @@
+export default {
+  beschrijf: {
+    form: {
+      location: {},
+      what: {},
+      datetime: {}
+    },
+    show: {
+
+    }
+  },
+  vulaan: {
+    form: {
+    },
+    show: {
+    }
+  },
+  email: {
+    form: {
+      email: {}
+    }
+  },
+  telefoon: {
+    form: {
+      telephone: {}
+    }
+  },
+  samenvatting: {
+    show: {
+      beschrijf: {
+        location: {},
+        what: {},
+        datetime: {}
+      },
+      email: {
+        email: {}
+      },
+      telefoon: {
+        telephone: {}
+      }
+    }
+  },
+  bedankt: {
+    show: {
+      bedankt: {}
+    }
+  }
+};
+
+/*
+
+Stap 1: bechrijf
+  waar?
+  wat?
+  wanneer?
+
+Stap 2: vulaan
+
+Stap 3: email
+  email
+
+Stap 4: telefoon
+    telefoonnummer
+
+Stap 5: samenvatting
+  plaats
+  wat
+  tijdstip
+  [uitgevraagde dingen]
+  email
+  telefoonnummer
+
+Stap 6: bedankt
+  tekst
+
+*/
