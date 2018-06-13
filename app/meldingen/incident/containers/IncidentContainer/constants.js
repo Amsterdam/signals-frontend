edef43ca420ae7b5357438b40f5875641cff9fa5
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-13 13:11:26 +0200

    reafctored action names

--- app/meldingen/incident/containers/IncidentContainer/constants.js
+++ app/meldingen/incident/containers/IncidentContainer/constants.js
@@ -5,6 +5,6 @@
  */
 
 export const SET_INCIDENT = 'app/IncidentContainer/SET_INCIDENT';
-export const SEND_INCIDENT = 'app/IncidentContainer/SEND_INCIDENT';
-export const SEND_INCIDENT_SUCCESS = 'app/IncidentContainer/SEND_INCIDENT_SUCCESS';
-export const SEND_INCIDENT_ERROR = 'app/IncidentContainer/SEND_INCIDENT_ERROR';
+export const CREATE_INCIDENT = 'app/IncidentContainer/CREATE_INCIDENT';
+export const CREATE_INCIDENT_SUCCESS = 'app/IncidentContainer/CREATE_INCIDENT_SUCCESS';
+export const CREATE_INCIDENT_ERROR = 'app/IncidentContainer/CREATE_INCIDENT_ERROR';
--- app/meldingen/incident/containers/IncidentContainer/saga.js
+++ app/meldingen/incident/containers/IncidentContainer/saga.js
@@ -1,23 +1,23 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
 import request from 'utils/request';
 
-import { SEND_INCIDENT } from './constants';
-import { sendIncidentSuccess, sendIncidentError } from './actions';
+import { CREATE_INCIDENT } from './constants';
+import { createIncidentSuccess, createIncidentError } from './actions';
 import makeSelectIncidentContainer from './selectors';
 
-export function* sendIncident() {
+export function* createIncident() {
   const requestURL = '/api/meldingen';
 
   try {
     const { filter } = yield select(makeSelectIncidentContainer());
     const incidents = yield call(request, requestURL, filter);
-    yield put(sendIncidentSuccess(incidents));
+    yield put(createIncidentSuccess(incidents));
   } catch (err) {
-    yield put(sendIncidentError(err));
+    yield put(createIncidentError(err));
   }
 }
 
 // Individual exports for testing
 export default function* watchSetIncidentSaga() {
-  yield takeLatest(SEND_INCIDENT, sendIncident);
+  yield takeLatest(CREATE_INCIDENT, createIncident);
 }
