af73728412d65259e2e43bfe1904bea130c9a5fc
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-06-15 15:44:20 +0200

    fix: requestAnimationFrame polifill test error

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,9 +1,11 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
+import 'raf/polyfill';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
+
