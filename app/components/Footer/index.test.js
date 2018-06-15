4c2f838f6cad57dcc4b8508f168c1d7262793fe5
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-06-15 15:29:26 +0200

    add: unittests for Header, Footer and MainMenu

--- app/components/Footer/index.test.js
+++ app/components/Footer/index.test.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { FormattedMessage } from 'react-intl';
 
-import messages from './messages';
 import Footer from './index';
 import './style.scss';
 
 describe('<Footer />', () => {
-  it('should render the copyright notice', () => {
+  it('should render correctly', () => {
     const renderedComponent = shallow(
       <Footer />
     );
-    expect(renderedComponent.contains(
-      <section>
-        <FormattedMessage {...messages.licenseMessage} />
-      </section>
-    )).toBe(true);
-  });
-
-  it('should render the credits', () => {
-    const renderedComponent = shallow(<Footer />);
-    expect(renderedComponent.contains(
-      <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <a href="https://data.amsterdam.nl/">Amsterdam City Data (Datapunt)</a>,
-          }}
-        />
-      </section>
-    )).toBe(true);
+    expect(renderedComponent).toMatchSnapshot();
   });
 });
