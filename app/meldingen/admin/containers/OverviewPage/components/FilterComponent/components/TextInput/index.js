5978b7d9ae2679c150f5aa6e754c3c12cdf73d2b
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-06-17 18:25:13 +0200

    add: TextInput refactored

--- app/meldingen/admin/containers/OverviewPage/components/FilterComponent/components/TextInput/index.js
+++ app/meldingen/admin/containers/OverviewPage/components/FilterComponent/components/TextInput/index.js
@@ -4,25 +4,23 @@ import PropTypes from 'prop-types';
 import { FieldControl } from 'react-reactive-form';
 import './style.scss';
 
-const TextInput = (p) => {
-  const { name, control } = p;
 
-  return (
-    <div>
-      <FieldControl
-        name={name}
-        control={control}
-        render={TextInputRender(name)}
-      />
-    </div>
-  );
+export const TextInput = ({ name, control, ...props }) => (
+  <div>
+    <FieldControl name={name} control={control} render={TextInputRender(props)} />
+  </div>);
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  control: PropTypes.object.isRequired
 };
 
-export const TextInputRender = (name) => {
+export const TextInputRender = (props) => {
+  const { name, display } = props;
   const render = ({ handler, touched, hasError }) => (<div>
     <div className="rij mode_input text rij_verplicht">
       <div className="label">
-        <label htmlFor={`form${name}`}>{name}</label>
+        <label htmlFor={`form${name}`}>{display}</label>
       </div>
 
       <div className="invoer">
@@ -37,6 +35,10 @@ export const TextInputRender = (name) => {
     </div>
   </div>);
 
+  render.defaultProps = {
+    touched: false
+  };
+
   render.propTypes = {
     handler: PropTypes.func.isRequired,
     hasError: PropTypes.func.isRequired,
@@ -45,4 +47,57 @@ export const TextInputRender = (name) => {
   return render;
 };
 
-export default TextInput;
+export class TextInputWrapper extends React.Component {
+  TextInputRender = (props) => {
+    // console.log(props);
+    const { name, display, control } = props;
+    const renderContent = ({ handler, touched, hasError }) => (<div>
+      <div className="rij mode_input text rij_verplicht">
+        <div className="label">
+          <label htmlFor={`form${name}`}>{display}</label>
+        </div>
+
+        <div className="invoer">
+          <input name="" id={`form${name}`} value="" className="input" type="text" {...handler()} />
+        </div>
+        <div>
+          {touched
+            && hasError('required')
+            && 'Name is required'}
+        </div>
+
+      </div>
+    </div>);
+
+    // render.defaultProps = {
+    //   touched: false
+    // };
+
+    // render.propTypes = {
+    //   handler: PropTypes.func.isRequired,
+    //   hasError: PropTypes.func.isRequired,
+    //   touched: PropTypes.boolean,
+    // };
+    return renderContent(control);
+  };
+
+  render() {
+    const { name, control } = this.props;
+    return (
+      <div>
+        <FieldControl name={name} control={control} render={TextInputRender} />
+      </div>
+    );
+  }
+}
+
+TextInputWrapper.propTypes = {
+  name: PropTypes.string.isRequired,
+  display: PropTypes.string.isRequired,
+  control: PropTypes.shape({
+    handler: PropTypes.func.isRequired,
+    hasError: PropTypes.func.isRequired,
+    touched: PropTypes.boolean,
+    pristine: PropTypes.boolean,
+  })
+};
--- app/meldingen/admin/containers/OverviewPage/components/FilterComponent/index.js
+++ app/meldingen/admin/containers/OverviewPage/components/FilterComponent/index.js
@@ -6,46 +6,46 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormBuilder, FieldGroup, FieldControl, Validators, } from 'react-reactive-form';
-
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { FormBuilder, FieldGroup } from 'react-reactive-form';
 
 import './style.scss';
-import { TextInputRender } from './components/TextInput';
+import { TextInput } from './components/TextInput';
 
 class FilterComponent extends React.Component { // eslint-disable-line react/prefer-stateless-function
   filterForm = FormBuilder.group({
-    id: ['', Validators.required],
-    name: ['', Validators.required],
+    id: [''],
+    stadsdeel: [''],
   });
   handleReset = () => {
+    // console.log('Form reset');
     this.filterForm.reset();
   }
-  handleSubmit = (e) => {
-    e.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
     // console.log('Form values', this.filterForm.value);
     this.props.filterIncidents(this.filterForm.value);
   }
   render() {
     return (
       <div className="filter-component col-4">
-        <FormattedMessage {...messages.header} />
-
         <FieldGroup
           control={this.filterForm}
           render={({ invalid }) => (
             <form onSubmit={this.handleSubmit}>
+              <div>
+                <TextInput name="id" display="Melding Id" control={this.filterForm.get('id')} />
+                <TextInput name="stadsdeel" display="Staadsdeel" control={this.filterForm.get('stadsdeel')} />
+                {/* <TextInputWrapper name="stadsdeel" display="Staadsdeel" control={this.filterForm.get('stadsdeel')} /> */}
+                {/* <FieldControl name="id" render={TextInputRender('id', 'Melding Id')} />
+              <FieldControl name="stadsdeel" render={TextInputRender('stadsdeel', 'Staadsdeel')} /> */}
 
-              <FieldControl name="id" render={TextInputRender('id')} />
-              <FieldControl name="name" render={TextInputRender('name')} />
-
-              <button className="action primary" onClick={this.handleReset}>
-                <span className="value">Reset</span>
-              </button>
-              <button className="action primary" type="submit" disabled={invalid}>
-                <span className="value">Submit</span>
-              </button>
+                <button className="action primary" onClick={this.handleReset} type="button">
+                  <span className="value">Reset filter</span>
+                </button>
+                <button className="action primary" type="submit" disabled={invalid}>
+                  <span className="value">Zoek</span>
+                </button>
+              </div>
             </form>
           )}
         />
