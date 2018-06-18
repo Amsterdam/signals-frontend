31cccabf22baaebbbe715ad3e0e2cb82d8ce7136
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-18 11:56:08 +0200

    refactored component loading in wizard

---
+++ app/meldingen/incident/components/IncidentForm/components/index.js
@@ -0,0 +1,7 @@
+import TextInput from './TextInput/';
+import DescriptionWithClassificationInput from './DescriptionWithClassificationInput/';
+
+export default {
+  TextInput,
+  DescriptionWithClassificationInput
+};
