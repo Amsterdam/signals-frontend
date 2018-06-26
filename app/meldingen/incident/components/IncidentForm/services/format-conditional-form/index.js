4489851ef69e3c99c2efc1809290f9ac172e2998
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-26 09:19:01 +0200

    added conditional fields to form

--- app/meldingen/incident/components/IncidentForm/services/format-conditional-form/index.js
+++ app/meldingen/incident/components/IncidentForm/services/format-conditional-form/index.js
@@ -1,23 +1,25 @@
-import { filter } from 'lodash';
+import { forEach } from 'lodash';
 
 const formatConditionalForm = (form, incident) => {
-  console.log('formatConditionalForm', form, incident);
   if (form && form.controls) {
-    const controls = filter(form.controls, (control) => {
-      console.log('yoooo', control);
+    const removeKeys = [];
+
+    forEach(form.controls, (control, key) => {
       if (control.meta && control.meta.if) {
         const ifKey = Object.keys(control.meta.if)[0];
         const ifValue = Object.values(control.meta.if)[0];
-        if (incident[ifKey] === ifValue) {
-          console.log('---------------------------------------------- IF', ifKey, ifValue);
-          return true;
+        if (incident[ifKey] !== ifValue) {
+          removeKeys.push(key);
         }
-        return false;
       }
-      return true;
     });
 
-    return { controls };
+    if (removeKeys.length) {
+      removeKeys.map((key) => {
+        delete form.controls[key]; // eslint-disable-line no-param-reassign
+        return true;
+      });
+    }
   }
 
   return form;
