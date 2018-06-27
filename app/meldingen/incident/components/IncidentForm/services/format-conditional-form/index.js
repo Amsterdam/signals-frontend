cafcebd733133a85155030abd0beacfd4885f688
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-27 14:01:03 +0200

    refactored formatConditionalForm

--- app/meldingen/incident/components/IncidentForm/services/format-conditional-form/index.js
+++ app/meldingen/incident/components/IncidentForm/services/format-conditional-form/index.js
@@ -2,8 +2,6 @@ import { forEach, every } from 'lodash';
 
 const formatConditionalForm = (form, incident) => {
   if (form && form.controls) {
-    const removeKeys = [];
-
     forEach(form.controls, (control, key) => {
       if (control.meta) {
         form.controls[key].meta.ifVisible = true;  // eslint-disable-line no-param-reassign
@@ -11,17 +9,10 @@ const formatConditionalForm = (form, incident) => {
 
       if (control.meta && control.meta.if) {
         if (!every(control.meta.if, (v, k) => v === incident[k])) {
-          removeKeys.push(key);
+          form.controls[key].meta.ifVisible = false;  // eslint-disable-line no-param-reassign
         }
       }
     });
-
-    if (removeKeys.length) {
-      removeKeys.map((key) => {
-        form.controls[key].meta.ifVisible = false;  // eslint-disable-line no-param-reassign
-        return true;
-      });
-    }
   }
 
   return form;
