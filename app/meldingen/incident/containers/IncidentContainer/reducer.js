cca2e5f888a700f6a1ae16b1f4d90b0326a8e2d1
Jasper Swart <jasper.g.swart@gmail.com>
2018-06-29 10:44:56 +0200

    removed test values

--- app/meldingen/incident/containers/IncidentContainer/reducer.js
+++ app/meldingen/incident/containers/IncidentContainer/reducer.js
@@ -17,9 +17,6 @@ import {
 
 const initialState = fromJS({
   incident: {
-    extra_boten_snelheid_rondvaartboot: 'Ja',
-    description: 'tering boot',
-    subcategory: 'Overlast op het water - snel varen',
     incident_time_hours: 9,
     incident_time_minutes: 0
   }
