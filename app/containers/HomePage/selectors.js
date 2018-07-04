561640272716d423d68ce6a8f5c29aa50096df60
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-07-04 16:49:14 +0200

    del: loadRepos example code

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -2,16 +2,8 @@
  * Homepage selectors
  */
 
-import { createSelector } from 'reselect';
-
 const selectHome = (state) => state.get('home');
-
-const makeSelectUsername = () => createSelector(
-  selectHome,
-  (homeState) => homeState.get('username')
-);
 
 export {
   selectHome,
-  makeSelectUsername,
 };
