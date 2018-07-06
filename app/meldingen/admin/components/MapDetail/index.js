c039fce13d0d8947c90b641aba2721cdd72d6691
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-07-06 12:10:32 +0200

    fix: map location

--- app/meldingen/admin/components/MapDetail/index.js
+++ app/meldingen/admin/components/MapDetail/index.js
@@ -4,14 +4,19 @@ import MapContainer from 'containers/MapContainer';
 
 import './style.scss';
 
-const MapDetail = ({ value }) => (
-  <div className="map-detail">
-    <MapContainer
-      preview
-      latlng={{ lat: value.latitude, lng: value.logitude }}
-    />
-  </div>
-);
+const MapDetail = ({ value }) => {
+  const location = value.geometrie.coordinates;
+  const latlng = { lat: location[0], lng: location[1] };
+  return (
+    <div className="map-detail">
+      <MapContainer
+        preview
+        latlng={latlng}
+      />
+    </div>
+  );
+};
+
 
 MapDetail.propTypes = {
   value: PropTypes.object
