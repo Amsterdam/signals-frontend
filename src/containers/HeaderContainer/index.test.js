c77586bbd590feaf34d89f4b066d35b20d4130e5
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-07-20 17:06:05 +0200

    add: unittests for HeaderContainer

--- src/containers/HeaderContainer/index.test.js
+++ src/containers/HeaderContainer/index.test.js
@@ -1,10 +1,62 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { HeaderContainer } from 'index';
+import { isAuthenticated } from 'shared/services/auth/auth';
+import { HeaderContainer } from './index';
+// import Event from ;
+
+jest.mock('shared/services/auth/auth');
 
 describe('<HeaderContainer />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let props;
+  const event = {
+    persist: jest.fn(),
+    preventDefault: jest.fn(),
+    stopPropagation: jest.fn(),
+  };
+
+  beforeEach(() => {
+    props = {
+      userName: 'user',
+      onLogin: jest.fn(),
+      onLogout: jest.fn()
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render correctly when authenticated', () => {
+    isAuthenticated.mockImplementation((() => true));
+
+    const renderedComponent = shallow(
+      <HeaderContainer {...props} />
+    );
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  describe('onLoginLogoutButtonClick', () => {
+    it('should login when not authenticated', () => {
+      isAuthenticated.mockImplementation((() => false));
+
+      const renderedComponent = shallow(
+        <HeaderContainer {...props} />
+      );
+
+      const domain = 'the-login-domain';
+      expect(renderedComponent.instance().onLoginLogoutButtonClick(event, domain));
+      expect(props.onLogin).toHaveBeenCalledWith(domain);
+    });
+
+    it('should logout when authenticated', () => {
+      isAuthenticated.mockImplementation((() => true));
+
+      const renderedComponent = shallow(
+        <HeaderContainer {...props} />
+      );
+      expect(renderedComponent.instance().onLoginLogoutButtonClick(event));
+      expect(props.onLogout).toHaveBeenCalled();
+    });
   });
 });
