c20ce83ca311a414c0e100b1c93556c95e492234
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-07-20 17:34:05 +0200

    del: unused files

--- src/components/Map/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  header: {
-    id: 'app.components.Map.header',
-    defaultMessage: 'This is the Map component !',
-  },
-});
