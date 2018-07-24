f7f196d790550c5476ccaf9a6c5e3edde6790231
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-07-24 17:25:42 +0200

    add: matamo scripts in index.html

--- src/signals/incident-management/containers/IncidentCategoryContainer/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  header: {
-    id: 'app.containers.IncidentCategoryContainer.header',
-    defaultMessage: 'This is IncidentCategoryContainer container !',
-  },
-});
