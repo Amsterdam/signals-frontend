431f29d0610a7b8300cf2a072a7de28591ca2a15
Jasper Swart <jasper.g.swart@gmail.com>
2018-07-27 14:48:16 +0200

    added unit test for GlobalErrror

--- src/containers/GlobalError/index.test.js
+++ src/containers/GlobalError/index.test.js
@@ -1,10 +1,44 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { GlobalError } from 'index';
+import { GlobalError } from './index';
 
 describe('<GlobalError />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  describe('rendering', () => {
+    it('should render showing no error by default', () => {
+      const renderedComponent = shallow(
+        <GlobalError />
+      );
+      expect(renderedComponent).toMatchSnapshot();
+    });
+
+    it('should render showing an error when defined', () => {
+      const props = {
+        error: true,
+        errorMessage: 'MOCK_ERROR'
+      };
+      const renderedComponent = shallow(
+        <GlobalError {...props} />
+      );
+      expect(renderedComponent).toMatchSnapshot();
+    });
+  });
+
+  describe('events', () => {
+    it('should render showing no error by default', () => {
+      const props = {
+        error: true,
+        errorMessage: 'MOCK_ERROR',
+        onClose: jest.fn()
+      };
+      const renderedComponent = shallow(
+        <GlobalError {...props} />
+      );
+      expect(renderedComponent).toMatchSnapshot();
+
+      renderedComponent.find('button').simulate('click');
+      expect(props.onClose).toHaveBeenCalled();
+      expect(renderedComponent).toMatchSnapshot();
+    });
   });
 });
