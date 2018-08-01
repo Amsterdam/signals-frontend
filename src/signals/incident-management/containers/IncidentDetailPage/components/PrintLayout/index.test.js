431ff3eb294440a0d69e9f7c6e4b26325fdd3a37
Adrian Tudorache <a.p.tudorache@gmail.com>
2018-08-01 09:18:27 +0200

    add: unittests

--- src/signals/incident-management/containers/IncidentDetailPage/components/PrintLayout/index.test.js
+++ src/signals/incident-management/containers/IncidentDetailPage/components/PrintLayout/index.test.js
@@ -1,10 +1,24 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import PrintLayout from 'index';
+import PrintLayout from './index';
 
 describe('<PrintLayout />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let props;
+
+  beforeEach(() => {
+    props = {
+      id: '100',
+      incident: {},
+      stadsdeelList: [],
+      onPrintView: jest.fn()
+    };
+  });
+
+  it('should render correctly', () => {
+    const renderedComponent = shallow(
+      <PrintLayout {...props} />
+    );
+    expect(renderedComponent).toMatchSnapshot();
   });
 });
--- src/signals/incident-management/containers/IncidentDetailPage/index.test.js
+++ src/signals/incident-management/containers/IncidentDetailPage/index.test.js
@@ -4,6 +4,7 @@ import { shallow } from 'enzyme';
 import { IncidentDetailPage, mapDispatchToProps } from './index';
 import { REQUEST_INCIDENT } from './constants';
 import stadsdeelList from '../../definitions/stadsdeelList';
+import PrintLayout from './components/PrintLayout';
 
 
 describe('<IncidentDetailPage />', () => {
@@ -11,6 +12,7 @@ describe('<IncidentDetailPage />', () => {
 
   beforeEach(() => {
     props = {
+      id: '100',
       incidentdetailpage: { incident: {}, stadsdeelList },
       onRequestIncident: jest.fn()
     };
@@ -20,6 +22,25 @@ describe('<IncidentDetailPage />', () => {
     const renderedComponent = shallow(
       <IncidentDetailPage {...props} />
     );
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('should change the state', () => {
+    const renderedComponent = shallow(
+      <IncidentDetailPage {...props} />
+    );
+    renderedComponent.instance().onPrintView();
+    renderedComponent.instance().onTabChanged(1);
+    expect(renderedComponent.instance().state).toEqual({ selectedTab: 1, printView: true });
+  });
+
+  it('should render the print view', () => {
+    const renderedComponent = shallow(
+      <IncidentDetailPage {...props} />
+    );
+    renderedComponent.instance().onPrintView();
+    renderedComponent.update();
+    expect(renderedComponent.find(PrintLayout).length).toEqual(1);
     expect(renderedComponent).toMatchSnapshot();
   });
 
