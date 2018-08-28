a9b6c52d5c1826a6c992f4be792d9bd4327a8e8f
Jasper Swart <jasper.g.swart@gmail.com>
2018-08-28 10:47:18 +0200

    added unit test for form service mapControlsToParams

---
+++ src/signals/incident/services/map-controls-to-params/index.test.js
@@ -0,0 +1,120 @@
+import moment from 'moment';
+
+import mapControlsToParams from './index';
+
+jest.mock('moment');
+
+
+describe('The map controls to params service', () => {
+  it('should map status by default', () => {
+    expect(mapControlsToParams({}, {})).toEqual({
+      status: {
+        state: 'm',
+        extra_properties: {}
+      }
+    });
+  });
+
+  it('should map date: Nu', () => {
+    moment.mockImplementation(() => ({
+      format: () => '2018-07-21T12:34:00+02:00'
+    }));
+
+    expect(mapControlsToParams({
+      incident_time_hours: 12,
+      incident_time_minutes: 34,
+      datetime: 'Nu'
+    }, {})).toMatchObject({
+      created_at: '2018-07-21T12:34:00+02:00',
+      incident_date_start: '2018-07-21T12:34:00+02:00'
+    });
+  });
+
+  it('should map date: Vandaag', () => {
+    moment.mockImplementation(() => ({
+      format: () => '2018-07-21T10:21:00+02:00'
+    }));
+
+    expect(mapControlsToParams({
+      incident_time_hours: 10,
+      incident_time_minutes: 21,
+      incident_date: 'Vandaag'
+    }, {})).toMatchObject({
+      created_at: '2018-07-21T10:21:00+02:00',
+      incident_date_start: '2018-07-21T10:21:00+02:00'
+    });
+  });
+
+  it('should map date: fixed date', () => {
+    moment.mockImplementation(() => ({
+      format: () => '2018-07-02T09:05:00+02:00'
+    }));
+
+    expect(mapControlsToParams({
+      incident_time_hours: 9,
+      incident_time_minutes: 5,
+      incident_date: '2018-04-02'
+    }, {})).toMatchObject({
+      created_at: '2018-07-02T09:05:00+02:00',
+      incident_date_start: '2018-07-02T09:05:00+02:00'
+    });
+  });
+
+  it('should map variables when they are present in the wizard definition', () => {
+    expect(mapControlsToParams({
+      description: 'bar',
+      title: 666
+    }, {
+      step: {
+        form: {
+          controls: {
+            description: {
+              meta: {
+                path: 'text'
+              }
+            },
+            title: {
+              meta: {
+                path: 'diabolo'
+              }
+            },
+            var_no_path: {}
+          }
+        }
+      }
+    })).toMatchObject({
+      text: 'bar',
+      diabolo: 666
+    });
+  });
+
+  it('should merge multiple variables into 1 object', () => {
+    expect(mapControlsToParams({
+      description: 'bar',
+      title: 666
+    }, {
+      step: {
+        form: {
+          controls: {
+            description: {
+              meta: {
+                pathMerge: 'extra_properties',
+              }
+            },
+            title: {
+              meta: {
+                pathMerge: 'extra_properties',
+              }
+            },
+            var_no_path: {}
+          }
+        }
+      }
+    })).toMatchObject({
+      extra_properties: {
+        description: 'bar',
+        title: 666
+      }
+    });
+  });
+});
