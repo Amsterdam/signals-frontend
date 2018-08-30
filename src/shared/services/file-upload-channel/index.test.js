3b81ca37c391fa7395fab1d013979832a2cf51d0
Jasper Swart <jasper.g.swart@gmail.com>
2018-08-30 15:42:52 +0200

    added unit test for form channel fileuploadchannel

---
+++ src/shared/services/file-upload-channel/index.test.js
@@ -0,0 +1,46 @@
+import fileUploadChannel from './index';
+
+describe('The file upload channel service', () => {
+  let addEventListener;
+  let removeEventListener;
+  let open;
+  let send;
+
+  beforeEach(() => {
+    addEventListener = jest.fn();
+    removeEventListener = jest.fn();
+    open = jest.fn();
+    send = jest.fn();
+
+    window.XMLHttpRequest = jest.fn();
+    window.XMLHttpRequest.mockImplementation(() => ({
+      abort: jest.fn(),
+      open,
+      send,
+      upload: {
+        addEventListener,
+        removeEventListener
+      }
+    }));
+  });
+
+  afterEach(() => {
+  });
+
+  it('should map status by default', () => {
+    const channel = fileUploadChannel('https://acc.api.data.amsterdam.nl/signals/signal/image/', 'blob:http://host/c00d2e14-ae1c-4bb3-b67c-86ea93130b1c', 666);
+
+    expect(open).toHaveBeenCalledWith('POST', 'https://acc.api.data.amsterdam.nl/signals/signal/image/', true);
+    expect(send).toHaveBeenCalledWith(expect.any(Object));
+
+    expect(addEventListener).toHaveBeenCalledWith('progress', expect.any(Function));
+    expect(addEventListener).toHaveBeenCalledWith('error', expect.any(Function));
+    expect(addEventListener).toHaveBeenCalledWith('abort', expect.any(Function));
+
+    channel.close();
+
+    expect(removeEventListener).toHaveBeenCalledWith('progress', expect.any(Function));
+    expect(removeEventListener).toHaveBeenCalledWith('error', expect.any(Function));
+    expect(removeEventListener).toHaveBeenCalledWith('abort', expect.any(Function));
+  });
+});
