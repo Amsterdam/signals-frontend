2d71c6bdd303c3a9a07120630793af31282346e5
Robert Lodder <r.lodder@amsterdam.nl>
2018-09-13 16:25:24 +0200

    SIG-435: Remove duplicate key/value from (main) categoryList and sort it alphabetically

--- src/signals/incident-management/definitions/categoryList.js
+++ src/signals/incident-management/definitions/categoryList.js
@@ -1,15 +1,14 @@
 const categoryList = [
     { key: '', value: 'Alles' },
     { key: 'Afval', value: 'Afval' },
-    { key: 'Wegen, verkeer, straatmeubilair', value: 'Wegen, verkeer, straatmeubilair' },
-    { key: 'Overlast in de openbare ruimte', value: 'Overlast in de openbare ruimte' },
     { key: 'Openbaar groen en water', value: 'Openbaar groen en water' },
+    { key: 'Overlast Bedrijven en Horeca', value: 'Overlast Bedrijven en Horeca' },
+    { key: 'Overlast in de openbare ruimte', value: 'Overlast in de openbare ruimte' },
+    { key: 'Overlast op het water', value: 'Overlast op het water' },
     { key: 'Overlast van dieren', value: 'Overlast van dieren' },
     { key: 'Overlast van en door personen of groepen', value: 'Overlast van en door personen of groepen' },
-    { key: 'Overlast Bedrijven en Horeca', value: 'Overlast Bedrijven en Horeca' },
-    { key: 'Overlast op het water', value: 'Overlast op het water' },
-    { key: 'Wegen, verkeer, straatmeubilair', value: 'Wegen, verkeer, straatmeubilair' },
     { key: 'Straatverlichting, Klok, Verkeerslicht', value: 'Straatverlichting, Klok, Verkeerslicht' },
+    { key: 'Wegen, verkeer, straatmeubilair', value: 'Wegen, verkeer, straatmeubilair' },
 ];
 
 export default categoryList;
