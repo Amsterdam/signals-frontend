47c86663bf1209da760052c5fa715f392d7a7dad
Jasper Swart <jasper.g.swart@gmail.com>
2018-09-19 10:34:02 +0200

    moved debug state SIG-549

--- src/signals/incident/containers/IncidentContainer/reducer.js
+++ src/signals/incident/containers/IncidentContainer/reducer.js
@@ -19,36 +19,11 @@ import {
   SET_PRIORITY_SUCCESS,
   SET_PRIORITY_ERROR
 } from './constants';
+// import debugInitialState from './debug/initialState';
 
 export const initialState = fromJS({
   incident: {
-    // location: {
-      // address: {
-        // openbare_ruimte: 'Diemerparklaan',
-        // huisnummer: '16',
-        // huisletter: '',
-        // huisnummer_toevoeging: '',
-        // postcode: '1087GE',
-        // woonplaats: 'Amsterdam'
-      // },
-      // buurt_code: 'M35c',
-      // stadsdeel: 'M',
-      // geometrie: {
-        // type: 'Point',
-        // coordinates: [
-          // 4.995174407958985,
-          // 52.35451699670258
-        // ]
-      // }
-    // },
-    // subcategory: 'Overlast op het water - snel varen',
-    // category: 'Overlast op het water',
-    // description: 'snel boot',
-    // phone: '020654321',
-    // email: 'a@b.com',
-    // extra_boten_snelheid_rondvaartboot: 'Ja',
-    // extra_boten_snelheid_rederij: 'Aemstelland',
-    // datetime: 'Nu',
+    // ...debugInitialState,
 
     incident_date: 'Vandaag',
     incident_time_hours: 9,
