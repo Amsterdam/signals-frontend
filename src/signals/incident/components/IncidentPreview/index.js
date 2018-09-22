7d4ea15380294abe3011c3c18eb5aa10f72d2043
Jasper Swart <jasper.g.swart@gmail.com>
2018-09-22 15:57:07 +0200

    added unit test for incident preview SIG-529

--- src/signals/incident/components/IncidentPreview/index.js
+++ src/signals/incident/components/IncidentPreview/index.js
@@ -7,40 +7,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { FormattedMessage } from 'react-intl';
-// import messages from './messages';
-//      <FormattedMessage {...messages.header} />
-
 import { WithWizard } from 'react-albus';
 
 import './style.scss';
 
 function IncidentPreview({ incidentContainer, preview }) {
   return (
-    <WithWizard
-      render={({ push }) => (
-        <div className="incident-preview">
-          {Object.keys(preview).map((key) => (
-            <div className="incident-preview__section" key={key}>
+    <div className="incident-preview">
+      {Object.keys(preview).map((key) => (
+        <div className="incident-preview__section" key={key}>
+          <WithWizard
+            render={({ push }) => (
               <button
                 className="incident-preview__button-edit link-functional edit"
                 onClick={() => push(`incident/${key}`)}
               />
+            )}
+          />
 
-              {Object.keys(preview[key]).map((subkey) => (
-                <div key={subkey}>
-                  {preview[key][subkey].render({
-                    ...preview[key][subkey],
-                    value: incidentContainer.incident[subkey],
-                    incident: incidentContainer.incident
-                  })}
-                </div>
-              ))}
+          {Object.keys(preview[key]).map((subkey) => (
+            <div key={subkey}>
+              {preview[key][subkey].render({
+                ...preview[key][subkey],
+                value: incidentContainer.incident[subkey],
+                incident: incidentContainer.incident
+              })}
             </div>
           ))}
         </div>
-      )}
-    />
+      ))}
+    </div>
   );
 }
 
--- src/signals/incident/components/IncidentPreview/index.test.js
+++ src/signals/incident/components/IncidentPreview/index.test.js
@@ -1,10 +1,44 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import IncidentPreview from 'index';
+import PreviewComponents from '../../components/IncidentPreview/components/';
+import IncidentPreview from './index';
+
+// jest.mock('react-albus');
 
 describe('<IncidentPreview />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let props;
+
+  beforeEach(() => {
+    props = {
+      incidentContainer: {
+        incident: {
+          phone: '0666 666 666',
+          email: 'devil@hell.com'
+        }
+      },
+      preview: {
+        part1: {
+          phone: {
+            label: 'Uw (mobiele) telefoon',
+            render: PreviewComponents.PlainText
+          }
+        },
+        part2: {
+          email: {
+            label: 'Uw e-mailadres',
+            render: PreviewComponents.PlainText
+          }
+        }
+      }
+    };
+  });
+
+  it('expect to render correctly', () => {
+    const wrapper = shallow(
+      <IncidentPreview {...props} />
+    );
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
