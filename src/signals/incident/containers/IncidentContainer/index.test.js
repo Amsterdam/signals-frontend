c426c6fb2e9a4d335f4a022aa50fd08b25e2904b
Jasper Swart <jasper.g.swart@gmail.com>
2018-09-26 13:21:37 +0200

    added basic unit test for incident container SIG-529

--- src/signals/incident/containers/IncidentContainer/index.test.js
+++ src/signals/incident/containers/IncidentContainer/index.test.js
@@ -1,10 +1,45 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { fromJS } from 'immutable';
 
-// import { IncidentContainer } from 'index';
+import IncidentContainer from './index';
+// import IncidentContainer from './index';
+
+jest.mock('../../components/IncidentWizard', () => () => 'IncidentWizard');
 
 describe('<IncidentContainer />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let props;
+
+  beforeEach(() => {
+    props = {
+      incidentContainer: { incident: {} },
+      getClassification: jest.fn(),
+      updateIncident: jest.fn(),
+      createIncident: jest.fn(),
+      isAuthenticated: false
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('rendering', () => {
+    // it('should render correctly', () => {
+      // const wrapper = shallow(
+        // <IncidentContainer {...props} />
+      // );
+      // expect(wrapper).toMatchSnapshot();
+    // });
+
+    it('should render correctly', () => {
+      const mockStore = configureStore();
+      const store = mockStore(fromJS({ incidentContainer: { incident: {} } }));
+      const wrapper = mount(<Provider store={store}><IncidentContainer {...props} /></Provider>);
+
+      expect(wrapper).toMatchSnapshot();
+    });
   });
 });
