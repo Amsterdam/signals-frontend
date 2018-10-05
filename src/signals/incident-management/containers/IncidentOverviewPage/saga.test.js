ff4a626dac0fe4c72c442a98666f7cc48c22f8d8
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-05 13:29:35 +0200

    fetchting categories part two SIG-678

--- src/signals/incident-management/containers/IncidentOverviewPage/saga.test.js
+++ src/signals/incident-management/containers/IncidentOverviewPage/saga.test.js
@@ -1,11 +1,13 @@
-import { put, takeLatest, select, all } from 'redux-saga/effects';
+import { call, put, takeLatest, select, all } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
 import { authCall } from 'shared/services/api/api';
+import request from 'utils/request';
 
 import { REQUEST_INCIDENTS, REQUEST_CATEGORIES, INCIDENT_SELECTED } from './constants';
-import { requestIncidentsError, filterIncidentsChanged, pageIncidentsChanged } from './actions';
+import { requestIncidentsError, requestCategoriesSuccess, requestCategoriesError, filterIncidentsChanged, pageIncidentsChanged } from './actions';
 import watchRequestIncidentSaga, { fetchIncidents, fetchCategories, openIncident } from './saga';
 import { makeSelectFilterParams } from './selectors';
+import mapCategories from './services/map-categories';
 
 jest.mock('shared/services/api/api');
 jest.mock('./selectors', () => {
@@ -14,7 +16,7 @@ jest.mock('./selectors', () => {
     makeSelectFilterParams: () => mockedMakeSelectFilterParams,
   });
 });
-
+jest.mock('./services/map-categories');
 
 describe('IncidentOverviewPage saga', () => {
   afterEach(() => {
@@ -46,7 +48,7 @@ describe('IncidentOverviewPage saga', () => {
     const page = 2;
     const action = { payload: { filter, page } };
 
-    const requestURL = 'https://acc.api.data.amsterdam.nl/signals/auth/signal';
+    const requestURL = 'https://acc.a pi.data.amsterdam.nl/signals/auth/signal';
     const params = { test: 'test' };
 
     const gen = fetchIncidents(action);
@@ -64,5 +66,26 @@ describe('IncidentOverviewPage saga', () => {
     const gen = fetchIncidents(action);
     gen.next();
     expect(gen.throw(error).value).toEqual(put(requestIncidentsError(error))); // eslint-disable-line redux-saga/yield-effects
+  });
+
+  describe('fetchCategories', () => {
+    it('should success', () => {
+      const categories = { categories: [1], subcategorie: [2] };
+
+      mapCategories.mockImplementation(() => categories);
+      const requestURL = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories';
+
+      const gen = fetchCategories();
+      expect(gen.next().value).toEqual(call(request, requestURL)); // eslint-disable-line redux-saga/yield-effects
+      expect(gen.next(categories).value).toEqual(put(requestCategoriesSuccess(categories))); // eslint-disable-line redux-saga/yield-effects
+    });
+
+    it('should error', () => {
+      const error = new Error('404 Not Found');
+
+      const gen = fetchCategories();
+      gen.next();
+      expect(gen.throw(error).value).toEqual(put(requestCategoriesError(error))); // eslint-disable-line redux-saga/yield-effects
+    });
   });
 });
