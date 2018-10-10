3af14243edd134452e0835238903f5ca094ab7bf
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-10 13:53:13 +0200

    refactored set classification SIG-680

--- src/signals/incident/services/set-classification/index.test.js
+++ src/signals/incident/services/set-classification/index.test.js
@@ -1,62 +1,49 @@
-import setClassification from './index';
+ import setClassification from './index';
 
-describe('The set classification service', () => {
+describe.only('The set classification service', () => {
   it('should return Overig by default', () => {
     expect(setClassification()).toEqual({
-      category: 'Overig',
-      subcategory: 'Overig'
+      category: 'overig',
+      subcategory: 'overig',
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
     });
   });
 
   it('should return correct classification when minimum subcategory chance is met', () => {
     expect(
       setClassification({
-        hoofdrubriek: [
-          [
-            'Overlast van en door personen of groepen',
-            'Afval',
-            'Overlast in de openbare ruimte'
-          ],
-          [0.32405101692765315, 0.2840341890693276, 0.24243376736492095]
-        ],
         subrubriek: [
           [
-            'Honden(poep)',
-            'Wildplassen / poepen / overgeven',
-            'Veeg- / zwerfvuil'
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep',
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/wildplassen-poepen-overgeven',
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/duiven'
           ],
           [0.40, 0.1424216693948545, 0.04586915076913858]
         ]
       })
     ).toEqual({
-      category: 'Overlast van en door personen of groepen',
-      subcategory: 'Honden(poep)'
+      category: 'overlast-in-de-openbare-ruimte',
+      subcategory: 'hondenpoep',
+      subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep'
     });
   });
 
   it('should return Oberig when minimum subcategory chance is met', () => {
     expect(
       setClassification({
-        hoofdrubriek: [
-          [
-            'Overlast van en door personen of groepen',
-            'Afval',
-            'Overlast in de openbare ruimte'
-          ],
-          [0.32405101692765315, 0.2840341890693276, 0.24243376736492095]
-        ],
         subrubriek: [
           [
-            'Honden(poep)',
-            'Wildplassen / poepen / overgeven',
-            'Veeg- / zwerfvuil'
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep',
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/wildplassen-poepen-overgeven',
+            'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/duiven'
           ],
           [0.39, 0.1424216693948545, 0.04586915076913858]
         ]
       })
     ).toEqual({
-      category: 'Overig',
-      subcategory: 'Overig'
+      category: 'overig',
+      subcategory: 'overig',
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
     });
   });
 });
