ce0aad38541c748eca8a758c20f9137fb3f933dc
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-15 15:58:58 +0200

    refactored map-categories

--- src/shared/services/map-categories/index.js
+++ src/shared/services/map-categories/index.js
@@ -3,22 +3,18 @@ function mapCategories(data) {
   const subcategories = [];
 
   if (data && data.results) {
-    data.results.map((category) => {
+    data.results.forEach((category) => {
       categories.push({
         key: category._links && category._links.self && category._links.self.href,
         value: category._display
       });
 
-      category.sub_categories.map((subcategory) => {
+      category.sub_categories.forEach((subcategory) => {
         subcategories.push({
           key: subcategory._links && subcategory._links.self && subcategory._links.self.href,
           value: subcategory._display
         });
-
-        return true;
       });
-
-      return true;
     });
   }
 
