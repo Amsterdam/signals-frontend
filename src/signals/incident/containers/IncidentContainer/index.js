0c83443124846db90bddb6dc24a6ab774bb6089a
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-22 14:18:25 +0200

    updated text

--- src/signals/incident/containers/IncidentContainer/index.js
+++ src/signals/incident/containers/IncidentContainer/index.js
@@ -35,7 +35,9 @@ export class IncidentContainer extends React.Component {
     return (
       <div className="incident-container">
         <div className="incident-container__alert">
-          Heeft u een melding over straatverlichting, verkeerslichten en klokken? <a href="https://formulieren.amsterdam.nl/TripleForms/DirectRegelen/formulier/nl-NL/evAmsterdam/scMeldingenovl.aspx">Gebruik tijdelijk dit aparte formulier!</a>
+          <b>*** BELANGRIJK ***</b><br />
+            Melding over horeca of evenementen? Vul dan bij de melding altijd uw contactgegevens in, dan kunnen wij u beter en sneller helpen.<br /><br />
+            Heeft u een melding over straatverlichting, verkeerslichten en klokken? <a href="https://formulieren.amsterdam.nl/TripleForms/DirectRegelen/formulier/nl-NL/evAmsterdam/scMeldingenovl.aspx">Gebruik tijdelijk dit aparte formulier!</a>
         </div>
         <IncidentWizard
           wizardDefinition={wizardDefinition}
