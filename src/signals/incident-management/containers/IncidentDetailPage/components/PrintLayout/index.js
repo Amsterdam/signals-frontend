e87709d3fcf07b22599faaa2da202b99c0253661
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-24 15:44:15 +0200

    added notes to print view

--- src/signals/incident-management/containers/IncidentDetailPage/components/PrintLayout/index.js
+++ src/signals/incident-management/containers/IncidentDetailPage/components/PrintLayout/index.js
@@ -10,8 +10,10 @@ import Img from 'shared/components/Img';
 import './style.scss';
 import MapDetail from '../MapDetail';
 import IncidentDetail from '../IncidentDetail';
-import List from '../../../IncidentStatusContainer/components/List';
+import StatusList from '../../../IncidentStatusContainer/components/List';
+import NotesList from '../../../IncidentNotesContainer/components/List';
 import makeSelectIncidentStatusContainer from '../../../IncidentStatusContainer/selectors';
+import makeSelectIncidentNotesContainer from '../../../IncidentNotesContainer/selectors';
 
 export class PrintLayout extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -25,7 +27,8 @@ export class PrintLayout extends React.Component { // eslint-disable-line react/
 
   render() {
     const { incident, stadsdeelList, priorityList, onPrintView } = this.props;
-    const { incidentStatusList, statusList } = this.props.incidentstatuscontainer;
+    const { incidentStatusList, statusList } = this.props.incidentStatusContainer;
+    const { incidentNotesList } = this.props.incidentNotesContainer;
     return (
       <div className="print-layout row container" >
         <div className="col-12">
@@ -48,7 +51,10 @@ export class PrintLayout extends React.Component { // eslint-disable-line react/
           }
         </div>
         <div className="col-12 print-layout__status-list">
-          <List incidentStatusList={incidentStatusList} statusList={statusList} />
+          <StatusList incidentStatusList={incidentStatusList} statusList={statusList} />
+        </div>
+        <div className="col-12 print-layout__status-list">
+          <NotesList incidentNotesList={incidentNotesList} statusList={statusList} />
         </div>
       </div>
     );
@@ -61,11 +67,13 @@ PrintLayout.propTypes = {
   stadsdeelList: PropTypes.array.isRequired,
   priorityList: PropTypes.array.isRequired,
   onPrintView: PropTypes.func.isRequired,
-  incidentstatuscontainer: PropTypes.object.isRequired,
+  incidentStatusContainer: PropTypes.object.isRequired,
+  incidentNotesContainer: PropTypes.object.isRequired
 };
 
 const mapStateToProps = createStructuredSelector({
-  incidentstatuscontainer: makeSelectIncidentStatusContainer(),
+  incidentStatusContainer: makeSelectIncidentStatusContainer(),
+  incidentNotesContainer: makeSelectIncidentNotesContainer()
 });
 
 const withConnect = connect(mapStateToProps);
