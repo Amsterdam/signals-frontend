e45d8e23265d77155575f9e8ed6e3f4fce64d7a5
Jasper Swart <jasper.g.swart@gmail.com>
2018-10-30 17:38:46 +0100

    renamed all constants

--- src/signals/incident-management/containers/IncidentDetailPage/constants.js
+++ src/signals/incident-management/containers/IncidentDetailPage/constants.js
@@ -1,7 +1,7 @@
-export const REQUEST_INCIDENT = 'src/IncidentDetailPage/REQUEST_INCIDENT';
-export const REQUEST_INCIDENT_SUCCESS = 'src/IncidentDetailPage/REQUEST_INCIDENT_SUCCESS';
-export const REQUEST_INCIDENT_ERROR = 'src/IncidentDetailPage/REQUEST_INCIDENT_ERROR';
+export const REQUEST_INCIDENT = 'sia/IncidentDetailPage/REQUEST_INCIDENT';
+export const REQUEST_INCIDENT_SUCCESS = 'sia/IncidentDetailPage/REQUEST_INCIDENT_SUCCESS';
+export const REQUEST_INCIDENT_ERROR = 'sia/IncidentDetailPage/REQUEST_INCIDENT_ERROR';
 
-export const REQUEST_NOTES_LIST = 'src/IncidentDetailPage/REQUEST_NOTES_LIST';
-export const REQUEST_NOTES_LIST_SUCCESS = 'src/IncidentDetailPage/REQUEST_NOTES_LIST_SUCCESS';
-export const REQUEST_NOTES_LIST_ERROR = 'src/IncidentDetailPage/REQUEST_NOTES_LIST_ERROR';
+export const REQUEST_NOTES_LIST = 'sia/IncidentDetailPage/REQUEST_NOTES_LIST';
+export const REQUEST_NOTES_LIST_SUCCESS = 'sia/IncidentDetailPage/REQUEST_NOTES_LIST_SUCCESS';
+export const REQUEST_NOTES_LIST_ERROR = 'sia/IncidentDetailPage/REQUEST_NOTES_LIST_ERROR';
