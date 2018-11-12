7d51274f52b519f4ba3632107686b0f4554767c1
kelvinpaska <k.paska@amsterdam.nl>
2018-11-12 14:28:40 +0100

    test bakkie config

--- scripts/bakkie.sh
+++ scripts/bakkie.sh
@@ -15,14 +15,14 @@ CYPRESS_SKIP_BINARY_INSTALL=1 npm install
 npm run build
 
 echo Publish distribution in web-dir
-OUTDIR=/var/www/html/signals/builds/$1
+OUTDIR=/var/www/html/signals-frontend/builds/$1
 if [ ! -d ${OUTDIR} ];
 	then mkdir -p ${OUTDIR};
 fi
 cp -r dist/* ${OUTDIR}
 
 echo Generate new index.html for all branches
-echo '<h1>signals links</h1>' > /var/www/html/signals/index.html
-echo "<h2>Generated at `date`</h2>" >> /var/www/html/signals/index.html
-find /var/www/html/signals -name index.html -printf "%T+\t%p\n" | sort -r | grep 'signals' | sed 's/^.*\/signals/\/signals/' | sed 's/\/index.html//' | sed 's/^.*$/<p><a href="\0">\0<\/a><p>/' >> /var/www/html/signals/index.html
+echo '<h1>signals-frontend links</h1>' > /var/www/html/signals-frontend/index.html
+echo "<h2>Generated at `date`</h2>" >> /var/www/html/signals-frontend/index.html
+find /var/www/html/signals-frontend -name index.html -printf "%T+\t%p\n" | sort -r | grep 'signals-frontend' | sed 's/^.*\/signals-frontend/\/signals-frontend/' | sed 's/\/index.html//' | sed 's/^.*$/<p><a href="\0">\0<\/a><p>/' >> /var/www/html/signals-frontend/index.html
 echo Done
