63d76358dbb11af326b5b3713a497112685f9dd8
Jasper Swart <jasper.g.swart@gmail.com>
2018-12-04 18:08:19 +0100

    added days in process to admin detail list SIG-429

--- src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import { string2date, string2time } from 'shared/services/string-parser/string-parser';
 import { getListValueByKey } from 'shared/services/list-helper/list-helper';
@@ -12,6 +13,15 @@ class List extends React.Component { // eslint-disable-line react/prefer-statele
     } else {
       this.props.onRequestIncidents({ sort });
     }
+  }
+
+  getExpiredDays(incident) {
+    if (incident.status.state !== 'o' && incident.status.state !== 'a') {
+      const end = moment(incident.created_at); // another date
+      const duration = moment.duration(moment().diff(end));
+      return Math.floor(duration.asDays());
+    }
+    return '-';
   }
 
   selectIncident = (incident) => () => {
@@ -27,6 +37,7 @@ class List extends React.Component { // eslint-disable-line react/prefer-statele
     return className;
   }
 
+
   render() {
     const { incidents, incidentsCount, priorityList, statusList, stadsdeelList } = this.props;
     return (
@@ -37,6 +48,7 @@ class List extends React.Component { // eslint-disable-line react/prefer-statele
             <thead>
               <tr>
                 <th onClick={this.onSort('id')} className={this.sortClassName('id')} >Id</th>
+                <th>Dgn</th>
                 <th onClick={this.onSort('created_at')} className={this.sortClassName('created_at')}>Datum en tijd</th>
                 <th onClick={this.onSort('stadsdeel,-created_at')} className={this.sortClassName('stadsdeel')}>Stadsdeel</th>
                 <th onClick={this.onSort('sub_category,-created_at')} className={this.sortClassName('sub_category')}>Subcategorie</th>
@@ -49,6 +61,7 @@ class List extends React.Component { // eslint-disable-line react/prefer-statele
               {incidents.map((incident) => (
                 <tr key={incident.id} onClick={this.selectIncident(incident)}>
                   <td>{incident.id}</td>
+                  <td>{this.getExpiredDays(incident)}</td>
                   <td className="no-wrap">{string2date(incident.incident_date_start)} {string2time(incident.incident_date_start)}</td>
                   <td>{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}</td>
                   <td>{incident.category && incident.category.sub}</td>
