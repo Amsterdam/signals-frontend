9198f77100275535151c06482a75d9649278af7c
Jasper Swart <jasper.g.swart@gmail.com>
2019-01-04 16:40:37 +0100

    added line chart as well SIG-890

--- src/signals/incident-management/containers/Dashboard/index.js
+++ src/signals/incident-management/containers/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Tooltip, Legend } from 'recharts';
+import { PieChart, Pie, Tooltip, Legend, LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -21,14 +21,33 @@ export class Dashboard extends React.PureComponent { // eslint-disable-line reac
 
   render() {
     const { dashboard } = this.props.incidentDashboard;
+
+    const data = [
+      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }
+    ];
+
     return (
       <div className="dashboard">
-        <PieChart width={730} height={450}>
+        <PieChart width={730} height={500}>
           <Tooltip />
           <Legend />
           <Pie data={dashboard.pie1 || []} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" />
           <Pie data={dashboard.pie2 || []} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={120} outerRadius={200} fill="#82ca9d" label />
         </PieChart>
+
+        <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
       </div>
     );
   }
