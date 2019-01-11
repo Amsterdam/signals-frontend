49e65055474dff6ef11f8b7e39ddad19a3231763
Jasper Swart <jasper.g.swart@gmail.com>
2019-01-11 16:09:57 +0100

    added unit test for. all chart components SIG-890

--- src/signals/incident-management/containers/Dashboard/components/HourChart/index.test.js
+++ src/signals/incident-management/containers/Dashboard/components/HourChart/index.test.js
@@ -1,10 +1,43 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import HourChart from 'index';
+import HourChart from './index';
 
 describe('<HourChart />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      data: [
+        { hour: 0, count: 4 },
+        { hour: 1, count: 8 },
+        { hour: 2, count: 9 },
+        { hour: 3, count: 13 },
+        { hour: 4, count: 8 },
+        { hour: 5, count: 42 },
+        { hour: 6, count: 70 },
+        { hour: 7, count: 60 },
+        { hour: 8, count: 93 },
+        { hour: 9, count: 137 },
+        { hour: 10, count: 64 },
+        { hour: 11, count: 71 },
+        { hour: 12, count: 12 }
+      ]
+    };
+
+    wrapper = shallow(
+      <HourChart {...props} />
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render correctly', () => {
+    wrapper = shallow(<HourChart {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
--- src/signals/incident-management/containers/Dashboard/components/StatusChart/index.test.js
+++ src/signals/incident-management/containers/Dashboard/components/StatusChart/index.test.js
@@ -1,10 +1,36 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import StatusGraph from 'index';
+import StatusChart from './index';
 
-describe('<StatusGraph />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
+describe('<StatusChart />', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      data: [
+        { name: 'Gemeld', count: 57, color: '#23B0C3' },
+        { name: 'In afhandeling van behandeling', count: 7, color: '#E8663F' },
+        { name: 'In behandeling', count: 20, color: '#FE952F' },
+        { name: 'Geannuleerd', count: 2, color: '#96C14F' },
+        { name: 'Afgehandeld', count: 13, color: '#9B4474' },
+        { name: 'On hold', count: 1, color: '#E8663F' }
+      ]
+    };
+
+    wrapper = shallow(
+      <StatusChart {...props} />
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render correctly', () => {
+    wrapper = shallow(<StatusChart {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
   });
 });
