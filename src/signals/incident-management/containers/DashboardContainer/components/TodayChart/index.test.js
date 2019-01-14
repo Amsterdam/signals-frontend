1756d3a00e96aaa57c57b38511aa3799335c6634
Jasper Swart <jasper.g.swart@gmail.com>
2019-01-14 14:56:53 +0100

    added moment mock SIG-890

--- src/signals/incident-management/containers/DashboardContainer/components/TodayChart/index.test.js
+++ src/signals/incident-management/containers/DashboardContainer/components/TodayChart/index.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 
 import TodayChart from './index';
+
+jest.mock('moment');
 
 describe('<TodayChart />', () => {
   let wrapper;
   let props;
 
   beforeEach(() => {
+    moment.mockImplementation(() => ({
+      format: () => 'vrijdag 11 januari 2019'
+    }));
+
     props = {
       data: { count: 639 }
     };
