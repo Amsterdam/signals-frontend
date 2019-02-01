8fc0e359772dd507385a443e9ae798dbef1942ae
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-01 12:30:34 +0100

    fixed ordering of days open SIG-903

--- src/signals/incident-management/containers/IncidentOverviewPage/saga.js
+++ src/signals/incident-management/containers/IncidentOverviewPage/saga.js
@@ -19,6 +19,13 @@ export function* fetchIncidents(action) {
     const sort = action.payload.sort;
     if (sort) yield put(sortIncidentsChanged(sort));
     const params = yield select(makeSelectFilterParams());
+    // TEMP remove when server can order days_open
+    if (params.ordering === 'days_open') {
+      params.ordering = '-created_at';
+    }
+    if (params.ordering === '-days_open') {
+      params.ordering = 'created_at';
+    }
     const incidents = yield authCall(requestURL, params);
 
     yield put(requestIncidentsSuccess(incidents));
