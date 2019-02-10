35956e25718475c8895c209f860ed7a6ddf999bb
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-10 20:43:04 +0100

    [SIG-924] temp try auto commit smg

--- src/models/incident/actions.js
+++ src/models/incident/actions.js
@@ -2,6 +2,7 @@ import {
   REQUEST_INCIDENT, REQUEST_INCIDENT_SUCCESS, REQUEST_INCIDENT_ERROR
 } from './constants';
 
+
 export function requestIncident(id) {
   return {
     type: REQUEST_INCIDENT,
