c5af43a08187a813678c4c98785caa320b0b1652
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-10 20:45:45 +0100

    [SIG-924] OMG it WORKS!

--- src/models/incident/actions.js
+++ src/models/incident/actions.js
@@ -2,7 +2,6 @@ import {
   REQUEST_INCIDENT, REQUEST_INCIDENT_SUCCESS, REQUEST_INCIDENT_ERROR
 } from './constants';
 
-
 export function requestIncident(id) {
   return {
     type: REQUEST_INCIDENT,
