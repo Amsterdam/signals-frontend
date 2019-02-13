57985ae477db4230f490116fa4ae7d0225ec8225
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-13 14:58:32 +0100

    [SIG-924] added incident part to split form

--- src/signals/incident-management/containers/IncidentSplitContainer/components/SplitForm/index.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/components/SplitForm/index.js
@@ -5,17 +5,14 @@ import { FormBuilder, FieldGroup } from 'react-reactive-form';
 
 import './style.scss';
 
-import FieldControlWrapper from '../../../../components/FieldControlWrapper';
-import CopyFileInput from '../../../../components/CopyFileInput';
-import RadioInput from '../../../../components/RadioInput';
-import SelectInput from '../../../../components/SelectInput';
-import TextAreaInput from '../../../../components/TextAreaInput';
+import IncidentPart from '../IncidentPart';
 
 class SplitForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      isVisible: props.isVisible,
       splitForm: FormBuilder.group({
         id: props.incident.id,
         part1subcategory: props.incident.category.sub_slug,
@@ -27,15 +24,25 @@ class SplitForm extends React.Component {
         part2text: props.incident.text,
         part2file: true,
         part2note: '',
-        part2priority: props.incident.priority.priority
+        part2priority: props.incident.priority.priority,
+        part3subcategory: props.incident.category.sub_slug,
+        part3text: props.incident.text,
+        part3file: true,
+        part3note: '',
+        part3priority: props.incident.priority.priority
       })
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setVisibility = this.setVisibility.bind(this);
   }
 
   handleSubmit() {
     this.props.handleSubmit(this.state.splitForm);
+  }
+
+  setVisibility(isVisible) {
+    this.setState({ isVisible });
   }
 
   render() {
@@ -46,93 +53,43 @@ class SplitForm extends React.Component {
           <div>
             <h1>Splitsen</h1>
 
-            <FieldGroup
-              control={this.state.splitForm}
-              render={({ invalid }) => (
-                <form onSubmit={this.handleSubmit}>
-                  <h2>Deelmelding 1</h2>
-                  <FieldControlWrapper
-                    render={SelectInput}
-                    name="part1subcategory"
-                    display="Subcategorie"
-                    control={this.state.splitForm.get('part1subcategory')}
-                    values={subcategories}
-                    useSlug
-                  />
-                  <FieldControlWrapper
-                    render={TextAreaInput}
-                    name="part1text"
-                    display="Omschrijving"
-                    control={this.state.splitForm.get('part1text')}
-                    rows={5}
-                  />
-                  {incident.image &&
-                    <FieldControlWrapper
-                      render={CopyFileInput}
-                      name="part1file"
-                      control={this.state.splitForm.get('part1file')}
-                      values={[{ key: '1', alt: `Foto bij melding ${incident.id}`, value: incident.image }]}
-                    />
-                  }
-                  <FieldControlWrapper
-                    render={TextAreaInput}
-                    name="part1note"
-                    display="Notitie"
-                    control={this.state.splitForm.get('part1note')}
-                    rows={5}
-                  />
-                  <FieldControlWrapper
-                    render={RadioInput}
-                    name="part1priority"
-                    display="Urgentie"
-                    control={this.state.splitForm.get('part1priority')}
-                    values={priorityList}
-                  />
-
-                  <h2>Deelmelding 2</h2>
-                  <FieldControlWrapper
-                    render={SelectInput}
-                    name="part2subcategory"
-                    display="Subcategorie"
-                    control={this.state.splitForm.get('part2subcategory')}
-                    values={subcategories}
-                    useSlug
-                  />
-                  <FieldControlWrapper
-                    render={TextAreaInput}
-                    name="part2text"
-                    display="Omschrijving"
-                    control={this.state.splitForm.get('part2text')}
-                    rows={5}
-                  />
-                  {incident.image &&
-                    <FieldControlWrapper
-                      render={CopyFileInput}
-                      name="part2file"
-                      control={this.state.splitForm.get('part2file')}
-                      values={[{ key: '1', alt: `Foto bij melding ${incident.id}`, value: incident.image }]}
-                    />
-                  }
-                  <FieldControlWrapper
-                    render={TextAreaInput}
-                    name="part2note"
-                    display="Notitie"
-                    control={this.state.splitForm.get('part2note')}
-                    rows={5}
-                  />
-                  <FieldControlWrapper
-                    render={RadioInput}
-                    name="part2priority"
-                    display="Urgentie"
-                    control={this.state.splitForm.get('part2priority')}
-                    values={priorityList}
-                  />
-
-                </form>
-              )}
+            <IncidentPart
+              index="1"
+              incident={incident}
+              subcategories={subcategories}
+              priorityList={priorityList}
+              splitForm={this.state.splitForm}
             />
-            <button onClick={this.handleSubmit} className="action primary">Splitsen</button>
-            <button onClick={handleCancel} className="action tertiair">Annuleer</button>
+
+            <IncidentPart
+              index="2"
+              incident={incident}
+              subcategories={subcategories}
+              priorityList={priorityList}
+              splitForm={this.state.splitForm}
+            />
+
+            {this.state.isVisible ?
+              <div>
+                <button onClick={() => this.setVisibility(false)} className="action reset">Verwijder</button>
+
+                <IncidentPart
+                  index="3"
+                  incident={incident}
+                  subcategories={subcategories}
+                  priorityList={priorityList}
+                  splitForm={this.state.splitForm}
+                />
+
+              </div>
+              :
+              <button onClick={() => this.setVisibility(true)} className="action tertiair">Deelmelding 3 toevoegen</button>
+            }
+
+            <div>
+              <button onClick={this.handleSubmit} className="action primary">Splitsen</button>
+              <button onClick={handleCancel} className="action tertiair">Annuleer</button>
+            </div>
           </div>
         )
       : ''}
@@ -148,11 +105,13 @@ SplitForm.defaultProps = {
       priority:  ''
     }
   },
+  isVisible: false,
   subcategories: []
 };
 
 SplitForm.propTypes = {
   incident: PropTypes.object,
+  isVisible: PropTypes.bool,
   subcategories: PropTypes.array,
   priorityList: PropTypes.array,
   handleSubmit: PropTypes.func.isRequired,
