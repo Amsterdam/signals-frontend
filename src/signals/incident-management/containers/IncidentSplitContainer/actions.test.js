e3f7cb19d989bee0170a22588153429702a5badc
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-20 11:55:49 +0100

    [SIG-924] fixec unit test for action and saga in split

--- src/signals/incident-management/containers/IncidentSplitContainer/actions.test.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/actions.test.js
@@ -1,9 +1,9 @@
 import {
-  SPLIT_INCIDENT // , REQUEST_INCIDENT_SUCCESS, REQUEST_INCIDENT_ERROR
+  SPLIT_INCIDENT, SPLIT_INCIDENT_SUCCESS, SPLIT_INCIDENT_ERROR
 } from './constants';
 
 import {
-  splitIncident // , requestIncidentSuccess, requestIncidentError
+  splitIncident, splitIncidentSuccess, splitIncidentError
 } from './actions';
 
 import { testActionCreator } from '../../../../../internals/testing/test-utils';
@@ -12,7 +12,7 @@ describe('IncidentSplitontainer actions', () => {
   it('should be created', () => {
     const payload = { prop: {} };
     testActionCreator(splitIncident, SPLIT_INCIDENT, payload);
-    // testActionCreator(requestIncidentSuccess, REQUEST_INCIDENT_SUCCESS, payload);
-    // testActionCreator(requestIncidentError, REQUEST_INCIDENT_ERROR, payload);
+    testActionCreator(splitIncidentSuccess, SPLIT_INCIDENT_SUCCESS, payload);
+    testActionCreator(splitIncidentError, SPLIT_INCIDENT_ERROR, payload);
   });
 });
--- src/signals/incident-management/containers/IncidentSplitContainer/reducer.test.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/reducer.test.js
@@ -1,9 +1,9 @@
 import { fromJS } from 'immutable';
 import incidentSplitContainerReducer, { initialState } from './reducer';
 import {
-  // SPLIT_INCIDENT,
-  // REQUEST_INCIDENT_SUCCESS,
-  // REQUEST_INCIDENT_ERROR
+  SPLIT_INCIDENT,
+  SPLIT_INCIDENT_SUCCESS,
+  SPLIT_INCIDENT_ERROR
 }
   from './constants';
 
@@ -11,53 +11,49 @@ jest.mock('../../definitions/stadsdeelList');
 
 describe('incidentSplitContainerReducer', () => {
   const reducer = incidentSplitContainerReducer;
-  // const expected = {
-  //   id: null,
-  //   loading: false,
-  //   error: false
-  // };
+  const expected = {
+    split: false,
+    loading: false,
+    error: false
+  };
 
   it('returns the initial state', () => {
     expect(reducer(undefined, {})).toEqual(fromJS(initialState));
   });
 
-  // it('should handle the SPLIT_INCIDENT', () => {
-  //   expect(
-  //     incidentSplitContainerReducer(undefined, {
-  //       type: SPLIT_INCIDENT,
-  //       payload: 42
-  //     }).toJS()
-  //   ).toEqual({
-  //     ...expected,
-  //     id: 42,
-  //     loading: true
-  //   });
-  // });
+  it('should handle the SPLIT_INCIDENT', () => {
+    expect(
+      incidentSplitContainerReducer(undefined, {
+        type: SPLIT_INCIDENT
+      }).toJS()
+    ).toEqual({
+      ...expected,
+      loading: true
+    });
+  });
 
-  // it('should handle the REQUEST_INCIDENT_SUCCESS', () => {
-  //   const payload = { id: 1 };
-  //   expect(
-  //     incidentSplitContainerReducer(undefined, {
-  //       type: REQUEST_INCIDENT_SUCCESS,
-  //       payload
-  //     }).toJS()
-  //   ).toEqual({
-  //     ...expected,
-  //     incident: {
-  //       id: 1
-  //     }
-  //   });
-  // });
+  it('should handle the SPLIT_INCIDENT_SUCCESS', () => {
+    const payload = { incident: { id: 42 } };
+    expect(
+      incidentSplitContainerReducer(undefined, {
+        type: SPLIT_INCIDENT_SUCCESS,
+        payload
+      }).toJS()
+    ).toEqual({
+      ...expected,
+      split: payload
+    });
+  });
 
-  // it('should handle the REQUEST_INCIDENT_ERROR', () => {
-  //   expect(
-  //     incidentSplitContainerReducer(undefined, {
-  //       type: REQUEST_INCIDENT_ERROR,
-  //       payload: true
-  //     }).toJS()
-  //   ).toEqual({
-  //     ...expected,
-  //     error: true
-  //   });
-  // });
+  it('should handle the SPLIT_INCIDENT_ERROR', () => {
+    expect(
+      incidentSplitContainerReducer(undefined, {
+        type: SPLIT_INCIDENT_ERROR,
+        payload: true
+      }).toJS()
+    ).toEqual({
+      ...expected,
+      error: true
+    });
+  });
 });
