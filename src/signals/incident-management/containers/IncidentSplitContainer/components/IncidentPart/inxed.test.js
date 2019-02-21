3321b97d12b6ecb108845af771e3a3e92da8abad
Jasper Swart <jasper.g.swart@gmail.com>
2019-02-21 17:18:40 +0100

    [SIG-924] added test for SplitNotificationBar

--- src/signals/incident-management/containers/IncidentSplitContainer/components/IncidentPart/inxed.test.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/components/IncidentPart/inxed.test.js
@@ -5,47 +5,59 @@
 // import { IncidentPart } from './index';
 // import priorityList from '../../../../definitions/priorityList';
 
-describe('<IncidentPart />', () => {
-  // let props;
+// describe('<IncidentPart />', () => {
+//   let props;
 
-  // beforeEach(() => {
-  //   props = {
-  //     index: '2',
-  //     incident: {
-  //       category: {},
-  //       priority: {
-  //         priority: ''
-  //       }
-  //     },
-  //     subcategories: [{
-  //       key: 'key',
-  //       value: 'value',
-  //       slug: 'slug'
-  //     }],
-  //     priorityList,
-  //     splitForm: FormBuilder.group({
-  //       part1: FormBuilder.group({
-  //         subcategory: 'slug',
-  //         text: 'er ligt hier alweer poep',
-  //         file: true,
-  //         note: '',
-  //         priority: 'high',
-  //       })
-  //     })
-  //   };
-  // });
+//   beforeEach(() => {
+//     const form = {
+//       get: jest.fn().mockImplementation((item) => {
+//         console.log('iterm', item);
+//         switch (item) {
+//           default:
+//             return '';
+//         }
+//       })
+//     };
 
-  // describe('rendering', () => {
-  //   it('should render correctly', () => {
-  //     const wrapper = shallow(
-  //       <IncidentPart {...props} />
-  //     );
-  //     expect(wrapper).toMatchSnapshot();
-  //     // expect(props.onRequestIncident).toHaveBeenCalledWith('42');
-  //   });
-  // });
+//     props = {
+//       index: '2',
+//       incident: {
+//         category: {},
+//         priority: {
+//           priority: ''
+//         }
+//       },
+//       subcategories: [{
+//         key: 'key',
+//         value: 'value',
+//         slug: 'slug'
+//       }],
+//       priorityList,
+//       splitForm: form
+//     };
+//   });
 
-  it('shoild', () => {
-    expect(true).toBe(true);
-  });
+//   // splitForm: FormBuilder.group({
+//   //   part1: FormBuilder.group({
+//   //     subcategory: 'slug',
+//   //     text: 'er ligt hier alweer poep',
+//   //     file: true,
+//   //     note: 'opmerking',
+//   //     priority: 'high',
+//   //   })
+//   // })
+
+//   describe('rendering', () => {
+//     it('should render correctly', () => {
+//       const wrapper = shallow(
+//         <IncidentPart {...props} />
+//       );
+//       expect(wrapper).toMatchSnapshot();
+//       // expect(props.onRequestIncident).toHaveBeenCalledWith('42');
+//     });
+//   });
+// });
+
+it('should', () => {
+  expect(1).toBe(1);
 });
