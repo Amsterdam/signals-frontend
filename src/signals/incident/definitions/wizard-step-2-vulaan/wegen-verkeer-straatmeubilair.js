48e11f4a41551325be98ae06360b47659d99afb1
Jasper Swart <jasper.g.swart@gmail.com>
2019-03-04 11:56:46 +0100

    added verkeerslicht uitrvraag

--- src/signals/incident/definitions/wizard-step-2-vulaan/wegen-verkeer-straatmeubilair.js
+++ src/signals/incident/definitions/wizard-step-2-vulaan/wegen-verkeer-straatmeubilair.js
@@ -63,56 +63,14 @@ export default {
         label: 'Wat is er aan de hand met de klok',
         ifAllOf: {
           subcategory: 'klok'
-        }
-      },
-      render: FormComponents.PlainText
-    },
-    extra_klok_niet_op_tijd: {
-      meta: {
-        ifAllOf: {
-          subcategory: 'klok'
         },
-        value: 'Loopt niet op tijd',
-        pathMerge: 'extra_properties'
-      },
-      render: FormComponents.CheckboxInput
-    },
-    extra_klok_stuk: {
-      meta: {
-        ifAllOf: {
-          subcategory: 'klok'
-        },
-        value: 'Lamp is stuk',
-        pathMerge: 'extra_properties'
-      },
-      render: FormComponents.CheckboxInput
-    },
-    extra_klok_aangereden: {
-      meta: {
-        ifAllOf: {
-          subcategory: 'klok'
-        },
-        value: 'Is aangereden',
-        pathMerge: 'extra_properties'
-      },
-      render: FormComponents.CheckboxInput
-    },
-    extra_klok_deurtje_open: {
-      meta: {
-        ifAllOf: {
-          subcategory: 'klok'
-        },
-        value: 'Het deurtje van de paal staat open',
-        pathMerge: 'extra_properties'
-      },
-      render: FormComponents.CheckboxInput
-    },
-    extra_klok_beschadigd: {
-      meta: {
-        ifAllOf: {
-          subcategory: 'klok'
-        },
-        value: 'Is zichtbaar beschadigd',
+        value: [
+          'Loopt niet op tijd',
+          'Lamp is stuk',
+          'Is aangereden',
+          'Het deurtje van de paal staat open',
+          'Is zichtbaar beschadigd'
+        ],
         pathMerge: 'extra_properties'
       },
       render: FormComponents.CheckboxInput
@@ -123,6 +81,88 @@ export default {
           subcategory: 'klok'
         },
         label: 'Hebt u een nummer de klok?',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+    extra_verkeerslicht_text: {
+      meta: {
+        className: 'col-sm-12 col-md-6',
+        ifAllOf: {
+          subcategory: 'verkeerslicht'
+        },
+        type: 'caution',
+        value: [
+          'Direct gevaar? Bel 14 020 en vul dit formulier niet verder in.',
+          'Direct gevaar is bijvoorbeeld:',
+          <DefinitionComponents.Ul
+            items={[
+              'Paal, stoplicht of lamp ligt op de grond of is verbogen',
+              'Deurtje in de paal staat open',
+              'Er zijn losse elektriciteitsdraden te zien of er hangt een lamp los'
+            ]}
+          />,
+          'Let op: met het nummer van het stoplicht (3 witte cijfers bij de lichten) kunnen wij de melding sneller oplossen.'
+        ],
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.PlainText
+    },
+    extra_verkeerslicht: {
+      meta: {
+        label: 'Om wat voor soort stoplicht(en) het gaat?',
+        ifAllOf: {
+          subcategory: 'verkeerslicht'
+        },
+        value: [
+          'Voetganger',
+          'Fiets',
+          'Blindentikker',
+          'Auto (algemeen) stoplicht'
+        ],
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.CheckboxInput
+    },
+    extra_verkeerslicht_wat: {
+      meta: {
+        label: 'Wat is er aan de hand met het/de stoplichten(en)?',
+        ifAllOf: {
+          subcategory: 'verkeerslicht'
+        },
+        value: [
+          'Rode licht is stuk',
+          'Oranje licht is stuk',
+          'Groene licht is stuk',
+          'Blijft (te lang) op rood staan',
+          'Is aangereden',
+          'Het deurtje van de paal staat open',
+          'Drukknop is stuk',
+          'Blindentikker is stuk',
+          'Anders:'
+        ],
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.CheckboxInput
+    },
+    extra_verkeerslicht_anders: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'verkeerslicht'
+        },
+        ifOneOf: {
+          extra_verkeerslicht_wat: 'Anders:'
+        },
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextareaInput
+    },
+    extra_verkeerslicht_nummer: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'verkeerslicht'
+        },
+        label: 'Hebt u een nummer van het stoplicht?',
         pathMerge: 'extra_properties'
       },
       render: FormComponents.TextInput
@@ -174,33 +214,33 @@ export default {
       render: FormComponents.TextareaInput
     },
 
-    redirect_to_kim: {
-      meta: {
-        ifOneOf: {
-          subcategory: [
-            'straatverlichting-openbare-klok',
-            'verkeerslicht'
-          ]
-        },
-        label: 'Redirect naar',
-        value: 'Voor meldingen over openbare verlichting, klokken en verkeerslichten is een apart formulier beschikbaar',
-        buttonLabel: 'Meteen doorgaan',
-        buttonAction: 'https://formulieren.amsterdam.nl/TripleForms/DirectRegelen/formulier/nl-NL/evAmsterdam/scMeldingenovl.aspx',
-        buttonTimeout: 5000
-      },
-      render: FormComponents.RedirectButton
-    },
-    hide_navigation_buttons: {
-      meta: {
-        ifOneOf: {
-          subcategory: [
-            'straatverlichting-openbare-klok',
-            'verkeerslicht'
-          ]
-        },
-        ignoreVisibility: true
-      }
-    },
+    // redirect_to_kim: {
+    //   meta: {
+    //     ifOneOf: {
+    //       subcategory: [
+    //         'straatverlichting-openbare-klok',
+    //         'verkeerslicht'
+    //       ]
+    //     },
+    //     label: 'Redirect naar',
+    //     value: 'Voor meldingen over openbare verlichting, klokken en verkeerslichten is een apart formulier beschikbaar',
+    //     buttonLabel: 'Meteen doorgaan',
+    //     buttonAction: 'https://formulieren.amsterdam.nl/TripleForms/DirectRegelen/formulier/nl-NL/evAmsterdam/scMeldingenovl.aspx',
+    //     buttonTimeout: 5000
+    //   },
+    //   render: FormComponents.RedirectButton
+    // },
+    // hide_navigation_buttons: {
+    //   meta: {
+    //     ifOneOf: {
+    //       subcategory: [
+    //         'straatverlichting-openbare-klok',
+    //         'verkeerslicht'
+    //       ]
+    //     },
+    //     ignoreVisibility: true
+    //   }
+    // },
     $field_0: {
       isStatic: false,
       render: IncidentNavigation
