71309968c84a1deb28576097252f893130e5c0fc
Jasper Swart <jasper.g.swart@gmail.com>
2019-03-11 17:38:55 +0100

    [SIG-993] added highlight to dwetail address and stadsdeel

--- src/signals/incident-management/containers/IncidentDetailPage/components/IncidentDetail/index.js
+++ src/signals/incident-management/containers/IncidentDetailPage/components/IncidentDetail/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { isEqual } from 'lodash';
 
 import { string2date, string2time } from 'shared/services/string-parser/string-parser';
 import { getListValueByKey } from 'shared/services/list-helper/list-helper';
@@ -8,11 +9,30 @@ import './style.scss';
 
 
 class IncidentDetail extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: props.incident.location,
+      locationUpdated: props.locationUpdated,
+      stadsdeelUpdated: props.stadsdeelUpdated
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return {
+      locationUpdated: !isEqual(props.incident.location, state.location),
+      stadsdeelUpdated: !isEqual(props.incident.location.stadsdeel, state.location.stadsdeel)
+    };
+  }
+
   render() {
     const { incident, stadsdeelList, priorityList } = this.props;
+    const { locationUpdated, stadsdeelUpdated } = this.state;
     const extraProperties = incident.extra_properties && Object.keys(incident.extra_properties).map((key) =>
       (<tr key={key}><td>{key}</td><td>{incident.extra_properties[key]}&nbsp;</td></tr>)
     );
+
     return (
       <div className="incident-detail">
         <div className="incident-detail__body">
@@ -28,8 +48,8 @@ class IncidentDetail extends React.Component { // eslint-disable-line react/pref
               <tr><td>Omschrijving</td><td className="pre-wrap">{incident.text}&nbsp;</td></tr>
               <tr><td>Aanvullende kenmerken</td><td>{incident.text_extra}&nbsp;</td></tr>
               {extraProperties}
-              <tr><td>Stadsdeel</td><td>{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}</td></tr>
-              <tr><td>Adres</td><td>{incident.location.address_text}</td></tr>
+              <tr><td>Stadsdeel</td><td className={stadsdeelUpdated ? 'incident-detail__body--highlight' : ''}>{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}</td></tr>
+              <tr><td>Adres</td><td className={locationUpdated ? 'incident-detail__body--highlight' : ''}>{incident.location.address_text}</td></tr>
               <tr><td>Email</td><td>{incident.reporter.email}</td></tr>
               <tr><td>Telefoonnummer</td><td>{incident.reporter.phone}</td></tr>
               <tr><td>Bron</td><td>{incident.source}</td></tr>
@@ -55,8 +75,17 @@ class IncidentDetail extends React.Component { // eslint-disable-line react/pref
   }
 }
 
+IncidentDetail.defaultProps = {
+  location: {},
+  locationUpdated: false,
+  stadsdeelUpdated: false
+};
+
 IncidentDetail.propTypes = {
   incident: PropTypes.object.isRequired,
+  location: PropTypes.object,
+  locationUpdated: PropTypes.bool,
+  stadsdeelUpdated: PropTypes.bool,
   priorityList: PropTypes.array.isRequired,
   stadsdeelList: PropTypes.array.isRequired
 };
