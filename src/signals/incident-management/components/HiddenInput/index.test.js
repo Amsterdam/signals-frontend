372673baa98369df7532456b59ce4617a5d70a50
Jasper Swart <jasper.g.swart@gmail.com>
2019-03-15 15:16:44 +0100

    [SIG-993] added unit test for HiddenInput

---
+++ src/signals/incident-management/components/HiddenInput/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import HiddenInput from './index';
+
+describe('<HiddenInput />', () => {
+  let wrapper;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      name: 'name',
+      handler: jest.fn()
+    };
+
+    const HiddenInputRender = HiddenInput(props);
+    wrapper = shallow(
+      <HiddenInputRender {...props} />
+    );
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render correctly', () => {
+    expect(wrapper).not.toBeNull();
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
