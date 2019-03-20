08ab50ffe140f83fc00972e81ec766e6d1434e02
Jasper Swart <jasper.g.swart@gmail.com>
2019-03-20 10:45:47 +0100

    [SIG-993] added dl parent to dt and dd

--- src/signals/incident-management/containers/IncidentDetailPage/components/IncidentDetail/index.js
+++ src/signals/incident-management/containers/IncidentDetailPage/components/IncidentDetail/index.js
@@ -65,63 +65,64 @@ class IncidentDetail extends React.Component { // eslint-disable-line react/pref
     const { incident, stadsdeelList, priorityList } = this.props;
     const { locationUpdated, stadsdeelUpdated } = this.state;
     const extraProperties = incident.extra_properties && Object.keys(incident.extra_properties).map((key) =>
-      (<span key={key}><dt className="incident-detail__body__definition">{key}</dt><dd className="incident-detail__body__value">{incident.extra_properties[key]}&nbsp;</dd></span>)
+      (<dl key={key}><dt className="incident-detail__body__definition">{key}</dt><dd className="incident-detail__body__value">{incident.extra_properties[key]}&nbsp;</dd></dl>)
     );
 
     return (
       <div className="incident-detail">
         <div className="incident-detail__body">
-          <dt className="incident-detail__body__definition">Datum</dt>
-          <dd className="incident-detail__body__value">{string2date(incident.created_at)}</dd>
-          <dt className="incident-detail__body__definition">Tijd</dt>
-          <dd className="incident-detail__body__value">{string2time(incident.created_at)}</dd>
-          <dt className="incident-detail__body__definition">Datum overlast</dt>
-          <dd className="incident-detail__body__value">{string2date(incident.incident_date_start)}</dd>
-          <dt className="incident-detail__body__definition">Tijd overlast</dt>
-          <dd className="incident-detail__body__value">{string2time(incident.incident_date_start)}</dd>
-          <dt className="incident-detail__body__definition">Urgentie</dt>
-          <dd className="incident-detail__body__value">{getListValueByKey(priorityList, incident.priority && incident.priority.priority)}&nbsp;</dd>
-          <dt className="incident-detail__body__definition">Hoofdcategorie</dt>
-          <dd className="incident-detail__body__value">{incident.category.main}&nbsp;</dd>
-          <dt className="incident-detail__body__definition">Subcategorie</dt>
-          <dd className="incident-detail__body__value">{incident.category.sub}&nbsp;</dd>
-          <dt className="incident-detail__body__definition">Omschrijving</dt>
-          <dd className="incident-detail__body__value pre-wrap">{incident.text}&nbsp;</dd>
-          <dt className="incident-detail__body__definition">Aanvullende kenmerken</dt>
-          <dd className="incident-detail__body__value">{incident.text_extra}&nbsp;</dd>
-          {extraProperties}
-          <div className={stadsdeelUpdated ? 'incident-detail__body--highlight' : ''}>
-            <dt className="incident-detail__body__definition">Stadsdeel</dt>
-            <dd className="incident-detail__body__value">{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}&nbsp;</dd>
-          </div>
-          <div className={locationUpdated ? 'incident-detail__body--highlight' : ''}>
-            <dt className="incident-detail__body__definition">Locatie</dt>
-            <dd className="incident-detail__body__value">{incident.location.address_text || 'Locatie is gepind op de kaart'}&nbsp;</dd>
-          </div>
-          <dt className="incident-detail__body__definition">Email</dt>
-          <dd className="incident-detail__body__value">{incident.reporter.email}</dd>
-          <dt className="incident-detail__body__definition">Telefoonnummer</dt>
-          <dd className="incident-detail__body__value">{incident.reporter.phone}</dd>
-          <dt className="incident-detail__body__definition">Bron</dt>
-          <dd className="incident-detail__body__value">{incident.source}</dd>
-          <dt className="incident-detail__body__definition">Verantwoordelijke afdeling</dt>
-          <dd className="incident-detail__body__value">{incident.category.department}&nbsp;</dd>
-          <dt className="incident-detail__body__definition">Telefoonnummer</dt>
-          <dd className="incident-detail__body__value">{incident.reporter.phone}</dd>
-          {incident.parent_id ?
+          <dl>
+            <dt className="incident-detail__body__definition">Datum</dt>
+            <dd className="incident-detail__body__value">{string2date(incident.created_at)}</dd>
+            <dt className="incident-detail__body__definition">Tijd</dt>
+            <dd className="incident-detail__body__value">{string2time(incident.created_at)}</dd>
+            <dt className="incident-detail__body__definition">Datum overlast</dt>
+            <dd className="incident-detail__body__value">{string2date(incident.incident_date_start)}</dd>
+            <dt className="incident-detail__body__definition">Tijd overlast</dt>
+            <dd className="incident-detail__body__value">{string2time(incident.incident_date_start)}</dd>
+            <dt className="incident-detail__body__definition">Urgentie</dt>
+            <dd className="incident-detail__body__value">{getListValueByKey(priorityList, incident.priority && incident.priority.priority)}&nbsp;</dd>
+            <dt className="incident-detail__body__definition">Hoofdcategorie</dt>
+            <dd className="incident-detail__body__value">{incident.category.main}&nbsp;</dd>
+            <dt className="incident-detail__body__definition">Subcategorie</dt>
+            <dd className="incident-detail__body__value">{incident.category.sub}&nbsp;</dd>
+            <dt className="incident-detail__body__definition">Omschrijving</dt>
+            <dd className="incident-detail__body__value pre-wrap">{incident.text}&nbsp;</dd>
+            <dt className="incident-detail__body__definition">Aanvullende kenmerken</dt>
+            <dd className="incident-detail__body__value">{incident.text_extra}&nbsp;</dd>
+            {extraProperties}
+            <div className={stadsdeelUpdated ? 'incident-detail__body--highlight' : ''}>
+              <dt className="incident-detail__body__definition">Stadsdeel</dt>
+              <dd className="incident-detail__body__value">{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}&nbsp;</dd>
+            </div>
+            <div className={locationUpdated ? 'incident-detail__body--highlight' : ''}>
+              <dt className="incident-detail__body__definition">Locatie</dt>
+              <dd className="incident-detail__body__value">{incident.location.address_text || 'Locatie is gepind op de kaart'}&nbsp;</dd>
+            </div>
+            <dt className="incident-detail__body__definition">Email</dt>
+            <dd className="incident-detail__body__value">{incident.reporter.email}</dd>
+            <dt className="incident-detail__body__definition">Telefoonnummer</dt>
+            <dd className="incident-detail__body__value">{incident.reporter.phone}</dd>
+            <dt className="incident-detail__body__definition">Bron</dt>
+            <dd className="incident-detail__body__value">{incident.source}</dd>
+            <dt className="incident-detail__body__definition">Verantwoordelijke afdeling</dt>
+            <dd className="incident-detail__body__value">{incident.category.department}&nbsp;</dd>
+            <dt className="incident-detail__body__definition">Telefoonnummer</dt>
+            <dd className="incident-detail__body__value">{incident.reporter.phone}</dd>
+            {incident.parent_id ?
             (<span>
               <dt className="incident-detail__body__definition">Oorspronkelijke melding</dt>
               <dd className="incident-detail__body__value"><NavLink className="incident-detail__body__link" to={`/manage/incident/${incident.parent_id}`}>{incident.parent_id}</NavLink></dd>
             </span>)
             : ''}
-
-          {incident.child_ids && incident.child_ids.length > 0 ?
+            {incident.child_ids && incident.child_ids.length > 0 ?
                 (<span>
                   <dt className="incident-detail__body__definition">Gesplitst in</dt>
                   <dd className="incident-detail__body__value">{incident.child_ids.map((child_id) =>
                     (<NavLink className="incident-detail__body__link" key={child_id} to={`/manage/incident/${child_id}`}>{child_id}</NavLink>))}</dd>
                 </span>)
                : ''}
+          </dl>
         </div>
       </div>
     );
