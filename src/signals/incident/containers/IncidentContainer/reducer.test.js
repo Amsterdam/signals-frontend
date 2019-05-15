eb4499e192c7d69c17973d96cbcefa169de54bb7
Jasper Swart <jasper.g.swart@gmail.com>
2019-05-15 13:32:34 +0200

    fixed samenvatting

--- src/signals/incident/containers/IncidentContainer/reducer.test.js
+++ src/signals/incident/containers/IncidentContainer/reducer.test.js
@@ -22,6 +22,18 @@ describe('incidentContainerReducer', () => {
     expect(incidentContainerReducer(undefined, {})).toEqual(fromJS(initialState));
   });
 
+  it('default wizard state should contain date, time, and priority', () => {
+    expect(initialState.get('incident')).toEqual(fromJS({
+      incident_date: 'Vandaag',
+      incident_time_hours: 9,
+      incident_time_minutes: 0,
+      priority: {
+        id: 'normal',
+        label: 'Normaal'
+      }
+    }));
+  });
+
   describe('UPDATE_INCIDENT', () => {
     it('sets new properties and keeps the old ones', () => {
       expect(
