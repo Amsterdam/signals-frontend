bc7133df170e9085ce663d04514014b6f82207e8
Jasper Swart <jasper.g.swart@gmail.com>
2019-05-17 13:28:28 +0200

    [SIG-1166] added HandlingMessage form component

---
+++ src/signals/incident/components/form/HandlingMessage/index.test.js
@@ -0,0 +1,79 @@
+/* eslint-disable  react/prop-types */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import HandlingMessage from './index';
+import mapDynamicFields from '../../../services/map-dynamic-fields';
+
+jest.mock('../../../services/map-dynamic-fields');
+
+describe('Form component <HandlingMessage />', () => {
+  const MockComponent = ({ children }) => <div>{children}</div>;
+  const incidentContainer = {
+    incident: {
+      id: 666
+    }
+  };
+  let wrapper;
+
+  beforeEach(() => {
+    mapDynamicFields.mockImplementation(() => 'Lorem Ipsum');
+
+    wrapper = shallow(<HandlingMessage />);
+  });
+
+  describe('rendering', () => {
+    it('should render plain text correctly', () => {
+      wrapper.setProps({
+        meta: {
+          value: 'Lorem Ipsum',
+          type: 'citation',
+          isVisible: true
+        },
+        parent: {
+          meta: {
+            incidentContainer
+          }
+        }
+      });
+
+      expect(wrapper).toMatchSnapshot();
+      expect(mapDynamicFields).toHaveBeenCalledWith('Lorem Ipsum', incidentContainer);
+    });
+
+    it('should render multiple parargraphs of text correctly', () => {
+      wrapper.setProps({
+        meta: {
+          value: [
+            'Lorem Ipsum',
+            'jumps over',
+            'DOG',
+            <MockComponent>Foo bar</MockComponent>
+          ],
+          type: 'citation',
+          isVisible: true
+        },
+        parent: {
+          meta: {
+            incidentContainer
+          }
+        }
+      });
+
+      expect(wrapper).toMatchSnapshot();
+
+      const wrap = wrapper.find(MockComponent).shallow();
+      expect(wrap).toMatchSnapshot();
+    });
+
+    it('should render no plain text when not visible', () => {
+      wrapper.setProps({
+        meta: {
+          isVisible: false
+        }
+      });
+
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+});
