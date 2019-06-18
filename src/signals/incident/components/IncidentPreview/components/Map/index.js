8540cb58a9a7d22ecc252c290f61cc14d97b5a19
RM <RRMoelker@users.noreply.github.com>
2019-06-18 15:48:22 +0200

    SIG-1157, Map preview props specified

--- src/signals/incident/components/IncidentPreview/components/Map/index.js
+++ src/signals/incident/components/IncidentPreview/components/Map/index.js
@@ -13,41 +13,39 @@ const formatAddress = (address) => {
 /**
  * Map preview with one or more markers
  */
-const MapPreview = ({ label, value }) => {
-  console.log(label);
-  console.log(value);
-  return (
-    <div className="preview-map">
-      <div className="row">
-        <div className="col-5 col-md-4">
-          <div className="preview-map__item-label">{label}</div>
-        </div>
-        <div className="col-5 col-md-7">
-          <div className="preview-map__item-value">
-            {value ?
+const MapPreview = ({label, value}) => (
+  <div className="preview-map">
+    <div className="row">
+      <div className="col-5 col-md-4">
+        <div className="preview-map__item-label">{label}</div>
+      </div>
+      <div className="col-5 col-md-7">
+        <div className="preview-map__item-value">
+          {value ?
+            <div>
               <div>
-                <div>
-                  {value.address ? formatAddress(value.address) : 'Geen adres gevonden'}
-                </div>
-                {value.geometrie && value.geometrie.coordinates ?
-                  <div className="preview-map__item-value-map">
-                    <Map
-                      latlng={{ latitude: value.geometrie.coordinates[1], longitude: value.geometrie.coordinates[0] }}
-                    />
-                  </div>
-                  : ''}
+                {value.address ? formatAddress(value.address) : 'Geen adres gevonden'}
               </div>
-              : ''}
-          </div>
+              {value.geometrie && value.geometrie.coordinates ?
+                <div className="preview-map__item-value-map">
+                  <Map
+                    latlng={{latitude: value.geometrie.coordinates[1], longitude: value.geometrie.coordinates[0]}}
+                  />
+                </div>
+                : ''}
+            </div>
+            : ''}
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
+
 
 MapPreview.propTypes = {
   label: PropTypes.string,
   value: PropTypes.shape({
+    address: PropTypes.object,
     geometrie: PropTypes.object
   })
 };
