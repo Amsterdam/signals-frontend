27a2643575a29429716468e903fbef56a8b4db30
RM <RRMoelker@users.noreply.github.com>
2019-06-18 13:19:44 +0200

    SIG-1157, extra initial state fields in separate file

--- src/signals/incident/containers/IncidentContainer/debug/initialState.js
+++ src/signals/incident/containers/IncidentContainer/debug/initialState.js
@@ -1,3 +1,5 @@
+import straatverlichting from './straatverlichting';
+
 export default {
   location: {
     address: {
@@ -20,41 +22,16 @@ export default {
   },
   category: 'wegen-verkeer-straatmeubilair',
   subcategory: 'lantaarnpaal-straatverlichting',
-  // subcategory: 'klok',
   subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/wegen-verkeer-straatmeubilair/sub_categories/lantaarnpaal-straatverlichting',
   description: 'lantaarnpaal-straatverlichting',
   phone: '020654321',
   email: 'a@b.com',
-  datetime: 'Nu',
 
-  extra_straatverlichting: {
-    id: 'niet_gevaarlijk',
-    label: 'Niet gevaarlijk'
+  datetime: {
+    id: 'Nu',
+    label: 'Nu'
   },
-  extra_straatverlichting_nummer: [
-    '003182',
-    '003226'
-  ],
-  extra_straatverlichting_probleem: {
-    id: 'lamp_is_zichtbaar_beschadigd',
-    label: 'Lichtpunt is zichtbaar beschadigd en/of incompleet'
-  },
-  extra_straatverlichting_hoeveel: {
-    id: '1_lichtpunt',
-    label: '1 lichtpunt'
-  },
-  extra_straatverlichting_niet_op_kaart: {
-    label: 'Het lichtpunt staat niet op de kaart',
-    value: true
-  },
-  extra_straatverlichting_niet_op_kaart_nummer: [
-    {
-      id: 'extra_straatverlichting_niet_op_kaart_nummer-1',
-      label: '24'
-    },
-    {
-      id: 'extra_straatverlichting_niet_op_kaart_nummer-2',
-      label: 'extrafoo'
-    }
-  ],
+
+  ...straatverlichting
+
 };
