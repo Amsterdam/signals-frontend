3a6fd5621073c596df932fceafed30548799bedd
RM <RRMoelker@users.noreply.github.com>
2019-06-25 16:14:44 +0200

    ML tool classification can handle acc urls and threshold increased to 0.41

--- src/signals/incident/services/set-classification/index.test.js
+++ src/signals/incident/services/set-classification/index.test.js
@@ -1,6 +1,6 @@
 import subcategories from 'subcategories.mock'; // eslint-disable-line import/extensions, import/no-unresolved
 
-import setClassification from './index';
+import setClassification, { MINIMUM_CERTAINTY } from './index';
 
 describe('The set classification service', () => {
   let hoofdrubriek;
@@ -9,18 +9,18 @@ describe('The set classification service', () => {
   beforeEach(() => {
     subrubriek = [
       [
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep',
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/wildplassen-poepen-overgeven',
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/duiven'
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep',
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/wildplassen-poepen-overgeven',
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/duiven'
       ],
       [0.39, 0.1424216693948545, 0.04586915076913858]
     ];
 
     hoofdrubriek = [
       [
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca',
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte',
-        'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen'
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca',
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte',
+        'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen'
       ],
       [0.39, 0.1424216693948545, 0.04586915076913858]
     ];
@@ -31,7 +31,7 @@ describe('The set classification service', () => {
       category: 'overig',
       subcategory: 'overig',
       handling_message: 'Niet gevonden.',
-      subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
     });
   });
 
@@ -40,12 +40,12 @@ describe('The set classification service', () => {
       category: 'overig',
       subcategory: 'overig',
       handling_message: '\nUw melding wordt ingepland: wij laten u binnen 5 werkdagen weten hoe en wanneer uw melding wordt afgehandeld. Dat doen we via e-mail.',
-      subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
     });
   });
 
   it('should return correct classification when minimum subcategory chance is met', () => {
-    subrubriek[1][0] = 0.4;
+    subrubriek[1][0] = MINIMUM_CERTAINTY;
 
     expect(
       setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -53,7 +53,7 @@ describe('The set classification service', () => {
       category: 'overlast-in-de-openbare-ruimte',
       subcategory: 'hondenpoep',
       handling_message: '\nWe laten u binnen 3 weken weten wat we hebben gedaan. En anders hoort u wanneer wij uw melding kunnen oppakken.\nWe houden u op de hoogte via e-mail.',
-      subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep'
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hondenpoep'
     });
   });
 
@@ -64,15 +64,15 @@ describe('The set classification service', () => {
       category: 'overig',
       subcategory: 'overig',
       handling_message: '\nUw melding wordt ingepland: wij laten u binnen 5 werkdagen weten hoe en wanneer uw melding wordt afgehandeld. Dat doen we via e-mail.',
-      subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
+      subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
     });
   });
 
   describe('use main classification when sub category fails', () => {
     describe('afval', () => {
       it('should return overig-afval when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/afval';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/afval';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -80,15 +80,15 @@ describe('The set classification service', () => {
           category: 'afval',
           subcategory: 'overig-afval',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/afval/sub_categories/overig-afval'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/afval/sub_categories/overig-afval'
         });
       });
     });
 
     describe('openbaar-groen-en-water', () => {
       it('should return overig-groen-en-water when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/openbaar-groen-en-water';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/openbaar-groen-en-water';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -96,15 +96,15 @@ describe('The set classification service', () => {
           category: 'openbaar-groen-en-water',
           subcategory: 'overig-groen-en-water',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/openbaar-groen-en-water/sub_categories/overig-groen-en-water'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/openbaar-groen-en-water/sub_categories/overig-groen-en-water'
         });
       });
     });
 
     describe('overlast-bedrijven-en-horeca', () => {
       it('should return overig-horecabedrijven when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -112,15 +112,15 @@ describe('The set classification service', () => {
           category: 'overlast-bedrijven-en-horeca',
           subcategory: 'overig-horecabedrijven',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca/sub_categories/overig-horecabedrijven'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-bedrijven-en-horeca/sub_categories/overig-horecabedrijven'
         });
       });
     });
 
     describe('overlast-in-de-openbare-ruimte', () => {
       it('should return overig-openbare-ruimte when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -128,15 +128,15 @@ describe('The set classification service', () => {
           category: 'overlast-in-de-openbare-ruimte',
           subcategory: 'overig-openbare-ruimte',
           handling_message: 'zou niet leeg moeten zijn',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/overig-openbare-ruimte'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/overig-openbare-ruimte'
         });
       });
     });
 
     describe('overlast-op-het-water', () => {
       it('should return overig-boten when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-op-het-water';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-op-het-water';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -144,15 +144,15 @@ describe('The set classification service', () => {
           category: 'overlast-op-het-water',
           subcategory: 'overig-boten',
           handling_message: '\nWe geven uw melding door aan onze handhavers. Zij beoordelen of het nodig is direct actie te ondernemen. Bijvoorbeeld omdat er olie lekt of omdat de situatie gevaar oplevert voor andere boten.\n\nAls er geen directe actie nodig is, dan pakken we uw melding op buiten het vaarseizoen (september - maart).\n',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-op-het-water/sub_categories/overig-boten'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-op-het-water/sub_categories/overig-boten'
         });
       });
     });
 
     describe('overlast-van-dieren', () => {
       it('should return overig-dieren when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -160,15 +160,15 @@ describe('The set classification service', () => {
           category: 'overlast-van-dieren',
           subcategory: 'overig-dieren',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/overig-dieren'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-dieren/sub_categories/overig-dieren'
         });
       });
     });
 
     describe('overlast-van-en-door-personen-of-groepen', () => {
       it('should return overige-overlast-door-personen when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -176,15 +176,15 @@ describe('The set classification service', () => {
           category: 'overlast-van-en-door-personen-of-groepen',
           subcategory: 'overige-overlast-door-personen',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/overige-overlast-door-personen'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overlast-van-en-door-personen-of-groepen/sub_categories/overige-overlast-door-personen'
         });
       });
     });
 
     describe('wegen-verkeer-straatmeubilair', () => {
       it('should return overig-wegen-verkeer-straatmeubilair when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/wegen-verkeer-straatmeubilair';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/wegen-verkeer-straatmeubilair';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -192,15 +192,15 @@ describe('The set classification service', () => {
           category: 'wegen-verkeer-straatmeubilair',
           subcategory: 'overig-wegen-verkeer-straatmeubilair',
           handling_message: '\nWij bekijken uw melding en zorgen dat het juiste onderdeel van de gemeente deze gaat behandelen. Heeft u contactgegevens achtergelaten? Dan nemen wij bij onduidelijkheid contact met u op.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/wegen-verkeer-straatmeubilair/sub_categories/overig-wegen-verkeer-straatmeubilair'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/wegen-verkeer-straatmeubilair/sub_categories/overig-wegen-verkeer-straatmeubilair'
         });
       });
     });
 
     describe('unknown-category', () => {
       it('should return overig when minimum subcategory chance is not met and maincategory chance is met', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/unknown-category';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/unknown-category';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek }, subcategories)
@@ -208,13 +208,13 @@ describe('The set classification service', () => {
           category: 'overig',
           subcategory: 'overig',
           handling_message: '\nUw melding wordt ingepland: wij laten u binnen 5 werkdagen weten hoe en wanneer uw melding wordt afgehandeld. Dat doen we via e-mail.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
         });
       });
 
       it('should return overig when minimum subcategory chance is not met and maincategory chance is met without subcategories', () => {
-        hoofdrubriek[0][0] = 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/unknown-category';
-        hoofdrubriek[1][0] = 0.4;
+        hoofdrubriek[0][0] = 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/unknown-category';
+        hoofdrubriek[1][0] = MINIMUM_CERTAINTY;
 
         expect(
           setClassification({ subrubriek, hoofdrubriek })
@@ -222,7 +222,7 @@ describe('The set classification service', () => {
           category: 'overig',
           subcategory: 'overig',
           handling_message: 'Niet gevonden.',
-          subcategory_link: 'https://api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
+          subcategory_link: 'https://acc.api.data.amsterdam.nl/signals/v1/public/terms/categories/overig/sub_categories/overig'
         });
       });
     });
