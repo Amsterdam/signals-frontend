1869b9f4d5e0ccea02d5ad1897d7b3c42c978c38
JJM van Deursen <janjaap@users.noreply.github.com>
2019-07-03 17:43:16 +0200

    Sets Sentry track URL

--- src/app.js
+++ src/app.js
@@ -49,11 +49,10 @@ import configureStore from './configureStore';
 import { translationMessages } from './i18n';
 
 const environment = process.env.NODE_ENV;
-const dsn = process.env.DSN;
 
 Sentry.init({
   environment,
-  dsn: `https://${dsn}@sentry.data.amsterdam.nl/15`,
+  dsn: 'https://3de59e3a93034a348089131aa565bdf4@sentry.data.amsterdam.nl/27',
 });
 
 // load locale for date formatting
