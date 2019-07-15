2dd4d1539f06f61db4b0eadb0209cffca08117f1
Jasper Swart <jasper.g.swart@gmail.com>
2019-07-15 16:53:09 +0200

    [SIG-1350] added stankoverlast

--- src/signals/incident/definitions/wizard-step-2-vulaan/overlast-bedrijven-en-horeca.js
+++ src/signals/incident/definitions/wizard-step-2-vulaan/overlast-bedrijven-en-horeca.js
@@ -38,6 +38,14 @@ export default {
         ifAllOf: {
           category: 'overlast-bedrijven-en-horeca'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         label: 'Wie of wat zorgt voor deze overlast, denkt u?',
         className: 'col-sm-12 col-md-8',
         pathMerge: 'extra_properties'
@@ -48,6 +56,14 @@ export default {
       meta: {
         ifAllOf: {
           category: 'overlast-bedrijven-en-horeca'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'Op welk adres ervaart u de overlast?',
         className: 'col-sm-12 col-md-8',
@@ -60,6 +76,14 @@ export default {
       meta: {
         ifAllOf: {
           subcategory: 'geluidsoverlast-muziek'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'Woont u direct boven of direct naast het pand waar het geluid vandaan komt?',
         className: 'col-sm-12 col-md-8',
@@ -79,6 +103,14 @@ export default {
         ifAllOf: {
           subcategory: 'geluidsoverlast-muziek'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         label: 'Hebt u ook last van het geluid als uw ramen en deuren gesloten zijn?',
         className: 'col-sm-12 col-md-8',
         pathMerge: 'extra_properties',
@@ -93,6 +125,14 @@ export default {
       meta: {
         ifAllOf: {
           subcategory: 'geluidsoverlast-muziek'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'Staan de ramen of deuren open van de horeca onderneming?',
         className: 'col-sm-12 col-md-8',
@@ -110,6 +150,14 @@ export default {
           subcategory: 'geluidsoverlast-muziek',
           extra_bedrijven_horeca_muziek_ramen_dicht_onderneming: 'ja'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         label: 'Gaan de ramen of deuren kort of lang open?',
         className: 'col-sm-12 col-md-8',
         pathMerge: 'extra_properties',
@@ -121,10 +169,220 @@ export default {
       render: FormComponents.RadioInput
     },
 
+    extra_bedrijven_horeca_muziek_evenement: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'geluidsoverlast-muziek'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Bent u geïnformeerd door de organisator van het evenement?',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties',
+        values: {
+          ja: 'Ja, ik ben geïnformeerd door de organisator',
+          nee: 'Nee, ik ben niet geïnformeerd door de organisator'
+        }
+      },
+      options: { validators: [Validators.required] },
+      render: FormComponents.RadioInput
+    },
+
+    extra_bedrijven_horeca_muziek_evenement_einde: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'geluidsoverlast-muziek',
+          extra_bedrijven_horeca_wat: 'evenement_festival_markt',
+          extra_bedrijven_horeca_muziek_evenement: 'ja'
+        },
+        label: 'Weet u hoe laat het evenement eindigt?',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+
+    extra_bedrijven_horeca_installaties: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'geluidsoverlast-installaties'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Van wat voor een soort installatie heeft u last?',
+        subtitle: 'Bijvoorbeeld een afzuiger of airconditioning',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+
+    extra_bedrijven_horeca_personen: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'overlast-door-bezoekers-niet-op-terras'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Wat is de oorzaak van de overlast?',
+        values: {
+          dronken_bezoekers: 'Dronken bezoekers',
+          schreeuwende_bezoekers: 'Schreeuwende bezoekers',
+          rokende_bezoekers: 'Rokende bezoekers',
+          teveel_fietsen: '(Teveel) fietsen',
+          wildplassen: 'Wildplassen',
+          overgeven: 'Overgeven'
+        },
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.CheckboxInput
+    },
+
+    extra_bedrijven_horeca_terrassen: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'overlast-terrassen'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Wat is de oorzaak van de overlast?',
+        values: {
+          uitgewaaierd_terras: 'Uitgewaaierd terras (buiten de toegestane grens)',
+          doorloop: 'Het terras belemmert de doorloop',
+          stoep_in_beslag: 'Terras / terrasbezoekers nemen hele stoep in zodat u via de weg erlangs moet',
+          bezoekers_op_straat: 'Bezoekers staan op straat',
+          bezoekers_op_terras: 'Bezoekers op terras',
+          opruimen_meubels: 'Opruimen meubels'
+        },
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.CheckboxInput
+    },
+
+    extra_bedrijven_horeca_stank: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'stankoverlast'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Welke geur ruikt u?',
+        subtitle: 'Beschrijf wat voor geur het is',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+
+    extra_bedrijven_horeca_stank_oorzaak: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'stankoverlast'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Wat is de vermoedelijke oorzaak van de geuroverlast?',
+        subtitle: 'Bijvoorbeeld afvoerpijp of schoorsteen',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+
+    extra_bedrijven_horeca_stank_weer: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'stankoverlast'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Wat zijn de weersomstandigheden tijdens de overlast?',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties'
+      },
+      render: FormComponents.TextInput
+    },
+
+    extra_bedrijven_horeca_stank_ramen: {
+      meta: {
+        ifAllOf: {
+          subcategory: 'stankoverlast'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
+        label: 'Staan de ramen of deuren open van de vermoedelijke veroorzaker?',
+        className: 'col-sm-12 col-md-8',
+        pathMerge: 'extra_properties',
+        values: {
+          ja: 'Ja, ramen of deuren staan open',
+          nee: 'Nee, ramen en deuren zijn gesloten'
+        }
+      },
+      render: FormComponents.RadioInput
+    },
+
     extra_bedrijven_horeca_vaker: {
       meta: {
         ifAllOf: {
           category: 'overlast-bedrijven-en-horeca'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'Gebeurt het vaker?',
         subtitle: 'Is de overlast vaker aanwezig of is dit een eenmalige gebeurtenis',
@@ -143,6 +401,14 @@ export default {
           category: 'overlast-bedrijven-en-horeca',
           extra_bedrijven_horeca_vaker: 'ja'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         label: 'Op welk(e) tijdstip(pen) ervaart u de overlast?',
         className: 'col-sm-12 col-md-8',
         pathMerge: 'extra_properties'
@@ -154,6 +420,14 @@ export default {
       meta: {
         ifAllOf: {
           subcategory: 'geluidsoverlast-muziek'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'We willen graag een geluidsmeting bij u thuis doen. Mogen we contact met u opnemen om een afspraak te maken?',
         className: 'col-sm-12 col-md-8',
@@ -171,6 +445,14 @@ export default {
           subcategory: 'geluidsoverlast-muziek',
           extra_bedrijven_horeca_muziek_geluidmeting: 'ja'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         value: 'Let op! Vul uw telefoonnummer in op de volgende pagina',
         className: 'col-sm-12 col-md-8',
         type: 'caution'
@@ -182,6 +464,14 @@ export default {
         ifAllOf: {
           subcategory: 'geluidsoverlast-muziek',
           extra_bedrijven_horeca_muziek_geluidmeting: 'ja'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         label: 'We willen graag een geluidsmeting bij u thuis doen. Mogen we contact met u opnemen om een afspraak te maken?',
         className: 'col-sm-12 col-md-8',
@@ -200,6 +490,14 @@ export default {
           subcategory: 'geluidsoverlast-muziek',
           extra_bedrijven_horeca_muziek_geluidmeting: 'nee'
         },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
+        },
         label: 'Waarom heeft u liever geen contact?',
         className: 'col-sm-12 col-md-8',
         pathMerge: 'extra_properties'
@@ -211,6 +509,14 @@ export default {
       meta: {
         ifAllOf: {
           category: 'overlast-bedrijven-en-horeca'
+        },
+        ifOneOf: {
+          extra_bedrijven_horeca_wat: [
+            'horecabedrijf',
+            'ander_soort_bedrijf',
+            'evenement_festival_markt',
+            'iets_anders'
+          ]
         },
         value: [
           'Uw gegevens worden vertrouwelijk behandeld en worden niet aan de (horeca)ondernemer of organisator bekend gemaakt.',
