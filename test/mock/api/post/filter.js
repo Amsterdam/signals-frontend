1d049796ad8f50787f566005897acbc03deba68b
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-07-29 13:39:35 +0200

    Adds mock endpoints for filters

---
+++ test/mock/api/post/filter.js
@@ -0,0 +1,52 @@
+//  import { pickRandomSublist } from '../../../lib/listRandomizer';
+import uuid from 'uuid/v1';
+import categories from '../get/categories.json';
+import status from '../get/status.json';
+import stadsdeel from '../get/stadsdeel.json';
+import priority from '../get/priority.json';
+
+const filter = {
+  path: '/me/filter',
+  method: 'POST',
+  cache: false,
+  status: (req, res, next) => {
+    res.status(201);
+    next();
+  },
+  template: (params, query, body) => {
+    const {
+      name = '',
+      incident_date_start = '',
+      location__address_text,
+      status__state = [],
+      priority__priority = [],
+      main_slug = [],
+      sub_slug = [],
+      location__stadsdeel = [],
+    } = body;
+
+    return {
+      id: uuid(),
+      name,
+      date_start: incident_date_start,
+      address: location__address_text,
+      status: status__state.map((statusKey) =>
+        status.find(({ key }) => key === statusKey),
+      ),
+      priority: priority__priority.map((prioKey) =>
+        priority.find(({ key }) => key === prioKey),
+      ),
+      main: main_slug.map((slugKey) =>
+        categories.main.find(({ key }) => key === slugKey),
+      ),
+      sub: sub_slug.map((slugKey) =>
+        categories.sub.find(({ key }) => key === slugKey),
+      ),
+      stadsdeel: location__stadsdeel.map((sdKey) =>
+        stadsdeel.find(({ key }) => key === sdKey),
+      ),
+    };
+  },
+};
+
+export default [filter];
