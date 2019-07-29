18518d80e483eb62c5f37aa9685a212843ceec39
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-07-29 13:38:08 +0200

    Merge epic branch

--- test/mock/api/get/_index.js
+++ test/mock/api/get/_index.js
@@ -2,8 +2,8 @@ export const path = '/';
 export function render(req, res) {
   let html = '<h1>dyson</h1><p>Example endpoints:</p>';
   const examples = [
-    '/user',
-    '/users'
+    '/filter',
+    '/filters'
   ];
   html += `<ul>${examples.map((example) => `<li><a href="${example}">${example}</a></li>`).join('')}</ul>`;
   res.send(200, html);
