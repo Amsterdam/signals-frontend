c4aff1dfaece0aac0896c3f04f981beff72dbdf4
Jasper Swart <jasper.g.swart@gmail.com>
2019-07-31 21:24:20 +0200

    added detailheader component

--- src/signals/incident-management/containers/IncidentDetail/components/DetailHeader/index.test.js
+++ src/signals/incident-management/containers/IncidentDetail/components/DetailHeader/index.test.js
@@ -1,28 +1,43 @@
 import React from 'react';
-// import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { withAppContext } from 'test/utils';
 
 import DetailHeader from './index';
+
+jest.mock('./components/DownloadButton', () => () => 'DownloadButton');
 
 describe('<DetailHeader />', () => {
   let props;
 
   beforeEach(() => {
     props = {
-      incident: { id: 42 },
+      incident: {
+        id: 42,
+        status: {
+          state: 'm'
+        },
+        _links: {
+          'sia:pdf': { href: 'https://api.data.amsterdam.nl/signals/v1/private/signals/3076/pdf' }
+        }
+      },
       baseUrl: '/manage',
-      accessToken: 'MOCK-TOKEN'
+      accessToken: 'MOCK-TOKEN',
+      onThor: jest.fn()
     };
   });
 
   describe('rendering', () => {
     it('should render correctly', () => {
       expect(10).toBe(10);
-      const { queryByTestId } = withAppContext(
-        <DetailHeader {...props} />
+      const { queryByTestId } = render(
+        withAppContext(<DetailHeader {...props} />)
       );
 
+      expect(queryByTestId('detail-header-button-back')).toHaveTextContent(/^Terug naar overzicht$/);
       expect(queryByTestId('detail-header-title')).toHaveTextContent(/^Melding 42$/);
+      expect(queryByTestId('detail-header-button-split')).toHaveTextContent(/^Splitsen$/);
+      expect(queryByTestId('detail-header-button-thor')).toHaveTextContent(/^THOR$/);
+      // expect(queryByTestId('detail-header-button-download')).toHaveTextContent(/^PDF$/);
     });
   });
 });
