0acb39b4c152a9dc447ce8a5ccd42c4091f28493
Jasper Swart <jasper.g.swart@gmail.com>
2019-07-31 14:10:27 +0200

    added utils

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,7 +1,29 @@
+import React from 'react';
+import { ConnectedRouter } from 'react-router-redux';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@datapunt/asc-ui';
+import MatchMediaMock from 'match-media-mock';
+
+import configureStore from '../configureStore';
+
+const history = createMemoryHistory();
+
+// set a default screenwidth of 2560 pixels
+const mmm = MatchMediaMock.create();
+
+mmm.setConfig({ type: 'screen', width: 2560 });
+
+// eslint-disable-next-line no-undef
+Object.defineProperty(global.window, 'matchMedia', {
+  value: mmm,
+  writable: true,
+});
+
 export const testActionCreator = (action, actionType, payload) => {
   const expected = {
     type: actionType,
-    payload
+    payload,
   };
   expect(action(payload)).toEqual(expected);
 };
@@ -14,8 +36,31 @@ export const getContext = (state) => {
     runSaga: jest.fn(),
     subscribe: jest.fn(),
     injectedReducers: {},
-    injectedSagas: {}
+    injectedSagas: {},
   };
 
   return { store };
 };
+
+const store = configureStore({}, history);
+
+export const withAppContext = (Component) => (
+  <ThemeProvider>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        {Component}
+      </ConnectedRouter>
+    </Provider>
+  </ThemeProvider>
+);
+
+// eslint-disable-next-line
+export const withCustomAppContext = (Component) => ({ themeCfg = {}, storeCfg = {}, routerCfg = {} }) => (
+  <ThemeProvider {...themeCfg}>
+    <Provider store={store} {...storeCfg}>
+      <ConnectedRouter history={history} {...routerCfg}>
+        {Component}
+      </ConnectedRouter>
+    </Provider>
+  </ThemeProvider>
+);
