d2656e78896735b87534f0e985b770cdd7a09d37
Jasper Swart <jasper.g.swart@gmail.com>
2019-08-20 09:54:29 +0200

    added sub ad main categories to FilterTagList

--- src/signals/incident-management/containers/FilterTagList/index.js
+++ src/signals/incident-management/containers/FilterTagList/index.js
@@ -2,33 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { makeSelectCategories } from 'containers/App/selectors';
 
 import './style.scss';
 
 import makeSelectOverviewPage from '../IncidentOverviewPage/selectors';
 
-const renderTag = (key, value, allLists) => {
-  const found = allLists.find((i) => i.key === key);
+const renderTag = (key, value, mainCategories, allLists) => {
+  const found = allLists.find((i) => i.key === key || i.slug === key);
   const display = (found && found.value) || value;
   if (!display) {
     return;
   }
+
+  const foundMain = mainCategories.find((i) => i.slug === value);
   // eslint-disable-next-line consistent-return
-  return <span className="filter-tag-list__item" key={key}>{display}</span>;
+  return <span className="filter-tag-list__item" key={key}>{display}{foundMain && ': Alles'}</span>;
 };
 
-export const FilterTagList = ({ tags, overviewpage: { priorityList, stadsdeelList, statusList } }) => (
+export const FilterTagList = ({ tags, overviewpage: { priorityList, stadsdeelList, statusList }, categories: { main, sub } }) => (
   <div className="filter-tag-list">
     {Object.values(tags).map((tag) => (Array.isArray(tag) ?
       <span key={tag}>{tag.map((item) =>
-        renderTag(item, item, [...priorityList, ...stadsdeelList, ...statusList]))}</span> :
-        renderTag(tag, tag, [...priorityList, ...stadsdeelList, ...statusList])))}
+        renderTag(item, item, main, [...priorityList, ...stadsdeelList, ...statusList, ...sub, ...main]))}</span> :
+        renderTag(tag, tag, main, [...priorityList, ...stadsdeelList, ...statusList, ...sub, ...main])))}
   </div>
 );
 
 FilterTagList.propTypes = {
   tags: PropTypes.object,
   overviewpage: PropTypes.object.isRequired,
+  categories: PropTypes.object.isRequired,
 };
 
 FilterTagList.defaultProps = {
@@ -37,6 +41,7 @@ FilterTagList.defaultProps = {
 
 const mapStateToProps = createStructuredSelector({
   overviewpage: makeSelectOverviewPage(),
+  categories: makeSelectCategories(),
 });
 
 const withConnect = connect(
