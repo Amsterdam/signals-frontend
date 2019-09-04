dad4faf675c8c353fdc92d32c97401b8e0e4c572
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-09-04 10:36:39 +0200

    Adjusts Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,47 +2,55 @@ import React from 'react';
 import { Row, Column, Heading, Link, Paragraph } from '@datapunt/asc-ui';
 import styled from 'styled-components';
 
-const Disclaimer = styled(Row)`
+const Disclaimer = styled.div`
   background-color: #666;
   padding-top: 20px;
   padding-bottom: 20px;
-  display: flex;
 
   * {
     color: white;
   }
 `;
 
-const Privacy = styled(Row)`
+const Privacy = styled.div`
   background: white;
   padding-top: 10px;
   padding-bottom: 10px;
 `;
 
 const FooterWrapper = styled.div`
-  flex-shrink: 0;
+  & {
+    display: flex;
+    flex-direction: column;
+    flex: 0 1 auto !important;
+    padding-bottom: 0 !important;
+  }
 `;
 
 const Footer = () => (
-  <FooterWrapper className="footer no-print">
+  <FooterWrapper className="app-container no-print">
     <Disclaimer>
-      <Column span={12}>
-        <Heading as="h2">Lukt het niet om een melding te doen?</Heading>
-        <Paragraph>&nbsp;</Paragraph>
-      </Column>
+      <Row>
+        <Column span={12}>
+          <Heading as="h2">Lukt het niet om een melding te doen?</Heading>
+          <Paragraph>&nbsp;</Paragraph>
+        </Column>
 
-      <Column span={12}>
-        <Paragraph>
-          Bel het Gemeentelijk informatienummer: 14 020 <br />
-          op werkdagen van 08.00 tot 18.00 uur.
-        </Paragraph>
-      </Column>
+        <Column span={12}>
+          <Paragraph>
+            Bel het Gemeentelijk informatienummer: 14 020 <br />
+            op werkdagen van 08.00 tot 18.00 uur.
+          </Paragraph>
+        </Column>
+      </Row>
     </Disclaimer>
 
     <Privacy>
-      <Column span={12}>
-        <Link href="https://www.amsterdam.nl/privacy/">Privacy</Link>
-      </Column>
+      <Row>
+        <Column span={12}>
+          <Link href="https://www.amsterdam.nl/privacy/">Privacy</Link>
+        </Column>
+      </Row>
     </Privacy>
   </FooterWrapper>
 );
