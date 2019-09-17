26b22d8c1a53ad8030d31c8edb37c5d413316f4e
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-09-17 17:39:50 +0200

    SIG-1562 Filter modal footer position fix This PR contains a fix for the filter form footer element that was misaligned in IE.

--- src/signals/incident-management/components/FilterForm/styled.js
+++ src/signals/incident-management/components/FilterForm/styled.js
@@ -19,7 +19,7 @@ export const FormFooter = styled.footer`
   background: white;
   height: 66px;
   padding: 10px 0;
-  position: fixed;
+  position: absolute;
   bottom: 0;
   width: 100%;
   left: 0;
