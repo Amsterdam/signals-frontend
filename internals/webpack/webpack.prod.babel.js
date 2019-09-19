bf449717c48d5f57977946b19d0d6e3153104030
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-09-19 14:14:33 +0200

    (Hotfix) formdata polyfill This PR adds the formdata polyfill as application entry

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -6,6 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = require('./webpack.base.babel')({
   // In production, we skip all hot-reloading stuff
   entry: [
+    'formdata-polyfill',
     'babel-polyfill', path.join(process.cwd(), 'src/app.js'),
   ],
 
