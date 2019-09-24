e4827f010cc878dc620b80e31ed1a2981b73f2af
Jasper Swart <jasper.g.swart@gmail.com>
2019-09-24 11:59:09 +0200

    [SIG-1672] disbled try catch

--- src/signals/incident/containers/IncidentContainer/saga.js
+++ src/signals/incident/containers/IncidentContainer/saga.js
@@ -1,5 +1,5 @@
 import { all, call, put, select, takeLatest } from 'redux-saga/effects';
-import { replace } from 'react-router-redux';
+// import { replace } from 'react-router-redux';
 import request from 'utils/request';
 
 import { authPostCall } from 'shared/services/api/api';
@@ -7,7 +7,7 @@ import CONFIGURATION from 'shared/services/configuration/configuration';
 import { CREATE_INCIDENT, GET_CLASSIFICATION, SET_PRIORITY } from './constants';
 import {
   createIncidentSuccess,
-  createIncidentError,
+  // createIncidentError,
   getClassificationSuccess,
   getClassificationError,
   setPriority,
@@ -41,40 +41,40 @@ export function* getClassification(action) {
 export function* createIncident(action) {
   console.log('createIncident 1', action);
   const requestURL = `${CONFIGURATION.API_ROOT}signals/signal/`;
-  try {
-    console.log('createIncident 2 requestURL', requestURL);
-    const result = yield call(request, requestURL, {
-      method: 'POST',
-      body: JSON.stringify(mapControlsToParams(action.payload.incident, action.payload.wizard)),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    console.log('createIncident 3 result', result);
-
-    if (action.payload.isAuthenticated && action.payload.incident.priority.id === 'high') {
-      console.log('createIncident 4 prio', result, action.payload.incident.priority.id);
-      yield put(setPriority({
-        priority: action.payload.incident.priority.id,
-        _signal: result.id
-      }));
+  // try {
+  console.log('createIncident 2 requestURL', requestURL);
+  const result = yield call(request, requestURL, {
+    method: 'POST',
+    body: JSON.stringify(mapControlsToParams(action.payload.incident, action.payload.wizard)),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+  console.log('createIncident 3 result', result);
 
-    if (action.payload.incident.images) {
-      console.log('createIncident 5 images', result, action.payload.incident.images);
-      yield all(action.payload.incident.images.map((image) =>
+  if (action.payload.isAuthenticated && action.payload.incident.priority.id === 'high') {
+    console.log('createIncident 4 prio', result, action.payload.incident.priority.id);
+    yield put(setPriority({
+      priority: action.payload.incident.priority.id,
+      _signal: result.id
+    }));
+  }
+
+  if (action.payload.incident.images) {
+    console.log('createIncident 5 images', result, action.payload.incident.images);
+    yield all(action.payload.incident.images.map((image) =>
         put(uploadRequest({
           file: image,
           id: result.signal_id
         }))
       ));
-    }
-    yield put(createIncidentSuccess(result));
-    console.log('createIncident 6 sucess');
-  } catch (error) {
-    yield put(createIncidentError());
-    yield put(replace('/incident/fout'));
   }
+  yield put(createIncidentSuccess(result));
+  console.log('createIncident 6 sucess');
+  // } catch (error) {
+  // yield put(createIncidentError());
+  // yield put(replace('/incident/fout'));
+  // }
 }
 
 export function* setPriorityHandler(action) {
