afa917a62d228463d5bd52118c264f8c10d447dc
Jasper Swart <jasper.g.swart@gmail.com>
2019-10-09 15:08:18 +0200

    [SIG-1660] SplitDetail is now styled comp

--- src/signals/incident-management/containers/IncidentSplitContainer/components/SplitDetail/index.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/components/SplitDetail/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Heading } from '@datapunt/asc-ui';
 import styled from '@datapunt/asc-core';
 import { string2date, string2time } from 'shared/services/string-parser/string-parser';
 import { getListValueByKey } from 'shared/services/list-helper/list-helper';
 
-import './style.scss';
+const StyledWrapper = styled.aside`
+  dt {
+    color: #787878;
+    margin-bottom: 5px;
+  }
+
+  dd {
+    font-family: 'Avenir Next LT W01 Demi', arial, sans-serif;
+    margin-bottom: 16px;
+  }
+`;
 
 const StyledH4 = styled(Heading)`
   font-weight: normal;
@@ -13,38 +23,38 @@ const StyledH4 = styled(Heading)`
 `;
 
 const SplitDetail = ({ incident, stadsdeelList }) => (
-  <aside className="split-detail">
+  <StyledWrapper>
     {incident ?
     (
-      <div>
+      <Fragment>
         <StyledH4 $as="h4">Melding {incident.id}</StyledH4>
 
         <dl>
-          <dt className="split-detail__definition">Datum</dt>
-          <dd className="split-detail__value">{string2date(incident.created_at)}</dd>
-          <dt className="split-detail__definition">Tijd</dt>
-          <dd className="split-detail__value">{string2time(incident.created_at)}</dd>
-          <dt className="split-detail__definition">Datum overlast</dt>
-          <dd className="split-detail__value">{string2date(incident.incident_date_start)}</dd>
-          <dt className="split-detail__definition">Tijd overlast</dt>
-          <dd className="split-detail__value">{string2time(incident.incident_date_start)}</dd>
-          <dt className="split-detail__definition">Stadsdeel</dt>
-          <dd className="split-detail__value">{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}</dd>
-          <dt className="split-detail__definition">Adres</dt>
-          <dd className="split-detail__value">{incident.location.address_text}</dd>
-          {incident.reporter.email ? <dt className="split-detail__definition">E-mailadres</dt> : ''}
-          {incident.reporter.email ? <dd className="split-detail__value">{incident.reporter.email}</dd> : ''}
-          {incident.reporter.phone ? <dt className="split-detail__definition">Telefonnummer</dt> : ''}
-          {incident.reporter.phone ? <dd className="split-detail__value">{incident.reporter.phone}</dd> : ''}
-          <dt className="split-detail__definition">Bron</dt>
-          <dd className="split-detail__value">{incident.source}</dd>
-          <dt className="split-detail__definition">Verantwoordelijke afdeling</dt>
-          <dd className="split-detail__value">{incident.category.department}</dd>
+          <dt>Datum</dt>
+          <dd>{string2date(incident.created_at)}</dd>
+          <dt>Tijd</dt>
+          <dd>{string2time(incident.created_at)}</dd>
+          <dt>Datum overlast</dt>
+          <dd>{string2date(incident.incident_date_start)}</dd>
+          <dt>Tijd overlast</dt>
+          <dd>{string2time(incident.incident_date_start)}</dd>
+          <dt>Stadsdeel</dt>
+          <dd>{getListValueByKey(stadsdeelList, incident.location.stadsdeel)}</dd>
+          <dt>Adres</dt>
+          <dd>{incident.location.address_text}</dd>
+          {incident.reporter.email ? <dt>E-mailadres</dt> : ''}
+          {incident.reporter.email ? <dd>{incident.reporter.email}</dd> : ''}
+          {incident.reporter.phone ? <dt>Telefonnummer</dt> : ''}
+          {incident.reporter.phone ? <dd>{incident.reporter.phone}</dd> : ''}
+          <dt>Bron</dt>
+          <dd>{incident.source}</dd>
+          {incident.category.department ? <dt>Verantwoordelijke afdeling</dt> : ''}
+          {incident.category.department ? <dd>{incident.category.department}</dd> : ''}
         </dl>
-      </div>
+      </Fragment>
     )
       : ''}
-  </aside>
+  </StyledWrapper>
 );
 
 
