b7ea1010f66bea0111cffb4e88398b56ba16f527
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-10-17 16:28:11 +0200

    Decreases size threshold for inliningsvgs in css

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -55,8 +55,8 @@ module.exports = options => ({
           {
             loader: 'svg-url-loader',
             options: {
-              // Inline files smaller than 10 kB
-              limit: 10 * 1024,
+              // Inline files smaller than 5 kB
+              limit: 5 * 1024,
               noquotes: true,
             },
           },
