b5240fb229895723a172d1061e9ec55b647d85ad
Jasper Swart <jasper.g.swart@gmail.com>
2019-10-28 10:16:47 +0100

    [SIG-1793] StatusForm is now styled component

--- src/signals/incident-management/containers/IncidentDetail/components/StatusForm/index.js
+++ src/signals/incident-management/containers/IncidentDetail/components/StatusForm/index.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { FormBuilder, FieldGroup, Validators } from 'react-reactive-form';
+import isEqual from 'lodash.isequal';
+import styled from 'styled-components';
 
 import { Button, Spinner, Heading, Row, Column } from '@datapunt/asc-ui';
 import { incidentType, dataListType, defaultTextsType } from 'shared/types';
@@ -11,11 +12,6 @@ import RadioInput from '../../../../components/RadioInput';
 import TextAreaInput from '../../../../components/TextAreaInput';
 import Label from '../../../../components/Label';
 import DefaultTexts from './components/DefaultTexts';
-
-const form = FormBuilder.group({
-  status: ['o', Validators.required],
-  text: [''],
-});
 
 const StyledColumn = styled(Column)`
   display: block;
@@ -35,152 +31,157 @@ const StyledButton = styled(Button)`
   margin-right: 10px;
 `;
 
-const StatusForm = ({
-  incident,
-  patching,
-  error,
-  statusList,
-  changeStatusOptionList,
-  defaultTextsOptionList,
-  defaultTexts,
-  onPatchIncident,
-  onDismissError,
-  onClose,
-}) => {
-  const [warning, setWarning] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-  const [hasDefaultTexts , setHasDefaultTexts] = useState(false);
+class StatusForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  form = FormBuilder.group({ // eslint-disable-line react/sort-comp
+    status: ['', Validators.required],
+    text: [''],
+  });
 
-  const reset = () => {
-    form.patchValue({status: '', text: '' });
-  };
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
-    form.controls.status.valueChanges.subscribe(status => {
-      const found = statusList.find(s => s.key === status);
-      setWarning((found && found.warning) || '');
-      onDismissError();
+    this.state = {
+      warning: props.warning,
+      hasDefaultTexts: props.hasDefaultTexts,
+    };
 
-      const textField = form.controls.text;
-      const hasfound = defaultTextsOptionList.find(s => s.key === status);
-      if (hasfound) {
-        setHasDefaultTexts(true);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleUseDefaultText = this.handleUseDefaultText.bind(this);
+  }
+
+  componentDidMount() {
+    this.form.controls.status.valueChanges.subscribe(status => {
+      const found = this.props.statusList.find(s => s.key === status);
+      this.setState({
+        warning: (found && found.warning) || '',
+      });
+      this.props.onDismissError();
+
+      const textField = this.form.controls.text;
+      const hasDefaultTexts = Boolean(this.props.defaultTextsOptionList.find(s => s.key === status));
+      if (hasDefaultTexts) {
         textField.setValidators([Validators.required]);
       } else {
-        setHasDefaultTexts(false);
         textField.clearValidators();
       }
+      this.setState({ hasDefaultTexts });
 
       textField.updateValueAndValidity();
     });
 
-    onDismissError();
-    form.updateValueAndValidity();
-  }, []);
+    this.props.onDismissError();
+  }
 
-  useEffect(() => {
-    const hasError = (error && error.response && !error.response.ok) || false;
-    const textField = form.controls.text;
-    if (submitting && patching.status === false) {
-      setSubmitting(false);
+  componentDidUpdate(prevProps) {
+    if (!isEqual(prevProps.patching && prevProps.patching.status, this.props.patching && this.props.patching.status) && this.props.patching.status === false) {
+      const hasError = (this.props.error && this.props.error.response && !this.props.error.response.ok) || false;
       if (!hasError) {
-        textField.clearValidators();
-        reset();
-        onClose();
+        this.form.reset();
+        this.props.onClose();
       }
     }
+    this.form.updateValueAndValidity();
+  }
 
-    form.updateValueAndValidity();
-  }, [patching.status, error.response]);
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    setSubmitting(true);
-    onPatchIncident({
-      id: incident.id,
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onPatchIncident({
+      id: this.props.incident.id,
       type: 'status',
-      patch: {
-        status: {
-          state: form.value.status,
-          text: form.value.text,
-        },
-      },
+      patch: { status: { state: this.form.value.status, text: this.form.value.text } },
     });
   }
 
-  const handleUseDefaultText = (e, text) => {
+  handleUseDefaultText(e, text) {
     e.preventDefault();
 
-    form.get('text').patchValue(text);
-  };
+    this.form.get('text').patchValue(text);
+  }
 
-  const currentStatus = statusList.find(status => status.key === incident.status.state);
-  return (
-    <Fragment>
-      <FieldGroup
-        control={form}
-        render={({ invalid }) => (
-          <form onSubmit={handleSubmit}>
-            <Row>
-              <StyledColumn span={6}>
-                <StyledH4 $as="h4">Status wijzigen</StyledH4>
+  render() {
+    const {
+      incident,
+      patching,
+      error,
+      statusList,
+      changeStatusOptionList,
+      onClose,
+      defaultTexts,
+    } = this.props;
+    const {
+      warning,
+      hasDefaultTexts,
+    } = this.state;
+    const currentStatus = statusList.find(status => status.key === incident.status.state);
+    return (
+      <Fragment>
+        <FieldGroup
+          control={this.form}
+          render={({ invalid }) => (
+            <form onSubmit={this.handleSubmit}>
+              <Row>
+                <StyledColumn span={6}>
+                  <StyledH4 $as="h4">Status wijzigen</StyledH4>
 
-                <StyledCurrentStatus>
-                  <Label htmlFor="currentStatus">Huidige status</Label>
-                  <div id="currentStatus">{currentStatus.value}</div>
-                </StyledCurrentStatus>
+                  <StyledCurrentStatus>
+                    <Label htmlFor="currentStatus">Huidige status</Label>
+                    <div id="currentStatus">{currentStatus.value}</div>
+                  </StyledCurrentStatus>
 
-                <FieldControlWrapper
-                  display="Nieuwe status"
-                  render={RadioInput}
-                  name="status"
-                  className="status-form__form-status"
-                  control={form.get('status')}
-                  values={changeStatusOptionList}
-                />
-                <FieldControlWrapper
-                  render={TextAreaInput}
-                  name="text"
-                  className="status-form__form-text"
-                  display="Toelichting"
-                  control={form.get('text')}
-                  rows={5}
-                />
+                  <FieldControlWrapper
+                    display="Nieuwe status"
+                    render={RadioInput}
+                    name="status"
+                    control={this.form.get('status')}
+                    values={changeStatusOptionList}
+                  />
+                  <FieldControlWrapper
+                    render={TextAreaInput}
+                    name="text"
+                    display="Toelichting"
+                    control={this.form.get('text')}
+                    rows={5}
+                  />
 
-                <div className="status-form__warning notification notification-red">
-                  {warning}
-                </div>
-                <div className="status-form__error notification notification-red">
-                  {error && error.response && error.response.status === 403 ? 'Je bent niet geautoriseerd om dit te doen.' : '' }
-                  {error && error.response && error.response.status !== 403 ? 'De gekozen status is niet mogelijk in deze situatie.' : '' }
-                </div>
+                  <div className="notification notification-red">
+                    {warning}
+                  </div>
+                  <div className="notification notification-red">
+                    {error && error.response && error.response.status === 403 ? 'Je bent niet geautoriseerd om dit te doen.' : '' }
+                    {error && error.response && error.response.status !== 403 ? 'De gekozen status is niet mogelijk in deze situatie.' : '' }
+                  </div>
 
-                <StyledButton
-                  variant="secondary"
-                  disabled={invalid}
-                  type="submit"
-                  iconRight={patching.status ? <Spinner /> : null}
-                >Status opslaan</StyledButton>
-                <StyledButton
-                  variant="tertiary"
-                >Annuleren</StyledButton>
-              </StyledColumn>
-              <StyledColumn span={6}>
-                <DefaultTexts
-                  defaultTexts={defaultTexts}
-                  status={form.get('status').value}
-                  hasDefaultTexts={hasDefaultTexts}
-                  onHandleUseDefaultText={handleUseDefaultText}
-                />
-              </StyledColumn>
-            </Row>
-          </form>
-        )}
-      />
+                  <StyledButton
+                    variant="secondary"
+                    disabled={invalid}
+                    type="submit"
+                    iconRight={patching.status ? <Spinner /> : null}
+                  >Status opslaan</StyledButton>
+                  <StyledButton
+                    variant="tertiary"
+                    onClick={onClose}
+                  >Annuleren</StyledButton>
+                </StyledColumn>
+                <StyledColumn span={6}>
+                  <DefaultTexts
+                    defaultTexts={defaultTexts}
+                    status={this.form.get('status').value}
+                    hasDefaultTexts={hasDefaultTexts}
+                    onHandleUseDefaultText={this.handleUseDefaultText}
+                  />
+                </StyledColumn>
+              </Row>
+            </form>
+          )}
+        />
+      </Fragment>
+    );
+  }
+}
 
-    </Fragment>
-  );
-  ;
+StatusForm.defaultProps = {
+  warning: '',
+  hasDefaultTexts: false,
 };
 
 StatusForm.propTypes = {
@@ -189,6 +190,8 @@ StatusForm.propTypes = {
   patching: PropTypes.shape({
     status: PropTypes.bool,
   }).isRequired,
+  warning: PropTypes.string,
+  hasDefaultTexts: PropTypes.bool,
   changeStatusOptionList: dataListType.isRequired,
   defaultTextsOptionList: dataListType.isRequired,
   statusList: dataListType.isRequired,
