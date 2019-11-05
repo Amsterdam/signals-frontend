e0156de028e85aee139a8e5c8a5e19146337b63b
Spaces <c.lanenga@amsterdam.nl>
2019-11-05 12:36:38 +0100

    [SIG-1809] - Update margin history item.

--- src/signals/incident-management/containers/IncidentDetail/components/History/index.js
+++ src/signals/incident-management/containers/IncidentDetail/components/History/index.js
@@ -1,27 +1,55 @@
 import React from 'react';
+
+import styled, { css } from '@datapunt/asc-core'
+import { themeColor, Heading, styles } from '@datapunt/asc-ui'
+import { string2date, string2time } from 'shared/services/string-parser/string-parser';
 import { historyType } from 'shared/types';
 
-import { string2date, string2time } from 'shared/services/string-parser/string-parser';
+const rowStyles = css`
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`;
 
-import './style.scss';
+const StyledHistoryListItem = styled.div`
+  margin-bottom: 16px;
+  line-height: 22px;
+  ${rowStyles}
+`;
+
+const StyledHistoryItemLeft = styled.div`
+  color: ${themeColor('tint', 'level5')};
+`;
+
+const StyledHistoryItemRight = styled(StyledHistoryItemLeft)`
+  color: ${themeColor('tint', 'level7')};
+`;
+
+const StyledH4 = styled(Heading)`
+  ${styles.HeaderStyles} {
+    margin-top: 1.33em;
+    margin-bottom: 1.33em;
+  }
+`;
 
 const History = ({ list }) => (
   <section className="history">
-    <h4 data-testid="history-title">Historie</h4>
+    <StyledH4 $as="h4" data-testid="history-title">Historie</StyledH4>
 
     {list.map(item => (
-      <div key={item.identifier} className="row history__item" data-testid="history-list-item">
-        <div className="col-5 history__item-left">
+      <StyledHistoryListItem key={item.identifier} data-testid="history-list-item">
+        <StyledHistoryItemLeft className="col-5">
           <span data-testid="history-list-item-when">
             {string2date(item.when)} om {string2time(item.when)}
           </span>
           <div data-testid="history-list-item-who">{item.who}</div>
-        </div>
-        <div className="col-7 history__item-right pre-wrap">
+        </StyledHistoryItemLeft>
+        <StyledHistoryItemRight className="col-7 pre-wrap">
           <div data-testid="history-list-item-action">{item.action}</div>
           <div data-testid="history-list-item-description">{item.description}</div>
-        </div>
-      </div>
+        </StyledHistoryItemRight>
+      </StyledHistoryListItem>
     ))}
   </section>
 );
