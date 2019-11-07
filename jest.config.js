5a40da2697ae1d66caaa7681c1f1ed908841736f
Jasper Swart <jasper.g.swart@gmail.com>
2019-11-07 16:41:55 +0100

    [SIG-1836] updated coverage

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 97.77,
-      branches: 96.74,
-      functions: 97.37,
-      lines: 97.78,
+      statements: 98.09,
+      branches: 96.93,
+      functions: 97.84,
+      lines: 98.04,
     },
   },
   moduleDirectories: ['node_modules', 'src'],
