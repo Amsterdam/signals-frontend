2b7e288009cb61745f8c59d5addf3faa793e64d1
Jasper Swart <jasper.g.swart@gmail.com>
2019-11-15 11:48:06 +0100

    [SIG-1775] aded test

---
+++ src/signals/settings/roles/containers/RolesOverview/__test__/RolesOverview.test.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { withAppContext } from 'test/utils';
+
+import RolesOverview from '..';
+
+describe('containers/RolesOverview', () => {
+  let props = {};
+
+  beforeEach(() => {
+    props = {
+      roles: {
+        list: [
+          {
+            _display: 'behandelaars',
+            id: 2,
+            name: 'behandelaars',
+            permissions: [
+              {
+                _display: 'signals | signal | Can read from SIA',
+                codename: 'sia_read',
+              },
+              {
+                _display: 'signals | signal | Can change the status of a signal',
+                codename: 'sia_signal_change_status',
+              },
+            ],
+          },
+          {
+            _display: 'coordinatoren',
+            id: 3,
+            name: 'coordinatoren',
+            permissions: [],
+          },
+        ],
+        loading: false,
+      },
+      onFetchRoles: jest.fn(),
+    };
+  });
+
+  it('should render correctly', () => {
+    console.log('props 1', props.roles.list);
+    const { container, debug } =
+      render(withAppContext(<RolesOverview {...props} />))
+    debug();
+
+    expect(container.querySelector('table')).toBeTruthy();
+  });
+
+  // it('renders column in the correct order', () => {
+  // const columnOrder = ['roles', 'username', 'id', 'is_active'];
+  // const { container } = render(
+  // withAppContext(<List items={users} columnOrder={columnOrder} />)
+  // );
+  //
+  // expect(
+  // [...container.querySelectorAll('th')].map(header => header.textContent)
+  // ).toEqual(columnOrder);
+  // });
+  //
+  // it('does not render columns marked as invisible', () => {
+  // const { container } = render(
+  // withAppContext(<List items={users} invisibleColumns={['id']} />)
+  // );
+  //
+  // container.querySelectorAll('thead td').forEach(element => {
+  // expect(element.textContent).not.toEqual('id');
+  // });
+  // });
+  //
+  // it('navigates on row click', () => {
+  // const primaryKeyColumn = 'id';
+  // const { container, rerender } = render(
+  // withAppContext(<List items={users} />)
+  // );
+  //
+  // fireEvent.click(container.querySelector('tbody > tr:nth-child(10)'));
+  //
+  // expect(history.location.pathname.endsWith('/')).toEqual(true);
+  //
+  // rerender(
+  // withAppContext(<List items={users} primaryKeyColumn={primaryKeyColumn} />)
+  // );
+  //
+  // fireEvent.click(container.querySelector('tbody > tr:nth-child(42)'));
+  //
+  // const primaryKey = users[41].id;
+  //
+  // expect(history.location.pathname.endsWith(`/${primaryKey}`)).toEqual(true);
+  // });
+});
