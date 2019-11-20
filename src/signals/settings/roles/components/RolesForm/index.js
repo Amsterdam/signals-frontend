eb024a0f345f2b0e80c15ad704ba464df4ed2865
Jasper Swart <jasper.g.swart@gmail.com>
2019-11-20 11:38:59 +0100

    [SIG-1697] added permissions to rolesform

--- src/signals/settings/roles/components/RolesForm/index.js
+++ src/signals/settings/roles/components/RolesForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button } from '@datapunt/asc-ui';
 
 import PageHeader from 'signals/settings/components/PageHeader';
 
@@ -8,14 +9,57 @@ export const RolesForm = ({
   list,
   permissions,
 }) => {
-  console.log('-------------------', permissions);
   const role = list.find(item => item.id === id * 1);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('handleSubmit', e);
+  }
+
   console.log('role', role);
   return (
     <div>
       <PageHeader title="Rol instellingen" />
 
-      {id}
+      {role &&
+        (
+          <form
+            onSubmit={handleSubmit}
+          >
+
+            <input name="name" type="text" defaultValue={role.name} />
+
+            {permissions.map(permission =>
+              <div key={permission.id} className="antwoord">
+                <input
+                  name={permission.codename}
+                  id={permission.codename}
+                  type="checkbox"
+                  checked={role.permissions.find(item => item.id === permission.id)}
+                />
+                <label htmlFor={permission.codename}>{permission._display}</label>
+              </div>)}
+
+            <div>
+              <Button
+                variant="secondary"
+                type="submit"
+              >
+                Opslaan
+              </Button>
+
+              <Button
+                variant="tertiary"
+                type="button"
+              >
+                Annuleren
+              </Button>
+            </div>
+          </form>
+
+        )
+      }
+
     </div>
   )
 };
