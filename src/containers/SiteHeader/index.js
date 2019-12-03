5503c9ab6f2e55d75e6b92bf783d0ad5f86b2fbb
JJM van Deursen <janjaap@users.noreply.github.com>
2019-12-03 11:10:24 +0100

    (chore) Simplify SiteHeader This PR: - removes all unneccessary code from the `SiteHeader` container - adds a `useLocation` hook - has the `SiteHeader` component use said hook as well as directly query `isAuthenticated` instead of getting it passed down from the container

--- src/containers/SiteHeader/index.js
+++ src/containers/SiteHeader/index.js
@@ -3,73 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose, bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
-import {
-  makeSelectUserName,
-  makeSelectUserPermissions,
-} from 'containers/App/selectors';
+import { makeSelectUserPermissions } from 'containers/App/selectors';
 import SiteHeader from 'components/SiteHeader';
-import { withRouter } from 'react-router-dom';
-import { isAuthenticated } from 'shared/services/auth/auth';
 
-import { doLogin, doLogout } from '../App/actions';
+import { doLogout } from '../App/actions';
 
-const HeaderWithRouter = compose(withRouter)(SiteHeader);
-
-export class SiteHeaderContainer extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
-  constructor(props) {
-    super(props);
-    this.onLoginLogoutButtonClick = this.onLoginLogoutButtonClick.bind(this);
-  }
-
-  onLoginLogoutButtonClick(event, domain) {
-    event.persist();
-    event.preventDefault();
-    event.stopPropagation();
-    if (!isAuthenticated()) {
-      this.props.onLogin(domain);
-    } else {
-      this.props.onLogout();
-    }
-  }
-
-  render() {
-    const { permissions, userName } = this.props;
-
-    return (
-      <HeaderWithRouter
-        isAuthenticated={isAuthenticated()}
-        onLoginLogoutButtonClick={this.onLoginLogoutButtonClick}
-        permissions={permissions}
-        userName={userName}
-      />
-    );
-  }
-}
+export const SiteHeaderContainer = ({ onLogOut, permissions }) => (
+  <SiteHeader onLogOut={onLogOut} permissions={permissions} />
+);
 
 SiteHeaderContainer.propTypes = {
-  onLogin: PropTypes.func,
-  onLogout: PropTypes.func,
+  onLogOut: PropTypes.func,
   permissions: PropTypes.arrayOf(PropTypes.string).isRequired,
-  userName: PropTypes.string,
 };
 
 const mapStateToProps = createStructuredSelector({
   permissions: makeSelectUserPermissions(),
-  userName: makeSelectUserName(),
 });
 
-export const mapDispatchToProps = dispatch => bindActionCreators(
-  {
-    onLogin: doLogin,
-    onLogout: doLogout,
-  },
-  dispatch,
-);
+export const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      onLogOut: doLogout,
+    },
+    dispatch
+  );
 
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
 
 export default compose(withConnect)(SiteHeaderContainer);
