6f5e05ae673d043b49f37f744fdf02f43c0b4763
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-12-04 11:24:34 +0100

    Adds Notification container

---
+++ src/containers/Notification/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose, bindActionCreators } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import Notification from 'components/Notification';
+import { makeSelectNotification } from 'containers/App/selectors';
+import { resetGlobalNotification } from 'containers/App/actions';
+
+export const NotificationContainer = ({
+  notification,
+  onResetNotification,
+}) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      onResetNotification();
+    });
+
+    return () => {
+      unlisten();
+    };
+  }, [history, onResetNotification]);
+
+  return <Notification {...notification} onClose={onResetNotification} />;
+};
+
+NotificationContainer.propTypes = {
+  notification: PropTypes.shape({}),
+  onResetNotification: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  notification: makeSelectNotification(),
+});
+
+export const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      onResetNotification: resetGlobalNotification,
+    },
+    dispatch
+  );
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+export default compose(withConnect)(NotificationContainer);
