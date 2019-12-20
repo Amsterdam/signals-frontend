6be9353c755a659de066f9898d6b8f10bdcb483a
JJM van Deursen <j.van.deursen@amsterdam.nl>
2019-12-20 09:23:47 +0100

    Reverts destructuring change

--- src/signals/incident-management/components/FilterForm/__test__/FilterForm.test.js
+++ src/signals/incident-management/components/FilterForm/__test__/FilterForm.test.js
@@ -506,7 +506,8 @@ describe('signals/incident-management/components/FilterForm', () => {
       const { container } = render(
         withAppContext(
           <FilterForm
-            {...formProps, ...handlers}
+            {...formProps}
+            {...handlers}
             filter={{
               name: '',
               options: { incident_date: '1970-01-01' },
@@ -544,7 +545,8 @@ describe('signals/incident-management/components/FilterForm', () => {
       const { container } = render(
         withAppContext(
           <FilterForm
-            {...formProps, ...handlers}
+            {...formProps}
+            {...handlers}
             filter={{
               name: 'My filter',
               options: {
