fb01d5b8b401dce22d7bf1063fb656c8030d92ed
Jasper Swart <jasper.g.swart@gmail.com>
2019-12-23 09:54:20 +0100

    [SIG-1915] moved all methods to outside the component

--- src/signals/incident-management/containers/FilterTagList/index.js
+++ src/signals/incident-management/containers/FilterTagList/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import styled from 'styled-components';
@@ -21,6 +21,64 @@ const StyledTag = styled(Tag)`
 
 export const allLabelAppend = ': Alles';
 
+const mapKeys = key => {
+  switch (key) {
+    case 'source':
+      return 'Bron'
+
+    default:
+      return key;
+  }
+}
+
+const renderGroup = (tag, main, list, tagKey) => {
+  if (tag.length === list.length) {
+    return (
+      <StyledTag
+        colorType="tint"
+        colorSubtype="level3"
+        key={tagKey}
+        data-testid="filterTagListTag"
+      >
+        {mapKeys(tagKey)}{allLabelAppend}
+      </StyledTag>
+    );
+  }
+  return tag.map(item => renderTag(item.key, main, list));
+};
+
+const renderTag = (key, mainCategories, list) => {
+  let found = false;
+
+  if (list) {
+    found = list.find(i => i.key === key || i.slug === key);
+  }
+
+  let display = (found && found.value) || key;
+  if (!display) {
+    return;
+  }
+
+  if (moment(display, 'YYYY-MM-DD', true).isValid()) {
+    display = moment(display).format('DD-MM-YYYY');
+  }
+
+  const foundMain = mainCategories.find(i => i.key === key);
+
+  display += foundMain ? allLabelAppend : '';
+  // eslint-disable-next-line consistent-return
+  return (
+    <StyledTag
+      colorType="tint"
+      colorSubtype="level3"
+      key={key}
+      data-testid="filterTagListTag"
+    >
+      {display}
+    </StyledTag>
+  );
+};
+
 export const FilterTagListComponent = props => {
   const {
     tags,
@@ -34,64 +92,12 @@ export const FilterTagListComponent = props => {
     category_slug: sub,
   };
 
-  const mapKeys = useCallback(key => {
-    switch (key) {
-      case 'source':
-        return 'bron';
-
-      default:
-        return key;
-    }
-  }, []);
-
-  const renderItem = useCallback((display, key) => (
-    <StyledTag
-      colorType="tint"
-      colorSubtype="level3"
-      key={key}
-      data-testid="filterTagListTag"
-    >
-      {display}
-    </StyledTag>
-  ), []);
-
-  const renderGroup = useCallback((tag, list, tagKey) => {
-    if (tag.length === list.length) {
-      return renderItem(`${mapKeys(tagKey)}${allLabelAppend}`, tagKey);
-    }
-    return tag.map(item => renderTag(item.key, list));
-  }, [mapKeys, renderItem, renderTag]);
-
-  const renderTag = useCallback((key, list) => {
-    let found = false;
-
-    if (list) {
-      found = list.find(i => i.key === key || i.slug === key);
-    }
-
-    let display = (found && found.value) || key;
-
-    if (!display) {
-      return;
-    }
-
-    if (moment(display, 'YYYY-MM-DD', true).isValid()) {
-      display = moment(display).format('DD-MM-YYYY');
-    }
-
-    const foundMain = main.find(i => i.key === key);
-
-    display += foundMain ? allLabelAppend : '';
-    // eslint-disable-next-line consistent-return
-    return renderItem(display, key);
-  }, [main, renderItem]);
-
   return (
     <FilterWrapper className="incident-overview-page__filter-tag-list">
       {Object.entries(tags).map(([tagKey, tag]) =>
         Array.isArray(tag)
-          ? renderGroup(tag, map[tagKey], tagKey)
-          : renderTag(tag, map[tagKey])
+          ? renderGroup(tag, main, map[tagKey], tagKey)
+          : renderTag(tag, main, map[tagKey])
       )}
     </FilterWrapper>
   );
