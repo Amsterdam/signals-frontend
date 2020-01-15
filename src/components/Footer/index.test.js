6865b9b24a19e379c7b03d171b8f5f398ab4aba0
Jasper Swart <jasper.g.swart@gmail.com>
2020-01-15 08:15:51 +0100

    [SIG-1927] fixed unit

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
+import { withAppContext } from 'test/utils';
 
 import Footer from './index';
 
 describe('<Footer />', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(
-      <Footer />
+    const { getByText } = render(
+      withAppContext(<Footer />)
     );
-    expect(wrapper).toMatchSnapshot();
+
+    expect(getByText('Lukt het niet om een melding te doen?')).toBeInTheDocument();
+    // @TODO
+    expect(getByText('Bel het Gemeentelijk informatienummer: 14 020')).toBeInTheDocument();
+    expect(getByText('op werkdagen van 08.00 tot 18.00 uur.')).toBeInTheDocument();
+
+    expect(getByText('Privacy')).toBeInTheDocument();
   });
 });
