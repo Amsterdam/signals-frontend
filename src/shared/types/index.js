3d386f1e101030bfb65053ea560ecfc27cb3a55d
Jasper Swart <jasper.g.swart@gmail.com>
2020-02-11 16:10:00 +0100

    [SIG-2083] fixed code review again

--- src/shared/types/index.js
+++ src/shared/types/index.js
@@ -87,7 +87,7 @@ export const filterType = PropTypes.shape({
     created_before: PropTypes.string,
     feedback: PropTypes.string,
     maincategory_slug: PropTypes.arrayOf(dataItemType),
-    priority: PropTypes.arrayOf(dataListType),
+    priority: PropTypes.arrayOf(dataItemType),
     stadsdeel: PropTypes.arrayOf(dataItemType),
     status: PropTypes.arrayOf(dataItemType),
   }),
