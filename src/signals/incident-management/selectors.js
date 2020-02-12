0a3c9e7db2880f424b3ab091f8c2af262a289b5d
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-02-12 10:51:14 +0100

    [SIG-2238] Incompatible filter data

--- src/signals/incident-management/selectors.js
+++ src/signals/incident-management/selectors.js
@@ -39,13 +39,23 @@ export const makeSelectAllFilters = createSelector(
   (stateMap, dataLists, categories) => {
     const filters = stateMap.get('filters').toJS();
 
-    return filters.map(filter =>
-      parseInputFormData(filter, {
+    return filters.map(filter => {
+      const { priority } = filter.options;
+      const converted = Array.isArray(priority) ? priority : [priority];
+      const fltr = {
+        ...filter,
+        options: {
+          ...filter.options,
+          priority: converted,
+        },
+      };
+
+      return parseInputFormData(fltr, {
         ...dataLists,
         maincategory_slug: categories.main,
         category_slug: categories.sub,
-      })
-    );
+      });
+    });
   }
 );
 
@@ -145,7 +155,10 @@ export const makeSelectIncidents = createSelector(
 export const makeSelectIncidentsCount = createSelector(
   selectIncidentManagementDomain,
   state => {
-    const { incidents: { count } } = state.toJS();
+    const {
+      incidents: { count },
+    } = state.toJS();
 
     return count;
-  });
+  }
+);
