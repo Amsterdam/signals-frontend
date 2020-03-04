aca2a68adf5f1569c4078ab28b0c17fbaa36d04d
Jasper Poppe <jgpoppe@gmail.com>
2020-03-04 15:29:48 +0100

    makeSelectRolesModel -> rolesModelSelector

--- src/signals/settings/roles/containers/RoleFormContainer/index.js
+++ src/signals/settings/roles/containers/RoleFormContainer/index.js
@@ -19,7 +19,7 @@ import {
   makeSelectUserCan,
 } from 'containers/App/selectors';
 
-import makeSelectRolesModel from 'models/roles/selectors';
+import rolesModelSelector from 'models/roles/selectors';
 import { patchRole, saveRole, resetResponse } from 'models/roles/actions';
 
 import RoleForm from './components/RoleForm';
@@ -137,7 +137,7 @@ RoleFormContainer.propTypes = {
 };
 
 const mapStateToProps = createStructuredSelector({
-  roles: makeSelectRolesModel,
+  roles: rolesModelSelector,
   userCan: makeSelectUserCan,
 });
 
