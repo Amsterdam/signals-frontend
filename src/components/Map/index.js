5ca9afd6668eb76965cb2acf9e4af438911cf3e0
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-03-09 14:10:03 +0100

    replaces the readonly map component

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,97 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import amaps from 'amsterdam-amaps/dist/amaps';
+import { Map as ArmMap, TileLayer, Marker } from '@datapunt/react-maps';
+import { getCrsRd } from '@datapunt/amsterdam-react-maps/lib/utils';
+import styled from '@datapunt/asc-core';
+import { AmsterdamMarkerIcon } from '../Geocoder/MarkerIcons';
 
-import 'leaflet/dist/leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'amsterdam-amaps/dist/nlmaps/dist/assets/css/nlmaps.css';
+const MapWrapperStyle = styled.div`
+  position: relative;
+`;
 
-import './style.scss';
+const Map = ({ latlng }) => {
+  const { latitude: lat, longitude: lon } = latlng;
+  const mapProps = {
+    options: {
+      center: [lat, lon],
+      zoomControl: false,
+      zoom: 16,
+      crs: getCrsRd(),
+      maxBounds: [
+        [52.25168, 4.64034],
+        [52.50536, 5.10737],
+      ],
+    },
+    style: {
+      width: '100%',
+      height: '450px',
+    },
+  };
 
-const PREVIEW_ZOOM_LEVEL = '16';
-const smallIcon = global.window.L.icon({
-  iconUrl: 'https://map.data.amsterdam.nl/dist/images/svg/marker.svg',
-  iconSize: [20, 20],
-  iconAnchor: [10, 19],
-});
-const defaultIcon = global.window.L.icon({
-  iconUrl: 'https://map.data.amsterdam.nl/dist/images/svg/marker.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 39],
-});
-
-class Map extends React.Component {
-  componentDidMount() {
-    this.map = amaps.createMap({
-      center: {
-        latitude: this.props.latlng.latitude,
-        longitude: this.props.latlng.longitude,
-      },
-      layer: 'standaard',
-      target: 'mapdiv',
-      marker: true,
-      search: false,
-      zoom: this.props.zoom,
-    });
-
-    if (this.props.hideAttribution) {
-      this.map.attributionControl.remove();
-    }
-
-    if (this.props.hideZoomControls) {
-      this.map.zoomControl.remove();
-    }
-
-    this.renderMarker(this.props.latlng.latitude, this.props.latlng.longitude);
-  }
-
-  componentDidUpdate(prevProps) {
-    const lat = this.props.latlng.latitude;
-    const lng = this.props.latlng.longitude;
-    if (lat !== prevProps.latlng.latitude || lng !== prevProps.latlng.longitude) {
-      this.renderMarker(lat, lng);
-    }
-  }
-
-  map = {};
-
-  renderMarker(lat, lng) {
-    L.DomUtil.empty(this.map.getPane('markerPane'));
-
-    this.map.setView({ lat, lng });
-
-    const marker = L.marker(
-      [lat, lng],
-      { icon: this.props.useSmallMarker ? smallIcon : defaultIcon }
-    );
-    marker.addTo(this.map);
-  }
-
-  render() {
-    return (
-      <div className="map-component">
-        <div className="map">
-          <div className="map-container" id="mapdiv" />
-        </div>
-      </div>
-    );
-  }
-}
-
-Map.defaultProps = {
-  latlng: {},
-  hideAttribution: false,
-  hideZoomControls: false,
-  useSmallMarker: false,
-  zoom: PREVIEW_ZOOM_LEVEL,
+  return (
+    <MapWrapperStyle>
+      <ArmMap {...mapProps}>
+        <Marker
+          args={[{ lat, lon }]}
+          options={{
+            icon: AmsterdamMarkerIcon,
+          }}
+        />
+        <TileLayer
+          args={['https://{s}.data.amsterdam.nl/topo_rd/{z}/{x}/{y}.png']}
+          options={{
+            subdomains: ['t1', 't2', 't3', 't4'],
+            tms: true,
+            attribution: 'Kaartgegevens CC-BY-4.0 Gemeente Amsterdam',
+          }}
+        />
+      </ArmMap>
+    </MapWrapperStyle>
+  );
 };
 
 Map.propTypes = {
-  latlng: PropTypes.object,
-  hideAttribution: PropTypes.bool,
-  hideZoomControls: PropTypes.bool,
-  useSmallMarker: PropTypes.bool,
-  zoom: PropTypes.string,
+  latlng: PropTypes.shape(
+    {
+      latitude: PropTypes.number,
+      longitude: PropTypes.number,
+    }
+  ).isRequired,
 };
 
 export default Map;
