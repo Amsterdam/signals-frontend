9d4d94d9ff484df564e1626f2b7e5b0c0d24a377
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-03-17 16:57:43 +0100

    renders the map with the default provided marker

--- src/components/MapEditor/index.js
+++ src/components/MapEditor/index.js
@@ -1,12 +1,13 @@
-import React, { memo } from 'react';
+import React, { memo , useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Map, Marker } from '@datapunt/react-maps';
 import { ViewerContainer } from '@datapunt/asc-ui';
 import { Zoom } from '@datapunt/amsterdam-react-maps/lib/components';
 import styled from '@datapunt/asc-core';
-import MAP_OPTIONS, { DEFAULT_MARKER_POSITION } from 'shared/services/configuration/map-options';
+import MAP_OPTIONS from 'shared/services/configuration/map-options';
 import BackgroundLayer from 'components/BackgroundLayer';
 import { markerIcon } from 'shared/services/configuration/map-markers';
+import { feature2location } from 'shared/services/map-location';
 
 
 const MapWrapper = styled.div`
@@ -22,15 +23,29 @@ const StyledViewerContainer = styled(ViewerContainer)`
   z-index: 400;
 `;
 
-const MapEditor = ({ location, onLocationChange }) => {
-  console.log('location', location, onLocationChange);
+const MapEditor = ({ location }) => {
+  const [marker, setMarker] = useState();
+
+  useEffect(() => {
+    if (!marker || !location.geometrie) return;
+
+    const opacity = 1;
+    const latlng = feature2location(location.geometrie);
+    marker.setLatLng(latlng);
+    marker.setOpacity(opacity);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [marker, location]);
 
   return (
     <MapWrapper>
       <StyledMap options={MAP_OPTIONS}>
         <StyledViewerContainer bottomRight={<Zoom />} />
         <Marker
-          args={[DEFAULT_MARKER_POSITION]}
+          setInstance={setMarker}
+          args={[{
+            lat: 0,
+            lng: 0,
+          }]}
           options={{
             icon: markerIcon,
             opacity: 0,
@@ -44,8 +59,10 @@ const MapEditor = ({ location, onLocationChange }) => {
 };
 
 MapEditor.propTypes = {
-  location: PropTypes.shape({}).isRequired,
-  onLocationChange: PropTypes.func.isRequired,
+  location: PropTypes.shape({
+    geometrie: PropTypes.shape({}),
+    address: PropTypes.shape({}),
+  }).isRequired,
 };
 
 export default memo(MapEditor);
--- src/shared/services/map-location/index.js
+++ src/shared/services/map-location/index.js
@@ -1,28 +1,114 @@
-function mapLocation(loc) {
+export const location2feature = location => ({
+  type: 'Point',
+  coordinates: [location.lng, location.lat],
+});
+
+export const feature2location = feature => {
+  const { coordinates } = feature;
+  return {
+    lat: coordinates[1],
+    lng: coordinates[0],
+  };
+};
+
+/**
+ * converts the sia location in latlon fromat
+ */
+export function mapLocation(loc) {
   const location = {};
 
-  if (loc.dichtstbijzijnd_adres) {
-    location.address = { ...loc.dichtstbijzijnd_adres };
-    location.address.huisnummer = `${location.address.huisnummer}`;
-    location.address.huisnummer_toevoeging = `${location.address.huisnummer_toevoeging}`;
+  if (loc.geometrie) {
+    location.location = feature2location(loc.geometrie);
   }
 
-  if (loc.omgevingsinfo) {
-    location.buurt_code = loc.omgevingsinfo.buurtcode;
-    location.stadsdeel = loc.omgevingsinfo.stadsdeelcode;
+  if (loc.buurt_code) {
+    location.buurtcode = loc.buurt_code;
   }
 
-  if (loc.query) {
-    location.geometrie = {
-      type: 'Point',
-      coordinates: [
-        loc.query.longitude,
-        loc.query.latitude,
-      ],
-    };
+  if (loc.stadsdeelcode) {
+    location.stadsdeelcode = loc.stadsdeel;
+  }
+
+  if (loc.address) {
+    location.address = address2pdok(loc.address);
   }
 
   return location;
 }
+
+export const pdok2address = pdokAddress => {
+  const {
+    straatnaam,
+    huisnummer,
+    huisletter,
+    huisnummertoevoeging,
+    postcode,
+    woonplaatsnaam,
+  } = pdokAddress;
+
+  return {
+    openbare_ruimte: straatnaam,
+    huisnummer: `${huisnummer}`,
+    huisletter: huisletter || '',
+    huisnummer_toevoeging: huisnummertoevoeging || '',
+    postcode,
+    woonplaats: woonplaatsnaam,
+  };
+};
+
+export const address2pdok = address => {
+  const {
+    openbare_ruimte,
+    huisnummer,
+    huisletter,
+    huisnummer_toevoeging,
+    postcode,
+    woonplaats,
+  } = address;
+
+  return {
+    straatnaam: openbare_ruimte,
+    huisnummer: `${huisnummer}`,
+    huisletter: huisletter || '',
+    huisnummertoevoeging: huisnummer_toevoeging || '',
+    postcode,
+    woonplaatsnaam: woonplaats,
+  };
+};
+
+/**
+ * converts the geocoder location in sia fromat
+ */
+export const getLocation = loc => {
+  const location = {};
+
+  if (loc.location) {
+    location.geometrie = location2feature(loc.location);
+  }
+
+  if (loc.buurtcode) {
+    location.buurt_code = loc.buurtcode;
+  }
+
+  if (loc.stadsdeel) {
+    location.stadsdeel = loc.stadsdeelcode;
+  }
+
+  if (loc.address) {
+    location.address = pdok2address(loc.address);
+  }
+
+  return location;
+};
+
+export const formatAddress = address => {
+  const toevoeging = address.huisnummer_toevoeging
+    ? `-${address.huisnummer_toevoeging}`
+    : '';
+  const display = address.openbare_ruimte
+    ? `${address.openbare_ruimte} ${address.huisnummer}${address.huisletter}${toevoeging}, ${address.postcode} ${address.woonplaats}`
+    : '_';
+  return display;
+};
 
 export default mapLocation;
