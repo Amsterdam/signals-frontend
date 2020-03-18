41f9a613a1c44e7bfd718d88bf778f92c8e1742f
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-03-18 14:30:00 +0100

    renders the marker only when there is a location passed to the map

--- src/components/MapEditor/index.js
+++ src/components/MapEditor/index.js
@@ -1,4 +1,4 @@
-import React, { memo , useState, useEffect } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Map as MapComponent, Marker } from '@datapunt/react-maps';
 import { ViewerContainer } from '@datapunt/asc-ui';
@@ -7,7 +7,6 @@ import styled from '@datapunt/asc-core';
 import BackgroundLayer from 'components/BackgroundLayer';
 import { markerIcon } from 'shared/services/configuration/map-markers';
 import { feature2location } from 'shared/services/map-location';
-
 
 const MapWrapper = styled.div`
   position: relative;
@@ -27,7 +26,6 @@ const Map = ({ location, options }) => {
 
   useEffect(() => {
     if (!marker || !location.geometrie) return;
-
     const opacity = 1;
     const latlng = feature2location(location.geometrie);
     marker.setLatLng(latlng);
@@ -38,24 +36,25 @@ const Map = ({ location, options }) => {
     <MapWrapper>
       <StyledMap options={options}>
         <StyledViewerContainer bottomRight={<Zoom />} />
-        <Marker
-          setInstance={setMarker}
-          args={[{
-            lat: 0,
-            lng: 0,
-          }]}
-          options={{
-            icon: markerIcon,
-            opacity: 0,
-          }}
-        />
+        {location && location.geometrie && (
+          <Marker
+            setInstance={setMarker}
+            args={[{
+              lat: 0,
+              lng: 0,
+            }]}
+            options={{
+              icon: markerIcon,
+              opacity: 0,
+            }}
+          />
+        )}
 
         <BackgroundLayer />
       </StyledMap>
     </MapWrapper>
   );
 };
-
 
 Map.propTypes = {
   location: PropTypes.shape({
@@ -72,7 +71,8 @@ Map.propTypes = {
       woonplaats: PropTypes.string,
     }),
   }).isRequired,
-  options: PropTypes.shape({ }).isRequired, /** leaflet options, See `https://leafletjs.com/reference-1.6.0.html#map-option` */
+  options: PropTypes.shape({})
+    .isRequired /** leaflet options, See `https://leafletjs.com/reference-1.6.0.html#map-option` */,
 };
 
 export default memo(Map);
