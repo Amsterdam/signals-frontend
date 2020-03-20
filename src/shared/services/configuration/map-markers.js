3a942797d0148942d33aac1ceab35550901cc51e
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-03-20 11:53:15 +0100

    moves the background layer to the MapEditor. Improves the tests

--- src/shared/services/configuration/map-markers.js
+++ src/shared/services/configuration/map-markers.js
@@ -1,14 +1,16 @@
 /* eslint-disable global-require */
 import L from 'leaflet';
 
-export const smallMarkerIcon = global.window.L.icon({
+export const smallMarkerIcon = L.icon({
   iconUrl: 'https://map.data.amsterdam.nl/dist/images/svg/marker.svg',
   iconSize: [20, 20],
   iconAnchor: [10, 19],
+  className: 'sia-marker-small',
 });
 
 export const markerIcon = L.icon({
   iconUrl: 'https://map.data.amsterdam.nl/dist/images/svg/marker.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 39],
+  className: 'sia-map-marker',
 });
