3f80b7afebb46469e9f96041e5447ac6a3dd1db8
JJM van Deursen <janjaap@users.noreply.github.com>
2020-03-25 09:59:16 +0100

    Reverts unrelated change

--- src/signals/incident-management/components/CheckboxList/index.js
+++ src/signals/incident-management/components/CheckboxList/index.js
@@ -139,9 +139,7 @@ const CheckboxList = ({
    * @returns {(Object|undefined)}
    */
   const getOption = useCallback(
-    // id is always a string, because it comes from an HTML data- attribute
-    // cast the comparing value to a string to make sure that we're comparing the same things
-    id => options.find(option => `${option.id}` === id || `${option.key}` === id),
+    id => options.find(option => option.id === id || option.key === id),
     [options]
   );
 
