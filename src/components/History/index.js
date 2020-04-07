778c6bab6928e0475f3abceb46f8acfc00718f0e
JJM van Deursen <janjaap@users.noreply.github.com>
2020-04-07 16:36:23 +0200

    Removes styled references

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@datapunt/asc-core';
+import styled from 'styled-components';
 import { themeColor, Heading, styles, themeSpacing } from '@datapunt/asc-ui';
 
 import { string2date, string2time } from 'shared/services/string-parser/string-parser';
