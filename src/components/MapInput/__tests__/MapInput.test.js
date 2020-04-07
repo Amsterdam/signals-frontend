60901dbdd06a8a29f4f3127cac2accc323eb13db
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-04-07 13:27:17 +0200

    adds unittests for MapInput

--- src/components/MapInput/__tests__/MapInput.test.js
+++ src/components/MapInput/__tests__/MapInput.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import MapContext from 'containers/MapContext';
 
 import MAP_OPTIONS from 'shared/services/configuration/map-options';
-import MapInput from "..";
+import MapInput from '..';
+
+const withMapContext = Component => <MapContext>{Component}</MapContext>;
 
 describe('components/MapInput', () => {
   const testLocation = {
@@ -12,11 +15,45 @@ describe('components/MapInput', () => {
     },
   };
 
-  it('should render the map', () => {
-    const { getByTestId } = render(<MapInput mapOptions={MAP_OPTIONS} value={testLocation}/>);
+  it('should render the map and the autosuggest', () => {
+    const { getByTestId } = render(withMapContext(<MapInput mapOptions={MAP_OPTIONS} value={testLocation} />));
 
     // Map
     expect(getByTestId('map-input')).toBeInTheDocument();
+    expect(getByTestId('autoSuggest')).toBeInTheDocument();
   });
 
+  it.only('should fill in the provided location/address value', () => {
+    const location = {
+      lat: 52.36058599633851,
+      lng: 4.894292258032637,
+    };
+    const value = {
+      location,
+      addressText: 'Nieuwe Looiersstr 47, 1017VB Amsterdam',
+      address: {
+        postcode: '1017VB',
+        huisletter: '',
+        huisnummer: '47',
+        woonplaats: 'Amsterdam',
+        openbare_ruimte: 'Nieuwe Looiersstr',
+        huisnummertoevoeging: '',
+      },
+    };
+    const mapOptions = {
+      ...MAP_OPTIONS,
+      center: [location.lat, location.lng],
+    };
+
+    const { container, getByTestId, debug } = render(
+      withMapContext(<MapInput mapOptions={mapOptions} value={value} />)
+    );
+    debug();
+
+    const inputElement = getByTestId('autoSuggest').querySelector('input');
+    expect(inputElement.value).toEqual(value.addressText);
+
+    // Marker
+    expect(container.querySelector('.sia-map-marker')).toBeInTheDocument();
+  });
 });
