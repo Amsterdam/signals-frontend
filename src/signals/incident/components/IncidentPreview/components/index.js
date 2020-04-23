a6c775cb1f4c5b44f14c8f41e71abf3d894f05b9
JJM van Deursen <janjaap@users.noreply.github.com>
2020-04-23 11:34:18 +0200

    [SIG-2589] IncidentPreview components cleanup

--- src/signals/incident/components/IncidentPreview/components/index.js
+++ src/signals/incident/components/IncidentPreview/components/index.js
@@ -1,19 +1,13 @@
-import PlainText from './PlainText';
 import DateTime from './DateTime';
 import Image from './Image';
-import ObjectValue from './ObjectValue';
 import Map from './Map';
-import CommaArray from './CommaArray';
 import MapSelectPreview from './MapSelect';
 import ListObjectValue from './ListObjectValue';
 
 export default {
-  CommaArray,
-  PlainText,
   DateTime,
   ListObjectValue,
   Image,
-  ObjectValue,
   Map,
   MapSelectPreview,
 };
