af8f4a93a278803b8993131252aab7db5b26a3a1
JJM van Deursen <janjaap@users.noreply.github.com>
2020-04-28 10:51:46 +0200

    Includes settings vars

--- src/hooks/useConfiguration.js
+++ src/hooks/useConfiguration.js
@@ -1,21 +1,9 @@
-const useConfiguration = () => {
-  const API_ROOT = window.API_ROOT;
 
-  return {
-    SITE: {
-      LANG: 'nl',
-      MOBILE_WEB_APP_TITLE: '',
-      TITLE: 'SIA (Signalen Informatievoorziening Amsterdam)',
-    },
-    ENDPOINTS: {
-      USERS: `${API_ROOT}signals/v1/private/users/`,
-    },
-    MAPS: {
-      GEMEENTE_NAAM: window.GEMEENTE_NAAM,
-      BOUNDS: window.BOUNDS,
-      LOCK_TO_BOUNDS: true,
-    },
-  };
-};
+
+const useConfiguration = () => ({
+  // should get custom validation (TS?)
+  urls: global.config.urls,
+  maps:  global.config.maps,
+});
 
 export default useConfiguration;
