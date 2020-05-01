ef4b33b87b31d9c4166df4009bce6d2693e7f70d
JJM van Deursen <janjaap@users.noreply.github.com>
2020-05-01 13:23:11 +0200

    Adds keyboard navigation for attachments

--- src/signals/incident-management/containers/IncidentDetail/components/AttachmentViewer/index.js
+++ src/signals/incident-management/containers/IncidentDetail/components/AttachmentViewer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -43,6 +43,35 @@ const AttachmentViewer = ({ href, attachments, onShowAttachment }) => {
   const index = attachments.findIndex(item => item.location === href);
   const previous = index > 0 ? attachments[index - 1].location : false;
   const next = index < attachments.length - 1 ? attachments[index + 1].location : false;
+
+  const handleKeyDown = useCallback(
+    event => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          if (previous) onShowAttachment(previous);
+          break;
+
+        case 'ArrowRight':
+          if (next) onShowAttachment(next);
+          break;
+
+        default:
+          break;
+      }
+    },
+    [next, previous, onShowAttachment]
+  );
+
+  /**
+   * Register and unregister listeners
+   */
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
   return (
     <Wrapper>
