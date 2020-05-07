6f82c63562070c0ec9596504e61e42558999f746
JJM van Deursen <janjaap@users.noreply.github.com>
2020-05-07 13:37:10 +0200

    Adds Proxy polyfill

--- src/shared/services/configuration/configuration.js
+++ src/shared/services/configuration/configuration.js
@@ -1,8 +1,11 @@
+import ProxyPolyfillBuilder from 'proxy-polyfill/src/proxy';
+const ProxyPolyfill = ProxyPolyfillBuilder();
+
 const applicationConfig = {
   ...(window.CONFIG || {}),
 };
 
-export default new Proxy(applicationConfig, {
+export default new ProxyPolyfill(applicationConfig, {
   get(target, name, receiver) {
     if (!Reflect.has(target, name)) {
       throw new Error(`Setting ${name} not available in configuration`);
