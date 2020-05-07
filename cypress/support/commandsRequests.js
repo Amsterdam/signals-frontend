7e93bc0c7a1712959328a59e60cf049ba7e1a299
Jasper Poppe <jgpoppe@gmail.com>
2020-05-07 10:05:18 +0200

    (Chore) added and applied eslint rule: quote-props

--- cypress/support/commandsRequests.js
+++ cypress/support/commandsRequests.js
@@ -4,22 +4,22 @@ export const createSignalOverviewMap = () => {
     url: 'http://localhost:8000/signals/v1/public/signals/',
     headers: { 'Content-Type': 'application/json' },
     body: {
-      "location": {
-        "geometrie": { "type": "Point", "coordinates": [4.89089949, 52.37316397] },
-        "address": {
-          "openbare_ruimte": "Nieuwezijds Voorburgwal",
-          "huisnummer": "147",
-          "postcode": "1012RJ",
-          "woonplaats": "Amsterdam",
+      location: {
+        geometrie: { type: "Point", coordinates: [4.89089949, 52.37316397] },
+        address: {
+          openbare_ruimte: "Nieuwezijds Voorburgwal",
+          huisnummer: "147",
+          postcode: "1012RJ",
+          woonplaats: "Amsterdam",
         },
-        "stadsdeel": "A",
+        stadsdeel: "A",
       },
-      "category": {
-        "sub_category": "http://localhost:8000/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/overig-openbare-ruimte",
+      category: {
+        sub_category: "http://localhost:8000/signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/overig-openbare-ruimte",
       },
-      "reporter": {},
-      "incident_date_start": "2020-04-16T14:06:31+02:00",
-      "text": "Er staat een paard in de gang, ja ja een paard in de gang.",
+      reporter: {},
+      incident_date_start: "2020-04-16T14:06:31+02:00",
+      text: "Er staat een paard in de gang, ja ja een paard in de gang.",
     },
   });
 };
@@ -30,22 +30,22 @@ export const createSignalFilters = () => {
     url: 'http://localhost:8000/signals/v1/public/signals/',
     headers: { 'Content-Type': 'application/json' },
     body: {
-      "location": {
-        "geometrie": { "type": "Point", "coordinates": [4.74948222, 52.41019044] },
-        "address": {
-          "openbare_ruimte": "Ruigoord",
-          "huisnummer": "36",
-          "postcode": "1047HH",
-          "woonplaats": "Amsterdam",
+      location: {
+        geometrie: { type: "Point", coordinates: [4.74948222, 52.41019044] },
+        address: {
+          openbare_ruimte: "Ruigoord",
+          huisnummer: "36",
+          postcode: "1047HH",
+          woonplaats: "Amsterdam",
         },
-        "stadsdeel": "B",
+        stadsdeel: "B",
       },
-      "category": {
-        "sub_category": "http://localhost:8000/signals/v1/public/terms/categories/afval/sub_categories/overig-afval",
+      category: {
+        sub_category: "http://localhost:8000/signals/v1/public/terms/categories/afval/sub_categories/overig-afval",
       },
-      "reporter": {},
-      "incident_date_start": "2020-04-21T09:17:15+02:00",
-      "text": "We gaan filteren!",
+      reporter: {},
+      incident_date_start: "2020-04-21T09:17:15+02:00",
+      text: "We gaan filteren!",
     },
   });
 };
