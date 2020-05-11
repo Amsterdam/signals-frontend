98d5472ef2c607408b01747fd9ebef882195224d
Arjan <>
2020-05-11 08:28:03 +0200

    Cypress tests for SIG-2380, change signal after submit

--- cypress/support/selectorsSignalDetails.js
+++ cypress/support/selectorsSignalDetails.js
@@ -1,9 +1,55 @@
 // General selectors for signal details
+export const CHANGE_CATEGORY = {
+  inputCategory: '[data-testid=input]',
+};
+
+export const CHANGE_LOCATION = {
+  buttonCancelEditLocation: '[data-testid=location-form-button-cancel]',
+  buttonEditLocation: '[class*=Location__Description] > [data-testid=editButton]',
+  buttonSubmitEditLocation: '[data-testid=location-form-button-submit]',
+};
+
+export const CHANGE_STATUS = {
+  buttonCancelEditStatus: '[data-testid=statusFormCancelButton]',
+  buttonCloseEditStatus: '[class*="incident-detail__preview-close"]',
+  buttonEditStatus: '[class*="MetaList__EditButton"]',
+  buttonSubmitStatus: '[data-testid=statusFormSubmitButton]',
+  currentStatus: '#currentStatus',
+  inputToelichting: '[data-testid=text]',
+  radioButtonGemeld: '[data-testid=status-m]',
+  radioButtonInAfwachting: '[data-testid=status-i]',
+  radioButtonIngepland: '[data-testid=status-ingepland]',
+  radioButtonInBehandeling: '[data-testid=status-b]',
+  radioButtonExtern: '[data-testid="status-closure requested"]',
+  radioButtonAfgehandeld: '[data-testid=status-o]',
+  radioButtonHeropend: '[data-testid=status-reopened]',
+  radioButtonGeannuleerd: '[data-testid=status-a]',
+};
+
+export const CHANGE_TYPE = {
+  radioButtonMelding: '[data-testid=input-SIG]',
+  radioButtonAanvraag: '[data-testid=input-REQ]',
+  radioButtonVraag: '[data-testid=input-QUE]',
+  radioButtonKlacht: '[data-testid=input-COM]',
+  radioButtonGrootOnderhoud: '[data-testid=input-MAI]',
+};
+
+export const CHANGE_URGENCY = {
+  radioButtonHoog: '[data-testid=input-high]',
+  radioButtonLaag: '[data-testid=input-low]',
+  radioButtonNormaal: '[data-testid=input-normal]',
+};
+
 export const SIGNAL_DETAILS = {
   addressCity: '[data-testid="location-value-address-city"]',
   addressStreet: '[data-testid="location-value-address-street"]',
+  buttonCancel: '[data-testid=cancelButton]',
+  buttonEdit: '[data-testid="editButton"]',
+  buttonSubmit: '[data-testid=submitButton]',
   department: '[data-testid="meta-list-department-value"]',
   email: '[data-testid="detail-email-value"]',
+  historyAction: '[class*="History__Action"]',
+  historyListItem: '[data-testid="history-list-item-description"]',
   mainCategory: '[data-testid="meta-list-main-category-value"]',
   phoneNumber: '[data-testid="detail-phone-value"]',
   source: '[data-testid="meta-list-source-value"]',
