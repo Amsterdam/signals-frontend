666006809c2a57d59563411182c1408888d158e1
JJM van Deursen <janjaap@users.noreply.github.com>
2020-05-18 15:55:19 +0200

    Removes bootstrap classes from RadioInput component

--- src/signals/incident/components/form/RadioInput/index.js
+++ src/signals/incident/components/form/RadioInput/index.js
@@ -2,13 +2,17 @@ import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import isObject from 'lodash.isobject';
-import { themeColor, RadioGroup } from '@datapunt/asc-ui';
+import { themeColor, RadioGroup, themeSpacing } from '@datapunt/asc-ui';
 
 import Header from '../Header';
 import InputUsingDispatch from './InputUsingDispatch';
 
 const Info = styled.p`
   color: ${themeColor('tint', 'level5')};
+`;
+
+const StyledHeader = styled(Header)`
+  margin-top: ${themeSpacing(5)};
 `;
 
 const RadioInput = ({ handler, touched, hasError, meta, parent, getError, validatorsOrOpts }) => {
@@ -23,35 +27,31 @@ const RadioInput = ({ handler, touched, hasError, meta, parent, getError, valida
   if (!meta?.isVisible) return null;
 
   return (
-    <div className="row">
-      <div className={`${meta.className || 'col-12'} mode_input`}>
-        <Header meta={meta} options={validatorsOrOpts} touched={touched} hasError={hasError} getError={getError}>
-          {meta.values && isObject(meta.values) && (
-            <RadioGroup name={meta.name}>
-              {Object.entries(meta.values).map(([key, value]) => (
-                <Fragment>
-                  <InputUsingDispatch
-                    checked={handler().value.id === key}
-                    id={key}
-                    idAttr={`${meta.name}-${key + 1}`}
-                    info={value.info}
-                    key={key}
-                    label={value.value || value}
-                    name={meta.name}
-                    resetsStateOnChange={meta.resetsStateOnChange}
-                  />
-                  {info && (
-                    <Info>
-                      {label}: {info}
-                    </Info>
-                  )}
-                </Fragment>
-              ))}
-            </RadioGroup>
-          )}
-        </Header>
-      </div>
-    </div>
+    <StyledHeader meta={meta} options={validatorsOrOpts} touched={touched} hasError={hasError} getError={getError}>
+      {meta.values && isObject(meta.values) && (
+        <RadioGroup name={meta.name}>
+          {Object.entries(meta.values).map(([key, value]) => (
+            <Fragment>
+              <InputUsingDispatch
+                checked={handler().value.id === key}
+                id={key}
+                idAttr={`${meta.name}-${key + 1}`}
+                info={value.info}
+                key={key}
+                label={value.value || value}
+                name={meta.name}
+                resetsStateOnChange={meta.resetsStateOnChange}
+              />
+              {info && (
+                <Info>
+                  {label}: {info}
+                </Info>
+              )}
+            </Fragment>
+          ))}
+        </RadioGroup>
+      )}
+    </StyledHeader>
   );
 };
 
