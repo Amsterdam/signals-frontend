7ad400824760c76fb89e7a3ee2c8c17c8f6898d9
JJM van Deursen <janjaap@users.noreply.github.com>
2020-05-27 15:37:24 +0200

    (Chore) amsterdam-stijl Removal helper components

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,6 @@ import { Button as AscButton } from '@datapunt/asc-ui';
 
 const Button = styled(AscButton)`
   font-family: Avenir Next LT W01 Demi, arial, sans-serif;
-  font-weight: 400;
 `;
 
 export default Button;
