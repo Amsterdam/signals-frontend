44d098d19e5370f98f4cb3f4fc8fd2075fe1d21f
Boris Arkenaar <boris@arkenaar.com>
2020-06-03 18:41:42 +0200

    Update logo size and link

--- src/components/Logo/__tests__/Logo.test.js
+++ src/components/Logo/__tests__/Logo.test.js
@@ -11,7 +11,10 @@ jest.mock('shared/services/configuration/configuration');
 describe('components/Logo', () => {
   beforeEach(() => {
     configuration.logoUrl = 'https://logoipsum.com/logo/logo-8.svg';
+    configuration.logoWidth = 80;
     configuration.logoHeight = 80;
+    configuration.logoWidthSmall = 40;
+    configuration.logoHeightSmall = 40;
   });
 
   afterEach(() => {
@@ -24,6 +27,25 @@ describe('components/Logo', () => {
     expect(container.querySelector(`a[href="${configuration.links.home}"]`)).toBeInTheDocument();
     expect(container.querySelector(`img[src="${configuration.logoUrl}"]`)).toBeInTheDocument();
     expect(container.querySelector(`img[height="${configuration.logoHeight}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[width="${configuration.logoWidth}"]`)).toBeInTheDocument();
+  });
+
+  it('should render the same when tall', () => {
+    const { container } = render(withAppContext(<Logo tall />));
+
+    expect(container.querySelector(`a[href="${configuration.links.home}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[src="${configuration.logoUrl}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[height="${configuration.logoHeight}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[width="${configuration.logoWidth}"]`)).toBeInTheDocument();
+  });
+
+  it('should render differently when not tall', () => {
+    const { container } = render(withAppContext(<Logo tall={false} />));
+
+    expect(container.querySelector(`a[href="/"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[src="${configuration.logoUrl}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[height="${configuration.logoHeightSmall}"]`)).toBeInTheDocument();
+    expect(container.querySelector(`img[width="${configuration.logoWidthSmall}"]`)).toBeInTheDocument();
   });
 
   it('should render extra props', () => {
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import configuration from 'shared/services/configuration/configuration';
 
 export const Logo = props => (
-  <a href={configuration.links.home}>
-    <img alt="Logo" src={configuration.logoUrl} height={configuration.logoHeight} {...props} />
+  <a href={props.tall ? configuration.links.home : '/'}>
+    <img
+      alt="Logo"
+      src={configuration.logoUrl}
+      width={props.tall ? configuration.logoWidth : configuration.logoWidthSmall}
+      height={props.tall ? configuration.logoHeight : configuration.logoHeightSmall}
+      {...props}
+    />
   </a>
 );
+
+Logo.defaultProps = {
+  tall: true,
+};
+
+Logo.propTypes = {
+  tall: PropTypes.bool,
+};
 
 export default Logo;
