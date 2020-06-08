35e0867970ed09509de7bc082bd0f6ad7e111f80
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-06-08 14:57:31 +0200

    Sets BABEL_ENV for multiple script commands

--- babel.config.js
+++ babel.config.js
@@ -1,43 +1,44 @@
-module.exports = {
-  plugins: [
-    'styled-components',
-    '@babel/plugin-proposal-class-properties',
-    [
-      'inline-react-svg',
-      {
-        ignorePattern: '.*marker\\.svg',
-      },
-    ],
-    '@babel/plugin-syntax-dynamic-import',
-  ],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        modules: false,
-        useBuiltIns: 'usage',
-        targets: {
-          esmodules: true,
+module.exports = api => {
+  const presetEnv =
+    api.env() === 'lint'
+      ? ''
+      : [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'usage',
+          targets: {
+            esmodules: true,
+          },
+          corejs: 3,
         },
-        corejs: 3,
-      },
+      ];
+
+  return {
+    plugins: [
+      'styled-components',
+      '@babel/plugin-proposal-class-properties',
+      [
+        'inline-react-svg',
+        {
+          ignorePattern: '.*marker\\.svg',
+        },
+      ],
+      '@babel/plugin-syntax-dynamic-import',
     ],
-    '@babel/preset-react',
-  ],
-  env: {
-    production: {
-      only: ['src'],
-      plugins: [
-        'transform-react-remove-prop-types',
-        '@babel/plugin-transform-react-inline-elements',
-        '@babel/plugin-transform-react-constant-elements',
-      ],
+    presets: [presetEnv, '@babel/preset-react'].filter(Boolean),
+    env: {
+      production: {
+        only: ['src'],
+        plugins: [
+          'transform-react-remove-prop-types',
+          '@babel/plugin-transform-react-inline-elements',
+          '@babel/plugin-transform-react-constant-elements',
+        ],
+      },
+      test: {
+        plugins: ['@babel/plugin-transform-modules-commonjs', 'dynamic-import-node'],
+      },
     },
-    test: {
-      plugins: [
-        '@babel/plugin-transform-modules-commonjs',
-        'dynamic-import-node',
-      ],
-    },
-  },
+  };
 };
