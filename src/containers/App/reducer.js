36544ad2a03596efd1a9a8525cb1197e99a7702f
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-06-15 09:11:46 +0200

    squashes common functionality for resetting the searchQuery

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -2,7 +2,7 @@ import { fromJS } from 'immutable';
 
 import { VARIANT_DEFAULT, TYPE_DEFAULT } from 'containers/Notification/constants';
 
-import { APPLY_FILTER } from "signals/incident-management/constants";
+import { APPLY_FILTER } from 'signals/incident-management/constants';
 import {
   LOGIN_FAILED,
   LOGOUT_FAILED,
@@ -81,8 +81,6 @@ function appReducer(state = initialState, action) {
       return state.set('searchQuery', action.payload);
 
     case RESET_SEARCH_QUERY:
-      return state.set('searchQuery', initialState.get('searchQuery'));
-
     case APPLY_FILTER:
       return state.set('searchQuery', initialState.get('searchQuery'));
 
