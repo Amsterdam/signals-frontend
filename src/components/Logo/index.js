9c4a1789212d54dbf25015f7effbef2d9905fad2
Jasper Poppe <jgpoppe@gmail.com>
2020-06-24 15:27:05 +0200

    fixed logo alignment

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import styled from 'styled-components';
+
 import configuration from 'shared/services/configuration/configuration';
+
+const StyledLogo = styled.img`
+  width: ${props => props.tall === true ? configuration.logo.width : configuration.logo.smallWidth};
+  height: ${props => props.tall === true ? configuration.logo.height : configuration.logo.smallHeight};
+  max-height: ${props => props.tall === true ? '56px' : '32px'};
+`;
 
 export const Logo = ({ tall, ... props }) => (
   <a href={tall ? configuration.links.home : '/'}>
-    <img
-      alt="Logo"
-      src={configuration.logo.url}
-      width={tall ? configuration.logo.width : configuration.logo.smallWidth}
-      height={tall ? configuration.logo.height : configuration.logo.smallHeight}
-      {...props}
-    />
+    <StyledLogo alt="Logo" tall={tall} src={configuration.logo.url} {...props} />
   </a>
 );
 
