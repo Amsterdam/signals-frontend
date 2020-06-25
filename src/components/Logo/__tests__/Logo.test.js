175f47e234a6bb8b78e0fd14cac187218f42dc0c
Jasper Poppe <jgpoppe@gmail.com>
2020-06-25 13:17:43 +0200

    use values which are more related to the real world

--- src/components/Logo/__tests__/Logo.test.js
+++ src/components/Logo/__tests__/Logo.test.js
@@ -13,10 +13,10 @@ jest.mock('shared/services/configuration/configuration');
 describe('components/Logo', () => {
   beforeEach(() => {
     configuration.logo.url = 'https://logoipsum.com/logo/logo-8.svg';
-    configuration.logo.width = 80;
-    configuration.logo.height = 80;
-    configuration.logo.smallWidth = 40;
-    configuration.logo.smallHeight = 40;
+    configuration.logo.width = '80px';
+    configuration.logo.height = '80px';
+    configuration.logo.smallWidth = '20%';
+    configuration.logo.smallHeight = '40%';
   });
 
   afterEach(() => {
@@ -24,15 +24,6 @@ describe('components/Logo', () => {
   });
 
   it('should render correctly', () => {
-    const { container, getByTestId } = render(withAppContext(<Logo />));
-
-    expect(container.querySelector(`a[href="${configuration.links.home}"]`)).toBeInTheDocument();
-    expect(container.querySelector(`img[src="${configuration.logo.url}"]`)).toBeInTheDocument();
-    expect(getByTestId('logo')).toHaveStyleRule('height', configuration.logo.height.toString());
-    expect(getByTestId('logo')).toHaveStyleRule('width', configuration.logo.width.toString());
-  });
-
-  it('should render the same when tall', () => {
     const { container, getByTestId } = render(withAppContext(<Logo />));
 
     expect(container.querySelector(`a[href="${configuration.links.home}"]`)).toBeInTheDocument();
