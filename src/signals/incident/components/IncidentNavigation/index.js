8ebb90a058a04cef2faacdda8d7af3f7fe1c992a
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-07-06 16:05:03 +0200

    updates the react-reactive-form to the last version

--- src/signals/incident/components/IncidentNavigation/index.js
+++ src/signals/incident/components/IncidentNavigation/index.js
@@ -16,41 +16,44 @@ const IncidentNavigation = ({ controls, meta: { wizard, submitting, handleSubmit
   const hideSubmit = controls?.hide_navigation_buttons?.meta ? controls.hide_navigation_buttons.meta.isVisible : false;
 
   return (
-    <WithWizard
-      render={({ next, previous, step }) => {
-        const currentStep = (step && step.id && step.id.split('/').pop()) || 0;
-        const wizardStep = currentStep && wizard[currentStep];
+    (wizard && (
+      <WithWizard
+        render={({ next, previous, step }) => {
+          const currentStep = (step && step.id && step.id.split('/').pop()) || 0;
+          const wizardStep = currentStep && wizard[currentStep];
 
-        return (
-          wizardStep && (
-            <div className="incident-navigation">
-              {!hideSubmit && wizardStep.previousButtonLabel ? (
-                <PreviousButton
-                  className={wizardStep.previousButtonClass}
-                  onClick={previous}
-                  data-testid="previousButton"
-                >
-                  {wizardStep.previousButtonLabel}
-                </PreviousButton>
-              ) : (
-                <span />
-              )}
+          return (
+            wizardStep && (
+              <div className="incident-navigation">
+                {!hideSubmit && wizardStep.previousButtonLabel ? (
+                  <PreviousButton
+                    className={wizardStep.previousButtonClass}
+                    onClick={previous}
+                    data-testid="previousButton"
+                  >
+                    {wizardStep.previousButtonLabel}
+                  </PreviousButton>
+                ) : (
+                  <span />
+                )}
 
-              {!hideSubmit && wizardStep.nextButtonLabel && (
-                <NextButton onClick={e => handleSubmit(e, next, wizardStep.formAction)} data-testid="nextButton">
-                  <span className="value">{wizardStep.nextButtonLabel}</span>
-                  {submitting && (
-                    <span className="working">
-                      <div className="progress-indicator progress-white"></div>
-                    </span>
-                  )}
-                </NextButton>
-              )}
-            </div>
-          )
-        );
-      }}
-    />
+                {!hideSubmit && wizardStep.nextButtonLabel && (
+                  <NextButton onClick={e => handleSubmit(e, next, wizardStep.formAction)} data-testid="nextButton">
+                    <span className="value">{wizardStep.nextButtonLabel}</span>
+                    {submitting && (
+                      <span className="working">
+                        <div className="progress-indicator progress-white"></div>
+                      </span>
+                    )}
+                  </NextButton>
+                )}
+              </div>
+            )
+          );
+        }}
+      />
+    )) ||
+    null
   );
 };
 
