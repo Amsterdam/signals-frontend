b2c778ed627a36ba1cb49d188904c30b63562f51
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-07-06 11:34:38 +0200

    fixes the unittest of the Footer

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { withAppContext } from 'test/utils';
+import configuration from 'shared/services/configuration/configuration';
 
 import Footer from './index';
 
 describe('<Footer />', () => {
   it('should render correctly', () => {
+    configuration.links.privacy = 'https://www.amsterdam.nl/privacy/';
     const { container, getByTestId } = render(
       withAppContext(<Footer />)
     );
