89d8d40c7b13064c4c511be50a1dad0ff52e07fc
Jasper Poppe <jgpoppe@gmail.com>
2020-07-08 18:10:58 +0200

    autofix unicorn/throw-new-error

--- src/shared/services/map-location/index.js
+++ src/shared/services/map-location/index.js
@@ -12,7 +12,7 @@ export const featureTolocation = ({ coordinates }) => ({
 
 export const wktPointToLocation = wktPoint => {
   if (!wktPoint.includes('POINT')) {
-    throw TypeError('Provided WKT geometry is not a point.');
+    throw new TypeError('Provided WKT geometry is not a point.');
   }
   const coordinate = wktPoint.split('(')[1].split(')')[0];
   const lat = parseFloat(coordinate.split(' ')[1]);
