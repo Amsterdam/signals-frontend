93dfa44965c485c7023c8cf16187ab44caf743cc
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-07-14 13:42:21 +0200

    Removes incident model

--- src/models/incident/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { initialState } from './reducer';
-
-const selectIncidentDomain = state => state.get('incidentModel') || initialState;
-
-const makeSelectIncidentModel = createSelector(selectIncidentDomain, substate => substate.toJS());
-
-export default makeSelectIncidentModel;
-export { selectIncidentDomain };
