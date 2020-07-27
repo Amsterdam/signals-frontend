0a3c5f794919007b968f5f58fa60027023a32f61
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-07-27 15:15:39 +0200

    updates the status warnings

--- src/signals/incident-management/definitions/statusList.js
+++ src/signals/incident-management/definitions/statusList.js
@@ -3,39 +3,46 @@ const GEMELD = {
   value: 'Gemeld',
   color: 'red',
 };
+
 const AFWACHTING = {
   key: 'i',
   value: 'In afwachting van behandeling',
-  warning: 'De melder ontvangt deze toelichting niet.',
   color: 'purple',
 };
+
 const BEHANDELING = {
   key: 'b',
   value: 'In behandeling',
-  warning: 'De melder ontvangt deze toelichting niet, maar kan die wel opvragen door te bellen.',
   color: 'blue',
 };
+
 const AFGEHANDELD = {
   key: 'o',
   value: 'Afgehandeld',
-  warning: 'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding. Gebruik deze status alleen als de melding ook echt is afgehandeld, gebruik anders de status Ingepland. Let op: als de huidige status “Verzoek tot heropenen” is, dan wordt er geen e-mail naar de melder gestuurd.',
+  warning:
+    'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding. Gebruik deze status alleen als de melding ook echt is afgehandeld, gebruik anders de status Ingepland. Let op: als de huidige status “Verzoek tot heropenen” is, dan wordt er geen e-mail naar de melder gestuurd.',
   color: 'lightgreen',
 };
+
 const GESPLITST = {
   key: 's',
   value: 'Gesplitst',
   color: 'lightgreen',
 };
+
 const INGEPLAND = {
   key: 'ingepland',
   value: 'Ingepland',
-  warning: 'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting.',
+  warning:
+    'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting.',
   color: 'grey',
 };
+
 const GEANNULEERD = {
   key: 'a',
   value: 'Geannuleerd',
-  warning: 'Bij deze status wordt de melding afgesloten en er wordt GEEN bericht naar de melder gestuurd. Gebruik deze status alleen voor test- en nepmeldingen of meldingen van veelmelders.',
+  warning:
+    'Bij deze status wordt de melding afgesloten. Gebruik deze status alleen voor test- en nepmeldingen of meldingen van veelmelders.',
   color: 'darkgrey',
 };
 
@@ -48,27 +55,31 @@ const VERZOEK_TOT_HEROPENEN = {
 const HEROPEND = {
   key: 'reopened',
   value: 'Heropend',
-  warning: 'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding.',
+  warning:
+    'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding.',
   color: 'orange',
 };
+
 const TE_VERZENDEN = {
   key: 'ready to send',
   value: 'Extern: te verzenden',
 };
+
 const VERZONDEN = {
   key: 'sent',
   value: 'Extern: verzonden',
 };
+
 const VERZENDEN_MISLUKT = {
   key: 'send failed',
   value: 'Extern: mislukt',
 };
+
 const VERZOEK_TOT_AFHANDELING = {
   key: 'closure requested',
   value: 'Extern: verzoek tot afhandeling',
-  warning: 'De melder ontvangt deze toelichting niet, maar kan die wel opvragen door te bellen.',
-
 };
+
 const AFGEHANDELD_EXTERN = {
   key: 'done external',
   value: 'Extern: afgehandeld',
@@ -104,8 +115,4 @@ export const changeStatusOptionList = [
   GEANNULEERD,
 ];
 
-export const defaultTextsOptionList = [
-  AFGEHANDELD,
-  INGEPLAND,
-  HEROPEND,
-];
+export const defaultTextsOptionList = [AFGEHANDELD, INGEPLAND, HEROPEND];
