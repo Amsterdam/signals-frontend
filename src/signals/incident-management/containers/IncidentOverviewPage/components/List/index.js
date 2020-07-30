167f9f41595af381f6ff0c80cd5f64592e716ac1
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-07-30 15:28:21 +0200

    [SIG-3013] Incidents list class => function

--- src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
@@ -1,91 +1,137 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import parseISO from 'date-fns/parseISO';
 import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
+
 import { string2date, string2time } from 'shared/services/string-parser';
 import { getListValueByKey } from 'shared/services/list-helper/list-helper';
 import * as types from 'shared/types';
+import { statusList } from 'signals/incident-management/definitions';
 
 import './style.scss';
 
-class List extends React.Component {
-  onSort = sort => () => {
-    const sortIsAsc = this.props.sort && this.props.sort.indexOf(sort) === 0;
+const List = ({ incidents, onChangeOrdering, priority, sort, stadsdeel, status }) => {
+  const onSort = useCallback(
+    srt => () => {
+      const sortIsAsc = sort && sort.indexOf(srt) === 0;
 
-    this.props.onChangeOrdering(sortIsAsc ? `-${sort}` : sort);
-  }
+      onChangeOrdering(sortIsAsc ? `-${srt}` : srt);
+    },
+    [onChangeOrdering, sort]
+  );
 
-  getDaysOpen(incident) {
-    const statusesWithoutDaysOpen = ['o', 'a', 's', 'reopen requested'];
+  const getDaysOpen = useCallback(incident => {
+    const statusesWithoutDaysOpen = statusList
+      .filter(({ shows_remaining_sla_days }) => shows_remaining_sla_days === false)
+      .map(({ key }) => key);
+
     if (incident.status && !statusesWithoutDaysOpen.includes(incident.status.state)) {
       const start = parseISO(incident.created_at);
       return -differenceInCalendarDays(start, new Date());
     }
 
     return '-';
-  }
+  }, []);
 
-  sortClassName(sortName) {
-    let className = '';
-    const currentSort = this.props.sort && this.props.sort.split(',')[0];
-    if (currentSort && currentSort.indexOf(sortName) > -1) {
-      className = currentSort.charAt(0) === '-' ? 'sort sort-down' : 'sort sort-up';
-    }
+  const sortClassName = useCallback(
+    sortName => {
+      let className = '';
+      const currentSort = sort && sort.split(',')[0];
 
-    return className;
-  }
+      if (currentSort && currentSort.indexOf(sortName) > -1) {
+        className = currentSort.charAt(0) === '-' ? 'sort sort-down' : 'sort sort-up';
+      }
 
-  render() {
-    const { incidents, priority, status, stadsdeel } = this.props;
-    return (
-      <div className="list-component" data-testid="incidentOverviewListComponent">
-        <div className="list-component__body">
-          <table className="list-component__table" cellSpacing="0" cellPadding="0">
-            <thead>
-              <tr>
-                <th onClick={this.onSort('id')} className={this.sortClassName('id')}>Id</th>
-                <th onClick={this.onSort('days_open')} className={this.sortClassName('days_open')}>Dag</th>
-                <th onClick={this.onSort('created_at')} className={this.sortClassName('created_at')}>Datum en tijd</th>
-                <th onClick={this.onSort('stadsdeel,-created_at')} className={this.sortClassName('stadsdeel')}>Stadsdeel</th>
-                <th onClick={this.onSort('sub_category,-created_at')} className={this.sortClassName('sub_category')}>Subcategorie</th>
-                <th onClick={this.onSort('status,-created_at')} className={this.sortClassName('status')}>Status</th>
-                <th onClick={this.onSort('priority,-created_at')} className={this.sortClassName('priority')}>Urgentie</th>
-                <th onClick={this.onSort('address,-created_at')} className={this.sortClassName('address')}>Adres</th>
-              </tr>
-            </thead>
-            <tbody>
-              {incidents.map(incident => {
-                const detailLink = `/manage/incident/${incident.id}`;
-                return (
-                  <tr key={incident.id}>
-                    <td><Link to={detailLink}>{incident.id}</Link></td>
-                    <td data-testid="incidentDaysOpen"><Link to={detailLink}>{this.getDaysOpen(incident)}</Link></td>
-                    <td className="no-wrap"><Link to={detailLink}>{string2date(incident.created_at)} {string2time(incident.created_at)}</Link></td>
-                    <td><Link to={detailLink}>{getListValueByKey(stadsdeel, incident.location && incident.location.stadsdeel)}</Link></td>
-                    <td><Link to={detailLink}>{incident.category && incident.category.sub}</Link></td>
-                    <td><Link to={detailLink}>{getListValueByKey(status, incident.status && incident.status.state)}</Link></td>
-                    <td><Link to={detailLink}>{getListValueByKey(priority, incident.priority && incident.priority.priority)}</Link></td>
-                    <td><Link to={detailLink}>{incident.location && incident.location.address_text}</Link></td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+      return className;
+    },
+    [sort]
+  );
+
+  return (
+    <div className="list-component" data-testid="incidentOverviewListComponent">
+      <div className="list-component__body">
+        <table className="list-component__table" cellSpacing="0" cellPadding="0">
+          <thead>
+            <tr>
+              <th onClick={onSort('id')} className={sortClassName('id')}>
+                Id
+              </th>
+              <th onClick={onSort('days_open')} className={sortClassName('days_open')}>
+                Dag
+              </th>
+              <th onClick={onSort('created_at')} className={sortClassName('created_at')}>
+                Datum en tijd
+              </th>
+              <th onClick={onSort('stadsdeel,-created_at')} className={sortClassName('stadsdeel')}>
+                Stadsdeel
+              </th>
+              <th onClick={onSort('sub_category,-created_at')} className={sortClassName('sub_category')}>
+                Subcategorie
+              </th>
+              <th onClick={onSort('status,-created_at')} className={sortClassName('status')}>
+                Status
+              </th>
+              <th onClick={onSort('priority,-created_at')} className={sortClassName('priority')}>
+                Urgentie
+              </th>
+              <th onClick={onSort('address,-created_at')} className={sortClassName('address')}>
+                Adres
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {incidents.map(incident => {
+              const detailLink = `/manage/incident/${incident.id}`;
+              return (
+                <tr key={incident.id}>
+                  <td>
+                    <Link to={detailLink}>{incident.id}</Link>
+                  </td>
+                  <td data-testid="incidentDaysOpen">
+                    <Link to={detailLink}>{getDaysOpen(incident)}</Link>
+                  </td>
+                  <td className="no-wrap">
+                    <Link to={detailLink}>
+                      {string2date(incident.created_at)} {string2time(incident.created_at)}
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to={detailLink}>
+                      {getListValueByKey(stadsdeel, incident.location && incident.location.stadsdeel)}
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to={detailLink}>{incident.category && incident.category.sub}</Link>
+                  </td>
+                  <td>
+                    <Link to={detailLink}>{getListValueByKey(status, incident.status && incident.status.state)}</Link>
+                  </td>
+                  <td>
+                    <Link to={detailLink}>
+                      {getListValueByKey(priority, incident.priority && incident.priority.priority)}
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to={detailLink}>{incident.location && incident.location.address_text}</Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 List.propTypes = {
   incidents: PropTypes.arrayOf(types.incidentType).isRequired,
-  priority: types.dataListType.isRequired,
-  status: types.dataListType.isRequired,
-  stadsdeel: types.dataListType.isRequired,
-
   onChangeOrdering: PropTypes.func.isRequired,
+  priority: types.dataListType.isRequired,
   sort: PropTypes.string,
+  stadsdeel: types.dataListType.isRequired,
+  status: types.dataListType.isRequired,
 };
 
 export default List;
--- src/signals/incident-management/definitions/statusList.js
+++ src/signals/incident-management/definitions/statusList.js
@@ -3,6 +3,7 @@ const GEMELD = {
   value: 'Gemeld',
   color: 'red',
   can_send_email: true,
+  shows_remaining_sla_days: true,
 };
 
 const AFWACHTING = {
@@ -10,6 +11,7 @@ const AFWACHTING = {
   value: 'In afwachting van behandeling',
   color: 'purple',
   can_send_email: true,
+  shows_remaining_sla_days: true,
 };
 
 const BEHANDELING = {
@@ -17,6 +19,7 @@ const BEHANDELING = {
   value: 'In behandeling',
   color: 'blue',
   can_send_email: true,
+  shows_remaining_sla_days: true,
 };
 
 const AFGEHANDELD = {
@@ -26,6 +29,7 @@ const AFGEHANDELD = {
     'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding. Gebruik deze status alleen als de melding ook echt is afgehandeld, gebruik anders de status Ingepland. Let op: als de huidige status “Verzoek tot heropenen” is, dan wordt er geen e-mail naar de melder gestuurd.',
   color: 'lightgreen',
   can_send_email: false,
+  shows_remaining_sla_days: false,
 };
 
 const GESPLITST = {
@@ -33,6 +37,7 @@ const GESPLITST = {
   value: 'Gesplitst',
   color: 'lightgreen',
   can_send_email: false,
+  shows_remaining_sla_days: false,
 };
 
 const INGEPLAND = {
@@ -42,6 +47,7 @@ const INGEPLAND = {
     'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting.',
   color: 'grey',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const GEANNULEERD = {
@@ -51,6 +57,7 @@ const GEANNULEERD = {
     'Bij deze status wordt de melding afgesloten. Gebruik deze status alleen voor test- en nepmeldingen of meldingen van veelmelders.',
   color: 'darkgrey',
   can_send_email: true,
+  shows_remaining_sla_days: false,
 };
 
 const VERZOEK_TOT_HEROPENEN = {
@@ -58,6 +65,7 @@ const VERZOEK_TOT_HEROPENEN = {
   value: 'Verzoek tot heropenen',
   color: 'orange',
   can_send_email: false,
+  shows_remaining_sla_days: false,
 };
 
 const HEROPEND = {
@@ -67,36 +75,42 @@ const HEROPEND = {
     'De melder ontvangt deze toelichting per e-mail, let dus op de schrijfstijl. De e-mail bevat al een aanhef en afsluiting. Verwijs nooit naar een andere afdeling; hercategoriseer dan de melding.',
   color: 'orange',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const TE_VERZENDEN = {
   key: 'ready to send',
   value: 'Extern: te verzenden',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const VERZONDEN = {
   key: 'sent',
   value: 'Extern: verzonden',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const VERZENDEN_MISLUKT = {
   key: 'send failed',
   value: 'Extern: mislukt',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const VERZOEK_TOT_AFHANDELING = {
   key: 'closure requested',
   value: 'Extern: verzoek tot afhandeling',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const AFGEHANDELD_EXTERN = {
   key: 'done external',
   value: 'Extern: afgehandeld',
   can_send_email: false,
+  shows_remaining_sla_days: true,
 };
 
 const statusList = [
