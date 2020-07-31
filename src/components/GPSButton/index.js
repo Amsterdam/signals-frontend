8e6151f72f57b81c407d625e90faae728b8b2695
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-07-31 10:31:54 +0200

    Ensure the correct color for the spinner in the GPS button

--- src/components/GPSButton/index.js
+++ src/components/GPSButton/index.js
@@ -76,7 +76,7 @@ const GPSButton = ({ className, onLocationChange, onLocationSuccess, onLocationE
     <StyledButton
       className={className}
       data-testid="gpsButton"
-      icon={loading ? <LoadingIndicator /> : <GPSIcon fill={toggled ? '#009de6' : 'black'} />}
+      icon={loading ? <LoadingIndicator color="black" /> : <GPSIcon fill={toggled ? '#009de6' : 'black'} />}
       iconSize={20}
       onClick={onClick}
       size={44}
