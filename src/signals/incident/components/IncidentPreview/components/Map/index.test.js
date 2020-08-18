99465a893b7bf69eaa8e2a01c27eee9ab49f9e3b
Boris Arkenaar <boris@arkenaar.com>
2020-08-18 10:37:51 +0200

    Update UTs

--- src/signals/incident/components/IncidentPreview/components/Map/index.test.js
+++ src/signals/incident/components/IncidentPreview/components/Map/index.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { withAppContext } from 'test/utils';
+import configuration from 'shared/services/configuration/configuration';
 
 import MapPreview from '.';
+
+jest.mock('shared/services/configuration/configuration');
 
 describe('signals/incident/components/IncidentPreview/components/Map', () => {
   const geometrie = {
     coordinates: [52, 4],
   };
+
+  afterEach(() => {
+    configuration.__reset();
+  });
 
   it('should show address fallback', async () => {
     const { getByText, findByTestId } = render(withAppContext(<MapPreview value={{ geometrie }} />));
@@ -17,11 +24,21 @@ describe('signals/incident/components/IncidentPreview/components/Map', () => {
     expect(getByText('Geen adres gevonden')).toBeInTheDocument();
   });
 
-  it('should render static map', async () => {
-    const { findByTestId } = render(withAppContext(<MapPreview value={{ geometrie }} />));
+  it('should render static map with useStaticMapServer enabled', async () => {
+    const { findByTestId, queryByTestId } = render(withAppContext(<MapPreview value={{ geometrie }} />));
 
-    const mapStatic = await findByTestId('mapStatic');
+    await findByTestId('mapStatic');
 
-    expect(mapStatic).toBeInTheDocument();
+    expect(queryByTestId('mapStatic')).toBeInTheDocument();
+    expect(queryByTestId('map-base')).not.toBeInTheDocument();
+  });
+
+  it('should render normal map with useStaticMapServer disabled', () => {
+    configuration.useStaticMapServer = false;
+
+    const { queryByTestId } = render(withAppContext(<MapPreview value={{ geometrie }} />));
+
+    expect(queryByTestId('mapStatic')).not.toBeInTheDocument();
+    expect(queryByTestId('map-base')).toBeInTheDocument();
   });
 });
