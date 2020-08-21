360d577bb34dce1c42e5dd0911305d9b5d3321fb
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-08-21 13:44:40 +0200

    Adds tests for model selectors

--- src/models/roles/selectors.test.js
+++ src/models/roles/selectors.test.js
@@ -1,5 +1,5 @@
 import { fromJS } from 'immutable';
-import { rolesModelSelector } from './selectors';
+import { rolesModelSelector, inputCheckboxRolesSelector, inputSelectRolesSelector } from './selectors';
 import { initialState } from './reducer';
 
 describe('rolesModelSelector', () => {
@@ -17,5 +17,46 @@ describe('rolesModelSelector', () => {
       roles,
     });
     expect(rolesModelSelector(mockedState)).toEqual(roles);
+  });
+
+  test('inputSelectRolesSelector', () => {
+    const roles = {
+      list: [
+        { id: 42, name: 'Role 42' },
+        { id: 425, name: 'Role 425' },
+      ],
+    };
+
+    const mockedState = fromJS({
+      roles,
+    });
+
+    const result = [
+      { key: 'all', name: 'Alles', value: '*' },
+      { key: '42', name: 'Role 42', value: 'Role 42' },
+      { key: '425', name: 'Role 425', value: 'Role 425' },
+    ];
+
+    expect(inputSelectRolesSelector(mockedState)).toEqual(result);
+  });
+
+  test('inputCheckboxRolesSelector', () => {
+    const roles = {
+      list: [
+        { id: 42, name: 'Role 42' },
+        { id: 425, name: 'Role 425' },
+      ],
+    };
+
+    const mockedState = fromJS({
+      roles,
+    });
+
+    const result = [
+      { key: '42', name: 'Role 42', value: 'Role 42' },
+      { key: '425', name: 'Role 425', value: 'Role 425' },
+    ];
+
+    expect(inputCheckboxRolesSelector(mockedState)).toEqual(result);
   });
 });
