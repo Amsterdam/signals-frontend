df14696fb16a8d739b48b293e8d5c18888b717b0
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-09-02 14:57:58 +0200

    Adds comment

--- src/components/ErrorMessage/index.js
+++ src/components/ErrorMessage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { ErrorMessage as AscErrorMessage } from '@datapunt/asc-ui';
 
+// custom error message component that renders the correct font according to the design system guidelines
 const StyledErrorMessage = styled(AscErrorMessage)`
   font-family: Avenir Next LT W01 Demi;
   font-weight: normal;
