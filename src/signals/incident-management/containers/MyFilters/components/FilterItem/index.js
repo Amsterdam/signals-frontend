c66248f3b5186c49cd0ce6faf7cf885f15a8254c
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-09-09 08:53:20 +0200

    [SIG-3113] 'My filters' styling adjustments

--- src/signals/incident-management/containers/MyFilters/components/FilterItem/index.js
+++ src/signals/incident-management/containers/MyFilters/components/FilterItem/index.js
@@ -1,101 +1,91 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Heading, Link } from '@datapunt/asc-ui';
+import { Heading, Link, themeSpacing, themeColor } from '@datapunt/asc-ui';
 import * as types from 'shared/types';
 import { parseToAPIData } from 'signals/shared/filter/parse';
 
 import Refresh from '../../../../../../shared/images/icon-refresh.svg';
 import FilterTagList from '../../../FilterTagList';
 
-import './style.scss';
+const Wrapper = styled.div`
+  margin-bottom: ${themeSpacing(7)};
+`;
 
 const StyledH4 = styled(Heading)`
-  margin-bottom: 8px;
-  color: #fe0000;
+  margin-bottom: ${themeSpacing(2)};
+  color: ${themeColor('secondary')};
   display: block;
 `;
 
 const StyledLink = styled(Link)`
-  margin-right: 20px;
+  margin-right: ${themeSpacing(5)};
+  font-size: 16px;
 `;
 
 const RefreshIcon = styled(Refresh).attrs({
-  color: '#ff0000',
   height: 18,
 })`
-  margin-right: 10px;
+  color: ${themeColor('secondary')};
+  margin-right: ${themeSpacing(2)};
   vertical-align: middle;
   margin-top: -2px;
   cursor: default;
 `;
 
 const FilterItem = ({ filter, onApplyFilter, onEditFilter, onRemoveFilter, onClose }) => {
-  const handleApplyFilter = e => {
-    e.preventDefault();
+  const handleApplyFilter = useCallback(
+    event => {
+      event.preventDefault();
 
-    onApplyFilter(parseToAPIData(filter));
+      onApplyFilter(parseToAPIData(filter));
 
-    onClose();
-  };
+      onClose();
+    },
+    [filter, onApplyFilter, onClose]
+  );
 
-  const handleEditFilter = e => {
-    e.preventDefault();
+  const handleEditFilter = useCallback(
+    event => {
+      event.preventDefault();
 
-    onEditFilter(parseToAPIData(filter));
+      onEditFilter(parseToAPIData(filter));
 
-    onClose();
-  };
+      onClose();
+    },
+    [filter, onEditFilter, onClose]
+  );
 
-  const handleRemoveFilter = e => {
-    e.preventDefault();
+  const handleRemoveFilter = useCallback(
+    event => {
+      event.preventDefault();
 
-    if (
-      global.confirm(
-        'Weet je zeker dat je dit filter wilt verwijderen?\nDit kan niet ongedaan worden gemaakt.',
-      )
-    ) {
-      onRemoveFilter(filter.id);
-    }
-  };
+      if (global.confirm('Weet je zeker dat je dit filter wilt verwijderen?\nDit kan niet ongedaan worden gemaakt.')) {
+        onRemoveFilter(filter.id);
+      }
+    },
+    [filter.id, onRemoveFilter]
+  );
 
   return (
-    <div className="filter-item">
+    <Wrapper>
       <StyledH4 forwardedAs="h4">
-        {filter.refresh && (<RefreshIcon />)}
+        {filter.refresh && <RefreshIcon />}
         {filter.name}
       </StyledH4>
 
-      <div className="filter-item__tag-list">
-        <FilterTagList tags={filter.options} />
-      </div>
-      <div className="filter-item__actions">
-        <StyledLink
-          href="/"
-          variant="inline"
-          onClick={handleApplyFilter}
-          data-testid="handleApplyFilterButton"
-        >
-          Toon resultaat
-        </StyledLink>
-        <StyledLink
-          href="/"
-          variant="inline"
-          onClick={handleEditFilter}
-          data-testid="handleEditFilterButton"
-        >
-          Wijzig
-        </StyledLink>
-        <StyledLink
-          href="/"
-          variant="inline"
-          onClick={handleRemoveFilter}
-          data-testid="handleRemoveFilterButton"
-        >
-          Verwijder
-        </StyledLink>
-      </div>
-    </div>
+      <FilterTagList tags={filter.options} />
+
+      <StyledLink href="/" variant="inline" onClick={handleApplyFilter} data-testid="handleApplyFilterButton">
+        Toon resultaat
+      </StyledLink>
+      <StyledLink href="/" variant="inline" onClick={handleEditFilter} data-testid="handleEditFilterButton">
+        Wijzig
+      </StyledLink>
+      <StyledLink href="/" variant="inline" onClick={handleRemoveFilter} data-testid="handleRemoveFilterButton">
+        Verwijder
+      </StyledLink>
+    </Wrapper>
   );
 };
 
