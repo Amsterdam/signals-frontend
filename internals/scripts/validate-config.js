08114ed22fc4fea38709d6dbd84ef491dd4adb82
Boris Arkenaar <boris@arkenaar.com>
2020-09-11 12:48:46 +0200

    Have config validator log all errors

--- internals/scripts/validate-config.js
+++ internals/scripts/validate-config.js
@@ -3,7 +3,7 @@ const { validator } = require('@exodus/schemasafe');
 const { config } = require('./helpers/config');
 const schema = require('../schemas/app.schema.json');
 
-const validate = validator(schema, { includeErrors: true });
+const validate = validator(schema, { includeErrors: true, allErrors: true });
 const valid = validate(config);
 
 if (!valid) {
