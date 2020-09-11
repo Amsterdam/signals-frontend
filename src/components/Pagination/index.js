df39de0e20257807e5d3799b299e25140fb416d8
JJM van Deursen <j.van.deursen@amsterdam.nl>
2020-09-11 09:13:16 +0200

    useMemo for Pagination component

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { themeColor, themeSpacing, Typography } from '@datapunt/asc-ui';
 import PaginationItem from './components/PaginationItem';
-import {
-  getPreviousIndex,
-  getNextIndex,
-  pageNumbersList,
-  FILLER,
-  NEXT,
-  PREVIOUS,
-} from './utils';
+import { getPreviousIndex, getNextIndex, pageNumbersList, FILLER, NEXT, PREVIOUS } from './utils';
 
 const List = styled.ul`
   display: flex;
@@ -48,76 +41,73 @@ const Filler = styled(Typography)`
 /**
  * Stateless component that renders pagination items
  */
-const Pagination = ({
-  className,
-  currentPage,
-  hrefPrefix,
-  onClick,
-  shouldPushToHistory,
-  totalPages,
-}) => {
-  const pagesInRange = pageNumbersList(currentPage, totalPages);
+const Pagination = ({ className, currentPage, hrefPrefix, onClick, shouldPushToHistory, totalPages }) => {
+  const pagesInRange = useMemo(() => pageNumbersList(currentPage, totalPages), [currentPage, totalPages]);
+
+  const items = useMemo(
+    () =>
+      pagesInRange.map((pageNum, index) => {
+        const prevIndex = getPreviousIndex(currentPage);
+        const nextIndex = getNextIndex(currentPage, totalPages);
+
+        switch (pageNum) {
+          case PREVIOUS:
+            return (
+              <PaginationItem
+                isNav
+                key={pageNum}
+                label="Vorige"
+                onClick={() => onClick && onClick(prevIndex)}
+                pageNum={pageNum}
+                shouldPushToHistory={shouldPushToHistory}
+                to={`${hrefPrefix}${prevIndex}`}
+              />
+            );
+
+          case NEXT:
+            return (
+              <PaginationItem
+                data-testid="pagination-next"
+                isNav
+                key={pageNum}
+                label="Volgende"
+                onClick={() => onClick && onClick(nextIndex)}
+                pageNum={pageNum}
+                shouldPushToHistory={shouldPushToHistory}
+                to={`${hrefPrefix}${nextIndex}`}
+              />
+            );
+
+          case FILLER:
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <Filler data-testid="pagination-filler" key={`${pageNum}${index}`}>
+                ...
+              </Filler>
+            );
+
+          case currentPage:
+            return <CurrentPage key={pageNum}>{pageNum}</CurrentPage>;
+
+          default:
+            return (
+              <PaginationItem
+                key={pageNum}
+                onClick={() => onClick && onClick(pageNum)}
+                pageNum={pageNum}
+                shouldPushToHistory={shouldPushToHistory}
+                to={`${hrefPrefix}${pageNum}`}
+                label={pageNum}
+              />
+            );
+        }
+      }),
+    [currentPage, hrefPrefix, onClick, pagesInRange, shouldPushToHistory, totalPages]
+  );
 
   if (totalPages <= 1) {
     return null;
   }
-
-  const items = pagesInRange.map((pageNum, index) => {
-    const prevIndex = getPreviousIndex(currentPage);
-    const nextIndex = getNextIndex(currentPage, totalPages);
-
-    switch (pageNum) {
-      case PREVIOUS:
-        return (
-          <PaginationItem
-            isNav
-            key={pageNum}
-            label="Vorige"
-            onClick={() => onClick && onClick(prevIndex)}
-            pageNum={pageNum}
-            shouldPushToHistory={shouldPushToHistory}
-            to={`${hrefPrefix}${prevIndex}`}
-          />
-        );
-
-      case NEXT:
-        return (
-          <PaginationItem
-            data-testid="pagination-next"
-            isNav
-            key={pageNum}
-            label="Volgende"
-            onClick={() => onClick && onClick(nextIndex)}
-            pageNum={pageNum}
-            shouldPushToHistory={shouldPushToHistory}
-            to={`${hrefPrefix}${nextIndex}`}
-          />
-        );
-
-      case FILLER:
-        return (
-          // eslint-disable-next-line react/no-array-index-key
-          <Filler data-testid="pagination-filler" key={`${pageNum}${index}`}>
-            ...
-          </Filler>
-        );
-
-      case currentPage:
-        return <CurrentPage key={pageNum}>{pageNum}</CurrentPage>;
-
-      default:
-        return (
-          <PaginationItem
-            key={pageNum}
-            onClick={() => onClick && onClick(pageNum)}
-            pageNum={pageNum}
-            shouldPushToHistory={shouldPushToHistory}
-            to={`${hrefPrefix}${pageNum}`}
-            label={pageNum}
-          />
-        );
-    }
-  });
 
   return (
     <nav className={className} data-testid="pagination">
