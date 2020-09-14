54e20a71238fa0b6e1fc273dded194c8a8db2d84
Boris Arkenaar <boris@arkenaar.com>
2020-09-14 15:11:42 +0200

    Revert "Remove $ from placeholders"
    This reverts commit f0151f275295d02e30a6b1cfd6f3bc9013174c5d.

--- internals/scripts/helpers/config.js
+++ internals/scripts/helpers/config.js
@@ -8,18 +8,18 @@ const extendedConfig = fs.existsSync(extendedConfigFile) ? require(extendedConfi
 const config = merge({}, baseConfig, extendedConfig);
 
 const placeholders = {
-  SIGNALS_ANDROID_ICON: config.head.androidIcon,
-  SIGNALS_BACKGROUND_COLOR: config.head.backgroundColor,
-  SIGNALS_CONFIG: JSON.stringify(config),
-  SIGNALS_FAVICON: config.head.favicon,
-  SIGNALS_IOS_ICON: config.head.iosIcon,
-  SIGNALS_MATOMO_SITE_ID: config.matomo.siteId,
-  SIGNALS_MATOMO_URL_BASE: config.matomo.urlBase,
-  SIGNALS_PWA_SHORT_TITLE: config.language.shortTitle,
-  SIGNALS_PWA_TITLE: config.language.title,
-  SIGNALS_SITE_TITLE: config.language.siteTitle,
-  SIGNALS_STATUS_BAR_STYLE: config.head.statusBarStyle,
-  SIGNALS_THEME_COLOR: config.head.themeColor,
+  $SIGNALS_ANDROID_ICON: config.head.androidIcon,
+  $SIGNALS_BACKGROUND_COLOR: config.head.backgroundColor,
+  $SIGNALS_CONFIG: JSON.stringify(config),
+  $SIGNALS_FAVICON: config.head.favicon,
+  $SIGNALS_IOS_ICON: config.head.iosIcon,
+  $SIGNALS_MATOMO_SITE_ID: config.matomo.siteId,
+  $SIGNALS_MATOMO_URL_BASE: config.matomo.urlBase,
+  $SIGNALS_PWA_SHORT_TITLE: config.language.shortTitle,
+  $SIGNALS_PWA_TITLE: config.language.title,
+  $SIGNALS_SITE_TITLE: config.language.siteTitle,
+  $SIGNALS_STATUS_BAR_STYLE: config.head.statusBarStyle,
+  $SIGNALS_THEME_COLOR: config.head.themeColor,
 };
 
 const inject = files =>
