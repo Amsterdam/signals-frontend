63d5d0cb3d9a9a6b3a122c2861958fd70134dea2
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-09-21 10:00:47 +0200

    adds unittests for DescriptionInput

---
+++ src/signals/incident/components/form/DescriptionInput/DescriptionInput.js
@@ -0,0 +1,49 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import TextArea from 'components/TextArea';
+import DescriptionInfo from '../DescriptionInfo';
+
+function get(e, meta, parent) {
+  const {
+    getClassification,
+    updateIncident,
+    incidentContainer: { usePredictions },
+  } = parent?.meta;
+  if (!usePredictions) return;
+  if (e.target.value) getClassification(e.target.value);
+  updateIncident({ [meta.name]: e.target.value });
+}
+
+const DescriptionInput = ({
+  handler,
+  value,
+  meta,
+  parent,
+}) => {
+  const getCharactersInfo = useCallback(
+    () => meta.maxLength > 0 && `${value ? value.length : '0'}/${meta.maxLength} tekens`,
+    [value, meta]
+  );
+
+  const handleBlur = useCallback(e => get(e, meta, parent), [meta, parent]);
+
+  return (
+    <TextArea
+      data-testid="descriptionInput"
+      rows={meta.rows || 6}
+      placeholder={meta.placeholder}
+      {...handler()}
+      onBlur={handleBlur}
+      helpText={<DescriptionInfo info={getCharactersInfo()} />}
+    />
+  );
+};
+
+DescriptionInput.propTypes = {
+  handler: PropTypes.func,
+  value: PropTypes.string,
+  meta: PropTypes.object,
+  parent: PropTypes.object,
+};
+
+export default DescriptionInput;
---
+++ src/signals/incident/components/form/DescriptionInput/DescriptionInput.test.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+import { withAppContext } from 'test/utils';
+
+import DescriptionInput from '.';
+
+describe('signals/incident/components/form/DescriptionInput', () => {
+  const metaFields = {
+    name: 'input-field-name',
+    isVisible: true,
+  };
+
+  const props = {
+    handler: jest.fn(),
+    parent: {
+      meta: {
+        updateIncident: jest.fn(),
+        getClassification: jest.fn(),
+        incidentContainer: { usePredictions: true },
+      },
+      value: jest.fn(),
+      controls: {
+        'input-field-name': {
+          updateValueAndValidity: jest.fn(),
+        },
+      },
+    },
+  };
+
+  describe('rendering', () => {
+    it('should render correctly', () => {
+      const { getByTestId } = render(
+        withAppContext(
+          <DescriptionInput
+            {...props}
+            meta={{
+              ...metaFields,
+              maxLength: 100,
+            }}
+          />
+        )
+      );
+
+      expect(getByTestId('descriptionInput')).toBeInTheDocument();
+      expect(getByTestId('descriptionInfo')).toBeInTheDocument();
+    });
+
+    it('should render with a character counter with value correctly', () => {
+      const description = 'the-description';
+      const { getByTestId } = render(
+        withAppContext(
+          <DescriptionInput
+            {...props}
+            meta={{
+              ...metaFields,
+              maxLength: 100,
+            }}
+            value={description}
+          />
+        )
+      );
+
+      expect(getByTestId('descriptionInfo')).toBeInTheDocument();
+      expect(getByTestId('descriptionInfo').textContent).toEqual(`${description.length}/100 tekens`);
+    });
+  });
+
+  describe('events', () => {
+    const event = { target: { value: 'diabolo' } };
+
+    beforeEach(() => {
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('sets incident when value changes', async () => {
+      const { getByTestId, findByTestId } = render(
+        withAppContext(
+          <DescriptionInput
+            {...props}
+            meta={{
+              ...metaFields,
+              maxLength: 100,
+            }}
+          />
+        )
+      );
+
+      const element = getByTestId('descriptionInput');
+      element.focus();
+      act(() => {
+        fireEvent.change(element, event);
+        element.blur();
+      });
+
+      await findByTestId('descriptionInput');
+      expect(props.parent.meta.getClassification).toHaveBeenCalledWith('diabolo');
+      expect(props.parent.meta.updateIncident).toHaveBeenCalledWith({
+        'input-field-name': 'diabolo',
+      });
+    });
+  });
+});
