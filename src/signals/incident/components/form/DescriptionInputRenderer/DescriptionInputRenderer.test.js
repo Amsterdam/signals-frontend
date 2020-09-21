a47ce0d2338fa3845e4270287486fd6c9069f701
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-09-21 09:22:19 +0200

    adds unittests for DescriptionInputRenderer

---
+++ src/signals/incident/components/form/DescriptionInputRenderer/DescriptionInputRenderer.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { withAppContext } from 'test/utils';
+
+import DescriptionInputRenderer from '.';
+
+describe('signals/incident/components/form/DescriptionInputRenderer', () => {
+  const props = {
+    handler: jest.fn(),
+    touched: false,
+    getError: jest.fn(),
+    hasError: jest.fn(),
+    value: 'the-description',
+    parent: {
+      meta: {
+        updateIncident: jest.fn(),
+      },
+      controls: {},
+    },
+    validatorsOrOpts: {},
+  };
+
+  describe('rendering', () => {
+    it('should render correctly', async () => {
+      const { queryByTestId } = render(
+        withAppContext(
+          <DescriptionInputRenderer
+            {...props}
+            meta={{
+              isVisible: true,
+              maxLength: 100,
+            }}
+          />
+        )
+      );
+      const element = queryByTestId('descriptionInput');
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
--- src/signals/incident/components/form/DescriptionInputRenderer/index.js
+++ src/signals/incident/components/form/DescriptionInputRenderer/index.js
@@ -1,36 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Header from '../Header';
-import DescriptionInput from '../DescriptionInput';
+import DescriptioInputRenderer from './DescriptionInputRenederer';
 
-const DescriptionInputRenderer = ({
-  handler,
-  touched,
-  value,
-  hasError,
-  meta,
-  parent,
-  getError,
-  validatorsOrOpts,
-}) => {
-  if (!meta?.isVisible) return null;
-
-  return (
-    <Header meta={meta} options={validatorsOrOpts} touched={touched} hasError={hasError} getError={getError}>
-      <DescriptionInput handler={handler} meta={meta} parent={parent} value={value} />
-    </Header>
-  );
-};
-
-DescriptionInputRenderer.propTypes = {
-  handler: PropTypes.func,
-  touched: PropTypes.bool,
-  value: PropTypes.string,
-  hasError: PropTypes.func,
-  meta: PropTypes.object,
-  parent: PropTypes.object,
-  getError: PropTypes.func,
-  validatorsOrOpts: PropTypes.object,
-};
-
-export default DescriptionInputRenderer;
+export default DescriptioInputRenderer;
