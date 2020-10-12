90dfa5617709f28100bcb9ef770a16320b67e338
Boris Arkenaar <boris@arkenaar.com>
2020-10-12 15:53:43 +0200

    Revert file system changes

--- src/signals/incident-management/containers/IncidentDetail/components/ChangeValue/index.js
+++ src/signals/incident-management/containers/IncidentDetail/components/ChangeValue/index.js
@@ -1,3 +1,222 @@
-import ChangeValue from './ChangeValue';
+import React, { useMemo, Fragment, useEffect, useState, useCallback, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { FormBuilder, FieldGroup, Validators } from 'react-reactive-form';
+import get from 'lodash.get';
+import set from 'lodash.set';
+import styled from 'styled-components';
+import { themeSpacing } from '@datapunt/asc-ui';
+
+import Button from 'components/Button';
+import { dataListType } from 'shared/types';
+import { getListValueByKey } from 'shared/services/list-helper/list-helper';
+import InfoText from 'components/InfoText';
+import SelectInput from 'signals/incident-management/components/SelectInput';
+import FieldControlWrapper from 'signals/incident-management/components/FieldControlWrapper';
+
+import EditButton from '../EditButton';
+import IncidentDetailContext from '../../context';
+
+const DisplayValue = styled.span`
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: calc(100% - ${themeSpacing(10)});
+`;
+
+const SaveButton = styled(Button)`
+  margin-right: ${themeSpacing(2)};
+`;
+
+const ButtonBar = styled.div`
+  margin-top: ${themeSpacing(6)};
+`;
+
+const ChangeValue = ({
+  component = SelectInput,
+  disabled = false,
+  display,
+  /** Indicator that is used to determine which list item prop should be used to display info text between the form field and the buttons */
+  infoKey = '',
+  list,
+  patch = {},
+  path,
+  sort,
+  type,
+  valueClass = '',
+  valuePath = '',
+}) => {
+  const { incident, update } = useContext(IncidentDetailContext);
+  const [showForm, setShowForm] = useState(false);
+  const [info, setInfo] = useState('');
+
+  const form = useMemo(
+    () =>
+      FormBuilder.group({
+        input: [get(incident, valuePath), Validators.required],
+      }),
+    [incident, valuePath]
+  );
+
+  const handleSubmit = useCallback(
+    event => {
+      event.preventDefault();
+
+      const payload = { ...patch };
+      const newValue = form.value.input || list.find(({ key }) => !key)?.key;
+
+      set(payload, path, newValue);
+
+      update({
+        type,
+        patch: { ...payload },
+      });
+
+      form.reset();
+      setShowForm(false);
+    },
+    [form, list, patch, path, type, update]
+  );
+
+  const handleCancel = useCallback(() => {
+    form.reset();
+    setShowForm(false);
+  }, [form]);
+
+  const handleKeyUp = useCallback(
+    event => {
+      switch (event.key) {
+        case 'Esc':
+        case 'Escape':
+          handleCancel();
+          break;
+
+        default:
+          break;
+      }
+    },
+    [handleCancel]
+  );
+
+  const handleChange = useCallback(
+    event => {
+      const { value } = event.target;
+
+      showInfo(value);
+    },
+    [showInfo]
+  );
+
+  const showInfo = useCallback(
+    value => {
+      if (infoKey) {
+        const valueItem = list.find(({ key }) => key === value);
+
+        if (valueItem) {
+          setInfo(valueItem[infoKey]);
+        }
+      }
+    },
+    [infoKey, list]
+  );
+
+  const onShowForm = useCallback(() => {
+    const value = get(incident, valuePath || path);
+
+    form.controls.input.setValue(value);
+    setShowForm(true);
+
+    showInfo(value);
+  }, [incident, valuePath, path, showInfo, form]);
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+    // Disabling linter; only execute on mount
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const editForm = (
+    <FieldGroup
+      strict={false}
+      control={form}
+      render={() => (
+        <form onSubmit={handleSubmit} onChange={handleChange} data-testid="changeValueForm">
+          <FieldControlWrapper
+            control={form.get('input')}
+            disabled={disabled}
+            name="input"
+            render={component}
+            sort={sort}
+            values={list}
+          />
+
+          {info && <InfoText text={info} />}
+
+          <ButtonBar>
+            <SaveButton
+              data-testid={`submit${type.charAt(0).toUpperCase()}${type.slice(1)}Button`}
+              variant="secondary"
+              type="submit"
+            >
+              Opslaan
+            </SaveButton>
+
+            <Button
+              data-testid={`cancel${type.charAt(0).toUpperCase()}${type.slice(1)}Button`}
+              variant="tertiary"
+              type="button"
+              onClick={handleCancel}
+            >
+              Annuleren
+            </Button>
+          </ButtonBar>
+        </form>
+      )}
+    />
+  );
+
+  return (
+    <Fragment>
+      <dt data-testid={`meta-list-${type}-definition`}>
+        <DisplayValue>{display}</DisplayValue>
+        {!showForm && (
+          <EditButton
+            data-testid={`edit${type.charAt(0).toUpperCase()}${type.slice(1)}Button`}
+            disabled={disabled}
+            onClick={onShowForm}
+          />
+        )}
+      </dt>
+
+      {showForm ? (
+        <dd data-testid={`meta-list-${type}-value`}>{editForm}</dd>
+      ) : (
+        <dd data-testid={`meta-list-${type}-value`} className={valueClass}>
+          <DisplayValue data-testid="valuePath">
+            {getListValueByKey(list, get(incident, valuePath || path))}
+          </DisplayValue>
+        </dd>
+      )}
+    </Fragment>
+  );
+};
+
+ChangeValue.propTypes = {
+  component: PropTypes.func,
+  disabled: PropTypes.bool,
+  display: PropTypes.string.isRequired,
+  infoKey: PropTypes.string,
+  list: dataListType.isRequired,
+  patch: PropTypes.object,
+  path: PropTypes.string.isRequired,
+  sort: PropTypes.bool,
+  type: PropTypes.string.isRequired,
+  valueClass: PropTypes.string,
+  valuePath: PropTypes.string,
+};
 
 export default ChangeValue;
--- src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/List/index.js
@@ -1,3 +1,216 @@
-import List from './List';
+import React, { useCallback, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import parseISO from 'date-fns/parseISO';
+import differenceInCalendarDays from 'date-fns/differenceInCalendarDays';
+import { ChevronUp, ChevronDown } from '@datapunt/asc-assets';
+import { Icon } from '@datapunt/asc-ui';
+import styled, { css } from 'styled-components';
+
+import { string2date, string2time } from 'shared/services/string-parser';
+import { getListValueByKey } from 'shared/services/list-helper/list-helper';
+import * as types from 'shared/types';
+import configuration from 'shared/services/configuration/configuration';
+import { statusList } from 'signals/incident-management/definitions';
+
+import IncidentManagementContext from '../../../../context';
+
+const getDaysOpen = incident => {
+  const statusesWithoutDaysOpen = statusList
+    .filter(({ shows_remaining_sla_days }) => shows_remaining_sla_days === false)
+    .map(({ key }) => key);
+  const hasDaysOpen = incident.status && !statusesWithoutDaysOpen.includes(incident.status.state);
+  const start = hasDaysOpen && parseISO(incident.created_at);
+  return hasDaysOpen ? -differenceInCalendarDays(start, new Date()) : '-';
+};
+
+const StyledList = styled.div`
+  width: 100%;
+
+  ${({ isLoading }) =>
+    isLoading &&
+    css`
+      opacity: 0.3;
+    `}
+`;
+
+const Table = styled.table`
+  border-collapse: separate;
+  width: 100%;
+  height: 100%;
+
+  td {
+    padding: 0;
+
+    a {
+      text-decoration: none;
+      color: black;
+      display: block;
+      width: 100%;
+      height: 100%;
+      padding: 8px;
+    }
+  }
+
+  tr:hover td,
+  td {
+    box-shadow: unset;
+  }
+`;
+
+const Th = styled.th`
+  cursor: pointer;
+  font-weight: normal;
+  white-space: nowrap;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Td = styled.td`
+  ${({ noWrap }) =>
+    noWrap &&
+    css`
+      white-space: nowrap;
+    `}
+`;
+
+const List = ({
+  className = '',
+  incidents,
+  isLoading = false,
+  onChangeOrdering,
+  priority,
+  sort,
+  stadsdeel,
+  status,
+}) => {
+  const { districts, users } = useContext(IncidentManagementContext);
+
+  const onSort = useCallback(
+    newSort => () => {
+      const sortIsAsc = sort?.indexOf(newSort) === 0;
+      onChangeOrdering(sortIsAsc ? `-${newSort}` : newSort);
+    },
+    [onChangeOrdering, sort]
+  );
+
+  const renderChevron = useCallback(
+    column => {
+      const currentSort = sort?.split(',')[0];
+      const isColumnSorted = currentSort?.indexOf(column) > -1;
+      const sortDirection = currentSort?.charAt(0) === '-' ? 'down' : 'up';
+
+      return isColumnSorted ? (
+        <Icon inline size={12}>
+          {sortDirection === 'up' ? <ChevronDown /> : <ChevronUp />}
+        </Icon>
+      ) : null;
+    },
+    [sort]
+  );
+
+  const getAssignedUserName = useCallback(userId => users?.find(user => user.id === userId)?.username, [users]);
+
+  return (
+    <StyledList isLoading={isLoading} className={className} data-testid="incidentOverviewListComponent">
+      <Table cellSpacing="0">
+        <thead>
+          <tr>
+            <Th data-testid="sortId" onClick={onSort('id')}>
+              Id {renderChevron('id')}
+            </Th>
+            <Th data-testid="sortDaysOpen" onClick={onSort('days_open')}>
+              Dag {renderChevron('days_open')}
+            </Th>
+            <Th data-testid="sortCreatedAt" onClick={onSort('created_at')}>
+              Datum en tijd {renderChevron('created_at')}
+            </Th>
+            {configuration.fetchDistrictsFromBackend ? (
+              <Th data-testid="sortDistrict" onClick={onSort('district,-created_at')}>
+                {configuration.language.district} {renderChevron('district')}
+              </Th>
+            ) : (
+              <Th data-testid="sortStadsdeel" onClick={onSort('stadsdeel,-created_at')}>
+                Stadsdeel {renderChevron('stadsdeel')}
+              </Th>
+            )}
+            <Th data-testid="sortSubcategory" onClick={onSort('sub_category,-created_at')}>
+              Subcategorie {renderChevron('sub_category')}
+            </Th>
+            <Th data-testid="sortStatus" onClick={onSort('status,-created_at')}>
+              Status {renderChevron('status')}
+            </Th>
+            <Th data-testid="sortPriority" onClick={onSort('priority,-created_at')}>
+              Urgentie {renderChevron('priority')}
+            </Th>
+            <Th data-testid="sortAddress" onClick={onSort('address,-created_at')}>
+              Adres {renderChevron('address')}
+            </Th>
+            {configuration.assignSignalToEmployee && users && <th data-testid="sortAssigedUserId">Toegewezen aan</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {incidents.map(incident => {
+            const detailLink = `/manage/incident/${incident.id}`;
+            return (
+              <tr key={incident.id}>
+                <Td>
+                  <Link to={detailLink}>{incident.id}</Link>
+                </Td>
+                <Td data-testid="incidentDaysOpen">
+                  <Link to={detailLink}>{getDaysOpen(incident)}</Link>
+                </Td>
+                <Td noWrap>
+                  <Link to={detailLink}>
+                    {string2date(incident.created_at)} {string2time(incident.created_at)}
+                  </Link>
+                </Td>
+                <Td>
+                  <Link to={detailLink}>
+                    {configuration.fetchDistrictsFromBackend
+                      ? getListValueByKey(districts, incident.location && incident.location.area_code)
+                      : getListValueByKey(stadsdeel, incident.location && incident.location.stadsdeel)}
+                  </Link>
+                </Td>
+                <Td>
+                  <Link to={detailLink}>{incident.category && incident.category.sub}</Link>
+                </Td>
+                <Td>
+                  <Link to={detailLink}>{getListValueByKey(status, incident.status && incident.status.state)}</Link>
+                </Td>
+                <Td>
+                  <Link to={detailLink}>
+                    {getListValueByKey(priority, incident.priority && incident.priority.priority)}
+                  </Link>
+                </Td>
+                <Td>
+                  <Link to={detailLink}>{incident.location && incident.location.address_text}</Link>
+                </Td>
+                {configuration.assignSignalToEmployee && users && (
+                  <Td>
+                    <Link to={detailLink}>{getAssignedUserName(incident.assigned_user_id)}</Link>
+                  </Td>
+                )}
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </StyledList>
+  );
+};
+
+List.propTypes = {
+  className: PropTypes.string,
+  incidents: PropTypes.arrayOf(types.incidentType).isRequired,
+  onChangeOrdering: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool,
+  priority: types.dataListType.isRequired,
+  sort: PropTypes.string,
+  stadsdeel: types.dataListType.isRequired,
+  status: types.dataListType.isRequired,
+};
 
 export default List;
