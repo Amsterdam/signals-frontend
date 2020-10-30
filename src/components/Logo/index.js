cb978cfedd805b6e7ba50f643d69cc7e4484ecd0
Boris Arkenaar <boris@arkenaar.com>
2020-10-30 10:20:49 +0100

    Use correct logo height on mobile

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,20 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { breakpoint, themeSpacing, themeColor } from '@datapunt/asc-ui';
 
 import configuration from 'shared/services/configuration/configuration';
 
 const StyledLogo = styled.img`
-  width: ${({ tall }) => tall ? configuration.logo.width : configuration.logo.smallWidth};
-  height: ${({ tall }) => tall ? configuration.logo.height : configuration.logo.smallHeight};
-  max-height: ${({ tall }) => tall ? '56px' : '32px'};
+  display: block;
+  width: ${({ tall }) => (tall ? configuration.logo.width : configuration.logo.smallWidth)};
+  height: ${({ tall }) => (tall ? configuration.logo.height : configuration.logo.smallHeight)};
+  max-height: ${({ tall }) => (tall ? '56px' : '32px')};
+
+  ${({ tall }) =>
+    tall &&
+    css`
+      @media screen and ${breakpoint('max-width', 'laptopM')} {
+        width: ${configuration.logo.smallWidth};
+        height: ${configuration.logo.smallHeight};
+      }
+    `};
+`;
+
+const StyledA = styled.a`
+  display: inline-block;
+  width: ${({ tall }) => (tall ? configuration.logo.width : configuration.logo.smallWidth)};
+  height: ${({ tall }) => (tall ? configuration.logo.height : configuration.logo.smallHeight)};
+  max-height: ${({ tall }) => (tall ? '56px' : '32px')};
+  margin-right: ${themeSpacing(3)};
+
+  &&:focus {
+    outline-color: ${themeColor('support', 'focus')};
+    outline-style: solid;
+    outline-offset: 0px;
+    outline-width: 3px;
+  }
+
+  ${({ tall }) =>
+    tall &&
+    css`
+      @media screen and ${breakpoint('min-width', 'laptopM')} {
+        width: ${configuration.logo.smallWidth};
+        height: ${configuration.logo.smallHeight};
+      }
+      @media screen and ${breakpoint('min-width', 'laptopM')} {
+        margin-right: ${themeSpacing(10)};
+      }
+    `};
 `;
 
 export const Logo = ({ tall, ...props }) => (
-  <a href={tall ? configuration.links.home : '/'}>
+  <StyledA href={tall ? configuration.links.home : '/'}>
     <StyledLogo data-testid="logo" alt="Logo" tall={tall} src={configuration.logo.url} {...props} />
-  </a>
+  </StyledA>
 );
 
 Logo.defaultProps = {
