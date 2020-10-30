fdfcb44a962ac479fce9060ade0dd7b67c5e2cc1
Jasper Poppe <jgpoppe@gmail.com>
2020-10-30 19:25:14 +0100

    moved passing testing-library react rules to plugin

--- eslint-plugin-amsterdam/testing-library-react.js
+++ eslint-plugin-amsterdam/testing-library-react.js
@@ -1,27 +1,34 @@
 module.exports = {
   extends: ['plugin:testing-library/react'],
   plugins: ['testing-library'],
-  env: { jest: true, node: true, },
+  env: { jest: true, node: true },
   overrides: [
     {
       files: ['**/*.test.js', 'src/test/utils.js'],
       rules: {
-        // proposed rules
-        'testing-library/prefer-find-by': 'off', // Needs some manual work before it can be enabled
-        'no-empty-function': 'off',
-
         // base overrides
-        'max-len': 'off',
         'max-nested-callbacks': 'off',
         'no-constructor-return': 'off',
-        'no-magic-numbers': 'off',
         'no-undef': 'off',
         'promise/always-return': 'off',
         'promise/avoid-new': 'off',
-        'promise/catch-or-return': 'off', // Shall we prefer await?
+        'promise/catch-or-return': 'off',
         'promise/no-callback-in-promise': 'off',
+
+        // proposed rules
+        'no-magic-numbers': 'off',
+        'max-len': 'off',
+        'testing-library/prefer-find-by': 'off', // Needs some manual work before it can be enabled
+        'no-empty-function': 'off',
         'require-await': 'off',
+
+        // rules
+        'testing-library/no-manual-cleanup': 'error',
+        'testing-library/no-wait-for-empty-callback': 'error',
+        'testing-library/no-wait-for-snapshot': 'error',
+        'testing-library/prefer-explicit-assert': 'error',
+        'testing-library/prefer-wait-for': 'error',
       },
-    }
+    },
   ],
 };
