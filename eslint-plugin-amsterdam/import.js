0bc19a31a99d7888033ea948688863eae0d270ac
Jasper Poppe <jgpoppe@gmail.com>
2020-10-31 11:00:43 +0100

    processed import plugin linting rules

--- eslint-plugin-amsterdam/import.js
+++ eslint-plugin-amsterdam/import.js
@@ -1,24 +1,48 @@
 module.exports = {
-  // extends: ['plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+  extends: ['plugin:import/errors', 'plugin:import/recommended', 'plugin:import/react'],
   plugins: ['import'],
   settings: {
-    'import/resolver': { node: { extensions: ['.js', 'ts', '.tsx'], moduleDirectory: ['node_modules', './src'] } },
+    'import/resolver': { node: { extensions: ['.js', '.ts', '.tsx'], moduleDirectory: ['node_modules', './src'] } },
   },
   rules: {
     // proposed rules
+    'import/dynamic-import-chunkname': 'error',
+    'import/no-anonymous-default-export': 'error',
+    'import/no-commonjs': 'error',
+    'import/no-unassigned-import': ['error', { allow: ['**/*.scss'] }],
     'import/no-webpack-loader-syntax': 'error',
+    'import/order': 'error',
+    'import/unambiguous': 'error',
 
     // disabled rules
+    'import/exports-last': 'off',
+    'import/group-exports': 'off',
+    'import/max-dependencies': 'off',
     'import/newline-after-import': 'off',
+    'import/no-default-export': 'off',
     'import/no-dynamic-require': 'off',
     'import/no-extraneous-dependencies': 'off',
+    'import/no-internal-modules': 'off',
     'import/no-named-as-default': 'off',
+    'import/no-named-export': 'off',
+    'import/no-namespace': 'off',
+    'import/no-relative-parent-imports': 'off',
     'import/prefer-default-export': 'off', // Would be nice on react components?
 
     // enabled rules
     'import/extensions': 'error',
     'import/first': 'error',
+    'import/no-absolute-path': 'error',
     'import/no-amd': 'error',
+    'import/no-cycle': 'error',
+    'import/no-deprecated': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-named-default': 'error',
+    'import/no-nodejs-modules': 'error',
+    'import/no-restricted-paths': 'error',
+    'import/no-self-import': 'error',
     'import/no-unresolved': ['error', { commonjs: true }],
+    'import/no-unused-modules': 'error',
+    'import/no-useless-path-segments': 'error',
   },
 };
