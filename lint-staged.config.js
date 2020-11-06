2eac7ef223521df40464c0c5a4fb0e493cdca8b9
Jasper Poppe <jgpoppe@gmail.com>
2020-11-06 08:39:24 +0100

    lint-staged is now working as intended,  minor yaml style changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,19 +1,19 @@
-// const ignoredFilter = require('./filter-ignored');
-//
-// const eslintFlags = '--max-warnings 0 --fix --cache --cache-location node_modules/.cache/eslint-cache/';
-//
-// module.exports = {
-//   '*.{js,ts,tsx}': 'eslint'
-// }
-
-// let split = require('split');
-const CLIEngine = require('eslint').CLIEngine;
-const cli = new CLIEngine({});
-
 module.exports = {
   '*.{js,ts,tsx}': files => {
-    const lintFiles = [];
-    files.forEach(file => { if (!cli.isPathIgnored(file)) lintFiles.push(file); });
-    return `eslint ${lintFiles.join(' ')}`;
+    const mainFiles = [];
+    const e2eTestingFiles = [];
+
+    files.forEach(file => {
+      if (file.match(/\/e2e-tests\/cypress\//)) {
+        e2eTestingFiles.push(file);
+      } else {
+        mainFiles.push(file);
+      }
+    });
+
+    return [
+      `eslint ${mainFiles.join(' ')}`,
+      `./e2e-tests/node_modules/.bin/eslint ${e2eTestingFiles.join(' ')}`,
+    ];
   },
 };
