95200f3e1f7a835218d62b29e7cfe8073967c7dc
Boris Arkenaar <boris@arkenaar.com>
2020-11-24 11:05:07 +0100

    Update Jest thresholds

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,8 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 98.97,
-      branches: 97.86,
-      functions: 98.92,
+      branches: 97.85,
+      functions: 98.88,
       lines: 99.02,
     },
   },
