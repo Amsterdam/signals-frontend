6f81d1a4a81ad64cbaa7f03c875fa87918268879
Adrian Tudorache <a.p.tudorache@gmail.com>
2020-12-01 07:52:47 +0100

    adds the navigation functionality

--- src/signals/incident/components/form/ContainerSelect/ContainerSelect.js
+++ src/signals/incident/components/form/ContainerSelect/ContainerSelect.js
@@ -1,31 +1,54 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useState, useContext } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { Paragraph } from '@amsterdam/asc-ui';
+import { ContainerSelectProvider } from './context';
+import Intro from './Intro';
+import Selector from './Selector';
+import Summary from './Summary';
 
-
-const ContainerSelect = ({ handler, meta, parent }) => {
+const ContainerSelect = ({ handler, parent }) => {
   const { value } = handler();
+  const [showMap, setShowMap] = useState(false);
 
-  const handleChange = useCallback(
-    event => {
-      parent.meta.updateIncident({ });
+  const update = useCallback(
+    selectedValue => {
+      parent.meta.updateIncident({ extra_afvalcontainer: selectedValue });
     },
     [parent]
   );
 
+  const edit = useCallback(
+    event => {
+      event.preventDefault();
+      setShowMap(true);
+    },
+    [setShowMap]
+  );
+
+  const close = useCallback(
+    event => {
+      event.preventDefault();
+      setShowMap(false);
+    },
+    [setShowMap]
+  );
+
   return (
-    <div>
-      This is the Afvalcontainer
-      <div>Intro</div>
-      <div>Selector</div>
-      <div>Summary</div>
-    </div>
+    <ContainerSelectProvider value={{ value, update, edit, close }}>
+      {!showMap && !value && <Intro />}
+
+      {showMap && <Selector />}
+
+      {!showMap && value && <Summary />}
+
+      <Paragraph as="h6">Geselecteerd: {value ? value : '<geen>'}</Paragraph>
+    </ContainerSelectProvider>
   );
 };
 
 ContainerSelect.propTypes = {
   handler: PropTypes.func,
-  meta: PropTypes.object,
   parent: PropTypes.object,
 };
 
---
+++ src/signals/incident/components/form/ContainerSelect/Intro/Intro.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Intro from './Intro';
+
+describe('signals/incident/components/form/ContainerSelect/Intro', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+    };
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should render ', () => {
+    expect(true).toBe(true);
+  });
+});
