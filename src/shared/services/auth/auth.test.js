7a828fc5a63a59dba03b0bf6fb3e38248eeec6c5
Jasper Poppe <jgpoppe@gmail.com>
2020-12-16 09:47:54 +0100

    [SIG-3207] Integrate keycloak (#1285)
    * exclude keycloak docker data mount from git
    
    * add `realm` value to Oidc configuration and a temporary Keycloak section
    
    * add Keycloak login button
    
    * add temporary Keycloak section
    
    * add keycloak service to docker compose for local keycloak testing
    
    - should be integrated with backend as soon as the backend is ready
      hint:
        - USER_ID_FIELD=email
        - JWKS_URL=.....
    
    * add Keycloak 'docker realm configurations' (and screenshots of current configurations)
    
    * integrated keycloak authentication
    
    * Changes search params to use values from config
    
    * Refactor authentication to be called asynchronously and only once
    
    * Fixes unit tests
    
    * Fixes unit test
    
    * Adds missing unit tests
    
    * Adds test for token flow
    
    * Removes changes for Keycloak in docker-compose (will be included in a separate PR)
    
    * Adds test for login with keycloak button
    
    * Adds test for login with keycloak button
    
    * Improves naming
    
    * Implements keycloak with keycloak-js
    
    * Adds implementation of keycloak-js
    
    * uses the login-required initialization strategy
    
    * Adds refresh
    
    * Adds tests
    
    * Address review comments
    
    * Refactor naming
    
    * Fixes unit tests
    
    * Prevent keycloak redirect when keycloak login was not successful
    
    * Convert to ternary expression
    
    * Move SW instantiation to separate function
    
    * Removes comment
    
    * Removes irrelevant test
    
    * Fixes indenting
    
    * Use arrow functions
    
    * Updates tests
    
    * Updates coverage
    
    * Adds redirect uri
    
    Co-authored-by: Maurits Ridder <maurits.ridder@gmail.com>
    Co-authored-by: Adrian Tudorache <a.p.tudorache@gmail.com>
    Co-authored-by: Maurits <10672773+MauritsR@users.noreply.github.com>

--- src/shared/services/auth/auth.test.js
+++ src/shared/services/auth/auth.test.js
@@ -1,367 +1,46 @@
 import {
-  initAuth,
+  getOauthDomain,
+  getAuth,
+  getAccessToken,
+  isAuthenticated,
+  getAuthHeaders,
+  authenticate,
   login,
   logout,
-  getOauthDomain,
-  getAuthHeaders,
-  isAuthenticated,
-  getScopes,
-  getName,
-  authenticate,
 } from './auth';
-import queryStringParser from './services/query-string-parser/query-string-parser';
-import randomStringGenerator from './services/random-string-generator/random-string-generator';
-import accessTokenParser from './services/access-token-parser/access-token-parser';
-import configuration from '../configuration/configuration';
+import configuration from 'shared/services/configuration/configuration';
+import Keycloak from './services/keycloak';
+import Authz from './services/authz';
 
 jest.mock('shared/services/configuration/configuration');
-jest.mock('./services/query-string-parser/query-string-parser');
-jest.mock('./services/random-string-generator/random-string-generator');
-jest.mock('./services/access-token-parser/access-token-parser');
 
-/* tokens generated with https://www.jsonwebtoken.io/ */
-// token contains 'exp' prop with a date in the past
-const expiredToken =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjZhNTc3NzZlLTczYWYtNDM3ZS1hMmJiLThmYTkxYWVhN2QxYSIsImlhdCI6MTU4ODE2Mjk2MywiZXhwIjoxMjQyMzQzfQ.RbJHkXRPmFZMYDJs-gxhk7vWYlIYZi8uik83Q0V1nas';
-
-// token doesn't have 'exp' prop
-const invalidToken =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-
-// token contains 'exp' prop with a date far into the future
-const validToken =
-  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6ImMxOWRhNDgwLTAyM2UtNGM2YS04NDM2LWNhMzNkYzZjYzVlMyIsImlhdCI6MTU4ODE2NDUyMCwiZXhwIjoxNTg4MTY4MTQ1MH0.LMA3E950H0EACrvME7Gps1Y-Q43Fux1q8YCJUl9pbYE';
-
-describe('The auth service', () => {
-  const noop = () => {};
-
-  let queryObject;
-  let savedAccessToken;
-  let savedStateToken;
-  let savedNonce;
+describe('auth', () => {
+  let keycloak;
+  let authz;
   let savedOauthDomain;
-  let randomString;
 
   beforeEach(() => {
+    keycloak = new Keycloak();
+    authz = new Authz();
     global.localStorage.getItem.mockImplementation(key => {
       switch (key) {
-        case 'accessToken':
-          return savedAccessToken;
-        case 'stateToken':
-          return savedStateToken;
-        case 'nonce':
-          return savedNonce;
         case 'oauthDomain':
           return savedOauthDomain;
         default:
           return '';
       }
     });
-
-    Object.defineProperties(global, {
-      location: {
-        writable: true,
-        value: {
-          ...global.location,
-          assign: jest.fn(),
-          reload: jest.fn(),
-        },
-      },
-    });
-
-    Object.defineProperty(global.history, 'replaceState', {
-      writable: true,
-      configurable: true,
-      value: jest.fn(),
-    });
-
-    jest.spyOn(global.history, 'replaceState').mockImplementation(noop);
-    jest.spyOn(global.location, 'assign').mockImplementation(noop);
-    jest.spyOn(global.location, 'reload').mockImplementation(noop);
-    global.location.search = '';
-    global.location.hash = '';
-
-    accessTokenParser.mockImplementation(() => ({}));
-    queryStringParser.mockImplementation(() => queryObject);
-    randomStringGenerator.mockImplementation(() => randomString);
-
-    queryObject = {};
-    randomString = 'random-string';
-    savedStateToken = '';
-    savedNonce = '';
-    savedAccessToken = '';
   });
 
   afterEach(() => {
     configuration.__reset();
-
-    global.history.replaceState.mockRestore();
-    global.location.assign.mockRestore();
-    global.location.reload.mockRestore();
-
-    global.localStorage.removeItem.mockReset();
     global.localStorage.setItem.mockReset();
+    global.localStorage.removeItem.mockReset();
+    jest.restoreAllMocks();
   });
 
-  describe('init funtion', () => {
-    describe('receiving response errors from the auth service', () => {
-      it('throws an error', () => {
-        const queryString = '?error=invalid_request&error_description=invalid%20request';
-
-        global.location.search = queryString;
-        queryObject = {
-          error: 'invalid_request',
-          error_description: 'invalid request',
-        };
-
-        expect(() => {
-          initAuth();
-        }).toThrow(
-          'Authorization service responded with error invalid_request [invalid request] ' +
-            '(The request is missing a required parameter, includes an invalid parameter value, ' +
-            'includes a parameter more than once, or is otherwise malformed.)'
-        );
-        expect(queryStringParser).toHaveBeenCalledWith(queryString);
-      });
-
-      it('throws an error without a description in the query string', () => {
-        queryObject = {
-          error: 'invalid_request',
-        };
-
-        expect(() => {
-          initAuth();
-        }).toThrow();
-      });
-
-      it('removes the state token from the local storage', () => {
-        queryObject = {
-          error: 'invalid_request',
-        };
-
-        expect(() => {
-          initAuth();
-        }).toThrow();
-        expect(global.localStorage.removeItem).toHaveBeenCalledWith('stateToken');
-      });
-
-      it('does not handle any errors without an error in the query string', () => {
-        queryObject = {};
-
-        expect(() => {
-          initAuth();
-        }).not.toThrow();
-        expect(global.localStorage.removeItem).not.toHaveBeenCalledWith('stateToken');
-      });
-
-      it('does not handle any errors without a query string', () => {
-        queryObject = undefined;
-
-        expect(() => {
-          initAuth();
-        }).not.toThrow();
-        expect(global.localStorage.removeItem).not.toHaveBeenCalledWith('stateToken');
-      });
-    });
-
-    describe('receiving a successful callback from the auth service', () => {
-      it('throws an error when the state token received does not match the one saved', () => {
-        const queryString = '?access_token=123AccessToken&token_type=token&expires_in=36000&state=invalid-state-token';
-        global.location.hash = `#${queryString}`;
-        queryObject = {
-          access_token: '123AccessToken',
-          token_type: 'token',
-          expires_in: '36000',
-          state: 'invalid-state-token',
-        };
-        savedStateToken = 'state-token';
-
-        expect(() => {
-          initAuth();
-        }).toThrow('Authenticator encountered an invalid state token (invalid-state-token)');
-        expect(queryStringParser).toHaveBeenLastCalledWith(`#${queryString}`);
-      });
-
-      it('throws an error when the nonce received does not match the one saved', () => {
-        accessTokenParser.mockImplementation(() => ({
-          nonce: 'invalid-random-nonce',
-        }));
-
-        const queryString = '?access_token=123AccessToken&token_type=token&expires_in=36000&state=state-token';
-        global.location.hash = `#${queryString}`;
-        queryObject = {
-          access_token: '123AccessToken',
-          token_type: 'token',
-          expires_in: '36000',
-          state: 'state-token',
-        };
-
-        savedStateToken = 'state-token';
-        savedNonce = 'random-nonce';
-
-        expect(() => {
-          initAuth();
-        }).toThrow('Authenticator encountered an invalid nonce (invalid-random-nonce)');
-        expect(queryStringParser).toHaveBeenLastCalledWith(`#${queryString}`);
-      });
-
-      it('Updates local storage', () => {
-        const queryString = '?access_token=123AccessToken&token_type=token&expires_in=36000&state=random-string';
-        global.location.hash = queryString;
-        queryObject = {
-          access_token: '123AccessToken',
-          token_type: 'token',
-          expires_in: '36000',
-          state: 'random-string',
-        };
-        savedStateToken = 'random-string';
-
-        initAuth();
-        expect(global.localStorage.setItem).toHaveBeenCalledWith('accessToken', '123AccessToken');
-        expect(global.localStorage.removeItem).toHaveBeenCalledWith('stateToken');
-      });
-
-      it('Works when receiving unexpected parameters', () => {
-        const queryString =
-          '?access_token=123AccessToken&token_type=token&expires_in=36000&state=random-string&extra=sauce';
-        global.location.hash = queryString;
-        queryObject = {
-          access_token: '123AccessToken',
-          token_type: 'token',
-          expires_in: '36000',
-          state: 'random-string',
-          extra: 'sauce',
-        };
-        savedStateToken = 'random-string';
-
-        initAuth();
-        expect(global.localStorage.setItem).toHaveBeenCalledWith('accessToken', '123AccessToken');
-      });
-
-      it('Does not work when a parameter is missing', () => {
-        const queryString = '?access_token=123AccessToken&token_type=token&state=random-string';
-        global.location.hash = queryString;
-        queryObject = {
-          access_token: '123AccessToken',
-          token_type: 'token',
-          state: 'random-string',
-        };
-        savedStateToken = 'random-string';
-
-        initAuth();
-        expect(global.localStorage.setItem).not.toHaveBeenCalledWith('accessToken', '123AccessToken');
-        expect(global.localStorage.removeItem).not.toHaveBeenCalledWith('stateToken');
-      });
-    });
-  });
-
-  describe('Login process', () => {
-    it('throws an error when there is no storage support', () => {
-      const storage = global.Storage;
-      global.Storage = undefined;
-
-      expect(() => {
-        login();
-      }).toThrow('Storage not available; cannot proceed with logging in');
-
-      global.Storage = storage;
-    });
-
-    it('throws an error when the crypto library is not supported by the browser', () => {
-      randomString = '';
-      expect(() => {
-        login();
-      }).toThrow('crypto library is not available on the current browser');
-    });
-
-    it('Updates the local storage', () => {
-      const hash = '#?the=current-hash';
-      global.location.hash = hash;
-
-      login();
-
-      expect(global.localStorage.removeItem).toHaveBeenCalledWith('accessToken');
-      expect(global.localStorage.setItem).toHaveBeenCalledWith('stateToken', randomString);
-      expect(global.localStorage.setItem).toHaveBeenCalledWith('nonce', randomString);
-    });
-
-    it('Redirects to the auth service', () => {
-      configuration.oidc.authEndpoint = 'https://example.com/oauth2/authorize';
-      configuration.oidc.clientId = 'test';
-      configuration.oidc.responseType = 'responseType';
-      configuration.oidc.scope = 'scope1 scope2';
-
-      login();
-
-      expect(window.location.assign).toHaveBeenCalledWith(
-        'https://example.com/oauth2/authorize' +
-          '?client_id=test' +
-          '&response_type=responseType' +
-          '&scope=scope1%20scope2' +
-          '&state=random-string' +
-          '&nonce=random-string' +
-          '&redirect_uri=http%3A%2F%2Flocalhost%2Fmanage%2Fincidents' +
-          '&idp_id=datapunt'
-      );
-    });
-  });
-
-  describe('Logout process', () => {
-    it('Removes the access token from local storage', () => {
-      logout();
-      expect(global.localStorage.removeItem).toHaveBeenCalledWith('accessToken');
-    });
-  });
-
-  describe('Retrieving the return path', () => {
-    it('returns the return path after initialized with a successful callback', () => {
-      queryObject = {
-        access_token: '123AccessToken',
-        token_type: 'token',
-        expires_in: '36000',
-        state: 'random-string',
-      };
-      savedStateToken = 'random-string';
-
-      initAuth();
-    });
-
-    it('returns an empty string when the callback was unsuccessful', () => {
-      initAuth();
-    });
-
-    it('returns an empty string when there was an error callback', () => {
-      queryObject = {
-        error: 'invalid_request',
-      };
-
-      expect(() => {
-        initAuth();
-      }).toThrow();
-    });
-  });
-
-  describe('Retrieving the auth headers', () => {
-    it('Creates an object defining the headers', () => {
-      savedAccessToken = '123AccessToken';
-      initAuth();
-      const authHeaders = getAuthHeaders();
-
-      expect(authHeaders).toEqual({
-        Authorization: 'Bearer 123AccessToken',
-      });
-    });
-
-    it('Creates an object defining no headers when no access token', () => {
-      initAuth();
-      const authHeaders = getAuthHeaders();
-
-      expect(authHeaders).toEqual({});
-    });
-  });
-
-  describe('Retrieving the auth domain', () => {
-    it('returns the auth domain', () => {
+  describe('getOauthDomain', () => {
+    it('returns current oAuthDomain', () => {
       savedOauthDomain = 'datapunt';
       const oauthDomain = getOauthDomain();
 
@@ -369,116 +48,125 @@ describe('The auth service', () => {
     });
   });
 
-  describe('isAuthenticated', () => {
-    it('returns false for expired token', () => {
-      global.localStorage.getItem.mockImplementation(key => {
-        switch (key) {
-          case 'accessToken':
-            return expiredToken;
-          default:
-            return '';
-        }
-      });
+  describe('getAuth', () => {
+    it('returns keycloak instance when keycloak is the current oAuth domain', () => {
+      savedOauthDomain = 'keycloak';
+      configuration.keycloak = {};
 
-      expect(isAuthenticated()).toEqual(false);
+      expect(getAuth()).toBeInstanceOf(Keycloak);
     });
 
-    it('returns false for invalid token', () => {
-      global.localStorage.getItem.mockImplementation(key => {
-        switch (key) {
-          case 'accessToken':
-            return invalidToken;
-          default:
-            return '';
-        }
-      });
+    it('returns authz instance when current oAuth domain is not keycloak', () => {
+      savedOauthDomain = 'other';
 
-      expect(isAuthenticated()).toEqual(false);
+      expect(getAuth()).toBeInstanceOf(Authz);
     });
 
-    it('returns true', () => {
-      const actual = jest.requireActual('./services/access-token-parser/access-token-parser').default;
-      accessTokenParser.mockImplementation(actual);
+    it('returns authz instance when current oAuth domain is not set', () => {
+      savedOauthDomain = null;
 
-      global.localStorage.getItem.mockImplementation(key => {
-        switch (key) {
-          case 'accessToken':
-            return validToken;
-          default:
-            return '';
-        }
-      });
+      expect(getAuth()).toBeInstanceOf(Authz);
+    });
 
-      expect(isAuthenticated()).toEqual(true);
+    it('returns authz instance when keycloak is not configured', () => {
+      delete configuration.keycloak;
+      savedOauthDomain = 'keycloak';
+
+      expect(getAuth()).toBeInstanceOf(Authz);
     });
   });
 
-  describe('getScopes', () => {
-    it('should return a an empty array', () => {
-      savedAccessToken = '123AccessToken';
-      initAuth();
-      const scopes = getScopes();
+  describe('getAccessToken', () => {
+    it('calls getAccessToken function on auth instance', () => {
+      savedOauthDomain = 'keycloak';
+      const auth = getAuth();
+      jest.spyOn(auth, 'getAccessToken');
 
-      expect(scopes).toEqual([]);
-    });
+      getAccessToken();
 
-    it('should return the scopes', () => {
-      accessTokenParser.mockImplementation(() => ({
-        scopes: ['SIG/ALL'],
-      }));
-
-      savedAccessToken = '123AccessToken';
-      initAuth();
-      const scopes = getScopes();
-
-      expect(scopes).toEqual(['SIG/ALL']);
+      expect(auth.getAccessToken).toHaveBeenCalled();
     });
   });
 
-  describe('getName', () => {
-    it('should return a an empty string', () => {
-      savedAccessToken = '123AccessToken';
-      initAuth();
-      const name = getName();
+  describe('getAccessToken', () => {
+    it('calls getAccessToken function on auth instance', () => {
+      savedOauthDomain = 'keycloak';
+      const auth = getAuth();
+      jest.spyOn(auth, 'isAuthenticated');
 
-      expect(name).toEqual('');
+      isAuthenticated();
+
+      expect(auth.isAuthenticated).toHaveBeenCalled();
     });
+  });
 
-    it('should return the scopes', () => {
-      accessTokenParser.mockImplementation(() => ({
-        name: 'Jan Klaasen',
-      }));
+  describe('getAuthHeaders', () => {
+    it('calls getAuthHeaders function on auth instance', () => {
+      savedOauthDomain = 'keycloak';
+      const auth = getAuth();
+      jest.spyOn(auth, 'getAuthHeaders');
 
-      savedAccessToken = '123AccessToken';
-      initAuth();
-      const name = getName();
+      getAuthHeaders();
 
-      expect(name).toEqual('Jan Klaasen');
+      expect(auth.getAuthHeaders).toHaveBeenCalled();
     });
   });
 
   describe('authenticate', () => {
-    it('should authenticate with credentials with accessToken', () => {
-      accessTokenParser.mockImplementation(() => ({
-        name: 'Jan Klaasen',
-        scopes: ['SIG/ALL'],
-      }));
-      savedAccessToken = '123AccessToken';
+    it('calls authenticate function on auth instance', () => {
+      savedOauthDomain = 'keycloak';
+      const auth = getAuth();
+      jest.spyOn(auth, 'authenticate');
 
-      expect(authenticate()).toEqual({
-        userName: 'Jan Klaasen',
-        userScopes: ['SIG/ALL'],
-        accessToken: '123AccessToken',
-      });
+      authenticate();
+
+      expect(auth.authenticate).toHaveBeenCalled();
+    });
+  });
+
+  describe('login', () => {
+    it('calls login for keycloak instance', () => {
+      configuration.keycloak = {};
+      savedOauthDomain = 'keycloak';
+      const auth = getAuth();
+      jest.spyOn(auth, 'login');
+
+      login('keycloak');
+
+      expect(auth.login).toHaveBeenCalled();
+      expect(global.localStorage.setItem).toHaveBeenCalledWith('oauthDomain', 'keycloak');
     });
 
-    it('should not authenticate without accessToken', () => {
-      accessTokenParser.mockImplementation(() => ({
-        name: 'Jan Klaasen',
-        scopes: ['SIG/ALL'],
-      }));
+    it('calls login for authz instance', () => {
+      savedOauthDomain = '';
+      const auth = getAuth();
+      jest.spyOn(auth, 'login');
 
-      expect(authenticate()).toEqual(null);
+      login('datapunt');
+
+      expect(auth.login).toHaveBeenCalledWith('datapunt');
+      expect(global.localStorage.setItem).toHaveBeenCalledWith('oauthDomain', 'datapunt');
+    });
+
+    it('throws when user has no storage support', () => {
+      const storage = global.Storage;
+      global.Storage = undefined;
+
+      expect(login('datapunt')).rejects.toThrow('Storage not available; cannot proceed with logging in');
+
+      global.Storage = storage;
+    });
+  });
+
+  describe('logout', () => {
+    it('calls logout function', () => {
+      const auth = getAuth();
+      jest.spyOn(auth, 'logout');
+
+      logout();
+
+      expect(auth.logout).toHaveBeenCalled();
+      expect(global.localStorage.removeItem).toHaveBeenCalledWith('oauthDomain');
     });
   });
 });
---
+++ src/shared/services/auth/services/keycloak.js
@@ -0,0 +1,112 @@
+import KeycloakJS from 'keycloak-js';
+
+import configuration from '../../configuration/configuration';
+import parseAccessToken from './parse-access-token/parse-access-token';
+
+const AUTH_REDIRECT_URI = `${global.location.protocol}//${global.location.host}/manage/incidents`;
+
+class Keycloak {
+  constructor() {
+    const { authEndpoint, realm, clientId } = configuration.keycloak || {};
+
+    this.refreshIntervalId = null;
+    this.keycloak = new KeycloakJS({
+      url: authEndpoint,
+      realm,
+      clientId,
+    });
+    this.isInitialized = false;
+
+    this.keycloak.onAuthSuccess = () => this.startRefreshInterval();
+
+    this.keycloak.onAuthError = () => this.stopRefreshInterval();
+    this.keycloak.onAuthRefreshError = () => this.stopRefreshInterval();
+    this.keycloak.onAuthLogout = () => this.stopRefreshInterval();
+    this.keycloak.onTokenExpired = () => {
+      // This should never happen (auto refresh should keep token valid)
+      this.stopRefreshInterval();
+      this.logout();
+    };
+  }
+
+  // eslint-disable-next-line require-await
+  async init(options) {
+    this.isInitialized = true;
+
+    return this.keycloak.init({
+      promiseType: 'native', // To enable async/await
+      'check-sso': false, // To enable refresh token
+      checkLoginIframe: false, // To enable refresh token
+      pkceMethod: 'S256',
+      redirectUri: AUTH_REDIRECT_URI,
+      ...options,
+    });
+  }
+
+  async authenticate() {
+    if (!this.isInitialized) {
+      await this.init({
+        onLoad: 'check-sso',
+      });
+    }
+
+    if (this.isAuthenticated()) {
+      const accessToken = this.getAccessToken();
+      const { name, scopes } = parseAccessToken(accessToken);
+
+      return {
+        userName: name,
+        userScopes: scopes,
+        accessToken,
+      };
+    }
+
+    return null;
+  }
+
+  isAuthenticated() {
+    return this.keycloak.authenticated && !this.keycloak.isTokenExpired();
+  }
+
+  getAuthHeaders() {
+    const token = this.getAccessToken();
+
+    return token ? { Authorization: `Bearer ${token}` } : {};
+  }
+
+  getAccessToken() {
+    return this.keycloak.token;
+  }
+
+  async login() {
+    if (!this.isInitialized) {
+      await this.init();
+    }
+
+    this.keycloak.login();
+  }
+
+  logout() {
+    this.keycloak.logout();
+  }
+
+  startRefreshInterval() {
+    // Refresh the token automatically once the user has been authenticated
+    const minValidity = 30; // Token should be valid for at least the next 30 seconds
+    const updateInterval = minValidity * 0.75; // Keep token valid by checking regularly
+
+    // Start a token updater, if not yet running
+    if (!this.refreshIntervalId) {
+      this.refreshIntervalId = setInterval(() => {
+        this.keycloak.updateToken(minValidity);
+      }, updateInterval * 1000);
+    }
+  }
+
+  stopRefreshInterval() {
+    clearInterval(this.refreshIntervalId);
+    this.refreshIntervalId = null;
+  }
+}
+
+export default Keycloak;
