f62cd257207a5dee7fd801e242694dc1cafe404b
Maurits Ridder <maurits.ridder@gmail.com>
2021-02-04 15:55:08 +0100

    Implement review comments

--- src/signals/incident-management/containers/IncidentDetail/components/Detail/components/ExtraProperties/index.tsx
+++ src/signals/incident-management/containers/IncidentDetail/components/Detail/components/ExtraProperties/index.tsx
@@ -4,15 +4,12 @@ import type { Answer, MappedLegacyItem, Item, CheckboxInput, ExtraPropertiesType
 
 const getValue = (answer: Answer | LegacyAnswer): string | JSX.Element[] => {
   if (Array.isArray(answer)) {
-    return answer.map((item, index) => {
+    return answer.map(item => {
       if (typeof item !== 'string') {
-        if (item.description) {
-          return <div key={item.id}>{`${item.description}${item.id && ` - ${item.id}`}`}</div>;
-        }
+        return <div key={item.id}>{`${item.description}${item.id && ` - ${item.id}`}`}</div>;
       }
 
-      // eslint-disable-next-line react/no-array-index-key
-      return <div key={index}>{item}</div>;
+      return <div key={item}>{item}</div>;
     });
   }
 
@@ -34,24 +31,21 @@ interface ExtraPropertiesProps {
 const ExtraProperties: FunctionComponent<ExtraPropertiesProps> = ({ items = [] }) => {
   // Some incidents have been stored with values for their extra properties that is incompatible with the current API
   // We therefore need to check if we're getting an array or an object
-  const itemList = Array.isArray(items)
+  const itemList: (Item | MappedLegacyItem)[] = Array.isArray(items)
     ? items
     : Object.entries(items).map(([question, answer], index) => ({ label: question, answer, id: index.toString() }));
 
-  const extraProperties: JSX.Element[] = [];
-
-  itemList.forEach((item: Item | MappedLegacyItem) => {
-    extraProperties.push(
-      <Fragment key={item.id}>
-        <dt data-testid="extra-properties-definition">{item.label}</dt>
-        <dd data-testid="extra-properties-value">{getValue(item.answer)}</dd>
-      </Fragment>
-    );
-  });
-
-  // TypeScript does not support arrays as a return type in function components - return a fragment as a workaround.
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <Fragment>{extraProperties}</Fragment>;
+  return (
+    // TypeScript does not support arrays as a return type in function components - return a fragment as a workaround.
+    <Fragment>
+      {itemList.map(item => (
+        <Fragment key={item.id}>
+          <dt data-testid="extra-properties-definition">{item.label}</dt>
+          <dd data-testid="extra-properties-value">{getValue(item.answer)}</dd>
+        </Fragment>
+      ))}
+    </Fragment>
+  );
 };
 
 export default ExtraProperties;
