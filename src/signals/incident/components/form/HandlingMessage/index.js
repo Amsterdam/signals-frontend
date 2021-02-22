1fb6056ad64b94e4fa845cf93f340aec2395f9c6
Maurits Ridder <maurits.ridder@gmail.com>
2021-02-22 15:27:31 +0100

    Clean up styling

--- src/signals/incident/components/form/HandlingMessage/index.js
+++ src/signals/incident/components/form/HandlingMessage/index.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 import isString from 'lodash.isstring';
 import get from 'lodash.get';
 
-import './style.scss';
-
 const renderText = (key, name, parent) => {
   const replacedValue = get(parent, `meta.incidentContainer.${key}`);
   if (replacedValue) {
     return replacedValue.split('\n\n').map((item, k) => (
-      <div key={`${name}-${k + 1}`} className="plain-text__box-p">
+      <div key={`${name}-${k + 1}`}>
         {item}
       </div>
     ));
@@ -21,13 +19,11 @@ const renderText = (key, name, parent) => {
 
 const HandlingMessage = ({ meta, parent }) =>
   meta?.isVisible && (
-    <div className={`${meta.className || 'col-12'} mode_input`}>
-      <div className="handling-message__box">
-        <Heading as="h2" styleAs="h3">
-          {meta.title}
-        </Heading>
-        {meta.key && isString(meta.key) && renderText(meta.key, meta.name, parent)}
-      </div>
+    <div className="col-12">
+      <Heading as="h2" styleAs="h3">
+        {meta.title}
+      </Heading>
+      {meta.key && isString(meta.key) && renderText(meta.key, meta.name, parent)}
     </div>
   );
 
