9042e8abfb75a6a801e73f7db7ec07dc2cf3b111
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-03-08 14:51:09 +0100

    adds a group for the checkboxes and fixes the distance

--- src/signals/incident/components/form/CheckboxInput/index.js
+++ src/signals/incident/components/form/CheckboxInput/index.js
@@ -7,6 +7,7 @@ import { Label } from '@amsterdam/asc-ui';
 import Checkbox from 'components/Checkbox';
 
 import Header from '../Header';
+import styled from 'styled-components';
 
 function updateIncidentCheckboxMulti(checked, value, key, oldValue, meta, parent) {
   let output = [...oldValue];
@@ -22,42 +23,57 @@ function updateIncidentCheckboxMulti(checked, value, key, oldValue, meta, parent
   parent.meta.updateIncident({ [meta.name]: output });
 }
 
+const CheckboxWrapper = styled.div`
+  & > :first-child {
+    margin-top: -6px; /* This margin enforces the 12px design distance from the title description */
+  }
+`;
+
+const CheckboxGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const CheckboxInput = ({ handler, touched, hasError, meta, parent, getError, validatorsOrOpts }) =>
   meta.isVisible && (
     <Header meta={meta} options={validatorsOrOpts} touched={touched} hasError={hasError} getError={getError}>
-      {isObject(meta.values) ? (
-        <Fragment>
-          <input type="hidden" {...handler()} />
+      <CheckboxWrapper>
+        {isObject(meta.values) ? (
+          <CheckboxGroup role="group">
+            <input type="hidden" {...handler()} />
 
-          {map(meta.values, (value, key) => (
-            <Label htmlFor={`${meta.name}-${key + 1}`} label={value} noActiveState>
-              <Checkbox
-                id={`${meta.name}-${key + 1}`}
-                name={`${meta.name}-${key + 1}`}
-                value={key}
-                checked={(handler().value || []).find(item => item.id === key)}
-                onClick={e => updateIncidentCheckboxMulti(e.target.checked, value, key, handler().value, meta, parent)}
-              />
-            </Label>
-          ))}
-        </Fragment>
-      ) : (
-        <Label htmlFor={meta.name} label={meta.value} noActiveState>
-          <Checkbox
-            id={meta.name}
-            name={meta.name}
-            checked={handler().value.value}
-            onClick={e => {
-              parent.meta.updateIncident({
-                [meta.name]: {
-                  label: meta.value,
-                  value: e.target.checked,
-                },
-              });
-            }}
-          />
-        </Label>
-      )}
+            {map(meta.values, (value, key) => (
+              <Label htmlFor={`${meta.name}-${key + 1}`} label={value} noActiveState>
+                <Checkbox
+                  id={`${meta.name}-${key + 1}`}
+                  name={`${meta.name}-${key + 1}`}
+                  value={key}
+                  checked={(handler().value || []).find(item => item.id === key)}
+                  onClick={e =>
+                    updateIncidentCheckboxMulti(e.target.checked, value, key, handler().value, meta, parent)
+                  }
+                />
+              </Label>
+            ))}
+          </CheckboxGroup>
+        ) : (
+          <Label htmlFor={meta.name} label={meta.value} noActiveState>
+            <Checkbox
+              id={meta.name}
+              name={meta.name}
+              checked={handler().value.value}
+              onClick={e => {
+                parent.meta.updateIncident({
+                  [meta.name]: {
+                    label: meta.value,
+                    value: e.target.checked,
+                  },
+                });
+              }}
+            />
+          </Label>
+        )}
+      </CheckboxWrapper>
     </Header>
   );
 
