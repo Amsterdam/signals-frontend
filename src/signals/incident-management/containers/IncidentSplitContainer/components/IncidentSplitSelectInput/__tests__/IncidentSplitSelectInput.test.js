55327e139acc5e6e9e2249018e34f9511a365fa7
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-03-10 10:06:39 +0100

    fixes unittests

--- src/signals/incident-management/containers/IncidentSplitContainer/components/IncidentSplitSelectInput/__tests__/IncidentSplitSelectInput.test.js
+++ src/signals/incident-management/containers/IncidentSplitContainer/components/IncidentSplitSelectInput/__tests__/IncidentSplitSelectInput.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { withAppContext } from 'test/utils';
 import priorityList from 'signals/incident-management/definitions/priorityList';
 
