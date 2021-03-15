d695046d499cc95a2c5e85ac0456ed632127d9b8
Maurits Ridder <maurits.ridder@gmail.com>
2021-03-15 13:15:00 +0100

    Update jest coverage

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 96.32,
+      branches: 96.30,
       statements: 99.59,
       functions: 98.69,
       lines: 99.65,
