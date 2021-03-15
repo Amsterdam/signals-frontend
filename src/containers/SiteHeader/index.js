1aec381c855224c52943090dbcebd6b613320168
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-03-15 13:40:36 +0100

    uses redux hooks

--- src/containers/SiteHeader/index.js
+++ src/containers/SiteHeader/index.js
@@ -1,49 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import { compose, bindActionCreators } from 'redux';
 import { createStructuredSelector } from 'reselect';
-import {
-  makeSelectUserCan,
-  makeSelectUserCanAccess,
-} from 'containers/App/selectors';
+import { makeSelectUserCan, makeSelectUserCanAccess } from 'containers/App/selectors';
 import SiteHeader from 'components/SiteHeader';
 
 import { doLogout } from '../App/actions';
 
-export const SiteHeaderContainer = ({ onLogOut, userCan, userCanAccess }) => (
-  <SiteHeader
-    onLogOut={onLogOut}
-    showItems={{
-      defaultTexts: userCan('sia_statusmessagetemplate_write'),
-      departments: userCanAccess('departments'),
-      groups: userCanAccess('groups'),
-      settings: userCanAccess('settings'),
-      users: userCanAccess('users'),
-      categories: userCanAccess('categories'),
-    }}
-  />
-);
-
-SiteHeaderContainer.propTypes = {
-  onLogOut: PropTypes.func,
-  userCan: PropTypes.func,
-  userCanAccess: PropTypes.func,
+export const SiteHeaderContainer = () => {
+  const userCan = useSelector(makeSelectUserCan);
+  const userCanAccess = useSelector(makeSelectUserCanAccess);
+  const dispatch = useDispatch();
+  return (
+    <SiteHeader
+      onLogOut={() => dispatch(doLogout)}
+      showItems={{
+        defaultTexts: userCan('sia_statusmessagetemplate_write'),
+        departments: userCanAccess('departments'),
+        groups: userCanAccess('groups'),
+        settings: userCanAccess('settings'),
+        users: userCanAccess('users'),
+        categories: userCanAccess('categories'),
+      }}
+    />
+  );
 };
 
-const mapStateToProps = createStructuredSelector({
-  userCan: makeSelectUserCan,
-  userCanAccess: makeSelectUserCanAccess,
-});
-
-export const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      onLogOut: doLogout,
-    },
-    dispatch
-  );
-
-const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-export default compose(withConnect)(SiteHeaderContainer);
+export default SiteHeaderContainer;
