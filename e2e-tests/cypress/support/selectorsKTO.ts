1516d520b9ba6fb57b1ca23bcc9c5d6eb9a917ee
arjandata <61686761+arjandata@users.noreply.github.com>
2021-03-24 15:47:42 +0100

    [SIG-2355] Cypress tests KTO form (#1485)

---
+++ e2e-tests/cypress/support/selectorsKTO.ts
@@ -0,0 +1,24 @@
+export const MAIL = {
+  lastMail: '.messages > :nth-child(1)',
+};
+
+export const KTO_FORM = {
+  checkboxContact: '[data-testid=ktoAllowsContact]',
+  errorMessage: '[class*="ErrorMessage"]',
+  inputAnders: '[data-testid=ktoText]',
+  inputVermelden: '[data-testid=ktoTextExtra]',
+  questionLabel: '[class*="KtoForm__StyledLabel"]',
+  radioButtonOntevredenAnders: '[data-testid="kto-anders"]',
+  radioButtonOntevredenContactSlecht: '[data-testid="kto-1"]',
+  radioButtonOntevredenNietSerieus: '[data-testid="kto-2"]',
+  radioButtonOntevredenNietsGedaan: '[data-testid="kto-3"]',
+  radioButtonOntevredenOnvoldoendeInfo: '[data-testid="kto-4"]',
+  radioButtonOntevredenOplossingVolstaatNiet: '[data-testid="kto-5"]',
+  radioButtonOntevredenTeLang: '[data-testid="kto-0"]',
+  radioButtonTevredenAnders: '[data-testid="kto-anders"]',
+  radioButtonTevredenCommunicatie: '[data-testid="kto-0"]',
+  radioButtonTevredenGoedOpgepakt: '[data-testid="kto-1"]',
+  radioButtonTevredenProbleemVerholpen: '[data-testid="kto-2"]',
+  radioButtonTevredenSnel: '[data-testid="kto-3"]',
+  subtitleQuestion: '#subtitle-kto',
+};
--- e2e-tests/cypress/support/texts.ts
+++ e2e-tests/cypress/support/texts.ts
@@ -2,6 +2,21 @@ export const ERROR_MESSAGES = {
   email: 'Vul een geldig e-mailadres in, met een @ en een domeinnaam. Bijvoorbeeld: naam@domein.nl',
   location: 'Kies een locatie op de kaart of vul een adres in',
   mandatoryField: 'Dit is een verplicht veld',
+  mandatoryFieldKTO: 'Dit veld is verplicht',
+};
+
+export const KTO = {
+  formTitelIsAlFeedback: 'Er is al feedback gegeven voor deze melding',
+  formTitleBedankt: 'Bedankt voor uw feedback!',
+  formTitleOnTevreden: 'Nee, ik ben niet tevreden met de behandeling van mijn melding',
+  formTitleTevreden: 'Ja, ik ben tevreden met de behandeling van mijn melding',
+  questionContact: 'Mogen wij contact met u opnemen naar aanleiding van uw feedback? (optioneel)',
+  questionVermelden: 'Wilt u verder nog iets vermelden of toelichten? (optioneel)',
+  questionWaaromOntevreden: 'Waarom bent u ontevreden?',
+  questionWaaromTevreden: 'Waarom bent u tevreden?',
+  subtitleQuestionTevreden: 'Een antwoord mogelijk, kies de belangrijkste reden',
+  textBedankt: 'We zijn voortdurend bezig onze dienstverlening te verbeteren.',
+  textNogmaalsBedankt: 'Nogmaals bedankt voor uw feedback. We zijn voortdurend bezig onze dienstverlening te verbeteren.',
 };
 
 export const NOTIFICATONS = {
