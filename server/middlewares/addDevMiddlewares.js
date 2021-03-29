749f92e0384dc45cf4292d713c0a2b7bcd2ae30e
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-03-29 12:31:39 +0200

    Revert "[SIG-3571] Upgrade to webpack 5"

--- server/middlewares/addDevMiddlewares.js
+++ server/middlewares/addDevMiddlewares.js
@@ -3,18 +3,25 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
+function createWebpackMiddleware(compiler, publicPath) {
+  return webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath,
+    silent: true,
+    stats: 'errors-only',
+  });
+}
+
 module.exports = function addDevMiddlewares(app, webpackConfig) {
   const compiler = webpack(webpackConfig);
-  const middleware = webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-  });
+  const middleware = createWebpackMiddleware(compiler, webpackConfig.output.publicPath);
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
 
   // Since webpackDevMiddleware uses memory-fs internally to store build
   // artifacts, we use it instead
-  const fs = middleware.context.outputFileSystem;
+  const fs = middleware.fileSystem;
 
   app.get('*', (req, res) => {
     fs.readFile(path.join(compiler.outputPath, 'index.html'), (err, file) => {
