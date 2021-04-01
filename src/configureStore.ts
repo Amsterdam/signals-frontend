287a9441d8e0753cec624506207f1454cad26e01
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-04-01 14:02:09 +0200

    converts configureStore to ts

---
+++ src/configureStore.ts
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2018 - 2021 Gemeente Amsterdam
+/**
+ * Create the store with dynamic reducers
+ */
+
+import type { Store } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'connected-react-router/immutable';
+import type { Saga } from 'redux-saga';
+import createSagaMiddleware from 'redux-saga';
+
+import { showGlobalNotification } from 'containers/App/actions'
+import { VARIANT_ERROR, TYPE_GLOBAL } from 'containers/Notification/constants'
+import { getErrorMessage } from 'shared/services/api/api'
+
+import type { History } from 'history';
+import createReducer from './reducers';
+
+const windowWithReduxDevTools: Window & typeof globalThis & {
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: (options: { trace: boolean; traceLimit: number }) => typeof compose;
+} = window;
+
+export interface InjectedStore extends Store {
+  injectedReducers: any;
+  injectedSagas: any;
+  runSaga: <S extends Saga>(saga: S, ...args: Parameters<S>) => any;
+}
+
+type JsonResponse = Response & { jsonBody: { message: string } };
+
+export class ResponseError extends Error {
+  public response: JsonResponse;
+
+  public message: string;
+
+  public constructor(response: JsonResponse, message = '') {
+    super(response.statusText);
+    this.response = response;
+    this.message = message;
+  }
+}
+
+export default function configureStore(initialState: Record<string, any>, history: History) {
+  let composeEnhancers = compose;
+  const reduxSagaMonitorOptions = {
+    onError: (error: ResponseError) => {
+      const message = (error.response?.jsonBody?.message) || error.message;
+      const notificationTitle = getErrorMessage(error);
+
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      store.dispatch(
+        showGlobalNotification({
+          title: notificationTitle,
+          message,
+          variant: VARIANT_ERROR,
+          type: TYPE_GLOBAL,
+        })
+      )
+    },
+  }
+
+  // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
+  /* istanbul ignore next */
+  if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
+    if (windowWithReduxDevTools.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+      composeEnhancers = windowWithReduxDevTools.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 });
+    }
+    /* eslint-enable */
+  }
+
+  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions)
+
+  // Create the store with two middlewares
+  // 1. sagaMiddleware: Makes redux-sagas work
+  // 2. routerMiddleware: Syncs the location/URL path to the state
+  const middlewares = [sagaMiddleware, routerMiddleware(history)]
+
+  const enhancers = [applyMiddleware(...middlewares)]
+
+  const store = createStore(
+    createReducer(),
+    initialState,
+    composeEnhancers(...enhancers)
+  ) as InjectedStore;
+
+  // Extensions
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  store.runSaga = sagaMiddleware.run;
+  store.injectedReducers = {}; // Reducer registry
+  store.injectedSagas = {}; // Saga registry
+
+  // Make reducers hot reloadable, see http://mxs.is/googmo
+  /* istanbul ignore next */
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      store.replaceReducer(createReducer(store.injectedReducers))
+    })
+  }
+
+  return store
+}
