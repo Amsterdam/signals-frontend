f20f685a5db36cb3c4a43dd7a79f19992749620c
Boris Arkenaar <boris@arkenaar.com>
2021-04-20 08:20:23 +0200

    Ignore dist directory

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
   },
   moduleDirectories: ['node_modules', 'src'],
-  modulePathIgnorePatterns: ['<rootDir>/internals/'],
+  modulePathIgnorePatterns: ['<rootDir>/internals/', '<rootDir>/dist/'],
   moduleNameMapper: {
     '.*\\.(css|less|styl|scss|sass)$': '<rootDir>/internals/mocks/cssModule.js',
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
