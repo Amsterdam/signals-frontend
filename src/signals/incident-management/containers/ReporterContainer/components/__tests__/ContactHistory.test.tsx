584e135a072a60808e270faf3eda14a8e1fa34c2
Maurits Ridder <maurits.ridder@gmail.com>
2021-05-10 17:55:39 +0200

    Implements new reporter detail view design

---
+++ src/signals/incident-management/containers/ReporterContainer/components/__tests__/ContactHistory.test.tsx
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2021 Gemeente Amsterdam
+import { render, screen, waitFor } from '@testing-library/react'
+import * as reactRedux from 'react-redux'
+import { showGlobalNotification } from 'containers/App/actions'
+import { withAppContext } from 'test/utils'
+
+import {
+  fetchMock,
+  mockRequestHandler,
+} from '../../../../../../../internals/testing/msw-server'
+
+import ContactHistory from '../ContactHistory'
+
+const dispatch = jest.fn()
+jest.spyOn(reactRedux, 'useDispatch').mockImplementation(() => dispatch)
+
+describe('ContactHistory', () => {
+  beforeAll(() => {
+    fetchMock.disableMocks()
+  })
+
+  afterEach(() => {
+    dispatch.mockReset()
+  })
+
+  it('renders loading indicator', () => {
+    render(withAppContext(<ContactHistory id={4440} />))
+
+    expect(screen.getByTestId('loadingIndicator')).toBeInTheDocument()
+  })
+
+  it('renders an error response', async () => {
+    mockRequestHandler({
+      status: 500,
+      body: 'Internal server error',
+    })
+
+    render(withAppContext(<ContactHistory id={4440} />))
+
+    await waitFor(() =>
+      expect(screen.queryByTestId('loadingIndicator')).not.toBeInTheDocument()
+    )
+
+    expect(dispatch).toHaveBeenCalledWith(
+      showGlobalNotification(
+        expect.objectContaining({
+          title:
+            'De data kon niet opgehaald worden. probeer het later nog eens.',
+        })
+      )
+    )
+  })
+
+  it('renders a list of history items', async () => {
+    render(withAppContext(<ContactHistory id={4440} />))
+
+    await screen.findByRole('list')
+
+    expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getAllByRole('listitem')).toHaveLength(2)
+  })
+})
