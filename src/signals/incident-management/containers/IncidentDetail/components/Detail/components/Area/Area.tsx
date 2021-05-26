19cae2eb04678ba7a1c391a6dc52459a0955daf5
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2021-05-26 10:00:23 +0200

    add Area unit tests

--- src/signals/incident-management/containers/IncidentDetail/components/Detail/components/Area/Area.tsx
+++ src/signals/incident-management/containers/IncidentDetail/components/Detail/components/Area/Area.tsx
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 Gemeente Amsterdam
-import { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import { Link as AscLink } from '@amsterdam/asc-ui'
 
 import { INCIDENT_URL } from 'signals/incident-management/routes'
 
 const Area: React.FC<{ count: number; id: number }> = ({ count, id }) => (
-  <Fragment>
+  <>
     <dt data-testid="detail-area-definition">Omgeving</dt>
     <dd data-testid="detail-area-value">
       <AscLink as={Link} variant="inline" to={`${INCIDENT_URL}/${id}/omgeving`}>
         {count} {count === 1 ? 'melding' : 'meldingen'} in deze omgeving
       </AscLink>
     </dd>
-  </Fragment>
+  </>
 )
 
 export default Area
