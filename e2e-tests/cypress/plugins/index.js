be6ced5038a908eb4708df2fe1bae53550656fa7
Arjan <arjan@desoftwaretesters.nl>
2021-06-02 09:15:40 +0200

    Revert change in plugin file

--- e2e-tests/cypress/plugins/index.js
+++ e2e-tests/cypress/plugins/index.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import webpack from '@cypress/webpack-preprocessor';
-export default on => {
+const webpack = require('@cypress/webpack-preprocessor');
+module.exports = on => {
   const options = {
     webpackOptions: require('../../webpack.config'),
     watchOptions: {}
