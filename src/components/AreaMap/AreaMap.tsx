0856d9cde88dcc4f9e1748107c80619addfeb278
Maurits Ridder <maurits.ridder@gmail.com>
2021-06-21 11:47:43 +0200

    Changes default zoom level

--- src/components/AreaMap/AreaMap.tsx
+++ src/components/AreaMap/AreaMap.tsx
@@ -28,7 +28,7 @@ import MapCloseButton from 'components/MapCloseButton'
 
 import { AreaFeature, AreaFeatureCollection, Property } from './types'
 
-const DEFAULT_ZOOM = 15
+const DEFAULT_ZOOM = 14
 const MAX_ZOOM = 15
 const FOCUS_RADIUS_METERS = 50
 const CURRENT_INCIDENT_MARKER_Z = -100 // Show below incident markers
