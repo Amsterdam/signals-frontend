103efd20887b0c5df40f53c5c02b0d0489932a23
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2021-06-22 15:11:32 +0200

    wreck e2e tests on github actions

--- e2e-tests/cypress/integration/createSignals/createSignalBedrijvenOverlastStank.spec.ts
+++ e2e-tests/cypress/integration/createSignals/createSignalBedrijvenOverlastStank.spec.ts
@@ -1,82 +1,122 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import { BEDRIJVEN_HORECA, STANK_OVERLAST } from '../../support/selectorsCreateSignal';
-import { MANAGE_SIGNALS } from '../../support/selectorsManageIncidents';
-import questions from '../../fixtures/questions/questions.json';
-import { generateToken } from '../../support/jwt';
-import signal from '../../fixtures/signals/bedrijvenStank.json';
-import * as routes from '../../support/commandsRouting';
-import * as createSignal from '../../support/commandsCreateSignal';
+import {
+  BEDRIJVEN_HORECA,
+  STANK_OVERLAST,
+} from '../../support/selectorsCreateSignal'
+import { MANAGE_SIGNALS } from '../../support/selectorsManageIncidents'
+import questions from '../../fixtures/questions/questions.json'
+import { generateToken } from '../../support/jwt'
+import signal from '../../fixtures/signals/bedrijvenStank.json'
+import * as routes from '../../support/commandsRouting'
+import * as createSignal from '../../support/commandsCreateSignal'
 
 describe('Create signal "Bedrijven overlast stank" and check signal details', () => {
   describe('Create signal overlast stank', () => {
     before(() => {
-      routes.postSignalRoutePublic();
-      routes.stubPreviewMap();
-      routes.stubMap();
-      cy.visit('incident/beschrijf');
-    });
+      routes.postSignalRoutePublic()
+      routes.stubPreviewMap()
+      routes.stubMap()
+      cy.visit('incident/beschrijf')
+    })
 
     it('Should create the signal', () => {
-      createSignal.setDescriptionPage(signal);
-      cy.contains('Volgende').click();
+      createSignal.setDescriptionPage(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.checkSpecificInformationPage(signal);
+      createSignal.checkSpecificInformationPage(signal)
 
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_wat.label).should('be.visible');
-      cy.get(BEDRIJVEN_HORECA.radioButtonAnderBedrijf).click({ force: true });
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_naam.label).should('be.visible');
-      cy.get(BEDRIJVEN_HORECA.inputWieWat).type('Klanten van de sportsschool');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_adres.label).should('be.visible');
-      cy.get(BEDRIJVEN_HORECA.inputAdres).type('Op de Klapperweg nummer 39');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank.label).should('be.visible');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank.subtitle).should('be.visible');
-      cy.get(STANK_OVERLAST.inputGeur).type('Een zeer indringende zweetgeur');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_oorzaak.label).should('be.visible');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_oorzaak.subtitle).should('be.visible');
-      cy.get(STANK_OVERLAST.inputOorzaakGeur)
-        .type('Klanten van de sportschool die voor de deur staan te sporten, maar ook binnen voor een open raam');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_weer.label).should('be.visible');
-      cy.get(STANK_OVERLAST.inputWeersomstandigheden).type('Het is erg warm buiten, de zon schijnt volop');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_ramen.label).should('be.visible');
-      cy.get(STANK_OVERLAST.radioButtonRaamOpen).click({ force: true });
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_vaker.label).should('be.visible');
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_vaker.subtitle).should('be.visible');
-      cy.get(BEDRIJVEN_HORECA.radioButtonVakerNee).click({ force: true });
-      cy.contains(questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_caution.answers).should('be.visible');
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_wat.label
+      ).should('not.be.visible')
+      cy.get(BEDRIJVEN_HORECA.radioButtonAnderBedrijf).click({ force: true })
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_naam.label
+      ).should('not.be.visible')
+      cy.get(BEDRIJVEN_HORECA.inputWieWat).type('Klanten van de sportsschool')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_adres.label
+      ).should('not.be.visible')
+      cy.get(BEDRIJVEN_HORECA.inputAdres).type('Op de Klapperweg nummer 39')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank.label
+      ).should('be.visible')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank
+          .subtitle
+      ).should('be.visible')
+      cy.get(STANK_OVERLAST.inputGeur).type('Een zeer indringende zweetgeur')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_oorzaak
+          .label
+      ).should('be.visible')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_oorzaak
+          .subtitle
+      ).should('be.visible')
+      cy.get(STANK_OVERLAST.inputOorzaakGeur).type(
+        'Klanten van de sportschool die voor de deur staan te sporten, maar ook binnen voor een open raam'
+      )
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_weer
+          .label
+      ).should('be.visible')
+      cy.get(STANK_OVERLAST.inputWeersomstandigheden).type(
+        'Het is erg warm buiten, de zon schijnt volop'
+      )
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_stank_ramen
+          .label
+      ).should('be.visible')
+      cy.get(STANK_OVERLAST.radioButtonRaamOpen).click({ force: true })
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_vaker.label
+      ).should('be.visible')
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_vaker
+          .subtitle
+      ).should('be.visible')
+      cy.get(BEDRIJVEN_HORECA.radioButtonVakerNee).click({ force: true })
+      cy.contains(
+        questions.overlastBedrijvenEnHoreca.extra_bedrijven_horeca_caution
+          .answers
+      ).should('be.visible')
 
-      cy.contains('Volgende').click();
+      cy.contains('Volgende').click()
 
-      createSignal.setPhonenumber(signal);
-      cy.contains('Volgende').click();
+      createSignal.setPhonenumber(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.setEmailAddress(signal);
-      cy.contains('Volgende').click();
+      createSignal.setEmailAddress(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.checkSummaryPage(signal);
-      cy.contains('Verstuur').click();
-      cy.wait('@postSignalPublic');
-      cy.get(MANAGE_SIGNALS.spinner).should('not.exist');
+      createSignal.checkSummaryPage(signal)
+      cy.contains('Verstuur').click()
+      cy.wait('@postSignalPublic')
+      cy.get(MANAGE_SIGNALS.spinner).should('not.exist')
 
-      createSignal.checkThanksPage();
-      createSignal.saveSignalId();
-    });
-  });
+      createSignal.checkThanksPage()
+      createSignal.saveSignalId()
+    })
+  })
   describe('Check data created signal', () => {
     before(() => {
-      localStorage.setItem('accessToken', generateToken('Admin', 'signals.admin@example.com'));
-      routes.getManageSignalsRoutes();
-      routes.getSignalDetailsRoutesById();
-      cy.visit('/manage/incidents/');
-      routes.waitForManageSignalsRoutes();
-    });
+      localStorage.setItem(
+        'accessToken',
+        generateToken('Admin', 'signals.admin@example.com')
+      )
+      routes.getManageSignalsRoutes()
+      routes.getSignalDetailsRoutesById()
+      cy.visit('/manage/incidents/')
+      routes.waitForManageSignalsRoutes()
+    })
 
     it('Should show the signal details', () => {
-      routes.stubPreviewMap();
-      createSignal.openCreatedSignal();
-      routes.waitForSignalDetailsRoutes();
+      routes.stubPreviewMap()
+      createSignal.openCreatedSignal()
+      routes.waitForSignalDetailsRoutes()
 
-      createSignal.checkAllDetails(signal, 'standaardmelding');
-    });
-  });
-});
+      createSignal.checkAllDetails(signal, 'standaardmelding')
+    })
+  })
+})
--- e2e-tests/cypress/integration/manageSettings/manageCategories.spec.ts
+++ e2e-tests/cypress/integration/manageSettings/manageCategories.spec.ts
@@ -1,222 +1,283 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import { CATEGORIES } from '../../support/selectorsSettings';
-import { CHANGE_CATEGORY, SIGNAL_DETAILS } from '../../support/selectorsSignalDetails';
-import { CREATE_SIGNAL } from '../../support/selectorsCreateSignal';
-import { MANAGE_SIGNALS } from '../../support/selectorsManageIncidents';
-import { generateToken } from '../../support/jwt';
-import signal from '../../fixtures/signals/signalForManageCategories.json';
-import * as routes from '../../support/commandsRouting';
-import * as createSignal from '../../support/commandsCreateSignal';
-import * as general from '../../support/commandsGeneral';
-import { SIZES } from '../../support/viewports';
+import { CATEGORIES } from '../../support/selectorsSettings'
+import {
+  CHANGE_CATEGORY,
+  SIGNAL_DETAILS,
+} from '../../support/selectorsSignalDetails'
+import { CREATE_SIGNAL } from '../../support/selectorsCreateSignal'
+import { MANAGE_SIGNALS } from '../../support/selectorsManageIncidents'
+import { generateToken } from '../../support/jwt'
+import signal from '../../fixtures/signals/signalForManageCategories.json'
+import * as routes from '../../support/commandsRouting'
+import * as createSignal from '../../support/commandsCreateSignal'
+import * as general from '../../support/commandsGeneral'
+import { SIZES } from '../../support/viewports'
 
 describe('Manage categories', () => {
   describe('Change category ', () => {
     beforeEach(() => {
-      general.setResolution(SIZES.laptopXS);
-      localStorage.setItem('accessToken', generateToken('Admin', 'signals.admin@example.com'));
-      routes.getManageSignalsRoutes();
-      routes.getCategoriesRoutes();
-      cy.visit('/manage/incidents/');
-      routes.waitForManageSignalsRoutes();
-    });
+      general.setResolution(SIZES.laptopXS)
+      localStorage.setItem(
+        'accessToken',
+        generateToken('Admin', 'signals.admin@example.com')
+      )
+      routes.getManageSignalsRoutes()
+      routes.getCategoriesRoutes()
+      cy.visit('/manage/incidents/')
+      routes.waitForManageSignalsRoutes()
+    })
 
     it('Should change the attributes of the category and shows changes in history', () => {
-      general.openMenu();
-      cy.contains('Instellingen').click();
-      cy.contains('Subcategorieën').click();
+      general.openMenu()
+      cy.contains('IInstellingen').click()
+      cy.contains('SSubcategorieën').click()
 
-      routes.waitForCategoriesRoutes();
-      general.checkHeaderText('Subcategorieën');
-      cy.url().should('include', '/instellingen/categorieen/');
+      routes.waitForCategoriesRoutes()
+      general.checkHeaderText('Subcategorieën')
+      cy.url().should('include', '/instellingen/categorieen/')
 
       // Open category Afwatering brug
-      cy.contains('Afwatering brug').click();
-      cy.url().should('include', 'instellingen/categorie/');
-      cy.wait('@getCategories');
-      cy.contains('Verantwoordelijke afdeling').should('be.visible');
-      cy.get(CATEGORIES.verantwoordelijkeAfdeling).should('have.text', 'VOR, STW').and('be.visible');
+      cy.contains('Afwatering brug').click()
+      cy.url().should('include', 'instellingen/categorie/')
+      cy.wait('@getCategories')
+      cy.contains('Verantwoordelijke afdeling').should('be.visible')
+      cy.get(CATEGORIES.verantwoordelijkeAfdeling)
+        .should('have.text', 'VOR, STW')
+        .and('be.visible')
 
       // Change category
-      cy.get(CATEGORIES.inputName).clear().type('Afgewaterde brug');
-      cy.get(CATEGORIES.inputDescription).clear().type('Dit is het verhaal van de brug die moest afwateren');
-      cy.get(CATEGORIES.inputDays).clear().type('4');
-      cy.get(CATEGORIES.dropdownTypeOfDays).select('Dagen');
+      cy.get(CATEGORIES.inputName).clear().type('Afgewaterde brug')
+      cy.get(CATEGORIES.inputDescription)
+        .clear()
+        .type('Dit is het verhaal van de brug die moest afwateren')
+      cy.get(CATEGORIES.inputDays).clear().type('4')
+      cy.get(CATEGORIES.dropdownTypeOfDays).select('Dagen')
       cy.get(CATEGORIES.inputMessage)
         .clear()
-        .type('Ik beoordeel deze melding niet, het lijkt me namelijk allemaal onzin');
-      cy.get(CATEGORIES.inputNote).clear().type('Naamloze notities noteren, oftwel nnn.');
-      cy.get(CATEGORIES.buttonOpslaan).click();
+        .type(
+          'Ik beoordeel deze melding niet, het lijkt me namelijk allemaal onzin'
+        )
+      cy.get(CATEGORIES.inputNote)
+        .clear()
+        .type('Naamloze notities noteren, oftwel nnn.')
+      cy.get(CATEGORIES.buttonOpslaan).click()
 
       // Wait for saving the data
-      cy.wait('@patchCategory');
-      cy.wait('@getCategories');
+      cy.wait('@patchCategory')
+      cy.wait('@getCategories')
 
-      cy.get(CATEGORIES.notification).should('contain', 'Subcategorie bijgewerkt');
+      cy.get(CATEGORIES.notification).should(
+        'contain',
+        'Subcategorie bijgewerkt'
+      )
 
       // Check if Subcategorieën page opens again
-      cy.url().should('include', '/instellingen/categorieen/page/1');
-      cy.contains('Asbest / accu').should('have.length', 1);
-      cy.contains('Afgewaterde brug').should('have.length', 1);
+      cy.url().should('include', '/instellingen/categorieen/page/1')
+      cy.contains('Asbest / accu').should('have.length', 1)
+      cy.contains('Afgewaterde brug').should('have.length', 1)
 
-      general.checkHeaderText('Subcategorieën');
-      cy.get(CATEGORIES.categoryValue).eq(0).should('contain', 'Afgewaterde brug');
-      cy.get(CATEGORIES.categoryValue).eq(1).should('contain', '4 dagen');
+      general.checkHeaderText('Subcategorieën')
+      cy.get(CATEGORIES.categoryValue)
+        .eq(0)
+        .should('contain', 'Afgewaterde brug')
+      cy.get(CATEGORIES.categoryValue).eq(1).should('contain', '4 dagen')
 
-      cy.contains('Afgewaterde brug').click();
-      cy.url().should('include', 'instellingen/categorie/');
-      cy.get(CATEGORIES.inputNote).should('have.text', 'Naamloze notities noteren, oftwel nnn.').and('be.visible');
-      cy.get(CATEGORIES.historyAction).eq(0).should('contain', 'Afhandeltermijn gewijzigd naar').and('contain', '4 weekdagen');
-      cy.get(CATEGORIES.historyAction).eq(0).should('contain', 'Naam gewijzigd naar:').and('contain', 'Afgewaterde brug');
-      cy.get(CATEGORIES.historyAction).eq(0).should('contain', 'Servicebelofte gewijzigd naar:').and('contain', 'Ik beoordeel deze melding niet, het lijkt me namelijk allemaal onzin');
-      cy.get(CATEGORIES.historyAction).eq(0).should('contain', 'Omschrijving gewijzigd naar:').and('contain', 'Dit is het verhaal van de brug die moest afwateren');
-    });
+      cy.contains('Afgewaterde brug').click()
+      cy.url().should('include', 'instellingen/categorie/')
+      cy.get(CATEGORIES.inputNote)
+        .should('have.text', 'Naamloze notities noteren, oftwel nnn.')
+        .and('be.visible')
+      cy.get(CATEGORIES.historyAction)
+        .eq(0)
+        .should('contain', 'Afhandeltermijn gewijzigd naar')
+        .and('contain', '4 weekdagen')
+      cy.get(CATEGORIES.historyAction)
+        .eq(0)
+        .should('contain', 'Naam gewijzigd naar:')
+        .and('contain', 'Afgewaterde brug')
+      cy.get(CATEGORIES.historyAction)
+        .eq(0)
+        .should('contain', 'Servicebelofte gewijzigd naar:')
+        .and(
+          'contain',
+          'Ik beoordeel deze melding niet, het lijkt me namelijk allemaal onzin'
+        )
+      cy.get(CATEGORIES.historyAction)
+        .eq(0)
+        .should('contain', 'Omschrijving gewijzigd naar:')
+        .and('contain', 'Dit is het verhaal van de brug die moest afwateren')
+    })
     it('Should change the status of a category to inactive', () => {
-      general.openMenu();
-      cy.contains('Instellingen').click();
-      cy.contains('Subcategorieën').click();
+      general.openMenu()
+      cy.contains('Instellingen').click()
+      cy.contains('Subcategorieën').click()
 
-      routes.waitForCategoriesRoutes();
-      general.checkHeaderText('Subcategorieën');
-      cy.url().should('include', '/instellingen/categorieen/');
+      routes.waitForCategoriesRoutes()
+      general.checkHeaderText('Subcategorieën')
+      cy.url().should('include', '/instellingen/categorieen/')
 
-      cy.contains('Beplanting').click();
-      cy.url().should('include', 'instellingen/categorie/');
-      cy.wait('@getCategories');
+      cy.contains('Beplanting').click()
+      cy.url().should('include', 'instellingen/categorie/')
+      cy.wait('@getCategories')
 
       // Change category
-      cy.get(CATEGORIES.radioButtonNietActief).click({ force: true }).should('be.checked');
-      cy.get(CATEGORIES.buttonOpslaan).click();
+      cy.get(CATEGORIES.radioButtonNietActief)
+        .click({ force: true })
+        .should('be.checked')
+      cy.get(CATEGORIES.buttonOpslaan).click()
 
       // Wait for saving the data
-      cy.wait('@patchCategory');
-      cy.wait('@getCategories');
+      cy.wait('@patchCategory')
+      cy.wait('@getCategories')
 
       // Check if Subcategorieën page opens again
-      cy.url().should('include', '/instellingen/categorieen/page/1');
+      cy.url().should('include', '/instellingen/categorieen/page/1')
       // Load page again, because page refresh is very slow and test fails
-      cy.visit('/instellingen/categorieen/page/1');
-      cy.get(MANAGE_SIGNALS.spinner).should('not.exist');
+      cy.visit('/instellingen/categorieen/page/1')
+      cy.get(MANAGE_SIGNALS.spinner).should('not.exist')
 
-      cy.contains('Beplanting').click();
-      cy.get(CATEGORIES.historyAction).eq(0).should('contain', 'Status gewijzigd naar:').and('contain', 'Inactief');
-      cy.get(CATEGORIES.buttonAnnuleer).click();
+      cy.contains('Beplanting').click()
+      cy.get(CATEGORIES.historyAction)
+        .eq(0)
+        .should('contain', 'Status gewijzigd naar:')
+        .and('contain', 'Inactief')
+      cy.get(CATEGORIES.buttonAnnuleer).click()
 
       // Load page again, because page refresh is very slow and test fails
-      cy.visit('/instellingen/categorieen/page/1');
-      general.openMenu();
-      cy.contains('Melden').click();
-      cy.url().should('include', '/incident/beschrijf');
-      cy.get(CREATE_SIGNAL.dropdownSubcategory).then($selectlist => {
-        expect($selectlist).to.contain('Afgewaterde brug (STW, VOR)');
-        expect($selectlist).to.not.contain('Beplanting');
-      });
-    });
-  });
+      cy.visit('/instellingen/categorieen/page/1')
+      general.openMenu()
+      cy.contains('Melden').click()
+      cy.url().should('include', '/incident/beschrijf')
+      cy.get(CREATE_SIGNAL.dropdownSubcategory).then(($selectlist) => {
+        expect($selectlist).to.contain('Afgewaterde brug (STW, VOR)')
+        expect($selectlist).to.not.contain('Beplanting')
+      })
+    })
+  })
   describe('Create a signal and validate changes of category', () => {
     beforeEach(() => {
-      localStorage.setItem('accessToken', generateToken('Admin', 'signals.admin@example.com'));
-    });
+      localStorage.setItem(
+        'accessToken',
+        generateToken('Admin', 'signals.admin@example.com')
+      )
+    })
 
     it('Should initiate create signal', () => {
-      routes.getManageSignalsRoutes();
+      routes.getManageSignalsRoutes()
 
-      cy.visit('/incident/beschrijf/');
+      cy.visit('/incident/beschrijf/')
 
-      general.checkHeaderText('Beschrijf uw melding');
-    });
+      general.checkHeaderText('Beschrijf uw melding')
+    })
 
     it('Should describe the signal', () => {
-      routes.stubPreviewMap();
-      routes.stubMap();
-      routes.postSignalRoutePrivate();
+      routes.stubPreviewMap()
+      routes.stubMap()
+      routes.postSignalRoutePrivate()
 
-      createSignal.setDescriptionPage(signal);
-      cy.contains('Volgende').click();
+      createSignal.setDescriptionPage(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.setPhonenumber(signal);
-      cy.contains('Volgende').click();
+      createSignal.setPhonenumber(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.setEmailAddress(signal);
-      cy.contains('Volgende').click();
+      createSignal.setEmailAddress(signal)
+      cy.contains('Volgende').click()
 
-      createSignal.checkSummaryPage(signal);
-      cy.contains('Verstuur').click();
-      cy.wait('@postSignalPrivate');
-      cy.get(MANAGE_SIGNALS.spinner).should('not.exist');
+      createSignal.checkSummaryPage(signal)
+      cy.contains('Verstuur').click()
+      cy.wait('@postSignalPrivate')
+      cy.get(MANAGE_SIGNALS.spinner).should('not.exist')
 
-      createSignal.checkThanksPage();
-      createSignal.saveSignalId();
-    });
+      createSignal.checkThanksPage()
+      createSignal.saveSignalId()
+    })
     it('Should show the change in category description', () => {
-      localStorage.setItem('accessToken', generateToken('Admin', 'signals.admin@example.com'));
-      routes.stubPreviewMap();
-      routes.getManageSignalsRoutes();
-      routes.getSignalDetailsRoutesById();
-      cy.visit('/manage/incidents/');
-      routes.waitForManageSignalsRoutes();
+      localStorage.setItem(
+        'accessToken',
+        generateToken('Admin', 'signals.admin@example.com')
+      )
+      routes.stubPreviewMap()
+      routes.getManageSignalsRoutes()
+      routes.getSignalDetailsRoutesById()
+      cy.visit('/manage/incidents/')
+      routes.waitForManageSignalsRoutes()
 
       // Open incident details
-      createSignal.openCreatedSignal();
-      routes.waitForSignalDetailsRoutes();
+      createSignal.openCreatedSignal()
+      routes.waitForSignalDetailsRoutes()
 
       // Edit signal category
       // Used a wait because sometimes the edit button is not clicked
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(500);
-      cy.get(CHANGE_CATEGORY.buttonEdit).click();
-      cy.get(CHANGE_CATEGORY.inputCategory).find(':selected').should('contain', 'Afgewaterde brug');
-      cy.get(SIGNAL_DETAILS.infoText).should('contain', 'Dit is het verhaal van de brug die moest afwateren');
-    });
-  });
+      cy.wait(500)
+      cy.get(CHANGE_CATEGORY.buttonEdit).click()
+      cy.get(CHANGE_CATEGORY.inputCategory)
+        .find(':selected')
+        .should('contain', 'Afgewaterde brug')
+      cy.get(SIGNAL_DETAILS.infoText).should(
+        'contain',
+        'Dit is het verhaal van de brug die moest afwateren'
+      )
+    })
+  })
   describe('Change back servicebelofte', () => {
     before(() => {
-      localStorage.setItem('accessToken', generateToken('Admin', 'signals.admin@example.com'));
-      routes.getManageSignalsRoutes();
-      routes.getCategoriesRoutes();
-      cy.visit('/manage/incidents/');
-      routes.waitForManageSignalsRoutes();
-    });
+      localStorage.setItem(
+        'accessToken',
+        generateToken('Admin', 'signals.admin@example.com')
+      )
+      routes.getManageSignalsRoutes()
+      routes.getCategoriesRoutes()
+      cy.visit('/manage/incidents/')
+      routes.waitForManageSignalsRoutes()
+    })
 
     it('Change back servicebelofte of category', () => {
-      general.openMenu();
-      cy.contains('Instellingen').click();
-      cy.contains('Subcategorieën').click();
+      general.openMenu()
+      cy.contains('Instellingen').click()
+      cy.contains('Subcategorieën').click()
 
-      routes.waitForCategoriesRoutes();
+      routes.waitForCategoriesRoutes()
 
-      cy.url().should('include', '/instellingen/categorieen/');
-      general.checkHeaderText('Subcategorieën');
+      cy.url().should('include', '/instellingen/categorieen/')
+      general.checkHeaderText('Subcategorieën')
 
-      cy.contains('Afgewaterde brug').click();
-      cy.url().should('include', 'instellingen/categorie/');
-      cy.wait('@getCategories');
+      cy.contains('Afgewaterde brug').click()
+      cy.url().should('include', 'instellingen/categorie/')
+      cy.wait('@getCategories')
 
       // Change category
-      cy.get(CATEGORIES.inputName).clear().type('Afwatering brug');
-      cy.get(CATEGORIES.inputDescription).clear().type('Dit is het verhaal van de brug die helemaal niet moest afwateren');
-      cy.get(CATEGORIES.inputDays).clear().type('5');
-      cy.get(CATEGORIES.dropdownTypeOfDays).select('Werkdagen');
+      cy.get(CATEGORIES.inputName).clear().type('Afwatering brug')
+      cy.get(CATEGORIES.inputDescription)
+        .clear()
+        .type(
+          'Dit is het verhaal van de brug die helemaal niet moest afwateren'
+        )
+      cy.get(CATEGORIES.inputDays).clear().type('5')
+      cy.get(CATEGORIES.dropdownTypeOfDays).select('Werkdagen')
       cy.get(CATEGORIES.inputMessage)
         .clear()
         .type(
           '  Wij beoordelen uw melding. Urgente meldingen pakken we zo snel mogelijk op. Overige meldingen handelen we binnen een week af. We houden u op de hoogte via e-mail.'
-        );
-      cy.get(CATEGORIES.buttonOpslaan).click();
+        )
+      cy.get(CATEGORIES.buttonOpslaan).click()
 
       // Wait for saving the data
-      cy.wait('@patchCategory');
-      cy.wait('@getCategories');
+      cy.wait('@patchCategory')
+      cy.wait('@getCategories')
 
       // Check if Subcategorieën page opens again
-      cy.url().should('include', '/instellingen/categorieen/page/1');
+      cy.url().should('include', '/instellingen/categorieen/page/1')
       // Load page again, because page refresh is very slow and test fails
-      cy.visit('/instellingen/categorieen/page/1');
-      general.checkHeaderText('Subcategorieën');
-      cy.get('[data-testid=dataViewBody] > [data-testid=dataViewBodyRow]', { timeout: 10000 })
+      cy.visit('/instellingen/categorieen/page/1')
+      general.checkHeaderText('Subcategorieën')
+      cy.get('[data-testid=dataViewBody] > [data-testid=dataViewBodyRow]', {
+        timeout: 10000,
+      })
         .first()
-        .contains('5 werkdagen');
-    });
-  });
-});
+        .contains('5 werkdagen')
+    })
+  })
+})
