2a9fa97a690917853860898dd81c97dc00b14b6e
Samuel <samleeuwenburg@gmail.com>
2021-06-23 09:15:11 +0200

    move aria-label to input, add screenreader text (#1680)

--- src/signals/incident/components/form/FileInput/index.js
+++ src/signals/incident/components/form/FileInput/index.js
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
+import styled from 'styled-components'
 import { Fragment, useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { TrashBin, Enlarge } from '@amsterdam/asc-assets'
@@ -15,6 +16,16 @@ import FileInputStyle, {
   FilePreview,
   FileLoading,
 } from './styles'
+
+const ScreenReaderOnly = styled.span`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`
 
 const FileInput = ({ handler, parent, meta }) => {
   const [errors, setErrors] = useState()
@@ -217,9 +228,11 @@ const FileInput = ({ handler, parent, meta }) => {
               accept={meta.allowedFileTypes}
               onChange={handleChange}
               multiple
+              aria-label="Toevoegen foto"
             />
             <label htmlFor="formUpload">
-              <AddButton aria-label="Toevoegen foto">
+              <AddButton as="span">
+                <ScreenReaderOnly>Toevoegen foto</ScreenReaderOnly>
                 <AddIcon size={22}>
                   <Enlarge />
                 </AddIcon>
