f80aa9196eedaa17e3a5ef903ed2613360de30c5
Maurits Ridder <maurits.ridder@gmail.com>
2021-06-24 09:41:14 +0200

    Removes unnecessary changes

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -100,6 +100,7 @@ export const AppContainer = () => {
       <AppContext.Provider value={contextValue}>
         <Fragment>
           <SiteHeaderContainer />
+
           <ContentContainer headerIsTall={headerIsTall}>
             <Suspense fallback={<LoadingIndicator />}>
               <Switch>
@@ -125,6 +126,7 @@ export const AppContainer = () => {
               </Switch>
             </Suspense>
           </ContentContainer>
+
           {!isAuthenticated() && (
             <FooterContainer>
               <Footer />
