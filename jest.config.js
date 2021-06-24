cdf6d4108d2a6cc9fcb979527c4977a19e00119f
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2021-06-24 16:21:03 +0200

    remove radiobutton chrome focus override

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
   coverageThreshold: {
     global: {
       statements: 99.49,
-      branches: 95.34,
+      branches: 95.33,
       lines: 99.56,
       functions: 98.6,
     },
