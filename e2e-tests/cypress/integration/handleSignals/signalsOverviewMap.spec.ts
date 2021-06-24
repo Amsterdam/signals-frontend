c8d50ad5018ee7ab7ca687d5d6b97cece3d95a43
Arjan <arjan@desoftwaretesters.nl>
2021-06-24 11:04:21 +0200

    small change

--- e2e-tests/cypress/integration/handleSignals/signalsOverviewMap.spec.ts
+++ e2e-tests/cypress/integration/handleSignals/signalsOverviewMap.spec.ts
@@ -50,7 +50,7 @@ describe('Signal overview Map', () => {
       cy.get(FILTER.buttonSubmitFilter).click();
       cy.wait('@getSignals');
       cy.wait('@getGeography');
-      cy.get(OVERVIEW_MAP.markerCluster).should('not.be.visible');
+      cy.get(OVERVIEW_MAP.markerCluster).should('not.exist');
     });
 
     it('Should show signals again', () => {
