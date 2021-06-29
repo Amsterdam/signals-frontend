0b8edafd0a7ac9a5d9105bacef77a3194cabcff7
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2021-06-29 15:13:45 +0200

    add BarGraph component

--- src/signals/incident-management/containers/Signaling/Signaling.tsx
+++ src/signals/incident-management/containers/Signaling/Signaling.tsx
@@ -1,6 +1,8 @@
-import { Heading, Row, themeSpacing } from '@amsterdam/asc-ui'
+import { Heading, Row, themeSpacing, Column } from '@amsterdam/asc-ui'
 import { FunctionComponent } from 'react'
 import styled from 'styled-components'
+import { Color as GraphColor } from './components/BarGraph/BarGraph'
+import BarGraph from './components/BarGraph'
 
 const StyledHeading = styled(Heading)`
   margin-bottom: ${themeSpacing(8)};
@@ -8,11 +10,38 @@ const StyledHeading = styled(Heading)`
 `
 
 const Signaling: FunctionComponent = () => {
+  const dataPrimary = [
+    { title: 'Boom - dood', value: 500 },
+    { title: 'Put', value: 100 },
+    { title: 'Geluidsoverlast', value: 750 },
+    { title: 'Verkeerssituaties', value: 1000 },
+    { title: 'Bouw/sloop', value: 900 },
+    { title: 'over de max', value: 1400 },
+    { title: 'Fietswrak', value: 250 },
+    { title: 'Onderhuur', value: 10 },
+    { title: 'afval', value: 1 },
+  ]
+
+  const dataSecondary = [
+    { title: 'Boom - dood', value: 1024 },
+    { title: 'Put', value: 140 },
+    { title: 'Geluidsoverlast', value: 750 },
+    { title: 'Verkeerssituaties', value: 1400 },
+    { title: 'Bouw/sloop', value: 1900 },
+    { title: 'over de max', value: 1550 },
+    { title: 'Fietswrak', value: 550 },
+    { title: 'Onderhuur', value: 190 },
+    { title: 'afval', value: 40 },
+    { title: 'Overig groen / water', value: 999 },
+  ]
+
   return (
     <Row>
       <StyledHeading>Signalering</StyledHeading>
-      {/* <Column span={12}> */}
-      {/* </Column> */}
+      <Column span={12}>
+        <BarGraph maxValue={1000} data={dataPrimary} color={GraphColor.Blue} />
+        <BarGraph maxValue={1000} data={dataSecondary} color={GraphColor.Red} />
+      </Column>
     </Row>
   )
 }
---
+++ src/signals/incident-management/containers/Signaling/components/BarGraph/BarGraph.tsx
@@ -0,0 +1,107 @@
+import { themeSpacing, themeColor } from '@amsterdam/asc-ui'
+import styled from 'styled-components'
+
+const VALUE_MARGIN = 70
+const BAR_HEIGHT = 25
+
+export enum Color {
+  Blue,
+  Red,
+}
+
+interface Data {
+  title: string
+  value: number
+}
+
+interface Props {
+  data: Data[]
+  maxValue?: number
+  color: Color
+}
+
+const getColorValue = (color: Color) => {
+  if (color === Color.Blue) {
+    return themeColor('primary')
+  }
+  if (color === Color.Red) {
+    return themeColor('secondary')
+  }
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+const ValueWrapper = styled.div`
+  margin-bottom: ${themeSpacing(4)};
+`
+
+const Description = styled.div`
+  margin-bottom: ${themeSpacing(2)};
+`
+
+const BarWrapper = styled.div`
+  width: calc(100% - ${VALUE_MARGIN}px);
+`
+
+const Bar = styled.div<{ c: Color; outOfBounds: boolean }>`
+  height: ${BAR_HEIGHT}px;
+  background: ${({ c }) => getColorValue(c)};
+  position: relative;
+
+  &::before,
+  &::after {
+    content: '';
+    display: ${({ outOfBounds }) => (outOfBounds ? 'block' : 'none')};
+    height: 100%;
+    width: 2px;
+    background: ${({ c }) => getColorValue(c)};
+    position: absolute;
+  }
+
+  &::before {
+    right: -4px;
+  }
+
+  &::after {
+    right: -8px;
+  }
+`
+
+const Value = styled.strong`
+  display: inline-block;
+  position: absolute;
+  right: -${themeSpacing(4)};
+  transform: translateX(100%);
+`
+
+const BarGraph = (props: Props) => {
+  const maxValue = props.data.reduce((max, x) => Math.max(max, x.value), 0)
+  const graphMaxValue = Math.min(props.maxValue || maxValue, maxValue)
+
+  const bars = props.data
+    .sort(({ value: x }, { value: y }) => y - x)
+    .map((data) => {
+      const outOfBounds = data.value > graphMaxValue
+      const ratio = outOfBounds ? 1 : data.value / graphMaxValue
+      const width = `${ratio * 100}%`
+
+      return (
+        <ValueWrapper>
+          <Description>{data.title}</Description>
+          <BarWrapper>
+            <Bar c={props.color} outOfBounds={outOfBounds} style={{ width }}>
+              <Value>{data.value}</Value>
+            </Bar>
+          </BarWrapper>
+        </ValueWrapper>
+      )
+    })
+
+  return <Wrapper>{bars}</Wrapper>
+}
+
+export default BarGraph
