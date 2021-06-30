c3e5217e0ccc48ac465829eb713f8f1a3dbd8655
Maurits Ridder <maurits.ridder@gmail.com>
2021-06-30 11:13:52 +0200

    Adds graph description

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import { Paragraph as AscParagraph } from '@amsterdam/asc-ui'
 
 const Paragraph = styled(AscParagraph)`
-  /* font-family: Avenir Next LT W01 Demi, arial, sans-serif; */
   font-size: 16px;
   margin-bottom: 0;
 `
--- src/signals/incident-management/containers/Signaling/components/GraphDescription/GraphDescription.tsx
+++ src/signals/incident-management/containers/Signaling/components/GraphDescription/GraphDescription.tsx
@@ -3,21 +3,44 @@ import Paragraph from 'components/Paragraph'
 import { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
+const GRAPH_DESCRIPTION_COLOR = themeColor('tint', 'level3')
+
 interface GraphDescriptionProps {
   title: string
   subtitle: string
   total: number
 }
 
-const Title = styled(Heading)`
-  /* margin-bottom: 0;
-  margin-top: 0; */
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `
 
-const Wrapper = styled.section`
+const Title = styled(Heading)`
+  font-size: 20px;
+  margin-bottom: ${themeSpacing(2)};
+  margin-top: 0;
+`
+
+const DescriptionWrapper = styled.section`
   display: flex;
   padding: ${themeSpacing(4)};
-  background-color: ${themeColor('tint', 'level3')};
+  background-color: ${GRAPH_DESCRIPTION_COLOR};
+`
+
+const Total = styled(Heading)`
+  margin-left: ${themeSpacing(4)};
+`
+
+const Triangle = styled.div`
+  display: flex;
+  align-self: center;
+  width: 0;
+  height: 0;
+  border-left: 20px solid transparent;
+  border-right: 20px solid transparent;
+  border-top: 24px solid ${GRAPH_DESCRIPTION_COLOR};
 `
 
 const GraphDescription: FunctionComponent<GraphDescriptionProps> = ({
@@ -26,11 +49,14 @@ const GraphDescription: FunctionComponent<GraphDescriptionProps> = ({
   total,
 }) => (
   <Wrapper>
-    <div>
-      <Heading as="h3">{title}</Heading>
-      <Paragraph>{subtitle}</Paragraph>
-    </div>
-    <Heading>{total}</Heading>
+    <DescriptionWrapper>
+      <div>
+        <Title as="h3">{title}</Title>
+        <Paragraph>{subtitle}</Paragraph>
+      </div>
+      <Total>{total.toLocaleString()}</Total>
+    </DescriptionWrapper>
+    <Triangle />
   </Wrapper>
 )
 
