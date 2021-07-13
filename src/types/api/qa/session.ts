d8b6335deeed18ae6bd63dbc3270267c4ace4e90
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2021-07-13 17:23:05 +0200

    add incident reply generic form state

--- src/types/api/qa/session.ts
+++ src/types/api/qa/session.ts
@@ -1,4 +1,12 @@
 export interface Session {
+  _links: {
+    'sia:questionnaire': {
+      href: string
+    }
+    'sia:public-signal': {
+      href: string
+    }
+  }
   uuid: string
   started_at: string | null
   submit_before: string
