e55cc3c9a8caafeba7ba659db9e5f73c77a21358
Maurits Ridder <maurits.ridder@gmail.com>
2021-07-19 11:59:28 +0200

    Updates txt

--- src/signals/incident/containers/IncidentReplyContainer/constants.ts
+++ src/signals/incident/containers/IncidentReplyContainer/constants.ts
@@ -1,4 +1,4 @@
 export const EXPIRED_TITLE = 'U kunt niet meer reageren op onze vragen'
 export const EXPIRED_CONTENT =
-  'Wij vroegen u om dit binnen 5 dagen te doen. Wij houden u verder op de hoogte via e-mail.'
+  'Helaas hebben wij geen reactie van u ontvangen binnen 5 dagen. Daarom hebben wij uw melding gesloten. U ontvangt daarover een e-mail.'
 export const EXPIRED_STATUS = 410
