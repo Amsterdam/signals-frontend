c0922d2f19651d7f4f3c57445a2def5908f241f0
Maurits Ridder <maurits.ridder@gmail.com>
2021-08-18 12:29:15 +0200

    Show single login button on login page: keycloak when config is present - else authz

--- src/components/LoginPage/index.test.js
+++ src/components/LoginPage/index.test.js
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2019 - 2021 Gemeente Amsterdam
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import configuration from 'shared/services/configuration/configuration'
 import { withAppContext } from 'test/utils'
@@ -16,7 +17,7 @@ describe('components/LoginPage', () => {
     configuration.__reset()
   })
 
-  it('should render correctly', () => {
+  it('should render with keycloak', () => {
     configuration.keycloak = {}
     render(withAppContext(<LoginPage />))
 
@@ -24,36 +25,41 @@ describe('components/LoginPage', () => {
       screen.getByText('Om deze pagina te zien dient u ingelogd te zijn.')
     ).toBeInTheDocument()
     expect(screen.getByText('Inloggen')).toBeInTheDocument()
-    expect(screen.getByText('Inloggen ADW')).toBeInTheDocument()
+    expect(screen.getByTestId('keycloakLoginButton')).toBeInTheDocument()
   })
 
-  it('should render correctly without Keycloak', () => {
+  it('should render without keycloak', () => {
+    configuration.keycloak = null
     render(withAppContext(<LoginPage />))
 
-    expect(screen.queryByText('Inloggen ADW')).not.toBeInTheDocument()
+    expect(
+      screen.getByText('Om deze pagina te zien dient u ingelogd te zijn.')
+    ).toBeInTheDocument()
+    expect(screen.getByText('Inloggen')).toBeInTheDocument()
+    expect(screen.getByTestId('datapuntLoginButton')).toBeInTheDocument()
   })
 
   it('should login on datapunt when Inloggen button is clicked', () => {
     const loginSpy = jest.spyOn(auth, 'login')
-    const { getByText } = render(withAppContext(<LoginPage />))
-    const button = getByText('Inloggen').parentNode
+    render(withAppContext(<LoginPage />))
+    const button = screen.getByText('Inloggen').parentNode
 
     expect(button.getAttribute('type')).toEqual('button')
 
-    fireEvent.click(button)
+    userEvent.click(button)
 
     expect(loginSpy).toHaveBeenCalledWith('datapunt')
   })
 
-  it('should login on keycloak when Inloggen ADW button is clicked', () => {
+  it('should login on keycloak when Inloggen button is clicked', () => {
     configuration.keycloak = {}
     const loginSpy = jest.spyOn(auth, 'login')
     render(withAppContext(<LoginPage />))
-    const button = screen.getByText('Inloggen ADW').parentNode
+    const button = screen.getByText('Inloggen').parentNode
 
     expect(button.getAttribute('type')).toEqual('button')
 
-    fireEvent.click(button)
+    userEvent.click(button)
 
     expect(loginSpy).toHaveBeenCalledWith('keycloak')
   })
