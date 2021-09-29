245fdc9c83881c78ed089c43b9e4db22f53ef45b
JJM van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-09-29 11:35:03 +0200

    (Chore) Questions field types

--- src/types/api/qa/question.ts
+++ src/types/api/qa/question.ts
@@ -1,6 +1,13 @@
 export enum FieldType {
   PlainText = 'plain_text',
   FileInput = 'file_input',
+  TextInput = 'text_input',
+  MultiTextInput = 'multi_text_input',
+  CheckboxInput = 'checkbox_input',
+  RadioInput = 'radio_input',
+  SelectInput = 'select_input',
+  TextareaInput = 'text_area_input',
+  MapSelect = 'map_select',
 }
 
 export interface Question {
