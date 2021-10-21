edec7be074680a7e637697727ed229f6207da050
Boris Arkenaar <boris@arkenaar.com>
2021-10-21 10:58:17 +0200

    Add fallback icon config

--- src/signals/incident/components/form/MapSelectors/Asset/AssetSelect.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/AssetSelect.tsx
@@ -1,15 +1,37 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import type { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect } from 'react'
 import { useCallback, useState } from 'react'
 import type { Incident } from 'types/incident'
 import type { LatLngExpression } from 'leaflet'
 import { ClickEventHandler } from '../types'
+import { UNREGISTERED_TYPE } from '../constants'
 import { AssetSelectProvider } from './context'
 import Intro from './Intro'
 import Selector from './Selector'
 import Summary from './Summary'
-import type { Item, Meta } from './types'
+import type { FeatureType, Item, Meta } from './types'
+
+const defaultIconConfig: FeatureType['icon'] = {
+  options: {
+    className: 'object-marker',
+    iconSize: [40, 40],
+  },
+  iconSvg:
+    '<svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>icon-select</title><g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="icon-select"><circle id="Oval-2-Copy-21" stroke="#000000" stroke-width="4" fill="#FFFFFF" cx="20" cy="20" r="18"></circle><circle id="Oval-Copy" fill="#000000" cx="20" cy="20" r="6"></circle></g></g></svg>',
+  selectedIconSvg:
+    '<svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>icon-select</title><g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="icon-select"><circle id="Oval-2-Copy-21" stroke="#EC0000" stroke-width="4" fill="#FFFFFF" cx="20" cy="20" r="18"></circle><circle id="Oval-Copy" fill="#EC0000" cx="20" cy="20" r="6"></circle></g></g></svg>',
+}
+const defaultUnregisteredIconConfig: FeatureType['icon'] = {
+  options: {
+    className: 'object-marker',
+    iconSize: [40, 40],
+  },
+  iconSvg:
+    '<svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>icon-unknown</title><g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="icon/onbekend" stroke-width="2"><g id="icon-unknown"><circle id="Oval-2-Copy-23" stroke="#FFFFFF" fill="#B4B4B4" cx="20" cy="20" r="19"></circle><g transform="translate(10.000000, 10.000000)" stroke="#000000"><circle id="Oval" cx="10" cy="10" r="9"></circle><line x1="13" y1="7" x2="7" y2="13" id="Line-3" stroke-linecap="square"></line><line x1="13" y1="13" x2="7" y2="7" id="Line-3-Copy" stroke-linecap="square"></line></g></g></g></g></svg>',
+  selectedIconSvg:
+    '<svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>icon-select</title><g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="icon-select"><circle id="Oval-2-Copy-21" stroke="#EC0000" stroke-width="4" fill="#FFFFFF" cx="20" cy="20" r="18"></circle><circle id="Oval-Copy" fill="#EC0000" cx="20" cy="20" r="6"></circle></g></g></svg>',
+}
 
 export interface AssetSelectProps {
   handler: () => { value: Item[] }
@@ -30,6 +52,7 @@ const AssetSelect: FunctionComponent<AssetSelectProps> = ({
   const value = handler().value
   const [showMap, setShowMap] = useState(false)
   const [message, setMessage] = useState<string>()
+  const [featureTypes, setFeatureTypes] = useState<FeatureType[]>([])
 
   const { coordinates } =
     parent.meta.incidentContainer.incident.location.geometrie
@@ -56,12 +79,37 @@ const AssetSelect: FunctionComponent<AssetSelectProps> = ({
     setShowMap(false)
   }, [setShowMap])
 
+  useEffect(() => {
+    setFeatureTypes(
+      meta.featureTypes.map((featureType) => {
+        const defaultConfig =
+          featureType.typeValue === UNREGISTERED_TYPE
+            ? defaultUnregisteredIconConfig
+            : defaultIconConfig
+        return {
+          ...featureType,
+          icon: {
+            ...defaultConfig,
+            ...(featureType.icon || {}),
+            options: {
+              ...defaultConfig.options,
+              ...(featureType.icon?.options || {}),
+            },
+          },
+        }
+      })
+    )
+  }, [meta.featureTypes])
+
   return (
     <AssetSelectProvider
       value={{
         selection: value,
         location,
-        meta,
+        meta: {
+          endpoint: meta.endpoint,
+          featureTypes,
+        },
         message,
         update,
         edit,
