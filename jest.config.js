c83a21470297f231f2947874c167abc5e0d683ed
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-03 11:29:55 +0100

    Reverts packages update

--- jest.config.js
+++ jest.config.js
@@ -34,7 +34,6 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/internals/mocks/image.ts',
   },
-  setupFiles: ['jest-canvas-mock'],
   setupFilesAfterEnv: [
     '<rootDir>/internals/testing/test-bundler.ts',
     '<rootDir>/internals/testing/jest-setup-msw.ts',
