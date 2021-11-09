b92fc2cfb8881c5e219a51850eb18e06bff51115
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-09 16:54:54 +0100

    - corrects sort order for all options (was reversed) - conditionally renders options and labels based on feature flags

--- src/signals/incident-management/containers/IncidentOverviewPage/components/Sort/Sort.test.tsx
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/Sort/Sort.test.tsx
@@ -1,14 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
+import configuration from 'shared/services/configuration/configuration'
 import { withAppContext } from 'test/utils'
 
 import Sort, { SortOptions } from '.'
 
 const onChangeOrdering = jest.fn()
 
+jest.mock('shared/services/configuration/configuration')
+
 describe('Sort', () => {
   afterEach(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    configuration.__reset()
     onChangeOrdering.mockReset()
   })
 
@@ -17,6 +23,17 @@ describe('Sort', () => {
 
     expect(screen.getByTestId('incidentSortSelect')).toBeInTheDocument()
     expect(screen.getAllByRole('option').length).toBeGreaterThan(0)
+    expect(screen.getAllByRole('option')).not.toContain(
+      document.querySelector(
+        `option[value="${SortOptions.ASSIGNED_USER_EMAIL_ASC}"]`
+      )
+    )
+    expect(screen.getAllByRole('option')).not.toContain(
+      document.querySelector(
+        `option[value="${SortOptions.ASSIGNED_USER_EMAIL_DESC}"]`
+      )
+    )
+    expect(screen.getAllByText(/Stadsdeel/)).toHaveLength(2)
   })
 
   it('pre-selects active sort option', () => {
@@ -49,5 +66,34 @@ describe('Sort', () => {
     ])
 
     expect(onChangeOrdering).toHaveBeenCalledTimes(1)
+  })
+
+  it('renders options when feature assignSignalToEmployee is set', () => {
+    configuration.featureFlags.assignSignalToEmployee = true
+
+    render(withAppContext(<Sort onChangeOrdering={onChangeOrdering} />))
+
+    expect(screen.getAllByRole('option')).toContain(
+      document.querySelector(
+        `option[value="${SortOptions.ASSIGNED_USER_EMAIL_ASC}"]`
+      )
+    )
+    expect(screen.getAllByRole('option')).toContain(
+      document.querySelector(
+        `option[value="${SortOptions.ASSIGNED_USER_EMAIL_DESC}"]`
+      )
+    )
+  })
+
+  it('renders the correct option label when feature fetchDistrictsFromBackend is set', () => {
+    const DISTRICT = 'District'
+
+    configuration.featureFlags.fetchDistrictsFromBackend = true
+    configuration.language.district = DISTRICT
+
+    render(withAppContext(<Sort onChangeOrdering={onChangeOrdering} />))
+
+    expect(screen.queryAllByText(/Stadsdeel/)).toHaveLength(0)
+    expect(screen.queryAllByText(new RegExp(`${DISTRICT}`))).toHaveLength(2)
   })
 })
--- src/signals/incident-management/containers/IncidentOverviewPage/components/Sort/Sort.tsx
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/Sort/Sort.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 
 import type { FC } from 'react'
 
+import configuration from 'shared/services/configuration/configuration'
+
 const SelectContainer = styled.div`
   display: flex;
   align-items: center;
@@ -25,24 +27,24 @@ type SortProps = {
 }
 
 export enum SortOptions {
-  ADDRESS_ASC = 'address',
-  ADDRESS_DESC = '-address',
-  ASSIGNED_USER_EMAIL_ASC = 'assigned_user_email',
-  ASSIGNED_USER_EMAIL_DESC = '-assigned_user_email',
-  BUROUGH_ASC = 'stadsdeel',
-  BUROUGH_DESC = '-stadsdeel',
-  CREATED_AT_ASC = 'created_at',
-  CREATED_AT_DESC = '-created_at',
-  PRIORITY_ASC = 'priority',
-  PRIORITY_DESC = '-priority',
-  STATUS_ASC = 'status',
-  STATUS_DESC = '-status',
-  SUBCATEGORY_ASC = 'sub_category',
-  SUBCATEGORY_DESC = '-sub_category',
+  ADDRESS_ASC = '-address',
+  ADDRESS_DESC = 'address',
+  ASSIGNED_USER_EMAIL_ASC = '-assigned_user_email',
+  ASSIGNED_USER_EMAIL_DESC = 'assigned_user_email',
+  BUROUGH_ASC = '-stadsdeel',
+  BUROUGH_DESC = 'stadsdeel',
+  CREATED_AT_ASC = '-created_at',
+  CREATED_AT_DESC = 'created_at',
+  PRIORITY_ASC = '-priority',
+  PRIORITY_DESC = 'priority',
+  STATUS_ASC = '-status',
+  STATUS_DESC = 'status',
+  SUBCATEGORY_ASC = '-sub_category',
+  SUBCATEGORY_DESC = 'sub_category',
 }
 
 const Sort: FC<SortProps> = ({
-  activeSort = SortOptions.CREATED_AT_DESC,
+  activeSort = SortOptions.CREATED_AT_ASC,
   className = '',
   onChangeOrdering,
 }) => {
@@ -61,6 +63,10 @@ const Sort: FC<SortProps> = ({
     onChangeOrdering(sort)
   }, [onChangeOrdering, sort])
 
+  const district = configuration.featureFlags.fetchDistrictsFromBackend
+    ? configuration.language.district
+    : 'Stadsdeel'
+
   return (
     <SelectContainer className={className}>
       <Select
@@ -76,18 +82,23 @@ const Sort: FC<SortProps> = ({
         <option value={SortOptions.SUBCATEGORY_DESC}>Subcategorie: Z-A</option>
         <option value={SortOptions.STATUS_ASC}>Status A-Z</option>
         <option value={SortOptions.STATUS_DESC}>Status Z-A</option>
-        <option value={SortOptions.BUROUGH_ASC}>Stadsdeel A-Z</option>
-        <option value={SortOptions.BUROUGH_DESC}>Stadsdeel Z-A</option>
+        <option value={SortOptions.BUROUGH_ASC}>{district} A-Z</option>
+        <option value={SortOptions.BUROUGH_DESC}>{district} Z-A</option>
         <option value={SortOptions.PRIORITY_ASC}>Urgentie: hoog - laag</option>
         <option value={SortOptions.PRIORITY_DESC}>Urgentie: laag - hoog</option>
         <option value={SortOptions.ADDRESS_ASC}>Adres A-Z</option>
         <option value={SortOptions.ADDRESS_DESC}>Adres Z-A</option>
-        <option value={SortOptions.ASSIGNED_USER_EMAIL_ASC}>
-          Toegewezen aan: A-Z
-        </option>
-        <option value={SortOptions.ASSIGNED_USER_EMAIL_DESC}>
-          Toegewezen aan: Z-A
-        </option>
+
+        {configuration.featureFlags.assignSignalToEmployee && (
+          <>
+            <option value={SortOptions.ASSIGNED_USER_EMAIL_ASC}>
+              Toegewezen aan: A-Z
+            </option>
+            <option value={SortOptions.ASSIGNED_USER_EMAIL_DESC}>
+              Toegewezen aan: Z-A
+            </option>
+          </>
+        )}
       </Select>
     </SelectContainer>
   )
