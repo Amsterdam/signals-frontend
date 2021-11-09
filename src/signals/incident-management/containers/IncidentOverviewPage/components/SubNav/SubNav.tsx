0e95db31432467dd1b5bc2ba8849e0cd944b8076
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-09 10:58:28 +0100

    Imports styled components

--- src/signals/incident-management/containers/IncidentOverviewPage/components/SubNav/SubNav.tsx
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/SubNav/SubNav.tsx
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import styled from 'styled-components'
-import { themeSpacing, themeColor, Heading } from '@amsterdam/asc-ui'
 import { Link } from 'react-router-dom'
 
 import type { FC } from 'react'
@@ -9,49 +7,7 @@ import type { FC } from 'react'
 import configuration from 'shared/services/configuration/configuration'
 
 import { MAP_URL, INCIDENTS_URL } from '../../../../routes'
-
-const MapHeading = styled(Heading).attrs({
-  forwardedAs: 'h2',
-})`
-  margin: 0;
-  font-size: 16px;
-  font-family: Avenir Next LT W01 Demi, arial, sans-serif;
-  font-weight: 400;
-`
-
-const TabContainer = styled.div`
-  border-bottom: 2px solid ${themeColor('tint', 'level3')};
-  display: flex;
-  padding-bottom: ${themeSpacing(1)};
-
-  a {
-    text-decoration: none;
-  }
-`
-
-const Tab = styled.span`
-  line-height: 24px;
-  font-size: 18px;
-  font-family: Avenir Next LT W01 Demi, arial, sans-serif;
-  box-shadow: 0px 6px 0px 0px rgba(0, 0, 0, 0);
-  padding: 0 10px 6px;
-  color: ${themeColor('tint', 'level6')};
-
-  &.active {
-    pointer-events: none;
-    box-shadow: 0px 6px 0px 0px ${themeColor('secondary')};
-    color: ${themeColor('secondary')};
-  }
-
-  &:not(.active):hover {
-    box-shadow: 0px 6px 0px 0px rgba(0, 0, 0, 1);
-    color: ${themeColor('tint', 'level6')};
-  }
-
-  & + & {
-    margin-left: ${themeSpacing(7)};
-  }
-`
+import { MapHeading, TabContainer, Tab } from './styled'
 
 type SubNavProps = {
   showsMap?: boolean
