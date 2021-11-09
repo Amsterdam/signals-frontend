563bc1374280db4a5d0ba0a8158126e910a5b7f3
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-09 12:25:59 +0100

    [SIG-4115] Sort dropdown (#1854)

---
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/Sort/Sort.tsx
@@ -0,0 +1,88 @@
+import { useCallback, useEffect, useState } from 'react'
+import { Select, themeSpacing } from '@amsterdam/asc-ui'
+import styled from 'styled-components'
+
+import type { FC } from 'react'
+
+const SelectContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 ${themeSpacing(8)} 0 auto;
+
+  label {
+    margin-right: ${themeSpacing(8)};
+  }
+`
+
+type SortProps = {
+  className?: ''
+  onChangeOrdering: (sort: string) => void
+}
+
+export enum SortOptions {
+  ADDRESS_ASC = 'address',
+  ADDRESS_DESC = '-address',
+  ASSIGNED_USER_EMAIL_ASC = 'assigned_user_email',
+  ASSIGNED_USER_EMAIL_DESC = '-assigned_user_email',
+  BUROUGH_ASC = 'stadsdeel',
+  BUROUGH_DESC = '-stadsdeel',
+  CREATED_AT_ASC = 'created_at',
+  CREATED_AT_DESC = '-created_at',
+  PRIORITY_ASC = 'priority',
+  PRIORITY_DESC = '-priority',
+  STATUS_ASC = 'status',
+  STATUS_DESC = '-status',
+  SUBCATEGORY_ASC = 'sub_category',
+  SUBCATEGORY_DESC = '-sub_category',
+}
+
+const Sort: FC<SortProps> = ({ onChangeOrdering }) => {
+  const [sort, setSort] = useState<SortOptions>()
+  const onChange = useCallback(
+    (event) => {
+      const { value } = event.target
+
+      if (sort !== value) setSort(value)
+    },
+    [sort]
+  )
+
+  useEffect(() => {
+    if (!sort) return
+    onChangeOrdering(sort)
+  }, [onChangeOrdering, sort])
+
+  return (
+    <SelectContainer>
+      <Select
+        data-testid="incidentSortSelect"
+        id="sortSelect"
+        label="Sorteren"
+        onChange={onChange}
+      >
+        <option value={SortOptions.CREATED_AT_ASC}>Datum: nieuw - oud</option>
+        <option value={SortOptions.CREATED_AT_DESC}>Datum: oud - nieuw</option>
+        <option value={SortOptions.PRIORITY_ASC}>Urgentie: hoog - laag</option>
+        <option value={SortOptions.PRIORITY_DESC}>Urgentie: laag - hoog</option>
+        <option value={SortOptions.BUROUGH_ASC}>Stadsdeel A-Z</option>
+        <option value={SortOptions.BUROUGH_DESC}>Stadsdeel Z-A</option>
+        <option value={SortOptions.STATUS_ASC}>Status A-Z</option>
+        <option value={SortOptions.STATUS_DESC}>Status Z-A</option>
+        <option value={SortOptions.ADDRESS_ASC}>Adres A-Z</option>
+        <option value={SortOptions.ADDRESS_DESC}>Adres Z-A</option>
+        <option value={SortOptions.CREATED_AT_ASC}>Dag: hoog - laag</option>
+        <option value={SortOptions.CREATED_AT_DESC}>Dag: laag - hoog</option>
+        <option value={SortOptions.ASSIGNED_USER_EMAIL_ASC}>
+          Toegewezen aan: A-Z
+        </option>
+        <option value={SortOptions.ASSIGNED_USER_EMAIL_DESC}>
+          Toegewezen aan: Z-A
+        </option>
+        <option value={SortOptions.SUBCATEGORY_ASC}>Subcategorie: A-Z</option>
+        <option value={SortOptions.SUBCATEGORY_DESC}>Subcategorie: Z-A</option>
+      </Select>
+    </SelectContainer>
+  )
+}
+
+export default Sort
