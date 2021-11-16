fd75127ab5b85e791803cb15ef32bd5f0ae5d67e
Kirsten Ruys <k.ruijs@amsterdam.nl>
2021-11-16 19:21:49 +0100

    Change text for no address in summary

--- src/signals/incident/components/IncidentPreview/components/Map/index.test.js
+++ src/signals/incident/components/IncidentPreview/components/Map/index.test.js
@@ -22,7 +22,7 @@ describe('signals/incident/components/IncidentPreview/components/Map', () => {
       withAppContext(<MapPreview value={{ geometrie }} />)
     )
 
-    expect(getByText('Geen adres gevonden')).toBeInTheDocument()
+    expect(getByText('Adres gepind op de kaart')).toBeInTheDocument()
   })
 
   it('should render static map with useStaticMapServer enabled', async () => {
