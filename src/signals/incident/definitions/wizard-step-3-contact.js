0e765048554aade240c8cb8f38983901ee5d409d
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-23 11:06:48 +0100

    Combines step into contact step

---
+++ src/signals/incident/definitions/wizard-step-3-contact.js
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2018 - 2021 Gemeente Amsterdam
+import { Validators } from 'react-reactive-form'
+
+import configuration from 'shared/services/configuration/configuration'
+import { validatePhoneNumber } from '../services/custom-validators'
+import IncidentNavigation from '../components/IncidentNavigation'
+import FormComponents from '../components/form'
+
+export default {
+  label: 'Contactgegevens',
+  nextButtonLabel: 'Volgende',
+  nextButtonClass: 'action primary arrow-right',
+  previousButtonLabel: 'Vorige',
+  previousButtonClass: 'action startagain',
+  formAction: 'UPDATE_INCIDENT',
+  form: {
+    controls: {
+      phone: {
+        meta: {
+          autoComplete: 'tel',
+          autoRemove: /[^\d ()+-]/g,
+          heading: 'Mogen we u bellen voor vragen?',
+          isFieldSet: true,
+          label: 'Wat is uw telefoonnummer?',
+          path: 'reporter.phone',
+          subtitle:
+            'Zo kunt u ons helpen het probleem sneller of beter op te lossen.',
+          type: 'tel',
+          wrap: FormComponents.TextInput,
+        },
+        render: FormComponents.WithHeading,
+        options: {
+          validators: [Validators.maxLength(17), validatePhoneNumber],
+        },
+      },
+      email: {
+        meta: {
+          autoComplete: 'email',
+          autoRemove: /[^\w!#$%&'*+./;=?@^`{|}~-]/g,
+          heading: 'Wilt u op de hoogte blijven?',
+          label: 'Wat is uw e-mailadres?',
+          path: 'reporter.email',
+          subtitle:
+            'We mailen om u te vertellen wat we met uw melding doen en wanneer het klaar is.',
+          type: 'email',
+          wrap: FormComponents.TextInput,
+        },
+        render: FormComponents.WithHeading,
+        options: {
+          validators: [Validators.email, Validators.maxLength(254)],
+        },
+      },
+      privacy_text: {
+        meta: {
+          isFieldSet: true,
+          heading: 'Uw privacy',
+          value:
+            'We gebruiken uw e-mailadres alléén om u op de hoogte houden, of wanneer wij een vraag hebben en u niet per telefoon kunnen bereiken. We gebruiken uw telefoonnummer alléén om nog iets te kunnen vragen over uw melding.',
+          wrap: FormComponents.PlainText,
+        },
+        render: FormComponents.WithHeading,
+      },
+      sharing_allowed: {
+        meta: {
+          shortLabel: 'Toestemming contactgegevens delen',
+          value: configuration.language?.consentToContactSharing,
+          path: 'reporter.sharing_allowed',
+        },
+        render: FormComponents.EmphasisCheckboxInput,
+      },
+      $field_0: {
+        isStatic: false,
+        render: IncidentNavigation,
+      },
+    },
+  },
+}
--- src/signals/incident/definitions/wizard.js
+++ src/signals/incident/definitions/wizard.js
@@ -2,18 +2,16 @@
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
 import beschrijf from './wizard-step-1-beschrijf'
 import vulaan from './wizard-step-2-vulaan'
-import telefoon from './wizard-step-3-telefoon'
-import email from './wizard-step-4-email'
-import samenvatting from './wizard-step-5-samenvatting'
+import contact from './wizard-step-3-contact'
+import summary from './wizard-step-4-summary'
 import bedankt from './wizard-step-6-bedankt'
 import fout from './wizard-step-7-fout'
 
 export default {
   beschrijf,
   vulaan,
-  telefoon,
-  email,
-  samenvatting,
+  contact,
+  summary,
   opslaan: {},
   bedankt,
   fout,
