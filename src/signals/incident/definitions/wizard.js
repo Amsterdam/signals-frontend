187d6fdbdbeffe7e837843e5ab4593de591c965a
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-11-23 11:42:54 +0100

    Corrects wizard imports

--- src/signals/incident/definitions/wizard.js
+++ src/signals/incident/definitions/wizard.js
@@ -4,8 +4,8 @@ import beschrijf from './wizard-step-1-beschrijf'
 import vulaan from './wizard-step-2-vulaan'
 import contact from './wizard-step-3-contact'
 import summary from './wizard-step-4-summary'
-import bedankt from './wizard-step-6-bedankt'
-import fout from './wizard-step-7-fout'
+import bedankt from './wizard-step-5-bedankt'
+import fout from './wizard-step-6-fout'
 
 export default {
   beschrijf,
