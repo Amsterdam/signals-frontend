903b329a035c9af57cf7223401e2fa2634a94f72
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-12-07 13:20:25 +0100

    Removes duplicates in form config

--- src/signals/incident/definitions/wizard-step-2-vulaan.js
+++ src/signals/incident/definitions/wizard-step-2-vulaan.js
@@ -57,14 +57,6 @@ const expandQuestions = memoize(
         }),
         {}
       ),
-      help_text: {
-        meta: {
-          label: configuration.language.helpTextHeader,
-          value: configuration.language.helpText,
-          ignoreVisibility: true,
-        },
-        render: FormComponents.PlainText,
-      },
       $field_0: {
         isStatic: false,
         render: IncidentNavigation,
