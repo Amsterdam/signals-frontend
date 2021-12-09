b5a09bf7f5e9b8d2d682e58bf394cd32a940f106
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-12-09 10:06:22 +0100

    [SIG-4182] AutoSuggest to ts (#1915)
    * Converts SuggestList component to TS
    
    * Converts AutoSuggest component to TS
    
    * Converts PDOKAutoSuggest component to TS
    
    * Removes act calls from SuggestList tests
    
    * Adds onClear callback type
    
    * Fixes AutoSuggest option type
    
    * Fixes SuggestList test

--- src/components/AutoSuggest/components/SuggestList/SuggestList.test.tsx
+++ src/components/AutoSuggest/components/SuggestList/SuggestList.test.tsx
@@ -1,16 +1,32 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-import { createEvent, render, fireEvent, act } from '@testing-library/react'
+import { createEvent, render, fireEvent } from '@testing-library/react'
+
+import type { AutoSuggestOption } from 'components/AutoSuggest'
+
 import { withAppContext } from 'test/utils'
 
-import SuggestList from '..'
+import SuggestList from '.'
 
 const onSelectOption = jest.fn()
 
-const options = [
-  { id: 1, value: 'Item 1' },
-  { id: 2, value: 'Item 2' },
-  { id: 3, value: 'Item 3' },
+const data = {
+  address: {
+    postcode: '1234 AB',
+    huisnummer: 1,
+    woonplaats: "A'dam",
+    openbare_ruimte: '',
+  },
+  location: {
+    lat: 0,
+    lng: 0,
+  },
+}
+
+const options: Array<AutoSuggestOption> = [
+  { id: 1, value: 'Item 1', data },
+  { id: 2, value: 'Item 2', data },
+  { id: 3, value: 'Item 3', data },
 ]
 
 describe('src/components/AutoSuggest/components/SuggestList', () => {
@@ -101,9 +117,7 @@ describe('src/components/AutoSuggest/components/SuggestList', () => {
 
     expect(onSelectOption).not.toHaveBeenCalled()
 
-    act(() => {
-      fireEvent.click(firstItem)
-    })
+    firstItem && fireEvent.click(firstItem)
 
     expect(onSelectOption).toHaveBeenCalledTimes(1)
     expect(onSelectOption).toHaveBeenCalledWith(options[0])
@@ -120,9 +134,8 @@ describe('src/components/AutoSuggest/components/SuggestList', () => {
 
     const secondItem = container.querySelector('li:nth-of-type(2)')
 
-    act(() => {
+    secondItem &&
       fireEvent.keyDown(secondItem, { key: 'Enter', code: 13, keyCode: 13 })
-    })
 
     expect(onSelectOption).toHaveBeenCalledTimes(2)
     expect(onSelectOption).toHaveBeenLastCalledWith(options[1])
@@ -143,15 +156,13 @@ describe('src/components/AutoSuggest/components/SuggestList', () => {
 
     expect(onSelectOption).not.toHaveBeenCalled()
 
-    act(() => {
+    secondItem &&
       fireEvent.keyDown(secondItem, { key: 'Up', code: 38, keyCode: 38 })
-    })
 
     expect(onSelectOption).not.toHaveBeenCalled()
 
-    act(() => {
+    secondItem &&
       fireEvent.keyDown(secondItem, { key: 'A', code: 65, keyCode: 65 })
-    })
 
     expect(onSelectOption).not.toHaveBeenCalled()
   })
@@ -168,32 +179,31 @@ describe('src/components/AutoSuggest/components/SuggestList', () => {
     )
 
     const thirdItem = container.querySelector('li:nth-of-type(3)')
-    const enterEvent = createEvent.keyDown(thirdItem, {
-      key: 'Enter',
-      code: 13,
-      keyCode: 13,
-    })
-    enterEvent.preventDefault = jest.fn()
 
-    expect(enterEvent.preventDefault).not.toHaveBeenCalled()
+    if (thirdItem) {
+      const enterEvent = createEvent.keyDown(thirdItem, {
+        key: 'Enter',
+        code: 13,
+        keyCode: 13,
+      })
+      enterEvent.preventDefault = jest.fn()
 
-    act(() => {
+      expect(enterEvent.preventDefault).not.toHaveBeenCalled()
+
       fireEvent(thirdItem, enterEvent)
-    })
 
-    expect(enterEvent.preventDefault).toHaveBeenCalled()
+      expect(enterEvent.preventDefault).toHaveBeenCalled()
 
-    const arrowUpEvent = createEvent.keyDown(thirdItem, {
-      key: 'ArrowUp',
-      code: 38,
-      keyCode: 38,
-    })
-    arrowUpEvent.preventDefault = jest.fn()
+      const arrowUpEvent = createEvent.keyDown(thirdItem, {
+        key: 'ArrowUp',
+        code: 38,
+        keyCode: 38,
+      })
+      arrowUpEvent.preventDefault = jest.fn()
 
-    expect(arrowUpEvent.preventDefault).not.toHaveBeenCalled()
+      expect(arrowUpEvent.preventDefault).not.toHaveBeenCalled()
 
-    act(() => {
       fireEvent(thirdItem, arrowUpEvent)
-    })
+    }
   })
 })
