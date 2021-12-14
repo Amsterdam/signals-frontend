bf1c23543fd7069e81646219368727930ded5158
Kirsten Ruys <k.ruijs@amsterdam.nl>
2021-12-14 17:40:10 +0100

    Fix reversed lat lng in reported layer

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/WfsLayer/ReportedLayer/ReportedLayer.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/WfsLayer/ReportedLayer/ReportedLayer.tsx
@@ -13,6 +13,8 @@ import type {
 import { Marker } from '@amsterdam/arm-core'
 import { getIconUrl } from 'signals/incident/components/form/MapSelectors/utils'
 import { reported as ReportedIcon } from 'signals/incident/definitions/wizard-step-2-vulaan/verlichting-icons'
+import { featureTolocation } from 'shared/services/map-location'
+import type { Geometrie } from 'types/incident'
 import WfsDataContext from '../context'
 
 const REPORTED_CLASS_MODIFIER = 'marker-reported'
@@ -34,38 +36,42 @@ const ReportedLayer: FC<DataLayerProps> = ({ featureTypes }) => {
     [featureTypes]
   )
 
-  const getMarker = useCallback(
-    (feat: any) => {
-      const feature = feat as Feature
-      const [lng, lat] = feature.geometry.coordinates
-      const latLng = { lat, lng }
-      const featureType = getFeatureType(feature)
-      if (!featureType) return
+  const getMarker = (feature: Feature) => {
+    const latLng = featureTolocation(feature.geometry as Geometrie)
+    const featureType = getFeatureType(feature)
 
-      const icon = L.icon({
-        iconSize: [20, 20],
-        iconUrl: getIconUrl(ReportedIcon),
-        className: REPORTED_CLASS_MODIFIER,
-      })
+    if (!featureType) return
 
-      const featureId = feature.properties[featureType.idField]
+    const icon = L.icon({
+      iconSize: [20, 20],
+      iconUrl: getIconUrl(ReportedIcon),
+      className: REPORTED_CLASS_MODIFIER,
+    })
 
-      return (
-        <Marker
-          key={featureId}
-          latLng={latLng}
-          options={{
-            zIndexOffset: 1000,
-            icon,
-            alt: `${featureType.description} - ${featureId}`,
-          }}
-        />
-      )
-    },
-    [getFeatureType]
+    const featureId = feature.properties[featureType.idField]
+
+    return (
+      <Marker
+        key={featureId}
+        latLng={latLng}
+        options={{
+          zIndexOffset: 1000,
+          icon,
+          alt: `${featureType.description} - ${featureId}`,
+        }}
+      />
+    )
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return (
+    <>
+      {data.features
+        .filter((feature) => feature.properties.meldingstatus === 1)
+        .map(getMarker)}
+    </>
   )
-
-  return <>{data.features.map(getMarker)}</>
 }
 
 export default ReportedLayer
