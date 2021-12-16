f4814329ab6e6929809e9198d83079d7c6140a16
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-12-16 16:09:51 +0100

    (Fix) Asset selector position

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/Selector.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/Selector.tsx
@@ -54,9 +54,11 @@ const MAP_CONTAINER_ZOOM_LEVEL: ZoomLevel = {
 }
 
 const Wrapper = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   height: 100%;
   width: 100%;
   box-sizing: border-box; // Override box-sizing: content-box set by Leaflet
@@ -236,6 +238,7 @@ const Selector = () => {
       </StyledMap>
     </Wrapper>
   )
+
   return ReactDOM.createPortal(mapWrapper, appHtmlElement)
 }
 
