8dc41ef496d299d2839b6f5b2bc4a22ed54f8175
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2021-12-21 08:01:34 +0100

    [SIG-4091] Restructure summary page

--- src/signals/incident/definitions/wizard-step-4-summary.js
+++ src/signals/incident/definitions/wizard-step-4-summary.js
@@ -136,7 +136,7 @@ const getExtraQuestions = (category, subcategory, questions) => {
 }
 
 export default {
-  label: 'Controleer uw gegevens',
+  label: '4. Versturen',
   subheader: 'Maak een aanpassing als dat nodig is.',
   nextButtonLabel: 'Verstuur',
   nextButtonClass: 'action primary',
@@ -144,12 +144,12 @@ export default {
   previousButtonClass: 'action startagain',
   sectionLabels: {
     heading: {
-      beschrijf: 'Melding',
-      vulaan: 'Aanvullende informatie',
-      contact: 'Contactgegevens',
+      beschrijf: '1. Beschrijf uw melding',
+      vulaan: '2. Locatie en vragen',
+      contact: '3. Contactgegevens',
     },
     edit: {
-      beschrijf: 'Wijzig melding',
+      beschrijf: 'Wijzig uw melding',
       vulaan: 'Wijzig aanvullende informatie',
       contact: 'Wijzig contactgegevens',
     },
@@ -181,7 +181,7 @@ export default {
         render: PreviewComponents.MapPreview,
       },
       description: {
-        label: 'Waar gaat het om?',
+        label: 'Uw melding gaat over:',
         render: ({ value }) => value,
       },
       classification: {
@@ -216,7 +216,7 @@ export default {
       },
 
       sharing_allowed: {
-        label: 'Melding doorsturen',
+        label: 'Melding delen',
         optional: true,
         render: ({ value }) => {
           if (!value) return null
