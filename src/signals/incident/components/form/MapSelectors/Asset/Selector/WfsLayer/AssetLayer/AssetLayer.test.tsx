975a638cfda2a64e8f52384a6f0fa53fbb524e05
Kirsten Ruys <k.ruijs@amsterdam.nl>
2022-01-10 14:47:18 +0100

    Extend asset layer test

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/WfsLayer/AssetLayer/AssetLayer.test.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/WfsLayer/AssetLayer/AssetLayer.test.tsx
@@ -6,12 +6,15 @@ import { Map } from '@amsterdam/react-maps'
 import type { FeatureCollection } from 'geojson'
 import type { AssetSelectValue } from 'signals/incident/components/form/MapSelectors/Asset/types'
 
-import assetsJson from 'utils/__tests__/fixtures/assets.json'
+import containerJson from 'utils/__tests__/fixtures/assets.json'
 import MAP_OPTIONS from 'shared/services/configuration/map-options'
 
 import withAssetSelectContext, {
   contextValue,
 } from 'signals/incident/components/form/MapSelectors/Asset/__tests__/withAssetSelectContext'
+import type { Geometrie } from 'types/incident'
+import userEvent from '@testing-library/user-event'
+import { featureToCoordinates } from 'shared/services/map-location'
 import { WfsDataProvider } from '../context'
 import AssetLayer from '.'
 
@@ -34,7 +37,7 @@ describe('AssetLayer', () => {
   const withAssetMap = (contextOverride = {}) =>
     withAssetSelectContext(
       <Map data-testid="map-test" options={MAP_OPTIONS}>
-        <WfsDataProvider value={assetsJson as FeatureCollection}>
+        <WfsDataProvider value={containerJson as FeatureCollection}>
           <AssetLayer featureTypes={featureTypes} />
         </WfsDataProvider>
       </Map>,
@@ -56,5 +59,38 @@ describe('AssetLayer', () => {
     ).toBeInTheDocument()
 
     expect(screen.getByTestId('map-test')).toBeInTheDocument()
+  })
+
+  it('should handle selecting a container', async () => {
+    const featureId = 'PAB00022'
+    const feature = containerJson.features.find(
+      ({ properties }) => properties.id_nummer === featureId
+    )
+    const coordinates = featureToCoordinates(feature?.geometry as Geometrie)
+    const newSelection = {
+      id: featureId,
+      isReported: false,
+      description: 'Papier container',
+      type: 'Papier',
+      location: {
+        coordinates: coordinates,
+      },
+    }
+
+    render(withAssetMap())
+
+    const container = screen.getByAltText(`Papier container (${featureId})`)
+
+    userEvent.click(container)
+
+    expect(
+      screen.queryByAltText(`Papier container, is geselecteerd (${featureId})`)
+    ).not.toBeInTheDocument()
+
+    render(withAssetMap({ selection: newSelection }))
+
+    expect(
+      screen.queryByAltText(`Papier container, is geselecteerd (${featureId})`)
+    ).toBeInTheDocument()
   })
 })
