3220ecd7234f324582d06bd360272a409870b99c
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-01-19 16:34:18 +0100

    - Converts AssetListPreview component to TS - Makes sure that the correct props are passed on to the AssetList component

--- src/signals/incident/components/IncidentPreview/components/AssetListPreview/AssetListPreview.tsx
+++ src/signals/incident/components/IncidentPreview/components/AssetListPreview/AssetListPreview.tsx
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 Gemeente Amsterdam
-import type {
-  FeatureType,
-  Item,
-} from 'signals/incident/components/form/MapSelectors/types'
-import AssetList from 'signals/incident/components/form/MapSelectors/Asset/AssetList'
 import type { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
-export interface AssetListPreviewProps {
-  value: Item
-  featureTypes: FeatureType[]
-}
+import type { AssetListProps } from 'signals/incident/components/form/MapSelectors/Asset/AssetList/AssetList'
+import type { Item } from 'signals/incident/components/form/MapSelectors/types'
+
+import AssetList from 'signals/incident/components/form/MapSelectors/Asset/AssetList'
 
 const StyledAssetList = styled(AssetList)`
   margin-bottom: 0;
 `
 
+export interface AssetListPreviewProps
+  extends Omit<AssetListProps, 'selection'> {
+  value: Item
+}
+
 const AssetListPreview: FunctionComponent<AssetListPreviewProps> = ({
   value,
-  featureTypes,
-}) => <StyledAssetList selection={value} featureTypes={featureTypes} />
+  ...props
+}) => <StyledAssetList {...props} selection={value} />
+
 export default AssetListPreview
