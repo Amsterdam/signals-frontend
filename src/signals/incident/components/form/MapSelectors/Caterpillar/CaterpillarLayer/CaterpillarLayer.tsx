be6223dcfbe40b2c6fbf3eb979cf89149ac3be24
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-01-20 14:19:35 +0100

    Sets item label in caterpillar layer

--- src/signals/incident/components/form/MapSelectors/Caterpillar/CaterpillarLayer/CaterpillarLayer.tsx
+++ src/signals/incident/components/form/MapSelectors/Caterpillar/CaterpillarLayer/CaterpillarLayer.tsx
@@ -69,6 +69,7 @@ export const CaterpillarLayer: FC = () => {
           location: {
             coordinates,
           },
+          label: [description, featureId].filter(Boolean).join(' - '),
         }
 
         meta.extraProperties?.forEach((propertyKey) => {
