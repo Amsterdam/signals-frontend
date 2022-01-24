e621efbb2799ee594aa75793c45233cfac04d5c2
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-01-24 17:10:54 +0100

    Removes .svg?url references

--- custom.d.ts
+++ custom.d.ts
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
-declare module '*.svg?url' {
-  const url: string
-  export default url
-}
 declare module '*.svg' {
   import type { FunctionComponent, SVGProps } from 'react'
   const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement>>
