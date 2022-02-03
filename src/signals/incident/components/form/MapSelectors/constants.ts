e3b12dd4098b7e0b59118de2592b020c768cb812
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-02-03 13:59:48 +0100

    Adds comment about constant usage

--- src/signals/incident/components/form/MapSelectors/constants.ts
+++ src/signals/incident/components/form/MapSelectors/constants.ts
@@ -1,7 +1,7 @@
 import type { Item } from './types'
 
 export const OBJECT_UNKNOWN = 'unknown'
-export const OBJECT_NOT_ON_MAP = 'not-on-map'
+export const OBJECT_NOT_ON_MAP = 'not-on-map' // Note: the value of this constant is used by other SIA instances so it is NOT to be changed
 
 export const selectionIsObject = (item: Item) =>
   item.type !== OBJECT_UNKNOWN && item.type !== OBJECT_NOT_ON_MAP
