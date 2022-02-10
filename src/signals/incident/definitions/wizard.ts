5f69a9c379674bbc41c13aa321bfb86ddd633f1a
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-02-10 15:02:08 +0100

    Adds wizard types

--- src/signals/incident/definitions/wizard.ts
+++ src/signals/incident/definitions/wizard.ts
@@ -1,11 +1,42 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
+import type { Incident } from 'types/incident'
+import type { SectionLabels } from '../components/IncidentPreview/IncidentPreview'
 import beschrijf from './wizard-step-1-beschrijf'
 import vulaan from './wizard-step-2-vulaan'
 import contact from './wizard-step-3-contact'
 import summary from './wizard-step-4-summary'
 import bedankt from './wizard-step-5-bedankt'
 import fout from './wizard-step-6-fout'
+
+export type Sections =
+  | 'beschrijf'
+  | 'vulaan'
+  | 'contact'
+  | 'summary'
+  | 'opslaan'
+  | 'bedankt'
+  | 'fout'
+
+export type FormAction = 'UPDATE_INCIDENT' | 'CREATE_INCIDENT'
+
+export type WizardSection = {
+  [key in Sections]: {
+    stepLabel?: string
+    countAsStep?: boolean
+    form?: any
+    formFactory?: any
+    label?: string
+    subHeader?: string
+    postponeSubmitWhenLoading?: string
+    previewFactory?: (incident: Incident) => any
+    sectionLabels?: SectionLabels
+    previousButtonLabel?: string
+    previousButtonClass?: string
+    nextButtonLabel?: string
+    formAction?: FormAction
+  }
+}
 
 export default {
   beschrijf: {
@@ -31,4 +62,4 @@ export default {
   opslaan: {},
   bedankt,
   fout,
-}
+} as WizardSection
