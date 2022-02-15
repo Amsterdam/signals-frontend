308d7345e14b87f00268b4b4985d715e6da8abe2
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-02-15 10:29:58 +0100

    Refactors LegendToggleButton: - removes arm-core dependency - simplifies rendering - makes component extensible (by adding the `className` prop)

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendToggleButton/LegendToggleButton.test.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendToggleButton/LegendToggleButton.test.tsx
@@ -2,75 +2,25 @@
 // Copyright (C) 2021 Gemeente Amsterdam
 import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
-import { MapPanelProvider } from '@amsterdam/arm-core'
-import { SnapPoint } from '@amsterdam/arm-core/lib/components/MapPanel/constants'
 
 import { withAppContext } from 'test/utils'
-
-import type { LegendToggleButtonProps } from './LegendToggleButton'
 
 import LegendToggleButton from '.'
 
 describe('LegendToggleButton', () => {
-  const props: LegendToggleButtonProps = {
-    onClick: jest.fn(),
-    isRenderingLegendPanel: true,
-  }
+  const onClick = jest.fn()
 
   afterEach(() => {
     jest.resetAllMocks()
   })
 
-  it('should render panel variant', () => {
-    render(
-      withAppContext(
-        <MapPanelProvider variant="panel" initialPosition={SnapPoint.Closed}>
-          <LegendToggleButton {...props} />
-        </MapPanelProvider>
-      )
-    )
+  it('handles onClick', () => {
+    render(withAppContext(<LegendToggleButton onClick={onClick} />))
 
-    expect(screen.getByRole('button').textContent).toBe('Legenda')
-  })
+    expect(onClick).not.toHaveBeenCalled()
 
-  it('should render drawer variant', () => {
-    render(
-      withAppContext(
-        <MapPanelProvider variant="drawer" initialPosition={SnapPoint.Closed}>
-          <LegendToggleButton {...props} />
-        </MapPanelProvider>
-      )
-    )
+    userEvent.click(screen.getByRole('button'))
 
-    expect(screen.getByRole('button')).toBeInTheDocument()
-    expect(screen.getByRole('button').textContent).toBe('Legenda')
-  })
-
-  it('should handle click when panel is closed', () => {
-    render(
-      withAppContext(
-        <MapPanelProvider variant="drawer" initialPosition={SnapPoint.Closed}>
-          <LegendToggleButton {...props} />
-        </MapPanelProvider>
-      )
-    )
-
-    userEvent.click(screen.getByText('Legenda'))
-
-    expect(props.onClick).toHaveBeenCalled()
-  })
-
-  it('should handle click when panel is open', () => {
-    render(
-      withAppContext(
-        <MapPanelProvider variant="drawer" initialPosition={SnapPoint.Halfway}>
-          <LegendToggleButton {...props} />
-        </MapPanelProvider>
-      )
-    )
-
-    userEvent.click(screen.getByText('Legenda'))
-
-    expect(props.onClick).toHaveBeenCalled()
+    expect(onClick).toHaveBeenCalled()
   })
 })
