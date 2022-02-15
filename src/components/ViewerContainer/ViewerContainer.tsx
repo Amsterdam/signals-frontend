f755bba4d0e4934579c0aad15e0a1269224b1583
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-02-15 16:06:06 +0100

    Removes arm-core dependency from ViewerContainer component

--- src/components/ViewerContainer/ViewerContainer.tsx
+++ src/components/ViewerContainer/ViewerContainer.tsx
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 Gemeente Amsterdam
-import { useContext } from 'react'
 import type { ReactNode, FunctionComponent } from 'react'
 import styled from 'styled-components'
 
-import {
-  MapPanelContext,
-  ViewerContainer as AscViewerContainer,
-} from '@amsterdam/arm-core'
+import { ViewerContainer as AscViewerContainer } from '@amsterdam/arm-core'
 
 // Should reflect ViewerContainer props from arm-core (which are not exported)
 interface ViewerContainerProps {
@@ -19,37 +15,15 @@ interface ViewerContainerProps {
   metaData?: string[]
 }
 
-interface StyledViewerContainerProps {
-  leftOffset: string
-  height: string
-}
-
-const StyledViewerContainer = styled(
-  AscViewerContainer
-)<StyledViewerContainerProps>`
-  left: ${({ leftOffset }) => leftOffset};
-  height: ${({ height }) => height};
+const StyledViewerContainer = styled(AscViewerContainer)`
   z-index: 400;
   transition: height 0.3s ease-in-out;
   touch-action: none;
   overflow: hidden;
 `
 
-const ViewerContainer: FunctionComponent<ViewerContainerProps> = (props) => {
-  const { drawerPosition, variant } = useContext(MapPanelContext)
-  const isDrawerVariant = variant === 'panel'
-
-  const height = isDrawerVariant ? '100%' : drawerPosition
-  const leftOffset = isDrawerVariant ? drawerPosition : '0'
-
-  return (
-    <StyledViewerContainer
-      {...props}
-      data-testid="viewer-asset"
-      height={height}
-      leftOffset={leftOffset}
-    />
-  )
-}
+const ViewerContainer: FunctionComponent<ViewerContainerProps> = (props) => (
+  <StyledViewerContainer {...props} data-testid="viewer-asset" />
+)
 
 export default ViewerContainer
