9285385a587c52bed4b0e83adc90ee596df68b50
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-02-22 16:51:41 +0100

    map-controls-to-params - removes clunky and hard to read code - formats incident `dateTime` prop by default as `incident_date_start`

--- src/signals/incident/services/map-controls-to-params/index.js
+++ src/signals/incident/services/map-controls-to-params/index.js
@@ -1,36 +1,13 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
-import format from 'date-fns/format'
 import formatISO from 'date-fns/formatISO'
-import parse from 'date-fns/parse'
 import mapValues from '../map-values'
 import mapPaths from '../map-paths'
 
 const mapControlsToParams = (incident, wizard) => {
-  let datetime
-  if (incident.datetime && incident.datetime.id === 'Nu') {
-    datetime = new Date()
-  } else if (incident.incident_date) {
-    const date =
-      incident.incident_date && incident.incident_date === 'Vandaag'
-        ? format(new Date(), 'yyyy-MM-dd')
-        : incident.incident_date
-
-    const datetimeString = `${date} ${String(
-      incident.incident_time_hours
-    ).padStart(2, '0')}:${String(incident.incident_time_minutes).padStart(
-      2,
-      '0'
-    )}`
-    datetime = parse(datetimeString, 'yyyy-MM-dd HH:mm', new Date())
-  }
-
   let params = {
     reporter: {},
-  }
-
-  if (datetime) {
-    params.incident_date_start = formatISO(datetime)
+    incident_date_start: formatISO(incident.dateTime),
   }
 
   params = mapValues(params, incident, wizard)
--- src/signals/incident/services/map-controls-to-params/index.test.js
+++ src/signals/incident/services/map-controls-to-params/index.test.js
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
+import formatISO from 'date-fns/formatISO'
+import { mock } from 'types/incident'
+
 import mapValues from '../map-values'
 import mapPaths from '../map-paths'
 
@@ -8,86 +11,20 @@ import mapControlsToParams from '.'
 jest.mock('../map-values')
 jest.mock('../map-paths')
 
+const someDate = new Date('2018-07-21')
+const incident = { ...mock, dateTime: someDate.getTime() }
+
 describe('The map controls to params service', () => {
   beforeEach(() => {
     mapValues.mockImplementation((params) => params)
     mapPaths.mockImplementation((params) => params)
   })
 
-  it('should map status by default', () => {
-    expect(mapControlsToParams({}, {})).toEqual({ reporter: {} })
-  })
-
-  it('should map date: Nu', () => {
-    const isodate = '2018-07-21T12:34:00+02:00'
-    const dateMock = new Date(isodate)
-    const spy = jest.spyOn(global, 'Date').mockImplementation(() => dateMock)
-
-    expect(
-      mapControlsToParams(
-        {
-          incident_time_hours: 12,
-          incident_time_minutes: 34,
-          datetime: {
-            id: 'Nu',
-            label: 'Nu',
-          },
-        },
-        {}
-      )
-    ).toEqual({
+  it('should map reporter and dateTime by default', () => {
+    expect(mapControlsToParams(incident, {})).toEqual({
       reporter: {},
-      incident_date_start: isodate,
+      incident_date_start: formatISO(incident.dateTime),
     })
-
-    spy.mockRestore()
-  })
-
-  it('should map date: Vandaag', () => {
-    const isodate = '2018-07-21T10:21:00+02:00'
-    const dateMock = new Date(isodate)
-    const spy = jest.spyOn(global, 'Date').mockImplementation(() => dateMock)
-    global.Date.now = jest.fn(() => new Date(isodate).getTime())
-    global.Date.UTC = jest.fn(() => new Date(isodate).getTime())
-
-    expect(
-      mapControlsToParams(
-        {
-          incident_time_hours: 10,
-          incident_time_minutes: 21,
-          incident_date: 'Vandaag',
-        },
-        {}
-      )
-    ).toEqual({
-      reporter: {},
-      incident_date_start: isodate,
-    })
-    spy.mockRestore()
-  })
-
-  it('should map date: fixed date', () => {
-    const isodate = '2018-07-02T09:05:00+02:00'
-    const testDate = '2018-04-02T09:05:00+02:00'
-    const dateMock = new Date(isodate)
-    const spy = jest.spyOn(global, 'Date').mockImplementation(() => dateMock)
-    global.Date.now = jest.fn(() => new Date(isodate).getTime())
-    global.Date.UTC = jest.fn(() => new Date(isodate).getTime())
-
-    expect(
-      mapControlsToParams(
-        {
-          incident_time_hours: 9,
-          incident_time_minutes: 5,
-          incident_date: '2018-04-02',
-        },
-        {}
-      )
-    ).toEqual({
-      reporter: {},
-      incident_date_start: testDate,
-    })
-    spy.mockRestore()
   })
 
   it('should expect to receive values from paths and values services', () => {
@@ -100,8 +37,9 @@ describe('The map controls to params service', () => {
       varFromMapPaths: 'bar',
     }))
 
-    expect(mapControlsToParams({}, {})).toEqual({
+    expect(mapControlsToParams(incident, {})).toEqual({
       reporter: {},
+      incident_date_start: formatISO(incident.dateTime),
       varFromMapValues: 'foo',
       varFromMapPaths: 'bar',
     })
