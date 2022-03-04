287c439f47c33f28598cd17392c17fa19f368a45
Kirsten Ruys <k.ruijs@amsterdam.nl>
2022-03-04 13:59:06 +0100

    Uitleg in black on iOS

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendToggleButton/LegendToggleButton.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendToggleButton/LegendToggleButton.tsx
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 Gemeente Amsterdam
 import styled from 'styled-components'
+import { themeColor } from '@amsterdam/asc-ui'
 
 import type { FC } from 'react'
 
@@ -14,6 +15,7 @@ export interface LegendToggleButtonProps {
 const StyledButton = styled(Button)`
   min-width: 0;
   box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.1);
+  color: ${themeColor('tint', 'level7')};
 
   svg path {
     fill: currentColor;
