75c656aa5875730cc521d1169f9775a1e932129a
Jan Jaap van Deursen <janjaap.van.deursen@amsterdam.nl>
2022-03-10 13:43:27 +0100

    - Removes temp file - Adds constant to distinguish between different selected items - uses Leaflet API instead of using @amsterdam/react-maps markers;   allows for grouping a collection of markers that wouldn't otherwise be possible

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/VectorLayer/Nearby/Nearby.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/VectorLayer/Nearby/Nearby.tsx
@@ -1,5 +1,5 @@
 import { useFetch } from 'hooks'
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import L from 'leaflet'
 import { useMapInstance } from '@amsterdam/react-maps'
@@ -7,17 +7,18 @@ import { useMapInstance } from '@amsterdam/react-maps'
 import type { FC } from 'react'
 import type { FeatureCollection, Feature } from 'geojson'
 import type { LatLngTuple, LeafletMouseEvent } from 'leaflet'
+import type { Location } from 'types/incident'
 
+import reverseGeocoderService from 'shared/services/reverse-geocoder'
 import { makeSelectCategory } from 'signals/incident/containers/IncidentContainer/selectors'
 import configuration from 'shared/services/configuration/configuration'
 import { featureToCoordinates } from 'shared/services/map-location'
 
+import { formatDate } from 'signals/incident/containers/IncidentReplyContainer/utils'
 import useBoundingBox from '../../../../hooks/useBoundingBox'
-
-interface NearbyProps {
-  className?: string
-  onMarkerSelect: (properties: Properties) => void
-}
+import type { Item } from '../../../../types'
+import { NEARBY_TYPE } from '../../../../constants'
+import AssetSelectContext from '../../../context'
 
 // Custom Point type, because the compiler complains about the coordinates type
 type Point = {
@@ -38,56 +39,92 @@ interface MarkerMouseEvent extends LeafletMouseEvent {
   sourceTarget: NearbyMarker
 }
 
-const nearbyMarkerIcon = L.icon({
+export const nearbyMarkerIcon = L.icon({
   iconSize: [24, 32],
   iconUrl: '/assets/images/area-map/icon-pin.svg',
 })
 
-const nearbyMarkerSelectedIcon = L.icon({
+export const nearbyMarkerSelectedIcon = L.icon({
   iconSize: [40, 40],
   iconUrl: '/assets/images/area-map/icon-pin-red.svg',
 })
 
-const Nearby: FC<NearbyProps> = ({ onMarkerSelect }) => {
+const formattedDate = (date: string) =>
+  formatDate(new Date(date), `'Gemeld op:' dd MMMM`)
+
+const onFeatureGroupClick = ({
+  sourceTarget, // clicked marker
+  target, // feature group
+}: LeafletMouseEvent) => {
+  const targetElement = sourceTarget.getElement()
+
+  target.eachLayer((layer: L.Marker<Properties>) => {
+    const element = layer.getElement()
+
+    if (!element) return
+
+    if (element !== targetElement) {
+      layer.setIcon(nearbyMarkerIcon)
+    }
+  })
+}
+
+/**
+ * Component using the Leaflet API to plot markers in a feature layer on a map
+ * Because the markers are rendered in a feature group, events can be captured by that group.
+ * Which is useful, because the all of the layers in the feature group can be easily targeted.
+ */
+const Nearby: FC = () => {
+  const { selection, setItem } = useContext(AssetSelectContext)
   const bbox = useBoundingBox()
   const mapInstance = useMapInstance()
   const { category, subcategory } = useSelector(makeSelectCategory)
-  const { get, data } = useFetch<FeatureCollection<Point, Properties>>()
-  // const [activeLayer, setActiveLayer] = useState()
-
-  const onFeatureClick = ({
-    sourceTarget, // clicked marker
-    target, // feature group
-  }: LeafletMouseEvent) => {
-    const targetElement = sourceTarget.getElement()
-
-    target.eachLayer((layer: L.Marker<Properties>) => {
-      const element = layer.getElement()
-
-      if (!element) return
-
-      if (element !== targetElement) {
-        layer.setIcon(nearbyMarkerIcon)
-      }
-    })
-  }
-
-  const featureGroup = useRef<L.FeatureGroup<NearbyMarker>>(
-    L.featureGroup().on('click', onFeatureClick).addTo(mapInstance)
-  )
+  const { get, data, error } = useFetch<FeatureCollection<Point, Properties>>()
+  const [activeLayer, setActiveLayer] = useState<NearbyMarker>()
+  const featureGroup = useRef<L.FeatureGroup<NearbyMarker>>(L.featureGroup())
 
   const onMarkerClick = useCallback(
     (feature: Feature<Point, Properties>) =>
-      ({ originalEvent, sourceTarget }: MarkerMouseEvent) => {
-        // prevent a location marker from being placed on the map
-        originalEvent.stopPropagation()
-
+      async ({ sourceTarget }: MarkerMouseEvent) => {
         sourceTarget.setIcon(nearbyMarkerSelectedIcon)
 
-        onMarkerSelect(feature.properties)
+        setActiveLayer(sourceTarget)
+
+        const coordinates = featureToCoordinates(feature.geometry)
+
+        const location: Location = {
+          coordinates,
+        }
+
+        const item: Item = {
+          label: feature.properties.category.name,
+          description: formattedDate(feature.properties.created_at),
+          type: NEARBY_TYPE,
+        }
+
+        setItem(item, location)
+
+        const response = await reverseGeocoderService(coordinates)
+
+        if (response) {
+          location.address = response.data.address
+        }
+
+        setItem(item, location)
       },
-    [onMarkerSelect]
+    [setItem]
   )
+
+  useEffect(() => {
+    if (!selection || selection.type !== NEARBY_TYPE) {
+      setActiveLayer(undefined)
+    }
+  }, [selection])
+
+  useEffect(() => {
+    featureGroup.current.on('click', onFeatureGroupClick)
+    featureGroup.current.addTo(mapInstance)
+  }, [mapInstance])
 
   useEffect(() => {
     if (!bbox || !category || !subcategory) return
@@ -103,56 +140,46 @@ const Nearby: FC<NearbyProps> = ({ onMarkerSelect }) => {
   }, [get, bbox, category, subcategory])
 
   useEffect(() => {
-    if (!data?.features) return
-
-    // if (featureGroup.getLayers().length > 0) {
-    // console.log('clearing layers')
-    // featureGroup.clearLayers()
     featureGroup.current.clearLayers()
-    // mapInstance.removeLayer(featureGroup.current)
-    // featureGroup.current.addTo(mapInstance)
-    // }
 
-    // featureGroup.clearAllEventListeners()
+    if (error) {
+      featureGroup.current.clearAllEventListeners()
+      mapInstance.removeLayer(featureGroup.current)
+      return
+    }
+
+    if (!data?.features) return
 
     data.features.forEach((feature) => {
       const { lat, lng } = featureToCoordinates(feature.geometry)
-      const icon = nearbyMarkerIcon
-      const marker = L.marker({ lat, lng }, { icon })
+      const uniqueId = `${lat}.${lng}.${feature.properties.created_at}`
+      const marker = L.marker(
+        { lat, lng },
+        {
+          alt: uniqueId,
+          title: `${feature.properties.category.name}, ${formattedDate(
+            feature.properties.created_at
+          )}`,
+        }
+      )
 
+      /**
+       * When the component updates (on bounding box change, for instance), markers are rerendered.
+       * We need to check if a particular marker isn't already present in the feature group to
+       * prevent duplicate markers.
+       */
       if (featureGroup.current.hasLayer(marker)) return
 
+      const isActiveMarker = activeLayer?.options.alt === marker.options.alt
+
+      marker.setIcon(
+        isActiveMarker ? nearbyMarkerSelectedIcon : nearbyMarkerIcon
+      )
       marker.addEventListener('click', onMarkerClick(feature))
 
       featureGroup.current.addLayer(marker)
     })
-    //     const { lat, lng } = featureToCoordinates(feature.geometry)
-    //     const uniqueId = `${lat}.${lng}.${feature.properties.created_at}`
-    //     const pin = activeMarkerId === uniqueId ? 'pin-red' : 'pin'
-
-    //     console.log('rendering ', uniqueId, '; pin: ', pin)
-    //     return (
-    //       <Marker
-    //         key={uniqueId}
-    //         latLng={{ lat, lng }}
-    //         options={{
-    //           icon: L.icon({
-    //             iconSize: [24, 32],
-    //             iconUrl: `/assets/images/area-map/icon-${pin}.svg`,
-    //           }),
-    //         }}
-    //         events={{
-    //           // Disabling linter; arm-core Marker component uses an incorrect type
-    //           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //           // @ts-ignore
-    //           click: onMarkerClick(feature, uniqueId),
-    //         }}
-    //       />
-    //     )
-    //   })
-
-    //   setMarkers(markersFromData)
-  }, [data, featureGroup, mapInstance, onMarkerClick])
+  }, [activeLayer, data, featureGroup, mapInstance, onMarkerClick, error])
 
   return <span data-testid="nearby" />
 }
