de4de2449dff5b97de08008180ba1de4b5bf52a7
Boris Arkenaar <boris@arkenaar.com>
2022-04-25 16:47:03 +0200

    Implement download functionality

--- src/signals/settings/export/export.tsx
+++ src/signals/settings/export/export.tsx
@@ -1,20 +1,39 @@
-import { Button, Column, Link, Row } from '@amsterdam/asc-ui'
+import { Button, Column, Row } from '@amsterdam/asc-ui'
 import PageHeader from 'signals/settings/components/PageHeader'
-import { Fragment } from 'react'
+import { Fragment, useCallback, useEffect } from 'react'
+import { useFetch } from 'hooks'
 
 const EXPORT_URL = 'http://localhost:8000/signals/v1/private/csv'
 
 const ExportContainer = () => {
+  const { get, data, error, isLoading } = useFetch<Blob>()
+
+  const download = useCallback(() => {
+    get(EXPORT_URL, {}, { responseType: 'blob' })
+  }, [get])
+
+  useEffect(() => {
+    if (data) {
+      window.location.assign(window.URL.createObjectURL(data))
+    }
+  }, [data])
+
   return (
     <Fragment>
       <PageHeader title="Export" />
       <Row>
         <Column span={12} wrap>
-          <Button variant="primary" forwardedAs={Link} as="a" href={EXPORT_URL}>
+          <Button variant="primary" onClick={download}>
             Download export
           </Button>
         </Column>
       </Row>
+      {isLoading && <Row>Downloading...</Row>}
+      {error && (
+        <Row>
+          Er ging iets mis: {(error as any).name} {(error as any).message}
+        </Row>
+      )}
     </Fragment>
   )
 }
