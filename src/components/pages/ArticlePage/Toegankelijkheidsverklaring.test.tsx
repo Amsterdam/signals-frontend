b2434cf422d4b612cd7fb8596f45cb279afe9a7a
Kirsten Ruys <k.ruijs@amsterdam.nl>
2022-05-31 16:36:48 +0200

    Increase test coverage

---
+++ src/components/pages/ArticlePage/Toegankelijkheidsverklaring.test.tsx
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2022 Gemeente Amsterdam
+import { render, screen } from '@testing-library/react'
+
+import { withAppContext } from 'test/utils'
+
+import Toegankelijkheidsverklaring from './Toegankelijkheidsverklaring'
+
+describe('components/Toegankelijkheidsverklaring', () => {
+  it('Renders Toegankelijkheidsverklaring', () => {
+    render(withAppContext(<Toegankelijkheidsverklaring />))
+
+    expect(
+      screen.getByText('Deze verklaring geldt voor de websites:')
+    ).toBeInTheDocument()
+  })
+})
