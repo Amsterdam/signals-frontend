77101d3f8c63f544a557a567a8379b845d7c52f5
Bart <bart@jeukendrup.nl>
2022-08-18 15:52:08 +0200

    fix: make category translations Amsterdam-specific (#2230)

--- src/shared/services/resolveClassification/index.js
+++ src/shared/services/resolveClassification/index.js
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2020 - 2021 Gemeente Amsterdam
+import configuration from 'shared/services/configuration/configuration'
+
 export const MINIMUM_CERTAINTY = 0.41
 export const DEFAULT_CLASSIFICATION = 'overig'
 
@@ -38,62 +40,83 @@ const resolveClassification = ({
 
   if (hoofdrubriekMeetsMinimumCertainty) {
     const [, category] = hoofdrubriek[0][0].match(reCategory)
-    let subcategory
 
-    switch (category) {
-      case 'afval':
-        subcategory = 'overig-afval'
-        break
-
-      case 'schoon':
-        subcategory = 'veegzwerfvuil'
-        break
-
-      case 'openbaar-groen-en-water':
-        subcategory = 'overig-groen-en-water'
-        break
-
-      case 'overlast-bedrijven-en-horeca':
-        subcategory = 'overig-horecabedrijven'
-        break
-
-      case 'overlast-in-de-openbare-ruimte':
-        subcategory = 'overig-openbare-ruimte'
-        break
-
-      case 'overlast-op-het-water':
-        subcategory = 'overig-boten'
-        break
-
-      case 'overlast-van-dieren':
-        subcategory = 'overig-dieren'
-        break
-
-      case 'overlast-van-en-door-personen-of-groepen':
-        subcategory = 'overige-overlast-door-personen'
-        break
-
-      case 'wegen-verkeer-straatmeubilair':
-        subcategory = 'overig-wegen-verkeer-straatmeubilair'
-        break
-
-      case 'wonen':
-        subcategory = 'wonen-overig'
-        break
-
-      default:
-        subcategory = DEFAULT_CLASSIFICATION
+    if (configuration.featureFlags.enableAmsterdamSpecificOverigCategories) {
+      return amsterdamSpecificOverigCategory(category)
     }
+
+    const genericOverigSubcategory = `${DEFAULT_CLASSIFICATION}-${category}`
 
     return {
       category,
-      subcategory,
+      subcategory: genericOverigSubcategory,
     }
   }
 
   return {
     category: DEFAULT_CLASSIFICATION,
     subcategory: DEFAULT_CLASSIFICATION,
+  }
+}
+
+/**
+ * Get Amsterdam-specific "overig" categories
+ *
+ * @param {String} category - The main category
+ *
+ * @returns {Object} With keys `category`, `subcategory`
+ */
+const amsterdamSpecificOverigCategory = (category) => {
+  let subcategory
+
+  switch (category) {
+    case 'afval':
+      subcategory = 'overig-afval'
+      break
+
+    case 'schoon':
+      subcategory = 'veegzwerfvuil'
+      break
+
+    case 'openbaar-groen-en-water':
+      subcategory = 'overig-groen-en-water'
+      break
+
+    case 'overlast-bedrijven-en-horeca':
+      subcategory = 'overig-horecabedrijven'
+      break
+
+    case 'overlast-in-de-openbare-ruimte':
+      subcategory = 'overig-openbare-ruimte'
+      break
+
+    case 'overlast-op-het-water':
+      subcategory = 'overig-boten'
+      break
+
+    case 'overlast-van-dieren':
+      subcategory = 'overig-dieren'
+      break
+
+    case 'overlast-van-en-door-personen-of-groepen':
+      subcategory = 'overige-overlast-door-personen'
+      break
+
+    case 'wegen-verkeer-straatmeubilair':
+      subcategory = 'overig-wegen-verkeer-straatmeubilair'
+      break
+
+    case 'wonen':
+      subcategory = 'wonen-overig'
+      break
+
+    default:
+      subcategory = DEFAULT_CLASSIFICATION
+  }
+
+  return {
+    category,
+    subcategory,
   }
 }
 
