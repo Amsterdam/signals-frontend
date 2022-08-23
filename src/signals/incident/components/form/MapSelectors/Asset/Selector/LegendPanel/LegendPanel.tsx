dba63096440c0876f12202df91b28e2dbbb3ba69
Vincent <vdegraaf.vincent@gmail.com>
2022-08-23 09:25:02 +0200

    make legend panel focusable

--- src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendPanel/LegendPanel.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector/LegendPanel/LegendPanel.tsx
@@ -11,6 +11,7 @@ export interface LegendPanelProps {
   items: { id: string; iconUrl: string; label: string }[]
   onClose: () => void
   slide?: 'in' | 'out'
+  buttonRef: React.ForwardedRef<HTMLButtonElement>
 }
 
 const LegendPanel: FunctionComponent<LegendPanelProps> = ({
@@ -18,6 +19,7 @@ const LegendPanel: FunctionComponent<LegendPanelProps> = ({
   items,
   onClose,
   slide = 'out',
+  buttonRef
 }) => (
   <Panel
     className={`${className} ${slide}`}
@@ -27,7 +29,7 @@ const LegendPanel: FunctionComponent<LegendPanelProps> = ({
   >
     <Title>Uitleg</Title>
 
-    <CloseBtn tabIndex={-1} title="Sluit uitleg" onClick={onClose} />
+    <CloseBtn data-testid="close-button" ref={buttonRef} tabIndex={0} title="Sluit uitleg" onClick={onClose}/>
 
     <ScrollWrapper>
       <IconList data-testid="legendPanelList">
