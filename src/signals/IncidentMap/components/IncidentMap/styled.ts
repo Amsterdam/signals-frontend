8716f9c477eee0e86b4fc10e875d3c2fade5af06
Vincent <vdegraaf.vincent@gmail.com>
2022-09-19 16:11:45 +0200

    double height

--- src/signals/IncidentMap/components/IncidentMap/styled.ts
+++ src/signals/IncidentMap/components/IncidentMap/styled.ts
@@ -18,7 +18,6 @@ export const Wrapper = styled.div`
   bottom: 0;
   height: calc(100% - ${themeSpacing(HEADER_HEIGHT_DESKTOP)});
   width: 100%;
-  height: 100vh;
   box-sizing: border-box;
   display: flex;
   flex-direction: row-reverse;
