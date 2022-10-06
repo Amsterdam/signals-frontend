fb5948e8de9d9d5b3f4c9d53641b16d87d9bd07e
Oscar <bakkeroscar@gmail.com>
2022-10-06 13:27:19 +0200

    Refactored the yup schema creator.

--- src/signals/incident/services/yup-resolver/index.test.ts
+++ src/signals/incident/services/yup-resolver/index.test.ts
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2022 Gemeente Amsterdam
 
-import { setUpSchema } from './index'
+import { validatePhoneNumber } from '../custom-validators'
+import { setupSchema } from './index'
 
 describe('Yup resolver takes a bunch of controls and returns it into a schema', () => {
   it('should return a schema', async () => {
@@ -11,14 +12,9 @@ describe('Yup resolver takes a bunch of controls and returns it into a schema', 
           validators: ['required'],
         },
       },
-      phone: {
+      locatie: {
         options: {
           validators: ['required'],
-        },
-      },
-      location: {
-        options: {
-          validators: [],
         },
       },
       email: {
@@ -31,25 +27,26 @@ describe('Yup resolver takes a bunch of controls and returns it into a schema', 
           validators: ['required', ['maxLength', 1]],
         },
       },
-      validatewithfunction: {
+      phoneNumber: {
         options: {
-          validators: [() => ({ custom: 'bad' })],
+          validators: [validatePhoneNumber],
         },
       },
     }
 
-    const schema = setUpSchema(controls)
+    const schema = setupSchema(controls)
     await expect(
       schema.validateAt('source', { source: 'online' })
     ).resolves.toBeTruthy()
     await expect(
-      schema.validateAt('phone', { phone: '23432423' })
-    ).resolves.toBeTruthy()
-    await expect(
-      schema.validateAt('location', { location: '' })
+      schema.validateAt('locatie', {
+        locatie: { location: { coordinates: undefined } },
+      })
     ).rejects.toBeTruthy()
     await expect(
-      schema.validateAt('location', { location: {} })
+      schema.validateAt('locatie', {
+        locatie: { location: { coordinates: 'blackrosemarchesa' } },
+      })
     ).resolves.toBeTruthy()
     await expect(
       schema.validateAt('email', { email: 'blackrosemarchesa' })
@@ -61,7 +58,10 @@ describe('Yup resolver takes a bunch of controls and returns it into a schema', 
       schema.validateAt('maxlen2', { maxlen2: 'b' })
     ).resolves.toBeTruthy()
     await expect(
-      schema.validateAt('validatewithfunction', { validatewithfunction: 'b' })
+      schema.validateAt('phoneNumber', { phoneNumber: 'armageddon' })
     ).rejects.toBeTruthy()
+    await expect(
+      schema.validateAt('phoneNumber', { phoneNumber: 666 })
+    ).resolves.toBeTruthy()
   })
 })
