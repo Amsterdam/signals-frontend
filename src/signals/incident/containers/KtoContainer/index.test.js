051a366e74cd9ecd0b12f08ffab3cebc809bdd6e
Oscar <bakkeroscar@gmail.com>
2022-10-06 15:52:07 +0200

    Add test.

--- src/signals/incident/containers/KtoContainer/index.test.js
+++ src/signals/incident/containers/KtoContainer/index.test.js
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2022 Gemeente Amsterdam
-import { render, act, fireEvent, screen } from '@testing-library/react'
-import * as reactRouterDom from 'react-router-dom'
-
-import configuration from 'shared/services/configuration/configuration'
-import ktoFixture from 'utils/__tests__/fixtures/kto.json'
-import { withAppContext } from 'test/utils'
-import { useParams } from 'react-router-dom'
-// eslint-disable-next-line no-restricted-imports
-import React from 'react'
-import userEvent from '@testing-library/user-event'
 import { waitFor } from '@babel/core/lib/gensync-utils/async'
+import { render, act, fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as reactRouterDom from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import configuration from 'shared/services/configuration/configuration'
+import { withAppContext } from 'test/utils'
+import ktoFixture from 'utils/__tests__/fixtures/kto.json'
+// eslint-disable-next-line no-restricted-imports
+
 import KTOContainer, { renderSections, successSections } from '.'
 
 jest.mock('react-router-dom', () => ({
@@ -113,7 +112,7 @@ describe('signals/incident/containers/KtoContainer', () => {
     expect(getByText(/nee, ik ben niet/i)).toBeInTheDocument()
   })
 
-  it('should PUT form data', async () => {
+  it('should PUT form data via checkbox fields', async () => {
     fetch.mockResponses(
       [JSON.stringify({}), { status: 200 }], // 'GET'
       [JSON.stringify(ktoFixture), { status: 200 }], // 'GET'
@@ -121,6 +120,7 @@ describe('signals/incident/containers/KtoContainer', () => {
     )
 
     configuration.featureFlags.reporterMailHandledNegativeContactEnabled = false
+    configuration.featureFlags.enableMultipleKtoQuestions = true
 
     const successHeaderText = 'Bedankt voor uw reactie'
     const { container, findByTestId, queryByText, getByText, rerender } =
@@ -132,6 +132,86 @@ describe('signals/incident/containers/KtoContainer', () => {
 
     act(() => {
       fireEvent.click(container.querySelector('input[type="checkbox"]'))
+    })
+
+    const ktoSubmit = await findByTestId('ktoSubmit')
+
+    expect(fetch).toHaveBeenCalledTimes(2)
+
+    expect(queryByText(successHeaderText)).not.toBeInTheDocument()
+
+    act(() => {
+      fireEvent.click(ktoSubmit)
+    })
+
+    await findByTestId('ktoFormContainer')
+
+    expect(fetch).toHaveBeenCalledTimes(3)
+
+    expect(fetch).toHaveBeenLastCalledWith(
+      `${configuration.FEEDBACK_FORMS_ENDPOINT}${uuid}`,
+      expect.objectContaining({
+        method: 'PUT',
+        body: expect.objectContaining({}),
+      })
+    )
+
+    expect(getByText(successHeaderText)).toBeInTheDocument()
+
+    expect(getByText(successSections['ja'].body)).toBeInTheDocument()
+
+    useParams.mockImplementation(() => ({
+      satisfactionIndication: 'nee',
+      uuid,
+    }))
+
+    rerender(withAppContext(<KTOContainer />))
+
+    expect(screen.getByTestId('succesSectionBody')).toContainHTML(
+      successSections['nee'].body
+    )
+    expect(
+      screen.queryByTestId('succesContactAllowedText')
+    ).not.toBeInTheDocument()
+
+    rerender(withAppContext(<KTOContainer />))
+
+    expect(screen.getByTestId('succesSectionBody')).toContainHTML(
+      successSections['nee'].body
+    )
+
+    useParams.mockImplementation(() => ({
+      satisfactionIndication: 'ja',
+      uuid,
+    }))
+
+    rerender(withAppContext(<KTOContainer />))
+
+    expect(screen.getByTestId('succesSectionBody')).toContainHTML(
+      successSections['ja'].body
+    )
+  })
+
+  it('should PUT form data via radio input fields', async () => {
+    fetch.mockResponses(
+      [JSON.stringify({}), { status: 200 }], // 'GET'
+      [JSON.stringify(ktoFixture), { status: 200 }], // 'GET'
+      [JSON.stringify({}), { status: 200 }] // 'PUT'
+    )
+
+    configuration.featureFlags.reporterMailHandledNegativeContactEnabled = false
+    configuration.featureFlags.enableMultipleKtoQuestions = false
+
+    const successHeaderText = 'Bedankt voor uw reactie'
+    const { container, findByTestId, queryByText, getByText, rerender } =
+      render(withAppContext(<KTOContainer />))
+
+    await findByTestId('ktoFormContainer')
+
+    // assuming the form renders a list of radio buttons and that only a checked button in that list is required
+
+    act(() => {
+      fireEvent.click(container.querySelector('input[type="radio"]'))
     })
 
     const ktoSubmit = await findByTestId('ktoSubmit')
