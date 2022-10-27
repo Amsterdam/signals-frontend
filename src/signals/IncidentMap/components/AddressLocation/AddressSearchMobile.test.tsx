53ab5115b0012ad5292119b76e4c57534808505c
Vincent <vdegraaf.vincent@gmail.com>
2022-10-27 13:19:16 +0200

    remove unncessecary comment

--- src/signals/IncidentMap/components/AddressLocation/AddressSearchMobile.test.tsx
+++ src/signals/IncidentMap/components/AddressLocation/AddressSearchMobile.test.tsx
@@ -78,7 +78,6 @@ describe('AddresLocation', () => {
 
     userEvent.click(screen.getByText('selectItem'))
 
-    //the mocked PDOKAutoSuggest triggers automatically onSelect/onAddressSelect, no userEvents are necessary
     expect(defaultProps.setCoordinates).toHaveBeenCalledWith(
       mockPDOKResponse.data.location
     )
