7b0d6073342ef603f97863e2ceed1e65e061f3b6
Vincent <vdegraaf.vincent@gmail.com>
2022-11-08 08:57:30 +0100

    run auto-order

--- src/signals/settings/users/Detail/components/UserForm/index.js
+++ src/signals/settings/users/Detail/components/UserForm/index.js
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (C) 2019 - 2021 Gemeente Amsterdam
+// Copyright (C) 2019 - 2022 Gemeente Amsterdam
 import { useReducer, useCallback } from 'react'
 
 import { themeSpacing, Row, Column } from '@amsterdam/asc-ui'
