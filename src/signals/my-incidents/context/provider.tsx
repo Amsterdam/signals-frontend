f78b52f7783df2741ee726aa387f9b240a4412c3
Vincent <vdegraaf.vincent@gmail.com>
2022-11-09 13:57:06 +0100

    remove unnecessary useMemo

--- src/signals/my-incidents/context/provider.tsx
+++ src/signals/my-incidents/context/provider.tsx
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2022 Gemeente Amsterdam
 import type { FC, ReactNode } from 'react'
-import { useMemo } from 'react'
 
 import type { MyIncidentsValue } from '../types'
 import { MyIncidentsContext } from './context'
@@ -16,20 +15,10 @@ interface Props {
   value?: MyIncidentsValue
 }
 
-export const MyIncidentsProvider: FC<Props> = ({ value, children }) => {
-  const newValue = useMemo(
-    () => ({
-      ...initialValue,
-      ...value,
-    }),
-    [value]
-  )
-
-  return (
-    <MyIncidentsContext.Provider value={newValue}>
-      {children}
-    </MyIncidentsContext.Provider>
-  )
-}
+export const MyIncidentsProvider: FC<Props> = ({ value, children }) => (
+  <MyIncidentsContext.Provider value={{ ...initialValue, ...value }}>
+    {children}
+  </MyIncidentsContext.Provider>
+)
 
 export default MyIncidentsContext
