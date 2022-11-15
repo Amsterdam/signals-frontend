b643b01fec6369404315d97e993279979a3d3eaa
Vincent <vdegraaf.vincent@gmail.com>
2022-11-15 12:05:33 +0100

    implement PR feedback

--- src/signals/my-incidents/pages/RequestAccess.tsx
+++ src/signals/my-incidents/pages/RequestAccess.tsx
@@ -6,12 +6,12 @@ import { BasePage } from './BasePage'
 export const RequestAccess = () => (
   <BasePage
     pageInfo={{
-      documentTitle: 'Inloggen Mijn Meldingen',
+      documentTitle: 'Inloggen',
       dataTestId: 'requestAccessMyIncidents',
       pageTitle: 'Mijn meldingen',
     }}
     paragraphs={[
-      `Log in met het e-mailadres waarmee u medlingen maakt. U krijgt dan een bevestigingsmail om naar het meldingenoverzicht te gaan.`,
+      `Log in met het e-mailadres waarmee u meldingen maakt. U krijgt dan een bevestigingsmail om naar het meldingenoverzicht te gaan.`,
     ]}
   >
     <EmailInput />
