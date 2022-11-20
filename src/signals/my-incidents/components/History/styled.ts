b7b3a4cd41e9cea0fa2f83ebb7238c8ec9d1499d
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2022-11-20 22:09:10 +0100

    styling-working-for-real

--- src/signals/my-incidents/components/History/styled.ts
+++ src/signals/my-incidents/components/History/styled.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2022 Gemeente Amsterdam
-import styled from 'styled-components'
 import {breakpoint, Heading, themeColor, themeSpacing} from '@amsterdam/asc-ui'
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
 display: flex;
