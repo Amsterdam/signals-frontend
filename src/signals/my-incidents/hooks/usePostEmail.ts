70ef5dfa59c72006e34a399722c01609316fc863
Vincent <vdegraaf.vincent@gmail.com>
2022-11-24 16:02:05 +0100

    refactor typing

--- src/signals/my-incidents/hooks/usePostEmail.ts
+++ src/signals/my-incidents/hooks/usePostEmail.ts
@@ -5,8 +5,6 @@ import { useCallback, useEffect, useState } from 'react'
 import useFetch from 'hooks/useFetch'
 import configuration from 'shared/services/configuration/configuration'
 
-import { isFetchError, isResponseError } from '../types'
-
 type PostEmail = (email: string) => Promise<void>
 
 export const usePostEmail = (): [
@@ -14,6 +12,7 @@ export const usePostEmail = (): [
   { errorMessage: string | null }
 ] => {
   const { post, error } = useFetch<null>()
+  const postError = error as Response | undefined
   const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   const endpoint = `${configuration.MY_SIGNALS_LOGIN_URL}`
@@ -30,19 +29,14 @@ export const usePostEmail = (): [
   )
 
   useEffect(() => {
-    if (
-      error &&
-      isFetchError(error) &&
-      isResponseError(error) &&
-      error.status === 429
-    ) {
+    if (postError?.status === 429) {
       setErrorMessage(
         `U hebt te vaak gevraagd om de e-mail opnieuw te versturen. Over 20 minuten kunt u het opnieuw proberen.`
       )
     } else {
       setErrorMessage(null)
     }
-  }, [error])
+  }, [postError])
 
   return [postEmail, { errorMessage }]
 }
