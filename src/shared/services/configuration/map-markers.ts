324ad750c7890e912cc68eddce68071301634567
Oscar <bakkeroscar@gmail.com>
2022-12-06 14:09:46 +0100

    Update spdx records.

--- src/shared/services/configuration/map-markers.ts
+++ src/shared/services/configuration/map-markers.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (C) 2020 - 2021 Gemeente Amsterdam
+// Copyright (C) 2020 - 2022 Gemeente Amsterdam
 /* eslint-disable global-require */
 import L from 'leaflet'
 
