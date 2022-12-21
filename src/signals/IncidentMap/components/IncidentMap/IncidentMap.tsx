48b50d8bca13e03c6678b9e121c433b68870c95e
Oscar <bakkeroscar@gmail.com>
2022-12-21 16:33:37 +0100

    Move logic for getting incidents to custom hook.

--- src/signals/IncidentMap/components/IncidentMap/IncidentMap.tsx
+++ src/signals/IncidentMap/components/IncidentMap/IncidentMap.tsx
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2022 Gemeente Amsterdam
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 import { ViewerContainer } from '@amsterdam/arm-core'
-import type { FeatureCollection } from 'geojson'
-import type { Feature } from 'geojson'
-import { useFetch } from 'hooks'
 import type { LatLngLiteral, Map as MapType } from 'leaflet'
-import configuration from 'shared/services/configuration/configuration'
 import { dynamicIcon } from 'shared/services/configuration/map-markers'
 import MAP_OPTIONS from 'shared/services/configuration/map-options'
 import { formatAddress } from 'shared/services/format-address'
@@ -16,7 +12,7 @@ import reverseGeocoderService from 'shared/services/reverse-geocoder'
 import { MapMessage } from 'signals/incident/components/form/MapSelectors/components/MapMessage'
 import type { Bbox } from 'signals/incident/components/form/MapSelectors/hooks/useBoundingBox'
 
-import type { Filter, Incident, PointLatLng, Properties } from '../../types'
+import type { Filter, Incident, Properties } from '../../types'
 import { AddressLocation } from '../AddressLocation'
 import { AddressSearchMobile } from '../AddressLocation/AddressSearchMobile'
 import { DrawerOverlay, DrawerState } from '../DrawerOverlay'
@@ -31,6 +27,7 @@ import {
 } from '../utils'
 import { Pin } from './Pin'
 import { StyledMap, StyledParagraph, Wrapper } from './styled'
+import usePaginatedIncidents from './usePaginatedIncidents'
 import { getZoom } from './utils'
 
 export const IncidentMap = () => {
@@ -51,9 +48,6 @@ export const IncidentMap = () => {
   const [filteredIncidents, setFilteredIncidents] = useState<Incident[]>()
 
   const mode = useDeviceMode()
-
-  const { get, data, error, isSuccess } =
-    useFetch<FeatureCollection<PointLatLng, Properties>>()
 
   const closeDrawerOverlay = useCallback(() => {
     setDrawerState(DrawerState.Closed)
@@ -99,69 +93,24 @@ export const IncidentMap = () => {
   }, [])
 
   /* istanbul ignore next */
-  const searchParams = useMemo(() => {
-    if (bbox) {
-      const { west, south, east, north } = bbox
-      return new URLSearchParams({
-        bbox: `${west},${south},${east},${north}`,
-      })
-    }
-  }, [bbox])
-
-  /* istanbul ignore next */
-  useEffect(() => {
-    if (bbox && searchParams) {
-      get(
-        `${configuration.GEOGRAPHY_PUBLIC_ENDPOINT}?${searchParams.toString()}`
-      )
-      paginatedIncidents.current.page = 2
-    }
-  }, [bbox, get, searchParams])
-
-  /* istanbul ignore next */
-  const paginatedIncidents = useRef<{
-    page: number
-    features: Feature<PointLatLng, Properties>[]
-  }>({ page: 2, features: [] })
-
-  /* istanbul ignore next */
-  const getPaginatedIncidents = useCallback(() => {
-    const incidents = data?.features || []
-
-    if (searchParams && incidents.length === 4000) {
-      searchParams.set('geopage', paginatedIncidents.current.page.toString())
-
-      get(
-        `${configuration.GEOGRAPHY_PUBLIC_ENDPOINT}?${searchParams.toString()}`
-      )
-
-      paginatedIncidents.current.page = paginatedIncidents.current.page + 1
-    }
-  }, [data?.features, get, searchParams])
-
-  /* istanbul ignore next */
   useEffect(() => {
     map?.on({
       click: resetSelectedMarker,
     })
   }, [resetSelectedMarker, map])
 
+  const { incidents, isSuccess, error, getIncidents } = usePaginatedIncidents()
+
+  useEffect(() => {
+    if (bbox) {
+      getIncidents(bbox)
+    }
+  }, [bbox, getIncidents])
+
   /* istanbul ignore next */
   useEffect(() => {
-    getPaginatedIncidents()
-    if (paginatedIncidents.current.page === 2 && data?.features) {
-      paginatedIncidents.current.features = data.features
-    }
-    if (paginatedIncidents.current.page > 2 && data?.features) {
-      paginatedIncidents.current.features = [
-        ...paginatedIncidents.current.features,
-        ...data.features,
-      ]
-    }
-
-    const incidents = paginatedIncidents.current.features || []
-
     const filteredIncidents = getFilteredIncidents(filters, incidents)
+
     setFilteredIncidents(filteredIncidents)
     const filterFromIncidents = countIncidentsPerFilter(filters, incidents)
     if (
@@ -171,7 +120,7 @@ export const IncidentMap = () => {
     ) {
       setFilters(filterFromIncidents)
     }
-  }, [data, filters, getPaginatedIncidents])
+  }, [bbox, filters, incidents])
 
   useEffect(() => {
     if (error) {
---
+++ src/signals/IncidentMap/components/IncidentMap/usePaginatedIncidents.ts
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2022 Gemeente Amsterdam
+import { useCallback, useEffect, useMemo, useRef } from 'react'
+
+import type { Feature } from 'geojson'
+import type { FeatureCollection } from 'geojson'
+
+import { useFetch } from '../../../../hooks'
+import configuration from '../../../../shared/services/configuration/configuration'
+import type { Bbox } from '../../../incident/components/form/MapSelectors/hooks/useBoundingBox'
+import type { PointLatLng, Properties } from '../../types'
+
+const useIsFrontOffice = () => {
+  const { get, data, error, isSuccess } =
+    useFetch<FeatureCollection<PointLatLng, Properties>>()
+
+  const paginatedIncidents = useRef<{
+    page: number
+    features: Feature<PointLatLng, Properties>[]
+    searchParams: URLSearchParams
+  }>({ page: 2, features: [], searchParams: new URLSearchParams() })
+
+  useEffect(() => {
+    const incidents = data?.features || []
+    const searchParams = paginatedIncidents.current.searchParams
+    if (incidents.length === 4000) {
+      searchParams.set('geopage', paginatedIncidents.current.page.toString())
+
+      get(
+        `${configuration.GEOGRAPHY_PUBLIC_ENDPOINT}?${searchParams.toString()}`
+      )
+
+      paginatedIncidents.current.page = paginatedIncidents.current.page + 1
+    }
+  }, [data?.features, get])
+
+  const getIncidents = useCallback(
+    (bbox: Bbox) => {
+      if (bbox) {
+        paginatedIncidents.current.searchParams = getSearchParams(bbox)
+        get(
+          `${
+            configuration.GEOGRAPHY_PUBLIC_ENDPOINT
+          }?${paginatedIncidents.current.searchParams.toString()}`
+        )
+        paginatedIncidents.current.page = 2
+      }
+    },
+    [get]
+  )
+
+  const getSearchParams = (bbox: Bbox) => {
+    const { west, south, east, north } = bbox
+    return new URLSearchParams({
+      bbox: `${west},${south},${east},${north}`,
+    })
+  }
+
+  const incidents = useMemo(() => {
+    if (paginatedIncidents.current.page === 2 && data?.features) {
+      paginatedIncidents.current.features = data.features
+    }
+
+    if (paginatedIncidents.current.page > 2 && data?.features) {
+      paginatedIncidents.current.features = [
+        ...data.features,
+        ...paginatedIncidents.current.features,
+      ]
+    }
+
+    return paginatedIncidents.current.features
+  }, [data])
+
+  return {
+    incidents: incidents || [],
+    isSuccess,
+    error,
+    getIncidents,
+  }
+}
+
+export default useIsFrontOffice
