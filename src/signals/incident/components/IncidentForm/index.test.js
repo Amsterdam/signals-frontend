7021f9878de3e09f57a55c86dd642eb118fe4858
Oscar <bakkeroscar@gmail.com>
2023-01-03 16:51:02 +0100

    Update test.

--- src/signals/incident/components/IncidentForm/index.test.js
+++ src/signals/incident/components/IncidentForm/index.test.js
@@ -406,7 +406,7 @@ describe('<IncidentForm />', () => {
           })
         })
 
-        expect(triggerSpy).toHaveBeenCalledTimes(6)
+        expect(triggerSpy).toHaveBeenCalledTimes(5)
       })
     })
   })
