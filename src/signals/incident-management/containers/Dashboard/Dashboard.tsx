d032c1145bd1833e9e4d637ae5b455dd234c2eb2
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2023-01-25 15:00:18 +0100

    Add barchart to dashboard
    Made barebones files that need to be filled in  for barchart in dashboard

--- src/signals/incident-management/containers/Dashboard/Dashboard.tsx
+++ src/signals/incident-management/containers/Dashboard/Dashboard.tsx
@@ -2,11 +2,14 @@
 // Copyright (C) 2023 Gemeente Amsterdam
 import { Filter } from './components/Filter'
 import { StyledRow } from './styled'
+import BarChart from './components/barchart'
+
 
 const Dashboard = () => (
   <StyledRow data-testid="menu">
     <Filter />
   </StyledRow>
+   <BarChart />
 )
 
 export default Dashboard
