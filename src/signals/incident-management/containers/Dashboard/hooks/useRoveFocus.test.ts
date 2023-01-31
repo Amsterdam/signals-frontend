3de8cd517c42318e148f5e3ebb9c2998f0e2cdeb
Oscar <bakkeroscar@gmail.com>
2023-01-31 20:24:44 +0100

    Add accessibility to dashboard filter
    Add custom accessibility simulating a native combobox.

---
+++ src/signals/incident-management/containers/Dashboard/hooks/useRoveFocus.test.ts
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MPL-2.0
+// Copyright (C) 2023 Gemeente Amsterdam
+import { renderHook } from '@testing-library/react-hooks'
+import userEvent from '@testing-library/user-event'
+import { act } from 'react-test-renderer'
+
+import { useRoveFocus } from './useRoveFocus'
+
+describe('useRoveFocus', () => {
+  it('should set a focus index', () => {
+    const { result } = renderHook(() => useRoveFocus(10))
+
+    act(() => {
+      userEvent.keyboard('{ArrowDown}')
+    })
+
+    expect(result.current.currentFocus).toBe(1)
+
+    act(() => {
+      userEvent.keyboard('{ArrowUp}')
+    })
+    act(() => {
+      userEvent.keyboard('{ArrowUp}')
+    })
+
+    expect(result.current.currentFocus).toBe(9)
+  })
+})
