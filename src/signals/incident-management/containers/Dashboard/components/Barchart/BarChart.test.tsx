8066f57a52f6242ca4941658cd7749f45378a3d7
SireeKoolenWijkstra <sireekoolenwijkstra@gmail.com>
2023-01-31 11:07:49 +0100

    fixed error in import BarChart in BarChart test file.

--- src/signals/incident-management/containers/Dashboard/components/Barchart/BarChart.test.tsx
+++ src/signals/incident-management/containers/Dashboard/components/Barchart/BarChart.test.tsx
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2023 Gemeente Amsterdam
 import { render, screen } from '@testing-library/react'
-import BarChart from './BarChart'
+import { BarChart } from './BarChart'
 
 describe('BarChart', () => {
   it('should render correctly', () => {
