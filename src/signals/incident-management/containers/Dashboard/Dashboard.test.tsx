0b67337b83488974168e915f391d848d89c1e3da
Vincent <vdegraaf.vincent@gmail.com>
2023-02-15 15:16:14 +0100

    Removed obsolete code Dashboard.tsx and update test

--- src/signals/incident-management/containers/Dashboard/Dashboard.test.tsx
+++ src/signals/incident-management/containers/Dashboard/Dashboard.test.tsx
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2023 Gemeente Amsterdam
 import { ThemeProvider } from '@amsterdam/asc-ui'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { withAppContext } from 'test/utils'
 
 import Dashboard from './Dashboard'
+
+jest.mock('./components', () => {
+  const actual = jest.requireActual('./components')
+  return {
+    ...actual,
+    AreaChart: jest.fn(() => <div>[AreaChart]</div>),
+    BarChart: jest.fn(() => <div>[BarChart]</div>),
+    Filter: jest.fn(() => <div>[Filter]</div>),
+  }
+})
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn()
 
@@ -16,8 +26,12 @@ const renderWithContext = () =>
     </ThemeProvider>
   )
 
-describe('signals/incident-management/containers/Dashboard', () => {
+describe('Dashboard', () => {
   it('should render correctly', () => {
     render(renderWithContext())
+
+    expect(screen.getByText('[Filter]')).toBeInTheDocument()
+    expect(screen.getByText('[BarChart]')).toBeInTheDocument()
+    expect(screen.getByText('[AreaChart]')).toBeInTheDocument()
   })
 })
--- src/signals/incident-management/containers/Dashboard/Dashboard.tsx
+++ src/signals/incident-management/containers/Dashboard/Dashboard.tsx
@@ -1,39 +1,18 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2023 Gemeente Amsterdam
-import { useCallback, useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import GlobalError from 'components/GlobalError'
-
-import { AreaChart } from './components'
-import { BarChart } from './components/BarChart'
-import { Filter } from './components/Filter'
+import { AreaChart, BarChart, Filter } from './components'
 import { StyledRow } from './styled'
 
 const Dashboard = () => {
-  const [error] = useState<boolean>(false)
-  const [showMessage, setShowMessage] = useState<boolean>(false)
-  const [errorMessage, setErrorMessage] = useState<JSX.Element | string>('')
   const [queryString, setQueryString] = useState<string>('')
-
-  const setNotification = useCallback(
-    (message: JSX.Element | string) => {
-      setErrorMessage(message)
-      setShowMessage(true)
-    },
-    [setErrorMessage, setShowMessage]
-  )
-  useEffect(() => {
-    if (error) {
-      setNotification('Er kan geen data worden geladen. Vernieuw de pagina.')
-    }
-  }, [error, setNotification])
 
   return (
     <StyledRow data-testid="menu">
       <Filter callback={setQueryString} />
       <BarChart />
       <AreaChart queryString={queryString} />
-      {errorMessage && showMessage && <GlobalError>{errorMessage}</GlobalError>}
     </StyledRow>
   )
 }
