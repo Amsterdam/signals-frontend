cb4b736798962644b5aeeb83bf216ea6966ff9f2
Vincent <vdegraaf.vincent@gmail.com>
2023-04-24 15:59:33 +0200

    Ignore detail.tsx implementations

--- src/signals/settings/categories/main-categories/Detail.tsx
+++ src/signals/settings/categories/main-categories/Detail.tsx
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2023 Gemeente Amsterdam
+/* istanbul ignore file */
 import { CategoryDetail } from '../components'
 
-export const DetailContainer = () => {
-  return (
-    <CategoryDetail
-      isMainCategory={true}
-      entityName="Hoofdcategorie"
-      isPublicAccessibleLabel="Toon meldingen van deze hoofdcategorie op openbare kaarten en op de kaart in het meldformulier."
-    />
-  )
-}
+export const DetailContainer = () => (
+  <CategoryDetail
+    isMainCategory={true}
+    entityName="Hoofdcategorie"
+    isPublicAccessibleLabel="Toon meldingen van deze hoofdcategorie op openbare kaarten en op de kaart in het meldformulier."
+  />
+)
 
 export default DetailContainer
--- src/signals/settings/categories/subcategories/Detail.tsx
+++ src/signals/settings/categories/subcategories/Detail.tsx
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2023 Gemeente Amsterdam
+/* istanbul ignore file */
 import { CategoryDetail } from '../components'
 
-export const DetailContainer = () => {
-  return (
-    <CategoryDetail
-      isMainCategory={false}
-      entityName="Subcategorie"
-      isPublicAccessibleLabel="Toon meldingen van deze subcategorie op openbare kaarten en op de kaart in het meldformulier."
-    />
-  )
-}
+export const DetailContainer = () => (
+  <CategoryDetail
+    isMainCategory={false}
+    entityName="Subcategorie"
+    isPublicAccessibleLabel="Toon meldingen van deze subcategorie op openbare kaarten en op de kaart in het meldformulier."
+  />
+)
 
 export default DetailContainer
