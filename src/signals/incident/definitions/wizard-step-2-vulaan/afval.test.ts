ae10eba3734d9bf1bf84ec1440c6f476fcb85dcb
Siree Koolen-Wijkstra <s.wijkstra@amsterdam.nl>
2023-05-22 12:49:47 +0200

    Revert "Change in aanvullende vragen afval (#2579)" (#2581)
    This reverts commit f49f8d45fac1a19733567ea9c51fd7d3feb4f96a.

--- src/signals/incident/definitions/wizard-step-2-vulaan/afval.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/afval.test.ts
@@ -4,12 +4,6 @@ describe('definition afval', () => {
   it('has a defined set of controls', () => {
     const keys = Object.keys(afval)
 
-    expect(keys).toStrictEqual([
-      'locatie',
-      'dateTime',
-      'extra_afval',
-      'extra_afval_eigenaar',
-      'extra_afval_eigenaar_ja',
-    ])
+    expect(keys).toStrictEqual(['locatie', 'dateTime', 'extra_afval'])
   })
 })
--- src/signals/incident/definitions/wizard-step-2-vulaan/afval.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/afval.ts
@@ -31,46 +31,11 @@ export const controls = {
       ifOneOf: {
         subcategory: ['grofvuil', 'huisafval', 'puin-sloopafval'],
       },
-      label: 'Welk afval is verkeerd neergezet?',
-      subtitle:
-        'U helpt ons door zoveel mogelijk informatie te geven over het soort afval: huisafval, bedrijfsafval, grofvuil of dozen en papier.',
-      shortLabel: 'Welke soort',
+      label: 'Waar komt het afval vandaan, denkt u?',
+      shortLabel: 'Waar vandaan',
       pathMerge: 'extra_properties',
     },
-    options: {
-      validators: ['required'],
-    },
-    render: QuestionFieldType.TextInput,
-  },
-  extra_afval_eigenaar: {
-    meta: {
-      ifOneOf: {
-        subcategory: ['grofvuil', 'huisafval', 'puin-sloopafval'],
-      },
-      label:
-        'Weet u wie de eigenaar is van het verkeerd geplaatste afval? Bijvoorbeeld omdat u dat ziet aan een adressticker of iets anders?',
-      shortLabel: 'Welke eigenaar',
-      pathMerge: 'extra_properties',
-      values: {
-        ja: 'Ja',
-        nee: 'Nee',
-      },
-    },
-    options: {
-      validators: ['required'],
-    },
-    render: QuestionFieldType.RadioInput,
-  },
-  extra_afval_eigenaar_ja: {
-    meta: {
-      ifOneOf: {
-        extra_afval_eigenaar: 'ja',
-      },
-      value:
-        'We willen graag telefonisch contact met u hierover. Als u dat goed vindt, vul dan uw telefoonnummer in op de volgende pagina.',
-      type: 'info',
-    },
-    render: QuestionFieldType.PlainText,
+    render: QuestionFieldType.TextareaInput,
   },
 }
 
