cdf9e5b554f1877c17b62c5f80d3a80d9aec574b
Siree Koolen-Wijkstra <s.wijkstra@amsterdam.nl>
2023-05-26 08:38:57 +0200

    [feat-5211] Aanvullende vragen handhaving-op-afval and Gedumpt-gevaarlijk-afval (#2582)
    * Created additional questions to subcategories:
    asbest-accu
    handhaving-op-afval
    
    First question is not required, as discussed with Linda. The second one is.
    
    * Now reference to key instead of value.

--- src/signals/incident/definitions/wizard-step-2-vulaan/afval.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/afval.test.ts
@@ -4,6 +4,13 @@ describe('definition afval', () => {
   it('has a defined set of controls', () => {
     const keys = Object.keys(afval)
 
-    expect(keys).toStrictEqual(['locatie', 'dateTime', 'extra_afval'])
+    expect(keys).toStrictEqual([
+      'locatie',
+      'dateTime',
+      'extra_afval_handhaving',
+      'extra_afval_handhaving_owner',
+      'extra_afval_handhaving_owner_confirmation',
+      'extra_afval',
+    ])
   })
 })
