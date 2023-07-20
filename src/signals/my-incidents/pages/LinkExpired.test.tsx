93246928a058bd5e0c4ba5c86e2c2981e51830ca
Oscar <bakkeroscar@gmail.com>
2023-07-20 13:46:01 +0200

    Coverage up!

--- src/signals/my-incidents/pages/LinkExpired.test.tsx
+++ src/signals/my-incidents/pages/LinkExpired.test.tsx
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2022 Gemeente Amsterdam
 import { screen, render } from '@testing-library/react'
+import * as reactRouterDom from 'react-router-dom'
 
 import { withAppContext } from 'test/utils'
 
 import { LinkExpired } from './LinkExpired'
 
+const navigateSpy = jest.fn()
+
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-router-dom'),
+}))
+
 describe('LinkExpired', () => {
+  beforeEach(() => {
+    jest
+      .spyOn(reactRouterDom, 'useNavigate')
+      .mockImplementation(() => navigateSpy)
+  })
+
   it('should render correctly', () => {
     render(withAppContext(<LinkExpired />))
 
@@ -19,5 +33,13 @@ describe('LinkExpired', () => {
     expect(
       screen.getByRole('button', { name: 'Begin opnieuw' })
     ).toBeInTheDocument()
+  })
+
+  it('should navigate to request access page when button is clicked', async () => {
+    render(withAppContext(<LinkExpired />))
+
+    const button = screen.getByRole('button', { name: 'Begin opnieuw' })
+    button.click()
+    expect(navigateSpy).toHaveBeenCalledTimes(1)
   })
 })
