0989c3e146915e370145834a0bd133655b745318
oscarbakker <bakkeroscar@gmail.com>
2023-10-19 15:53:49 +0200

    Add locatie on desktop
    Change styling and remove no container found on map functionality.

--- src/signals/incident/components/form/MapSelectors/Asset/Selector_v2_removeafterfinishepic5440/DetailPanel/DetailPanel.test.tsx
+++ src/signals/incident/components/form/MapSelectors/Asset/Selector_v2_removeafterfinishepic5440/DetailPanel/DetailPanel.test.tsx
@@ -1,13 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 - 2023 Gemeente Amsterdam, Vereniging van Nederlandse Gemeenten
 
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  within,
-} from '@testing-library/react'
+import { render, screen, waitFor, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import * as reactRedux from 'react-redux'
 
@@ -17,7 +11,6 @@ import { closeMap } from 'signals/incident/containers/IncidentContainer/actions'
 import MockInstance = jest.MockInstance
 import type { DetailPanelProps } from './DetailPanel'
 import DetailPanel from './index'
-import type { Address } from '../../../../../../../../types/address'
 import { NEARBY_TYPE, UNKNOWN_TYPE } from '../../../constants'
 import withAssetSelectContext, {
   contextValue,
@@ -135,13 +128,7 @@ describe('DetailPanel', () => {
       })
     )
 
-    expect(screen.getByText('Locatie')).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('checkbox', {
-        name: 'Het object staat niet op de kaart',
-      })
-    ).toBeInTheDocument()
+    expect(screen.getByText('Selecteer de locatie')).toBeInTheDocument()
 
     expect(screen.getByTestId('asset-select-submit-button')).toBeInTheDocument()
 
@@ -182,56 +169,6 @@ describe('DetailPanel', () => {
     expect(currentContextValue.removeItem).toHaveBeenCalled()
   })
 
-  it('adds asset not on map', () => {
-    render(
-      withAssetSelectContext(<DetailPanel {...props} />, {
-        ...currentContextValue,
-        selection: undefined,
-      })
-    )
-
-    expect(
-      screen.queryByText('Nummer van de container')
-    ).not.toBeInTheDocument()
-
-    expect(currentContextValue.setItem).not.toHaveBeenCalled()
-
-    userEvent.click(
-      screen.getByRole('checkbox', {
-        name: 'Het object staat niet op de kaart',
-      })
-    )
-    expect(currentContextValue.setItem).toHaveBeenCalledTimes(1)
-    expect(currentContextValue.setItem).toHaveBeenCalledWith({
-      id: '',
-      label: 'Het object staat niet op de kaart',
-      type: UNKNOWN_TYPE,
-    })
-
-    expect(screen.getByText('Nummer van de container')).toBeInTheDocument()
-
-    const unregisteredObjectId = '897 6238'
-
-    userEvent.type(
-      screen.getByTestId('unregistered-asset-input'),
-      unregisteredObjectId
-    )
-
-    fireEvent.blur(screen.getByTestId('unregistered-asset-input'))
-
-    expect(currentContextValue.removeItem).toHaveBeenCalledTimes(2)
-    expect(currentContextValue.setItem).toHaveBeenCalledTimes(2)
-    expect(currentContextValue.setItem).toHaveBeenLastCalledWith({
-      id: unregisteredObjectId,
-      type: UNKNOWN_TYPE,
-      label: `Het object staat niet op de kaart - ${unregisteredObjectId}`,
-    })
-
-    fireEvent.submit(screen.getByTestId('unregistered-asset-input'))
-
-    expect(dispatch).toHaveBeenCalledWith(closeMap())
-  })
-
   it('closes/submits the panel', () => {
     render(
       withAssetSelectContext(<DetailPanel {...props} />, {
@@ -245,135 +182,6 @@ describe('DetailPanel', () => {
     userEvent.click(screen.getByTestId('asset-select-submit-button'))
 
     expect(dispatch).toHaveBeenCalledWith(closeMap())
-  })
-
-  it('handles Enter key on input', () => {
-    render(
-      withAssetSelectContext(<DetailPanel {...props} />, {
-        ...currentContextValue,
-        selection: selectionUnregistered,
-      })
-    )
-
-    userEvent.type(
-      screen.getByLabelText('Nummer van de container (niet verplicht)'),
-      '5'
-    )
-
-    expect(currentContextValue.setItem).not.toHaveBeenCalled()
-
-    userEvent.type(
-      screen.getByLabelText('Nummer van de container (niet verplicht)'),
-      '{Enter}'
-    )
-
-    expect(currentContextValue.setItem).toHaveBeenCalledWith({
-      id: '5',
-      type: UNKNOWN_TYPE,
-      label: 'Het object staat niet op de kaart - 5',
-    })
-    expect(dispatch).toHaveBeenCalledWith(closeMap())
-  })
-
-  it('renders default labels', () => {
-    const language = undefined
-
-    const propsWithLanguage = {
-      ...props,
-      language,
-    }
-
-    render(
-      withAssetSelectContext(<DetailPanel {...propsWithLanguage} />, {
-        ...currentContextValue,
-        selection: undefined,
-      })
-    )
-
-    expect(screen.getByText('Locatie')).toBeInTheDocument()
-    expect(
-      screen.getByText('Het object staat niet op de kaart')
-    ).toBeInTheDocument()
-  })
-
-  it('renders custom labels', () => {
-    const language = {
-      title: 'Locatie',
-      subTitle: 'Kies een container op de kaart',
-      unregistered: 'De container staat niet op de kaart',
-      description: 'Beschrijving',
-    }
-
-    const propsWithLanguage = {
-      ...props,
-      language,
-    }
-
-    render(
-      withAssetSelectContext(<DetailPanel {...propsWithLanguage} />, {
-        ...currentContextValue,
-        selection: undefined,
-      })
-    )
-
-    Object.values(language).forEach((label) => {
-      expect(screen.getByText(label)).toBeInTheDocument()
-    })
-  })
-
-  it('renders the legend panel, always containing the nearby type item', () => {
-    const noFeatureTypesContext = {
-      ...contextValue,
-      meta: {
-        ...contextValue.meta,
-        featureTypes: [],
-      },
-    }
-
-    const address: Address = {
-      postcode: '1234AB',
-      huisletter: 'A',
-      huisnummer: '1',
-      woonplaats: 'Amsterdam',
-      openbare_ruimte: 'Straat',
-      huisnummer_toevoeging: 'A',
-    }
-
-    const { rerender } = render(
-      withAssetSelectContext(<DetailPanel {...props} />, {
-        ...contextValue,
-        selection: undefined,
-      })
-    )
-
-    expect(screen.getByTestId('unregistered-object-panel')).toBeInTheDocument()
-
-    fireEvent.click(screen.getByTestId('unregistered-asset-checkbox'))
-    fireEvent.click(screen.getByTestId('unregistered-asset-checkbox'))
-    fireEvent.click(screen.getByTestId('unregistered-asset-checkbox'))
-
-    expect(currentContextValue.setItem).toHaveBeenCalledTimes(3)
-
-    expect(screen.getByTestId('legend-panel')).toBeInTheDocument()
-    expect(screen.getByText('Bestaande melding')).toBeInTheDocument()
-    expect(screen.getByTestId('legend-toggle-button')).toBeInTheDocument()
-
-    // Add address to test the condition $noFeatureTypes && $address
-    rerender(
-      withAssetSelectContext(<DetailPanel {...props} />, {
-        ...noFeatureTypesContext,
-        address,
-        selection: undefined,
-      })
-    )
-
-    expect(
-      screen.queryByTestId('unregistrered-object-panel')
-    ).not.toBeInTheDocument()
-
-    expect(screen.queryByTestId('legend-panel')).toBeInTheDocument()
-    expect(screen.getByText('Bestaande melding')).toBeInTheDocument()
-    expect(screen.queryByTestId('legend-toggle-button')).toBeInTheDocument()
   })
 
   it('toggles the position of the legend panel', () => {
@@ -394,6 +202,49 @@ describe('DetailPanel', () => {
     await waitFor(() => {
       expect(screen.getByTestId('close-button')).toHaveFocus()
     })
+  })
+
+  /**
+   * This test is  only there to trigger the no features types && address conditional styling.
+   *
+   */
+  it('should trigger styling when no feature types', () => {
+    render(
+      withAssetSelectContext(<DetailPanel {...props} />, {
+        ...currentContextValue,
+        meta: {
+          ...currentContextValue.meta,
+          featureTypes: [],
+        },
+      })
+    )
+  })
+
+  it('renders closes the map when clicking on the close button', async () => {
+    render(withAssetSelectContext(<DetailPanel {...props} />))
+
+    userEvent.click(screen.getByLabelText('Terug'))
+
+    await waitFor(() => {
+      expect(dispatch).toHaveBeenCalledWith(closeMap())
+    })
+  })
+
+  it('should not render submit button when no address is selected', () => {
+    const ctxValue = {
+      ...contextValue,
+      address: undefined,
+    }
+
+    render(
+      withAssetSelectContext(<DetailPanel {...props} />, {
+        ...ctxValue,
+      })
+    )
+
+    expect(
+      screen.queryByTestId('asset-select-submit-button')
+    ).not.toBeInTheDocument()
   })
 
   it('selection nearby details', () => {
