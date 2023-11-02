a6b285ab8caa0b77922fb10bc6c4bf57229fc0be
Siree Koolen-Wijkstra <sireekoolenwijkstra@gmail.com>
2023-11-02 09:28:51 +0100

    Changed slideup_timeout of notification to 3 seconds

--- src/containers/Notification/constants.ts
+++ src/containers/Notification/constants.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (C) 2019 - 2021 Gemeente Amsterdam
+// Copyright (C) 2019 - 2023 Gemeente Amsterdam
 export const VARIANT_ERROR = 'error'
 export const VARIANT_NOTICE = 'notice'
 export const VARIANT_SUCCESS = 'success'
@@ -7,5 +7,5 @@ export const VARIANT_SUCCESS = 'success'
 export const TYPE_GLOBAL = 'global'
 export const TYPE_LOCAL = 'local'
 
-export const SLIDEUP_TIMEOUT = 8000
+export const SLIDEUP_TIMEOUT = 3000
 export const ONCLOSE_TIMEOUT = 200
