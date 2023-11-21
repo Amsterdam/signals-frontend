a2345931a788cf8aa38499ba6068af0cc38a75d3
oscarbakker <bakkeroscar@gmail.com>
2023-11-21 10:47:07 +0100

    Add IconList test

--- src/components/IconList/IconList.test.tsx
+++ src/components/IconList/IconList.test.tsx
@@ -2,11 +2,22 @@
 // Copyright (C) 2021 - 2022 Gemeente Amsterdam
 import { render, screen } from '@testing-library/react'
 
+import type configurationType from 'shared/services/configuration/__mocks__/configuration'
+import configuration from 'shared/services/configuration/configuration'
 import { withAppContext } from 'test/utils'
 
 import IconList, { IconListItem } from './IconList'
 
+jest.mock('shared/services/configuration/configuration')
+
+const mockConfiguration = configuration as typeof configurationType
+
 describe('IconList', () => {
+  afterEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    mockConfiguration.__reset()
+  })
+
   it('renders correctly', () => {
     render(
       withAppContext(
@@ -19,6 +30,8 @@ describe('IconList', () => {
     expect(screen.queryByRole('img')).not.toBeInTheDocument()
     expect(screen.getByRole('list')).toBeInTheDocument()
     expect(screen.getAllByRole('listitem').length).toBe(1)
+
+    expect(screen.queryByRole('checkbox')).not.toBeInTheDocument()
   })
 
   it('renders an empty list', () => {
@@ -26,5 +39,20 @@ describe('IconList', () => {
 
     expect(screen.getByRole('list')).toBeInTheDocument()
     expect(screen.queryAllByRole('listitem').length).toBe(0)
+  })
+
+  it('renders a list with a checkbox', () => {
+    mockConfiguration.featureFlags.showSelectorV2removeafterfinishepic5440 =
+      true
+
+    render(
+      withAppContext(
+        <IconList>
+          <IconListItem iconUrl="">Icon</IconListItem>
+        </IconList>
+      )
+    )
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
   })
 })
