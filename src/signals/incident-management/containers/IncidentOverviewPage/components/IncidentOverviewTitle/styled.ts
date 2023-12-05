b74c1d04e728fa4881ba776f6db265d5ae1f7f50
Vincent de Graaf <vdegraaf.vincent@gmail.com>
2023-12-05 09:04:34 +0100

    [feat-5482]: zoeken en filteren (#2775)
    * [feat-5577]: filter disabling (#2766)
    
    * Fix close button globalnotification message
    
    * Disabled filter buttons and show message on click
    
    * Fix notification test
    
    * Add tests
    
    * Bump notification test coverage
    
    * [feat-5578]: Add disable sorting functionality (#2768)
    
    * Add disable sorting functionality
    
    * Add tests
    
    * Disable sorting functionality
    
    * Fix linting warnings
    
    * Update selectors test
    
    * Update FilterForm test
    
    * Add sort reset info and btn test

--- src/signals/incident-management/containers/IncidentOverviewPage/components/IncidentOverviewTitle/styled.ts
+++ src/signals/incident-management/containers/IncidentOverviewPage/components/IncidentOverviewTitle/styled.ts
@@ -1,6 +1,34 @@
-import { Paragraph, themeSpacing } from '@amsterdam/asc-ui'
+import { Link, Paragraph, themeSpacing, themeColor } from '@amsterdam/asc-ui'
 import styled from 'styled-components'
+
+import Refresh from '../../../../../../images/icon-refresh.svg'
 
 export const SubTitle = styled(Paragraph)`
   margin: ${themeSpacing(3)} 0;
+  line-height: 1.5rem;
+  font-size: 1rem;
+`
+
+export const RefreshIcon = styled(Refresh).attrs({
+  height: 30,
+})`
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 15px;
+`
+
+export const StyledLink = styled(Link)`
+  margin-left: ${themeSpacing(2)};
+  color: ${themeColor('primary')};
+  font-size: 1rem;
+  line-height: 1.5rem;
+
+  :hover {
+    color: ${themeColor('primary', 'dark')};
+    cursor: pointer;
+  }
+`
+
+export const StyledSpan = styled.span`
+  line-height: 1.5rem;
 `
