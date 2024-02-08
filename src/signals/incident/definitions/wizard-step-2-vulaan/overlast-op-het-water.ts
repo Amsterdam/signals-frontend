d6eda58066843995def97dea3fdf09080b9e380d
Aram Limpens <a.limpens@amsterdam.nl>
2024-02-08 16:36:11 +0100

    Implement rest of questions

--- src/signals/incident/definitions/wizard-step-2-vulaan/overlast-op-het-water.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/overlast-op-het-water.ts
@@ -12,32 +12,19 @@ export const overlastOpHetWater = {
   locatie,
   dateTime: {
     meta: {
-      ifOneOf: {
-        subcategory: [
-          'blokkade-van-de-vaarweg',
-          'overig-boten',
-          'overlast-op-het-water-geluid',
-          'scheepvaart-nautisch-toezicht', // Wat is deze?
-          'overlast-op-het-water-snel-varen',
-          'overlast-op-het-water-gezonken-boot',
-        ],
-      },
       ignoreVisibility: true,
-      label: 'Wanneer was het?',
+      label: 'Wanneer heeft u de overlast?',
       canBeNull: true,
-      timeSelectorDisabled: true, // TODO: geldt dit ook voor de niet overig-boten cats?
+      timeSelectorDisabled: true,
     },
     options: {
-      validators: [falsyOrNumberOrNow, inPast],
+      validators: [falsyOrNumberOrNow, inPast, 'required'],
     },
     render: QuestionFieldType.DateTimeInput,
   },
 
   extra_boten_frequentie: {
     meta: {
-      ifOneOf: {
-        subcategory: ['blokkade-van-de-vaarweg', 'overig-boten'],
-      },
       values: {
         ja: 'Ja, het gebeurt vaker',
         nee: 'Nee, het is de eerste keer',
@@ -65,7 +52,13 @@ export const overlastOpHetWater = {
   extra_boten_beweging: {
     meta: {
       ifOneOf: {
-        subcategory: ['blokkade-van-de-vaarweg', 'overig-boten'],
+        subcategory: [
+          'blokkade-van-de-vaarweg',
+          'overig-boten',
+          'overlast-op-het-water-geluid',
+          'overlast-op-het-water-snel-varen',
+          'overlast-op-het-water-vaargedrag',
+        ],
       },
       values: {
         ja: 'Ja, de boot ligt stil',
@@ -81,7 +74,12 @@ export const overlastOpHetWater = {
   extra_boten_soort: {
     meta: {
       ifOneOf: {
-        subcategory: ['blokkade-van-de-vaarweg', 'overig-boten'],
+        subcategory: [
+          'overig-boten',
+          'overlast-op-het-water-geluid',
+          'overlast-op-het-water-snel-varen',
+          'overlast-op-het-water-vaargedrag',
+        ],
       },
       values: {
         plezier: '(Plezier)boot in privé-eigendom',
@@ -94,13 +92,19 @@ export const overlastOpHetWater = {
       shortLabel: 'Soort boot',
       pathMerge: 'extra_properties',
     },
+    options: { validators: ['required'] },
     render: QuestionFieldType.RadioInput,
   },
 
   extra_boten_open_gesloten: {
     meta: {
       ifOneOf: {
-        subcategory: ['blokkade-van-de-vaarweg', 'overig-boten'],
+        subcategory: [
+          'overig-boten',
+          'overlast-op-het-water-geluid',
+          'overlast-op-het-water-snel-varen',
+          'overlast-op-het-water-vaargedrag',
+        ],
       },
       values: {
         ja: 'Ja, het is een open boot (bijvoorbeeld een sloep, roeiboot, rondvaartboot met dak open)',
@@ -111,63 +115,70 @@ export const overlastOpHetWater = {
       shortLabel: 'Soort boot',
       pathMerge: 'extra_properties',
     },
+    options: { validators: ['required'] },
     render: QuestionFieldType.RadioInput,
   },
 
-  // extra_boten_snelheid_typeboot: {
-  //   meta: {
-  //     label: 'Wat voor type boot is het?',
-  //     shortLabel: 'Type boot',
-  //     pathMerge: 'extra_properties',
-  //     values: {
-  //       pleziervaart: 'Pleziervaart',
-  //       rondvaartboot_of_salonboot: 'Rondvaartboot of salonboot',
-  //       vrachtschip_of_binnenvaartschip: 'Vrachtschip of binnenvaartschip',
-  //       overig: 'Overig',
-  //     },
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-snel-varen',
-  //     },
-  //   },
-  //   options: { validators: ['required'] },
-  //   render: QuestionFieldType.RadioInput,
-  // },
-  // extra_boten_snelheid_rederij: {
-  //   meta: {
-  //     label: 'Wat is de naam van de rederij?',
-  //     shortLabel: 'Rederij',
-  //     pathMerge: 'extra_properties',
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-snel-varen',
-  //       extra_boten_snelheid_typeboot: 'rondvaartboot_of_salonboot',
-  //     },
-  //   },
-  //   render: QuestionFieldType.TextInput,
-  // },
-  // extra_boten_snelheid_naamboot: {
-  //   meta: {
-  //     label: 'Wat is de naam van de boot?',
-  //     shortLabel: 'Naam boot',
-  //     pathMerge: 'extra_properties',
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-snel-varen',
-  //       ifOneOf: {
-  //         extra_boten_snelheid_typeboot: [
-  //           'pleziervaart',
-  //           'rondvaartboot_of_salonboot',
-  //           'vrachtschip_of_binnenvaartschip',
-  //           'overig',
-  //         ],
-  //       },
-  //     },
-  //   },
-  //   render: QuestionFieldType.TextInput,
-  // },
+  extra_boten_drijfkracht: {
+    meta: {
+      ifOneOf: {
+        subcategory: 'overlast-op-het-water-gezonken-boot',
+      },
+      values: {
+        ja: 'Ja, de boot ligt volledig onder water',
+        nee: 'Nee, de boot kan nog drijven',
+      },
+      label: 'Ligt de boot volledig onder water of kan hij nog drijven?',
+      shortLabel: 'Drijfkracht',
+      pathMerge: 'extra_properties',
+    },
+    render: QuestionFieldType.RadioInput,
+  },
+
+  extra_boten_vast: {
+    meta: {
+      ifOneOf: {
+        subcategory: 'overlast-op-het-water-gezonken-boot',
+      },
+      values: {
+        ja: 'Ja, de boot ligt vast',
+        nee: 'Nee, de boot kan wegdrijven',
+      },
+      label:
+        'Ligt de boot nog vast? Of kan de boot wegdrijven zodat hij de vaarweg blokkeert?',
+      shortLabel: 'Boot vast?',
+      pathMerge: 'extra_properties',
+    },
+    render: QuestionFieldType.RadioInput,
+  },
+
+  extra_boten_lekken: {
+    meta: {
+      ifOneOf: {
+        subcategory: 'overlast-op-het-water-gezonken-boot',
+      },
+      values: {
+        ja: 'Ja, boot lekt vloeistof',
+        nee: 'Nee, boot lekt geen vloeistof',
+        weetniet: 'Weet ik niet',
+      },
+      label: 'Lekt de boot olie of een andere vloeistof?',
+      shortLabel: 'Boot vast?',
+      pathMerge: 'extra_properties',
+    },
+    render: QuestionFieldType.RadioInput,
+  },
 
   extra_boten_meer: {
     meta: {
       ifOneOf: {
-        subcategory: ['blokkade-van-de-vaarweg', 'overig-boten'],
+        subcategory: [
+          'blokkade-van-de-vaarweg',
+          'overig-boten',
+          'overlast-op-het-water-geluid',
+          'overlast-op-het-water-snel-varen',
+          'overlast-op-het-water-vaargedrag',
+        ],
       },
       label: 'Wat weet u nog meer over deze situatie?',
       shortLabel: 'Extra informatie',
@@ -177,56 +188,6 @@ export const overlastOpHetWater = {
     },
     render: QuestionFieldType.TextareaInput,
   },
-
-  // extra_boten_snelheid_meer: {
-  //   meta: {
-  //     label: 'Wat weet u nog meer over deze situatie?',
-  //     shortLabel: 'Extra informatie',
-  //     subtitle:
-  //       'Bijvoorbeeld: de kleur van de boot, aantal passagiers, vaarrichting, Y of Vignet nummer etc.',
-  //     pathMerge: 'extra_properties',
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-snel-varen',
-  //       ifOneOf: {
-  //         extra_boten_snelheid_typeboot: [
-  //           'pleziervaart',
-  //           'rondvaartboot_of_salonboot',
-  //           'vrachtschip_of_binnenvaartschip',
-  //           'overig',
-  //         ],
-  //       },
-  //     },
-  //   },
-  //   render: QuestionFieldType.TextareaInput,
-  // },
-
-  // extra_boten_geluid_meer: {
-  //   meta: {
-  //     label: 'Wat weet u nog meer over deze situatie?',
-  //     shortLabel: 'Extra informatie',
-  //     subtitle:
-  //       'Bijvoorbeeld: waar de boot naar toe vaart, kleur van de boot, aantal passagiers, kenteken, vignet, etc.',
-  //     pathMerge: 'extra_properties',
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-geluid',
-  //     },
-  //   },
-  //   render: QuestionFieldType.TextareaInput,
-  // },
-
-  // extra_boten_gezonken_meer: {
-  //   meta: {
-  //     label: 'Wat weet u nog meer over deze situatie?',
-  //     shortLabel: 'Extra informatie',
-  //     subtitle:
-  //       'Bijvoorbeeld: "er lekt olie", "gevaar voor andere boten", etc.',
-  //     pathMerge: 'extra_properties',
-  //     ifAllOf: {
-  //       subcategory: 'overlast-op-het-water-gezonken-boot',
-  //     },
-  //   },
-  //   render: QuestionFieldType.TextareaInput,
-  // },
 }
 
 export default overlastOpHetWater
