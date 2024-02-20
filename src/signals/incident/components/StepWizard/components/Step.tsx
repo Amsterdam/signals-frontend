ab93c90c8ba9e72dd42afa554511a1cf8ae605e2
Aram Limpens <a.limpens@amsterdam.nl>
2024-02-20 13:14:42 +0100

    Log piwik event on step

--- src/signals/incident/components/StepWizard/components/Step.tsx
+++ src/signals/incident/components/StepWizard/components/Step.tsx
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: MPL-2.0
+
+import { useEffect } from 'react'
+
 // Copyright (C) 2022 - 2022 Vereniging van Nederlandse Gemeenten, Gemeente Amsterdam
 export default function Step({
   render,
@@ -7,5 +10,16 @@ export default function Step({
   render: () => JSX.Element | null
   id: string
 }) {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log(`/incident/${id}`)
+    ;(window as any).dataLayer.push({
+      event: 'interaction.component.virtualPageview',
+      meta: {
+        vpv_url: `/incident/${id}`, // TODO: moet de base url hier bij?
+      },
+    })
+  }, [id])
+
   return <div id={id}>{render()}</div>
 }
