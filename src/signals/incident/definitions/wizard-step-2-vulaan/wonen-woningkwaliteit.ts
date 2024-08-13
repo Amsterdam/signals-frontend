de607253823031539672631a36067de064fa6faa
Aram <37216945+alimpens@users.noreply.github.com>
2024-08-13 15:03:28 +0200

    Remove woningkwaliteit questions (#2922)

--- src/signals/incident/definitions/wizard-step-2-vulaan/wonen-woningkwaliteit.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/wonen-woningkwaliteit.ts
@@ -1,21 +1,6 @@
 import { QuestionFieldType } from '../../../../types/question'
-import { falsyOrNumberOrNow, inPast } from '../../services/custom-validators'
 
 export const woningkwaliteit = {
-  woningkwaliteit_dateTime: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      label: 'Wanneer was het?',
-    },
-    options: {
-      validators: [falsyOrNumberOrNow, inPast],
-    },
-    render: QuestionFieldType.DateTimeInput,
-  },
-
   extra_wonen_woonkwaliteit_direct_gevaar: {
     meta: {
       ifOneOf: {
@@ -88,107 +73,5 @@ export const woningkwaliteit = {
         'Meld uw klacht eerst bij de verhuurder, eigenaar of VvE. Krijgt u geen antwoord of wordt de klacht niet verholpen, vul dan dit formulier in.',
     },
     render: QuestionFieldType.PlainText,
-  },
-  extra_wonen_woonkwaliteit_bewoner: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      ifAllOf: {
-        extra_wonen_woonkwaliteit_direct_gevaar: 'nee',
-        extra_wonen_woonkwaliteit_gemeld_bij_eigenaar: 'ja',
-      },
-      label: 'Bent u zelf bewoner van het adres?',
-      shortLabel: 'Bewoner',
-      pathMerge: 'extra_properties',
-      values: {
-        ja: 'Ja, ik woon op dit adres',
-        nee: 'Nee, ik woon niet op dit adres',
-      },
-    },
-    options: {
-      validators: ['required'],
-    },
-    render: QuestionFieldType.RadioInput,
-  },
-  extra_wonen_woonkwaliteit_namens_bewoner: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      ifAllOf: {
-        extra_wonen_woonkwaliteit_bewoner: 'nee',
-        extra_wonen_woonkwaliteit_direct_gevaar: 'nee',
-      },
-      label: 'Doet u de melding namens de bewoner van het adres?',
-      shortLabel: 'Namens bewoner',
-      pathMerge: 'extra_properties',
-      values: {
-        ja: 'Ja, ik doe de melding namens de bewoner',
-        nee: 'Nee',
-      },
-    },
-    options: {
-      validators: ['required'],
-    },
-    render: QuestionFieldType.RadioInput,
-  },
-  extra_wonen_woonkwaliteit_toestemming_contact: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      ifAllOf: {
-        extra_wonen_woonkwaliteit_gemeld_bij_eigenaar: 'ja',
-        extra_wonen_woonkwaliteit_direct_gevaar: 'nee',
-      },
-      label: 'Mogen we contact met u opnemen om een afspraak te maken?',
-      subtitle:
-        'Om uw klacht goed te kunnen behandelen willen we vaak even komen kijken of met u overleggen',
-      shortLabel: 'Toestemming contact opnemen',
-      pathMerge: 'extra_properties',
-      values: {
-        ja: 'Ja, u mag contact met mij opnemen',
-        nee: 'Nee, liever geen contact',
-      },
-    },
-    options: {
-      validators: ['required'],
-    },
-    render: QuestionFieldType.RadioInput,
-  },
-  extra_wonen_woonkwaliteit_toestemming_contact_ja: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      ifAllOf: {
-        extra_wonen_woonkwaliteit_toestemming_contact: 'ja',
-        extra_wonen_woonkwaliteit_direct_gevaar: 'nee',
-      },
-      type: 'caution',
-      value: 'Let op! Vul uw telefoonnummer in op de volgende pagina.',
-    },
-    render: QuestionFieldType.PlainText,
-  },
-  extra_wonen_woonkwaliteit_geen_contact: {
-    meta: {
-      ifOneOf: {
-        subcategory: 'woningkwaliteit',
-        wonen_overig: 'woningkwaliteit',
-      },
-      ifAllOf: {
-        extra_wonen_woonkwaliteit_toestemming_contact: 'nee',
-        extra_wonen_woonkwaliteit_direct_gevaar: 'nee',
-      },
-      label: 'Waarom hebt u liever geen contact?',
-      shortLabel: 'Liever geen contact',
-      pathMerge: 'extra_properties',
-    },
-    render: QuestionFieldType.TextInput,
   },
 }
