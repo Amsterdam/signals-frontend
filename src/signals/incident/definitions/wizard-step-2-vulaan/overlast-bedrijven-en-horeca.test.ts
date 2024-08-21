86cab4bdcfb899607b204a705d19164efc5dbdad
Vincent <vdegraaf.vincent@gmail.com>
2024-08-21 11:31:59 +0200

    Add extra questions overlast-door-bezoekers-niet-op-terras

--- src/signals/incident/definitions/wizard-step-2-vulaan/overlast-bedrijven-en-horeca.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/overlast-bedrijven-en-horeca.test.ts
@@ -7,6 +7,8 @@ describe('definition overlast-bedrijven-en-horeca', () => {
     expect(keys).toStrictEqual([
       'locatie',
       'dateTime',
+      'dateTimePersonenOverlast',
+      'extra_personen_overig',
       'extra_bedrijven_horeca_frequentie',
       'extra_bedrijven_horeca_moment',
       'extra_bedrijven_horeca_wat',
