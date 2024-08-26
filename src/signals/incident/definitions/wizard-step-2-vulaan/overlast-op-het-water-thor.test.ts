201a01ef086e402969ed2c2092300cbce82399ac
alimpens <a.limpens@amsterdam.nl>
2024-08-26 11:32:26 +0200

    Fix tests

--- src/signals/incident/definitions/wizard-step-2-vulaan/overlast-op-het-water-thor.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/overlast-op-het-water-thor.test.ts
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2024 Gemeente Amsterdam
-import overlastOpHetWater from './overlast-op-het-water'
+import overlastOpHetWaterThor from './overlast-op-het-water-thor'
 
 describe('definition overlast-op-het-water', () => {
   it('has a defined set of controls', () => {
-    const keys = Object.keys(overlastOpHetWater)
+    const keys = Object.keys(overlastOpHetWaterThor)
 
     expect(keys).toStrictEqual([
       'locatie',
