a83559b4e3a7ce4ca919c0a368c7e574b4e70a28
Vincent <vdegraaf.vincent@gmail.com>
2025-03-03 14:45:00 +0100

    Cleanup code

--- src/signals/incident/containers/IncidentReplyContainer/constants.ts
+++ src/signals/incident/containers/IncidentReplyContainer/constants.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2021 Gemeente Amsterdam
-// import configuration from 'shared/services/configuration/configuration'
+
 /**
  * Possible responses
  */
