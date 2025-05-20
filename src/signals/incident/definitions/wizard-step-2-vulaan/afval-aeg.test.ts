944c0232d85e84ce735dd6f838f9db4af142cf90
alimpens <a.limpens@amsterdam.nl>
2025-05-20 16:51:25 +0200

    Fix test

--- src/signals/incident/definitions/wizard-step-2-vulaan/afval-aeg.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/afval-aeg.test.ts
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2024 Gemeente Amsterdam
-import afval from './afval'
+import afvalAEG from './afval-aeg'
 
-describe('definition afval', () => {
+describe('definition afvalAEG', () => {
   it('has a defined set of controls', () => {
-    const keys = Object.keys(afval)
+    const keys = Object.keys(afvalAEG)
 
     expect(keys).toStrictEqual(['locatie', 'extra_wanneer', 'extra_afval'])
   })
