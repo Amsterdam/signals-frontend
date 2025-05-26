e06fa27a919204f61c2625a509b30242aa911ca2
alimpens <a.limpens@amsterdam.nl>
2025-05-26 11:57:33 +0200

    Update license header

--- src/signals/incident/definitions/wizard-step-2-vulaan/afval-aeg.test.ts
+++ src/signals/incident/definitions/wizard-step-2-vulaan/afval-aeg.test.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
-// Copyright (C) 2018 - 2024 Gemeente Amsterdam
+// Copyright (C) 2025 Gemeente Amsterdam
 import afvalAEG from './afval-aeg'
 
 describe('definition afvalAEG', () => {
