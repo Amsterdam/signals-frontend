d1f3131a589c486b4c23c1fa214f2de8d0b53f39
Aram <37216945+alimpens@users.noreply.github.com>
2026-08-10 15:04:21 +0200

    [fix]: Add question header to field type constant (#3365)
    * Add question header to field type constant
    
    * Add test

--- src/signals/incident/containers/IncidentContainer/constants.ts
+++ src/signals/incident/containers/IncidentContainer/constants.ts
@@ -53,6 +53,7 @@ export const FIELD_TYPE_MAP = {
   map_input: QuestionFieldType.MapInput,
   multi_text_input: QuestionFieldType.MultiTextInput,
   plain_text: QuestionFieldType.PlainText,
+  question_header: QuestionFieldType.QuestionHeader,
   radio_input: QuestionFieldType.RadioInput,
   select_input: QuestionFieldType.SelectInput,
   streetlight_select: QuestionFieldType.StreetlightSelect,
